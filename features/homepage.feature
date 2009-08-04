d87d65f4733345799b0279f4b15ba2048c248e4b
Loren Siebert <loren@siebert.org>
2009-08-04 10:19:32 -0700

    compass/SASS/Haml setup

---
+++ features/homepage.feature
@@ -0,0 +1,8 @@
+Feature: Homepage
+  In order to use the USASearch.gov website
+  As a viewer
+  I want to see a well-designed page
+
+  Scenario: Visiting the home page
+    Given I am on the homepage
+    Then I should see "USASearch"
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -1,16 +1,10 @@
 module NavigationHelpers
-  # Maps a name to a path. Used by the
-  #
-  #   When /^I go to (.+)$/ do |page_name|
-  #
-  # step definition in webrat_steps.rb
-  #
   def path_to(page_name)
     case page_name
-    
+
     when /the homepage/
-      '/'
-    
+      root_path
+
     # Add more mappings here.
     # Here is a more fancy example:
     #
@@ -18,8 +12,7 @@ module NavigationHelpers
     #     user_profile_path(User.find_by_login($1))
 
     else
-      raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
-        "Now, go and add a mapping in #{__FILE__}"
+      raise "Can't find mapping from \"#{page_name}\" to a path in #{__FILE__}"
     end
   end
 end
