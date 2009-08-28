8b0df600a65bcc1f8a3aae7689a6a29755d2e4ea
Loren Siebert <loren@siebert.org>
2009-08-27 22:39:47 -0700

    turn on more logging info until traffic dictates otherwise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,7 @@ config.cache_classes = true
 config.action_controller.consider_all_requests_local = false
 config.action_controller.perform_caching             = true
 config.action_view.cache_template_loading            = true
+config.log_level = :debug
 
 # See everything in the log (default is :info)
 # config.log_level = :debug
