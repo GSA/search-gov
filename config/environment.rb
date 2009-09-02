48b5710196b4f2f404ab1a6e7ef3f1c04697e3fc
Loren Siebert <loren@siebert.org>
2009-09-01 17:36:02 -0700

    changed default time zone to east coast

--- config/environment.rb
+++ config/environment.rb
@@ -21,8 +21,8 @@ Rails::Initializer.run do |config|
   config.gem 'mislav-will_paginate', :version => '~> 2.3.11', :lib => 'will_paginate', :source => 'http://gems.github.com'
   config.gem "chriseppstein-compass", :lib => 'compass', :source => 'http://gems.github.com', :version => '>= 0.8.9'
   config.gem "hpricot"
-  
+
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
-  config.time_zone = 'UTC'
+  config.time_zone = 'Eastern Time (US & Canada)'
 end
