7dce7068371d2fc08d2618a0be572b2c6d11d34f
Loren Siebert <loren@siebert.org>
2009-09-14 18:04:22 -0700

    Forgot to remove debugging line for GSS

--- app/models/gss.rb
+++ app/models/gss.rb
@@ -22,7 +22,7 @@ class Gss < AbstractEngine
     }
     #logger.debug "debugger opts: #{opts.inspect}"
     request_url = prepare_url(opts)
-    logger.debug "Request URL: #{request_url}"
+    #logger.debug "Request URL: #{request_url}"
     begin
       res = Net::HTTP.get_response(URI::parse(request_url))
       #logger.debug "HTTP status: #{res.code} #{res.message}"
