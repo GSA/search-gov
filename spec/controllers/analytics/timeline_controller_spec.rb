f2575434c180485f7594821d66a9fd3d15794e3c
Loren Siebert <loren@siebert.org>
2009-09-17 12:37:40 -0700

    Added another test case for "9/11"

--- spec/controllers/analytics/timeline_controller_spec.rb
+++ spec/controllers/analytics/timeline_controller_spec.rb
@@ -8,11 +8,19 @@ describe Analytics::TimelineController do
       it "should map { :controller => 'analytics/timeline', :action => 'show', :query=>'foo.com 9/11' } to /analytics/timeline/foo.com%209%2F11" do
         route_for(:controller => "analytics/timeline", :action => "show", :query=>"foo.com 9/11").should == "/analytics/timeline/foo.com%209%2F11"
       end
+
+      it "should map { :controller => 'analytics/timeline', :action => 'show', :query=>'9/11' } to /analytics/timeline/9%2F11" do
+        route_for(:controller => "analytics/timeline", :action => "show", :query=>"9/11").should == "/analytics/timeline/9%2F11"
+      end
     end
 
     describe "- route recognition" do
       it "should generate params { :controller => 'analytics/timeline', :action => 'show', :query=>'foo.com 9/11'  } from GET /analytics/timeline/foo.com%209%2F11" do
         params_from(:get, "/analytics/timeline/foo.com%209%2F11").should == { :controller => 'analytics/timeline', :action => 'show', :query=>'foo.com 9/11'  }
+      end
+
+      it "should generate params { :controller => 'analytics/timeline', :action => 'show', :query=>'9/11'  } from GET /analytics/timeline/9%2F11" do
+        params_from(:get, "/analytics/timeline/9%2F11").should == { :controller => 'analytics/timeline', :action => 'show', :query=>'9/11'  }
       end
     end
 
