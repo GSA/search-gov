391a0aa1972ce3403d0b8483f897d445e0287cfe
Loren Siebert <loren@siebert.org>
2009-09-17 11:24:31 -0700

    Biggest movers calculation now always considers 1000 top scoring terms before sorting on query popularity

--- app/models/daily_query_stat.rb
+++ app/models/daily_query_stat.rb
@@ -19,7 +19,7 @@ class DailyQueryStat < ActiveRecord::Base
   def self.biggest_movers(end_date, window_size, num_results = RESULTS_SIZE)
     return nil if end_date.nil?
     start_date = end_date - window_size.days + 1.day
-    results = DailyQueryStat.find_by_sql("SELECT * FROM query_accelerations qa, (SELECT sum(times) AS sum_times, query AS query FROM daily_query_stats WHERE (day between '#{start_date}' AND '#{end_date}') GROUP BY query  ) as dqs WHERE (qa.day = '#{end_date}' AND qa.window_size = #{window_size} and qa.query=dqs.query) ORDER BY score DESC LIMIT #{num_results}").sort_by {|dqs| dqs[:sum_times].to_i}.reverse
+    results = DailyQueryStat.find_by_sql("SELECT * FROM query_accelerations qa, (SELECT sum(times) AS sum_times, query AS query FROM daily_query_stats WHERE (day between '#{start_date}' AND '#{end_date}') GROUP BY query  ) as dqs WHERE (qa.day = '#{end_date}' AND qa.window_size = #{window_size} and qa.query=dqs.query) ORDER BY score DESC LIMIT 1000").sort_by {|dqs| dqs[:sum_times].to_i}.reverse[0,num_results]
     results.empty? ? nil : results
   end
 
