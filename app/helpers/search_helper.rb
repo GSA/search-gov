472c19fffc03a53ec65ef1c252be375aa04aec4f
Loren Siebert <loren@siebert.org>
2009-10-08 22:52:09 -0700

    Show deep links when available thru Bing

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,16 +1,34 @@
 module SearchHelper
   def display_result_links (result)
     url = shorten_url "#{result['unescapedUrl']}"
-    html = link_to("#{h url}" , "#{h result['unescapedUrl']}")
+    html = link_to("#{h url}", "#{h result['unescapedUrl']}")
     unless result['cacheUrl'].blank?
       html << " - "
-      html << link_to("Cached" , "#{result['cacheUrl']}")
+      html << link_to("Cached", "#{result['cacheUrl']}")
     end
     html
   end
 
+  def display_deep_links_for(result, query)
+    return if result["deepLinks"].nil?
+    rows = []
+    result["deepLinks"].in_groups_of(2)[0, 4].each do |row_pair|
+      row = content_tag(:td, row_pair[0].nil? ? "" : link_to(row_pair[0].title, row_pair[0].url))
+      row << content_tag(:td, row_pair[1].nil? ? "" : link_to(row_pair[1].title, row_pair[1].url))
+      rows << content_tag(:tr, row)
+    end
+    hostname_from_url = result['unescapedUrl'].split("/")[2]
+    site_query = "site:#{hostname_from_url}"
+    if !query.match(site_query)
+      more_results_path = search_path(:query=> "#{site_query} #{query}")
+      more_results_link = link_to "Show more results from #{hostname_from_url}", more_results_path, :class=>"more_results"
+      rows << content_tag(:tr, content_tag(:td, more_results_link, :colspan=>2))
+    end
+    content_tag(:table, rows, :class=>"deep_links")
+  end
+
   def display_result_title (result)
-    link_to "#{result['title']}" , "#{h result['unescapedUrl']}"
+    link_to "#{result['title']}", "#{h result['unescapedUrl']}"
   end
 
   private
@@ -19,6 +37,6 @@ module SearchHelper
     arr = url.split('/')
     host= arr[0]+"//"+arr[2]
     doc = arr.last.split('?').first
-    [host,"...",doc].join('/')
+    [host, "...", doc].join('/')
   end
 end
--- app/models/bing.rb
+++ app/models/bing.rb
@@ -12,7 +12,14 @@ class Bing < AbstractEngine
       response = bing.web(q, options)
       self.total = response.web.total
       pagination_total = [DEFAULT_PER_PAGE * 20, self.total ].min
-      results_array = self.total > 0 ? response.web.results.collect { |r| {'title' => r.title, 'unescapedUrl'=> r.url, 'content'=> (r.description rescue ""), 'cacheUrl'=> (r.CacheUrl rescue "")} } : []
+      results_array = self.total > 0 ? response.web.results.collect do |r|
+        {'title' => r.title,
+        'unescapedUrl'=> r.url,
+        'content'=> (r.description rescue ""),
+        'cacheUrl'=> (r.CacheUrl rescue ""),
+        'deepLinks' => r["DeepLinks"]
+        }
+      end : []
       self.results = WillPaginate::Collection.create(@page+1, DEFAULT_PER_PAGE, pagination_total) { |pager| pager.replace(results_array) }
       self.startrecord = @page * DEFAULT_PER_PAGE + 1
       self.endrecord = self.startrecord + self.results.size - 1
