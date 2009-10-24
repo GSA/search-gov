ff1441bfac0a643fd4482cbe828901b5539ec90c
Loren Siebert <loren@siebert.org>
2009-10-24 11:40:17 -0700

    Upgraded to RSpec 1.2.9

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -27,8 +27,8 @@ config.action_mailer.delivery_method = :test
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
 
-config.gem "rspec",            :lib => false
-config.gem "rspec-rails",      :lib => false
+config.gem "rspec",            :lib => false, :version => '>= 1.2.9'
+config.gem "rspec-rails",      :lib => false, :version => '>= 1.2.9'
 config.gem "remarkable_rails", :lib => false, :version => '>= 3.1.10'
 config.gem "webrat",           :lib => false
 config.gem "nokogiri",         :lib => false
