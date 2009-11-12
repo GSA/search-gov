e26cac6cf117a2ae73b9494105464b2bf199d09f
Loren Siebert <loren@siebert.org>
2009-11-11 19:02:42 -0800

    Upgraded haml to 2.3.3

--- config/environment.rb
+++ config/environment.rb
@@ -14,13 +14,13 @@ Rails::Initializer.run do |config|
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
   # Specify gems that this application depends on and have them installed with rake gems:install
-  config.gem "haml", :version => '2.2.2'
+  config.gem "haml", :version => '2.2.3'
   config.gem "json", :version => '>= 1.1.7'
   config.gem 'mislav-will_paginate', :version => '~> 2.3.11', :lib => 'will_paginate', :source => 'http://gems.github.com'
   config.gem "chriseppstein-compass", :lib => 'compass', :source => 'http://gems.github.com', :version => '>= 0.8.9'
-  config.gem "hpricot"
-  config.gem "calendar_date_select"
-  config.gem "bcrypt-ruby", :lib => "bcrypt"
+  config.gem "hpricot", :version => '>= 0.8.1'
+  config.gem "calendar_date_select", :version => '>= 1.15'
+  config.gem "bcrypt-ruby", :lib => "bcrypt", :version => '>= 2.1.1'
   config.gem "authlogic", :version => '>= 2.1.1'
 
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
