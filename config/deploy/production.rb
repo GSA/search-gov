e94ffb45ca642080356e597cc770708bf65f8ea2
Loren Siebert <loren@siebert.org>
2009-11-18 10:53:01 -0800

    Override database.yml for production stage

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,3 +3,9 @@ set :deploy_to,   "/var/www/#{application}"
 role :app, "10.153.9.203", "10.153.9.211"
 role :web, "10.153.9.203", "10.153.9.211"
 role :db,  "10.153.9.203", :primary => true
+
+before "deploy:setup", "database_yaml"
+
+task :database_yaml, :except => { :no_release => true } do
+  run "cp /shared/system/database.yml #{release_path}/config/database.yml"
+end
