9930f871e2ecf594e8868e7a0739522543936fa9
Loren Siebert <loren@siebert.org>
2009-12-07 11:34:48 -0800

    Added contact_email and contact_name to active scaffold

--- app/controllers/admin/affiliates_controller.rb
+++ app/controllers/admin/affiliates_controller.rb
@@ -2,7 +2,7 @@ class Admin::AffiliatesController < Admin::AdminController
   before_filter :require_affiliate_admin
 
   active_scaffold :affiliate do |config|
-    config.columns = [:name, :domains, :header, :footer]
+    config.columns = [:name, :contact_email, :contact_name, :domains, :header, :footer]
     config.list.sorting = { :name => :asc }
   end
 
