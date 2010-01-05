5cf02e52c4fe56cb9255e2c408aa71af9bbe978c
Loren Siebert <loren@siebert.org>
2010-01-05 12:47:40 -0800

    Removed HTTP basic auth from analytics controller, since it interferes with other basic auth on staging box

--- app/controllers/analytics/analytics_controller.rb
+++ app/controllers/analytics/analytics_controller.rb
@@ -1,12 +1,4 @@
 class Analytics::AnalyticsController < ActionController::Base
-  layout "analytics"
   helper :all
-  before_filter :check_access if Rails.env == "production"
-
-  private
-  def check_access
-    authenticate_or_request_with_http_basic do |user_name, password|
-      user_name == "analytics" && password == "4n4lyt1cs"
-    end
-  end
+  layout "analytics"
 end
