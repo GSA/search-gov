be147b74e4914fad3b346f7c6b6efb48dedbd9f3
Loren Siebert <loren@siebert.org>
2010-01-05 12:41:16 -0800

    Added HTTP basic auth into analytics controller, since the global auth is going away

--- app/controllers/analytics/analytics_controller.rb
+++ app/controllers/analytics/analytics_controller.rb
@@ -1,4 +1,12 @@
 class Analytics::AnalyticsController < ActionController::Base
-  helper :all
   layout "analytics"
+  helper :all
+  before_filter :check_access if Rails.env == "production"
+
+  private
+  def check_access
+    authenticate_or_request_with_http_basic do |user_name, password|
+      user_name == "analytics" && password == "4n4lyt1cs"
+    end
+  end
 end
