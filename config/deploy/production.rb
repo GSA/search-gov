cd07f8f01410a0bc05f3104f433bb929c5046730
Loren Siebert <loren@siebert.org>
2010-01-14 20:41:11 -0800

    Capistrano deployment for production stage now copies sunspot.yml file into place

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,8 +4,9 @@ role :app, "10.153.9.203", "10.153.9.211"
 role :web, "10.153.9.203", "10.153.9.211"
 role :db,  "10.153.9.203", :primary => true
 
-before "deploy:symlink", "database_yaml"
+before "deploy:symlink", "production_yaml_files"
 
-task :database_yaml, :except => { :no_release => true } do
+task :production_yaml_files, :except => { :no_release => true } do
   run "cp #{shared_path}/system/database.yml #{release_path}/config/database.yml"
+  run "cp #{shared_path}/system/sunspot.yml #{release_path}/config/sunspot.yml"
 end
