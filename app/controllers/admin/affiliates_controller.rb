a560c7705996de5d9b02d119ed3843c3818efdf1
Greg Gershman <greggersh@MacGregory.local>
2010-02-03 12:13:42 -0500

    Add last login, created and updated times to users and affiliate display on admin

--- app/controllers/admin/affiliates_controller.rb
+++ app/controllers/admin/affiliates_controller.rb
@@ -1,7 +1,7 @@
 class Admin::AffiliatesController < Admin::AdminController
 
   active_scaffold :affiliate do |config|
-    config.columns = [:name, :domains, :header, :footer, :boosted_sites]
+    config.columns = [:name, :domains, :header, :footer, :boosted_sites, :created_at, :updated_at]
     config.list.sorting = { :name => :asc }
   end
 
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,7 +1,7 @@
 class Admin::UsersController < Admin::AdminController
   active_scaffold :user do |config|
     config.actions.exclude :create, :delete
-    config.columns = [:email, :contact_name, :affiliates]
+    config.columns = [:email, :contact_name, :affiliates, :last_login_at]
     config.list.sorting = { :email => :asc }
     config.columns[:affiliates].form_ui= :select
   end
