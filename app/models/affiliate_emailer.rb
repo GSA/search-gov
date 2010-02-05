74f07f07e35bed91044ce28b68e24bcd7c94424c
John Cromartie <john@tanto.local>
2010-02-04 22:07:40 -0500

    fixed bug where visited links were bold in affiliate searches

--- app/models/affiliate_emailer.rb
+++ app/models/affiliate_emailer.rb
@@ -3,7 +3,7 @@ class AffiliateEmailer < ActionMailer::Base
 
   def email(affiliate_user, subject, body)
     @recipients = affiliate_user.email
-    @from = "***REMOVED***"
+    @from = "support@searchusa.zendesk.com"
     @subject = subject
     @sent_on = Time.now
     @headers['Content-Type'] = "text/plain; charset=utf-8; format=flowed"
