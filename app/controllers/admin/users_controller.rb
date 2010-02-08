2518d2776cf36be39f03efa70aed8fe76706b294
Loren Siebert <loren@siebert.org>
2010-02-08 06:52:02 -0800

    Added editable fields to User active scaffolding
    - Updated rcov coverage

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,7 +2,9 @@ class Admin::UsersController < Admin::AdminController
   active_scaffold :user do |config|
     config.actions.exclude :create, :delete
     config.columns = [:email, :contact_name, :affiliates, :last_login_at]
+    config.update.columns = [:affiliates, :email, :contact_name, :organization_name, :address, :address2, :phone, :city, :state, :zip]
     config.list.sorting = { :email => :asc }
     config.columns[:affiliates].form_ui= :select
+    config.columns[:state].form_ui= :usa_state
   end
 end
