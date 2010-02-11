ece7e6dcb2d460d7cecea442e4e8dcb376e64e7d
John Cromartie <john@tanto.local>
2010-02-11 18:11:20 -0500

    removing Mobile link in footer until every view has a mobile counterpart (Rails does not support fallback for formats, might have to use respond_to in controllers for that)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -58,7 +58,7 @@ module ApplicationHelper
   end
 
   def footer_links
-    iterate_links(FOOTER_LINKS[I18n.locale.to_sym].clone << ["Mobile", url_for_mobile_mode(true)])
+    iterate_links(FOOTER_LINKS[I18n.locale.to_sym].clone)
   end
 
   def render_webtrends_code
