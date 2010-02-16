86c64ff55c10b3f1f078baeb9ac7c9041b54a2c4
Loren Siebert <loren@siebert.org>
2010-02-16 11:26:37 -0800

    Removed unused variables/lines from log_file rake

--- app/models/log_file.rb
+++ app/models/log_file.rb
@@ -22,7 +22,7 @@ class LogFile < ActiveRecord::Base
     query_string = log.path.split('?')[1]
     parsed_log = CGI.parse(query_string)
     query = parsed_log["query"][0]
-    affiliate = parsed_log["affiliate"][0].blank? ? "usasearch.gov" : parsed_log["affiliate"][0] 
+    affiliate = parsed_log["affiliate"][0].blank? ? "usasearch.gov" : parsed_log["affiliate"][0]
     return if query.nil?
     noquery = parsed_log["noquery"][0]
     Query.create!(:query => query.strip, :affiliate => affiliate, :ipaddr => ipaddr, :timestamp => datetime) if noquery.nil?
@@ -38,11 +38,10 @@ class LogFile < ActiveRecord::Base
       RAILS_DEFAULT_LOGGER.warn("File #{filepath} has #{failures.size} errors: #{failures.inspect}") if failures.size > 0
     end
   end
-  
+
   def self.parse_line_for_click(log_entry)
     log = Apache::Log::Combined.parse log_entry
     queried_at = log.time
-    ip_address = log.remote_ip
     if log.path.include?('?')
       query_string = log.path.split('?')[1]
       parsed_log = CGI.parse(query_string)
@@ -52,24 +51,22 @@ class LogFile < ActiveRecord::Base
         host = captures[1]
         if host
           tld = host.split('.').last
-          #unless tld == 'gov' || tld == 'mil'
-            serp_position = parsed_log["rrank"][0].to_i
-            source = parsed_log["rsource"][0]
-            project = parsed_log["v:project"][0]
-            affiliate = parsed_log["affiliate"][0].blank? ? "usasearch.gov" : parsed_log["affiliate"][0] 
-            referrer = log.referer
-            if referrer
-              referrer_query_string = referrer.split('?')[1]
-              if referrer_query_string
-                parsed_referrer = CGI.parse(referrer_query_string)
-                query = parsed_referrer['query'][0]
-                if affiliate.blank? || affiliate == 'usasearch.gov'
-                  affiliate = parsed_referrer["affiliate"][0].blank? ? "usasearch.gov" : parsed_referrer["affiliate"][0]
-                end
+          serp_position = parsed_log["rrank"][0].to_i
+          source = parsed_log["rsource"][0]
+          project = parsed_log["v:project"][0]
+          affiliate = parsed_log["affiliate"][0].blank? ? "usasearch.gov" : parsed_log["affiliate"][0]
+          referrer = log.referer
+          if referrer
+            referrer_query_string = referrer.split('?')[1]
+            if referrer_query_string
+              parsed_referrer = CGI.parse(referrer_query_string)
+              query = parsed_referrer['query'][0]
+              if affiliate.blank? || affiliate == 'usasearch.gov'
+                affiliate = parsed_referrer["affiliate"][0].blank? ? "usasearch.gov" : parsed_referrer["affiliate"][0]
               end
             end
-            Click.create!(:query => query, :queried_at => queried_at, :url => url, :serp_position => serp_position, :source => source, :project => project, :affiliate => affiliate, :host => host, :tld => tld)
-          #end
+          end
+          Click.create!(:query => query, :queried_at => queried_at, :url => url, :serp_position => serp_position, :source => source, :project => project, :affiliate => affiliate, :host => host, :tld => tld)
         end
       end
     end
