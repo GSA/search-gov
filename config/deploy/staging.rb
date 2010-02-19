ad1ab08cc62f110161e871b57c86f8d96c0a9e65
Loren Siebert <loren@siebert.org>
2010-02-19 12:45:31 -0800

    Changed deployment to staging server to use VPN

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,4 @@
 set :user,        "xcet_admin"
 set :deploy_to,   "/home/xcet_admin/#{application}"
-set :domain,      "209.251.180.31"
+set :domain,      "10.153.9.201"
 server domain, :app, :web, :db, :primary => true
