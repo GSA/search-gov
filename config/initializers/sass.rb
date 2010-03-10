4636fd71ee1a6f9c5f41a8e011dd320246a38c97
Loren Siebert <loren@siebert.org>
2010-03-09 16:16:47 -0800

    Changed Sass initializer to compress when in production/test/cucumber RAILS_ENV
    - Sass was slowing down test suite by over 4x

--- config/initializers/sass.rb
+++ config/initializers/sass.rb
@@ -1,15 +1,15 @@
 require 'sass'
 
-if /production|staging/.match(RAILS_ENV)
+if /production|test|cucumber/.match(RAILS_ENV)
   # Compress CSS (a small file is preferable in production)
   Sass::Plugin.options[:style] = :compressed
-else  
+else
   # Expand CSS
   Sass::Plugin.options[:style] = :expanded
-  
+
   # Generate CSS from SASS every time a controller is accessed
   Sass::Plugin.options[:always_update] = true
-  
+
   # Insert comments in the CSS about the line numbers of the Sass source
   Sass::Plugin.options[:line_comments] = true
 end
