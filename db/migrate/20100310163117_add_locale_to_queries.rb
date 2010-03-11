f4b6db0c523380a723131192a945b1a8adb4919b
Loren Siebert <loren@siebert.org>
2010-03-10 17:45:38 -0800

    Ugly fix to migration to get around not having legacy queries table on production environment

--- db/migrate/20100310163117_add_locale_to_queries.rb
+++ db/migrate/20100310163117_add_locale_to_queries.rb
@@ -1,5 +1,6 @@
 class AddLocaleToQueries < ActiveRecord::Migration
   def self.up
+    execute "CREATE TABLE IF NOT EXISTS `queries` ( `ipaddr` varchar(17) default NULL, `query` varchar(100) default NULL, `affiliate` varchar(32) default NULL, `timestamp` timestamp NOT NULL default '0000-00-00 00:00:00', KEY `timestamp` (`timestamp`), KEY `queryindex` (`query`)) ENGINE=MyISAM DEFAULT CHARSET=latin1"  
     add_column :queries, :locale, :string, :limit => 5
   end
 
