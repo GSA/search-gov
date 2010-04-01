af89196e16bd12c132bb4078d0110916112bbe96
Loren Siebert <loren@siebert.org>
2010-04-01 10:33:44 -0700

    Changed migration to only run strip() on phrases that would be affected by it, otherwise it takes forever to run the migration
    - Every call to save() on the SaytFilter causes a callback to check the phrase against the SaytSuggestion list, which is very long on staging/production, so the migration took a very long time and eventually 'cap deploy' would time out, leaving apache off, the migration unfinished, and the rake task spinning away in the background.

--- db/migrate/20100401021109_trim_sayt_filters.rb
+++ db/migrate/20100401021109_trim_sayt_filters.rb
@@ -1,6 +1,6 @@
 class TrimSaytFilters < ActiveRecord::Migration
   def self.up
-    SaytFilter.all.each { |sf| sf.update_attribute(:phrase, sf.phrase.strip) }
+    SaytFilter.all.each { |sf| sf.update_attribute(:phrase, sf.phrase.strip) unless sf.phrase == sf.phrase.strip }
   end
 
   def self.down
