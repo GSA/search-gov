a7fffcba8de4a895f8494039e27f75a49be0c472
Loren Siebert <loren@siebert.org>
2010-05-06 14:33:41 -0700

    Not much point in raising an exception if the click doesn't have all the necessary info
    - These occasional requests might be coming in manually (people experimenting), as there's no good reason for them to come in with missing parameters

--- app/controllers/clicks_controller.rb
+++ app/controllers/clicks_controller.rb
@@ -1,14 +1,14 @@
 class ClicksController < ApplicationController
   def create
-    Click.create!(:url => params['u'],
-                  :query => params['q'],
-                  :serp_position => params['p'],
-                  :queried_at => Time.at(params['t'].to_i),
-                  :affiliate => params['a'].present? ? params['a'] : nil,
-                  :results_source => params['s'],
-                  :clicked_at => DateTime.now,
-                  :click_ip => request.env['REMOTE_ADDR'],
-                  :user_agent => request.user_agent)
+    Click.create(:url => params['u'],
+                 :query => params['q'],
+                 :serp_position => params['p'],
+                 :queried_at => Time.at(params['t'].to_i),
+                 :affiliate => params['a'].present? ? params['a'] : nil,
+                 :results_source => params['s'],
+                 :clicked_at => DateTime.now,
+                 :click_ip => request.env['REMOTE_ADDR'],
+                 :user_agent => request.user_agent)
     render :nothing => true
   end
 end
