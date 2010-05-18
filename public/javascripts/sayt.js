5b7335d98a33a7990dfb65985363c5490a1afe8c
Greg Gershman <greggersh@yahoo.com>
2010-05-18 09:48:08 -0400

    Fixed domain in sayt.js to point to searchdemo

--- public/javascripts/sayt.js
+++ public/javascripts/sayt.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $("#usagov-search-field").autocomplete("/searches/auto_complete_for_search_query", {
+  $("#usagov-search-field").autocomplete("http://searchdemo.usa.gov/searches/auto_complete_for_search_query", {
       max: 15,
       extraParams: {
           locale: "en",
