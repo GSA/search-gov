3a8a339c69b2bad0e38857720f88f0ed25999303
Loren Siebert <loren@siebert.org>
2010-06-30 16:16:08 -0700

    Assigned special Hoptoad app key to rackspace branch so deployments would not conflict with terremark deployments

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,6 +1,6 @@
 require 'rexml/document'
 HoptoadNotifier.configure do |config|
-  config.api_key = '***REMOVED***'
+  config.api_key = 'cbe1c27890a51f3829e6476388b3d0c8'
   config.ignore << ActionController::MethodNotAllowed
   config.ignore << ActionController::UnknownHttpMethod
   config.ignore << REXML::ParseException
