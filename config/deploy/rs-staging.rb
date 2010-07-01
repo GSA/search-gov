d25ee7f777ee9aeaaa4ed4a1dc1b7d893fe2f9f5
Loren Siebert <loren@siebert.org>
2010-07-01 10:39:46 -0700

    Using VPN for connections/deployment to Rackspace now

--- config/deploy/rs-staging.rb
+++ config/deploy/rs-staging.rb
@@ -1,5 +1,5 @@
 set :user,        "search"
 set :deploy_to,   "/home/jwynne/#{application}"
-set :domain,      "173.203.40.160"
+set :domain,      "192.168.100.160"
 set :branch,      "rackspace"
 server domain, :app, :web, :db, :primary => true
