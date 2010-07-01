b234d0dcc17045d096d3dd6058030c49636cbbf3
Loren Siebert <loren@siebert.org>
2010-06-30 17:45:00 -0700

    New servers are all standardized to use UTC time zone, so we don't have to ever deal with daylight savings

--- config/environment.rb
+++ config/environment.rb
@@ -30,7 +30,7 @@ Rails::Initializer.run do |config|
 
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
-  config.time_zone = 'Eastern Time (US & Canada)'
+  config.time_zone = 'UTC'
 end
 
 APP_EMAIL_ADDRESS = "***REMOVED***"
