5befb792c0c7ae7263b703108fe827711ab97057
Greg Gershman <greggersh@yahoo.com>
2010-07-21 13:27:52 -0400

    Methods on proessed_query

--- app/models/processed_query.rb
+++ app/models/processed_query.rb
@@ -1,2 +1,30 @@
 class ProcessedQuery < ActiveRecord::Base
+  validates_presence_of :query, :affiliate, :day, :times
+  validates_numericality_of :times
+     
+  searchable do
+    text :query, :stored => true
+    string :affiliate, :stored => true
+    integer :times, :stored => true
+    time :day, :trie => true, :stored => true
+  end
+  
+  def self.related_to(query, options = {})
+    ProcessedQuery.search do
+      request_handler 'relatedSearch'
+      keywords query
+      with(:affiliate).equal_to(options[:affiliate]) unless options[:affiliate].blank?
+      paginate :page => page, :per_page => per_page
+    end  
+  end  
+  
+  def self.load_csv(filename)
+    File.open(filename) do |file|
+      while line = file.gets
+        row = line.split("\t")
+        day = Date.parse(row[3][0..10])
+        ProcessedQuery.create(:query => row[0], :affiliate => row[1], :times => row[2], :day => day) rescue nil
+      end
+    end
+  end
 end
