2d0d57a87a8314e4d47993490728a46c2739e0b9
Greg Gershman <greggersh@yahoo.com>
2010-07-22 09:00:42 -0400

    Little fixes

--- spec/models/processed_query_spec.rb
+++ spec/models/processed_query_spec.rb
@@ -9,8 +9,23 @@ describe ProcessedQuery do
       :times => 20
     }
   end
+  
+  should_validate_presence_of :query, :affiliate, :day, :times
+  should_validate_numericality_of :times
 
   it "should create a new instance given valid attributes" do
     ProcessedQuery.create!(@valid_attributes)
+  end
+  
+  describe "#related_to" do
+    before do
+      ProcessedQuery.create!(@valid_attributes.merge(:query => 'barack h. obama'))
+      ProcessedQuery.create!(@valid_attributes.merge(:query => 'barack obama'))      
+      Sunspot.commit
+    end
+    
+    it "should return results that are similar to the query specified" do
+      ProcessedQuery.related_to('obama').total.should == 2
+    end
   end
 end
