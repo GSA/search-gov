39330d1c49ab4b32fe0e1e7fb625b88ad0d791f0
Greg Gershman <greggersh@yahoo.com>
2010-08-01 14:58:40 -0400

    updates to related search to format results properly and specs

--- app/models/related_search.rb
+++ app/models/related_search.rb
@@ -6,7 +6,7 @@ class RelatedSearch
     related_queries = RelatedQuery.find_all_by_query(query, :order => 'score desc').collect{|related_query| related_query.related_query}
     if related_queries.size < 5
       related_processed_queries = ProcessedQuery.related_to(query, :per_page => 5 - related_queries.size).results.collect{|processed_query| processed_query.query}
-      related_queries << related_processed_queries unless related_processed_queries.empty?
+      related_queries.concat(related_processed_queries)
     end
     related_queries
   end
--- spec/models/related_search_spec.rb
+++ spec/models/related_search_spec.rb
@@ -10,14 +10,50 @@ describe RelatedSearch do
     end
     
     it "should return queries related to the query specified ordered by score descending" do
-      related_queries = RelatedSearch.related_to('barack obama')
-      related_queries.size.should == 2
-      related_queries.first.related_query.should == 'joe biden'
-      related_queries.last.related_query.should == 'michelle obama'
+      related_search = RelatedSearch.related_to('barack obama')
+      related_search.size.should == 2
+      related_search.first.should == 'joe biden'
+      related_search.last.should == 'michelle obama'
     end
     
     it "should return an empty result if there are no related queries to the query specified" do
       RelatedSearch.related_to('margaret thatcher').should be_empty
+    end
+    
+    context "when ProcessedQueries that are related to the query are present" do
+      before do
+        ProcessedQuery.create(:query => 'barack hussein obama', :times => 100, :day => Date.yesterday, :affiliate => 'usasearch.gov')
+        ProcessedQuery.create(:query => 'barack h. obama', :times => 100, :day => Date.yesterday, :affiliate => 'usasearch.gov')
+        ProcessedQuery.create(:query => 'president barack obama', :times => 100, :day => Date.yesterday, :affiliate => 'usasearch.gov')
+        ProcessedQuery.create(:query => 'barak obama', :times => 100, :day => Date.yesterday, :affiliate => 'usasearch.gov')
+        Sunspot.commit
+        ProcessedQuery.reindex
+      end
+      
+      it "should backfill the related search results with matching ProcessedQuery values, so that a total of values are returned" do
+        related_search = RelatedSearch.related_to('barack obama')
+        related_search.size.should == 5
+        related_search.first.should == 'joe biden'
+        related_search[1].should == 'michelle obama'
+        related_search[2].should == 'president barack obama'
+        related_search[3].should == 'barack hussein obama'
+        related_search.last.should == 'barack h. obama'
+      end
+      
+      it "should return an empty array if nothing matches" do
+        RelatedSearch.related_to('margaret thatcher').should be_empty
+      end
+      
+      context "when there are no RelatedQuery records" do
+        before do
+          RelatedQuery.destroy_all
+        end
+        
+        it "should return up to 5 ProcessedQuery results" do
+          related_search = RelatedSearch.related_to('barack obama')
+          related_search.size.should == 3
+        end
+      end
     end
   end
 end
