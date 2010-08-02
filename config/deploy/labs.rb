1a75c65d11f6bbcbae4229c3fcf24100d22fe039
Greg Gershman <greggersh@yahoo.com>
2010-08-02 11:10:28 -0400

    Update config for labs deployment to point to EC2 server

--- config/deploy/labs.rb
+++ config/deploy/labs.rb
@@ -1,6 +1,6 @@
-set :user,        "xcet_admin"
-set :deploy_to,   "/home/xcet_admin/#{application}_labs"
-set :domain,      "10.153.9.201"
+set :user,        "labs"
+set :deploy_to,   "/home/labs/#{application}"
+set :domain,      "184.72.238.88"
 server domain, :app, :web, :db, :primary => true
 set :branch, "labs"
 set :rails_env, "labs"
