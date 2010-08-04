63f15fa344c2ee319e4423304810f14de30b450a
Greg Gershman <greggersh@yahoo.com>
2010-08-04 12:23:53 -0400

    Enable Image Search for Affiliates
     - Update specs, add locale-specific page titles
    
     - remove unused code from image search controller (was used in search controller for staging); Updated coverage

--- app/controllers/image_searches_controller.rb
+++ app/controllers/image_searches_controller.rb
@@ -4,18 +4,35 @@ class ImageSearchesController < ApplicationController
   def index
     @search = ImageSearch.new(@search_options)
     @search.run
-    respond_to do |format|
-      format.html
-      format.mobile
-      format.json { render :json => @search }
+    handle_affiliate_search
+    if @search_options[:affiliate]
+      render :action => "affiliate_index", :layout => "affiliate"
+    else
+      respond_to do |format|
+        format.html
+        format.mobile
+        format.json { render :json => @search }
+      end
     end
   end
 
   private
+    
+  def handle_affiliate_search
+    if @search_options[:affiliate]
+      @affiliate = @search_options[:affiliate]
+      @scope_id = @search_options[:scope_id]
+      @page_title = "#{t :image_search_results_for} #{@affiliate.name}: #{@search.query}"
+    end
+  end
+  
   def set_search_options
+    affiliate = params["affiliate"] ? Affiliate.find_by_name(params["affiliate"]) : nil
     @search_options = {
       :page => (params[:page].to_i - 1),
       :query => params["query"],
+      :affiliate => affiliate,
+      :scope_id => params["scope_id"] || nil,
       :results_per_page => 30
     }
   end
