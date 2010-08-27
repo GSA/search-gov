c79635947b8148026bcd04aa98efd754dd364db2
Greg Gershman <greggersh@yahoo.com>
2010-08-27 12:26:45 -0400

    Streamline migration so we only alter table once.

--- db/migrate/20100827144830_change_charset_on_queries_table_to_utf8.rb
+++ db/migrate/20100827144830_change_charset_on_queries_table_to_utf8.rb
@@ -1,11 +1,9 @@
 class ChangeCharsetOnQueriesTableToUtf8 < ActiveRecord::Migration
   def self.up
-    execute "ALTER TABLE queries DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci"
-    execute "ALTER TABLE queries CONVERT TO CHARSET utf8 COLLATE utf8_unicode_ci"
+    execute "ALTER TABLE queries DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci, CONVERT TO CHARSET utf8 COLLATE utf8_unicode_ci"
   end
 
   def self.down
-    execute "ALTER TABLE queries CONVERT TO CHARSET latin1"
-    execute "ALTER TABLE queries DEFAULT CHARSET=latin1"
+    execute "ALTER TABLE queries CONVERT TO CHARSET latin1, DEFAULT CHARSET=latin1"
   end
 end
