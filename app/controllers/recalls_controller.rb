2fc27d9885b54c22afd58788cf2b7f46e9da66f3
Loren Siebert <loren@siebert.org>
2010-11-29 19:21:57 -0800

    Caching and error handling for Recalls API
    - Added explicit input checks for several params/options
    - Added Redis caching for 30 minutes
    - Updated recalls API description page to be more accurate

--- app/controllers/recalls_controller.rb
+++ app/controllers/recalls_controller.rb
@@ -1,24 +1,41 @@
 class RecallsController < ApplicationController
+  @@redis = Redis.new(:host => REDIS_HOST, :port => REDIS_PORT)
+  RECALLS_CACHE_DURATION_IN_SECONDS = 60 * 30
+
   def index
     valid_options = %w{start_date end_date upc sort code organization make model year food_type}
-    begin
-      search = Recall.search_for(params[:query],
-                                 params.reject {|k,| !valid_options.include?k.to_s},
-                                 params[:page])
-    rescue Exception => exception
+    valid_params = params.reject { |k,| !valid_options.include? k.to_s }
+    error_message = verify_params(valid_params.merge(:page => params[:page]))
+    if error_message
+      render :json => {:error => error_message}
+      return
     end
+
+    query = params[:query]
+    page = params[:page]
+    cache_key = [valid_params.to_s, query, page].join(':')
+    success_total_results_json = @@redis.get(cache_key) rescue nil
+    if success_total_results_json.nil?
+      search = Recall.search_for(query, valid_params, page)
+      success_total_results_json = {:success => {:total => search.total, :results => search.results}}.to_json
+      @@redis.setex(cache_key, RECALLS_CACHE_DURATION_IN_SECONDS, success_total_results_json) rescue nil
+    end
+
     respond_to do |format|
-      format.json {
-        if exception
-          render :json => { :error => exception.to_s }
-        else
-          render :json => { :success => { :total => search.total, :results => search.results } }
-        end
-      }
-      format.any {
-        render :text => 'Not Implemented'
-      }
+      format.json { render :text => success_total_results_json, :content_type => "application/json" }
+      format.any { render :text => 'Not Implemented' }
     end
+  end
+
+  private
+  def verify_params(p)
+    error_message = nil
+    error_message = "invalid date" if (p[:start_date] and not p[:start_date] =~ /^\d{4}-\d{1,2}-\d{1,2}$/) or (p[:end_date] and not p[:end_date] =~ /^\d{4}-\d{1,2}-\d{1,2}$/)
+    error_message = "invalid organization" if p[:organization] and not %w{NHTSA CPC CDC}.include? p[:organization]
+    error_message = "invalid code" if p[:code] and not %w{E V I T C X}.include? p[:code]
+    error_message = "invalid year" if p[:year] and not p[:year] =~ /^\d{4}$/
+    error_message = "invalid page" if p[:page] and not p[:page] =~ /^\d+$/
+    return error_message
   end
 end
 
--- spec/controllers/recalls_controller_spec.rb
+++ spec/controllers/recalls_controller_spec.rb
@@ -4,28 +4,48 @@ describe RecallsController do
   describe "#index" do
     context "when all parameters specified" do
       before do
+        @redis = RecallsController.send(:class_variable_get, :@@redis)
         @search = mock(Sunspot::Search)
         @search.stub!(:total).and_return 1
         @search.stub!(:results).and_return [{:key1=>"val1"}, {:key2=>"val2"}]
         @query_string = 'stroller'
         @page = "2"
-        valid_options = %w{start_date end_date upc sort code organization make model year}
-        @valid_options_hash = valid_options.inject({}) { |s, e| s.merge( { e.to_s => "MY_#{e.upcase}" } ) }
-        @valid_params = @valid_options_hash.merge(:format => 'json', :query => @query_string, :page => @page)
+        @valid_options_hash = {"start_date"=> "2010-11-10", "end_date"=> "2010-11-20"}
+        @valid_params = @valid_options_hash.merge("format" => 'json', "query" => @query_string, "page" => @page)
       end
 
-      it "should perform a search with the relevant parameters passed in" do
-        Recall.should_receive(:search_for).with(@query_string, @valid_options_hash, @page).and_return(@search)
-        param_to_be_ignored = {:ignore_me => "foo bar"}
-        get :index, @valid_params.merge(param_to_be_ignored)
+      context "when result is not cached" do
+        before do
+          @redis.stub!(:get).and_return nil
+        end
+
+        it "should perform a search with the relevant parameters passed in" do
+          Recall.should_receive(:search_for).with(@query_string, @valid_options_hash, @page).and_return(@search)
+          param_to_be_ignored = {:ignore_me => "foo bar"}
+          get :index, @valid_params.merge(param_to_be_ignored)
+        end
+
+        it "should cache the result" do
+          @redis.should_receive(:setex).and_return 1
+          get :index, :format => 'json'
+        end
+
+        it "should return parsable JSON" do
+          Recall.stub!(:search_for).and_return(@search)
+          get :index, @valid_params
+          parsed_response = JSON.parse(response.body)
+          parsed_response["success"]["total"].should == 1
+          parsed_response["success"]["results"].should == [{"key1"=>"val1"}, {"key2"=>"val2"}]
+        end
+
       end
 
-      it "should return parsable JSON" do
-        Recall.stub!(:search_for).and_return(@search)
-        get :index, @valid_params
-        parsed_response = JSON.parse(response.body)
-        parsed_response["success"]["total"].should == 1
-        parsed_response["success"]["results"].should == [{"key1"=>"val1"}, {"key2"=>"val2"}]
+      context "when result is cached" do
+        it "should fetch the result from cache" do
+          @redis.should_receive(:get).once.and_return "some valid results in JSON"
+          get :index, :format => 'json'
+          response.body.should == "some valid results in JSON"
+        end
       end
     end
 
@@ -38,28 +58,53 @@ describe RecallsController do
         response.body.should contain('Not Implemented')
       end
     end
-    
-    context "when recalls search returns an error" do
-      it "should pass along the error message in a JSON error object" do
-        Recall.stub!(:search_for).and_raise(StandardError.new("Some error occured"))
-        get :index, :query => "something"
-        parsed_response = JSON.parse(response.body)
-        parsed_response["error"].should == "Some error occured"
-      end
-    end
-    
+
     context "when dates are submitted in an invalid format" do
       before do
-        @start_date = '05-20-2010'
-        @end_date = '06-20-2010'
+        @good_date = '2010-5-20'
+        @bad_date = '05-20-2010'
         @query = "stroller"
       end
-      
-      it "should throw an error and return a JSON error object" do
-        Recall.should_receive(:search_for).and_raise('invalid date')
-        get :index, :query => @query, :start_date => @start_date, :end_date => @end_date
+
+      it "should return a JSON error object" do
+        get :index, :query => @query, :start_date => @bad_date, :end_date => @good_date
         parsed_response = JSON.parse(response.body)
         parsed_response["error"].should == "invalid date"
+        get :index, :query => @query, :end_date => @bad_date, :start_date => @good_date
+        parsed_response = JSON.parse(response.body)
+        parsed_response["error"].should == "invalid date"
+      end
+    end
+
+    context "when invalid organzation is specified" do
+      it "should return a JSON error object" do
+        get :index, :organization => "bogus"
+        parsed_response = JSON.parse(response.body)
+        parsed_response["error"].should == "invalid organization"
+      end
+    end
+
+    context "when invalid code is specified" do
+      it "should return a JSON error object" do
+        get :index, :code => "bogus"
+        parsed_response = JSON.parse(response.body)
+        parsed_response["error"].should == "invalid code"
+      end
+    end
+
+    context "when invalid year is specified" do
+      it "should return a JSON error object" do
+        get :index, :year => "bogus"
+        parsed_response = JSON.parse(response.body)
+        parsed_response["error"].should == "invalid year"
+      end
+    end
+
+    context "when invalid page is specified" do
+      it "should return a JSON error object" do
+        get :index, :page => "bogus"
+        parsed_response = JSON.parse(response.body)
+        parsed_response["error"].should == "invalid page"
       end
     end
   end
