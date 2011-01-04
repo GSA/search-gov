12b4a5444d3c96baf7b0b71d861b70e9c9c76560
Loren Siebert <loren@siebert.org>
2011-01-04 14:58:08 -0800

    Typo fix on spec

--- spec/views/layouts/application.mobile.haml_spec.rb
+++ spec/views/layouts/application.mobile.haml_spec.rb
@@ -1,7 +1,7 @@
 require "#{File.dirname(__FILE__)}/../../spec_helper"
 describe "layouts/application.mobile.haml" do
   context "when a mobile page is displayed" do
-    it "should should show the movile webtrends javascript" do
+    it "should show the mobile webtrends javascript" do
       template.stub!(:is_device?).and_return false
       render
       response.body.should have_tag("script[src=/javascripts/webtrends_mobile.js][type=text/javascript]")
