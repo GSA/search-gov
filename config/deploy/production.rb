94f396147b3a8fe82b6c60a085f4a1099c02913d
Loren Siebert <loren@siebert.org>
2011-01-13 17:33:10 -0800

    [Fixes #8229655] Updated production.rb to restart Resque workers on cron machine after production deployment
    - Uses two new scripts in ~/scripts on cron to start up five workers and kill them all off (forked processes, too). These scripts are also used by the new /etc/rc.d/init.d/resque_workers script.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,8 +4,15 @@ set :branch,      "production"
 role :app, "192.168.100.164", "192.168.100.165", "192.168.100.166", "192.168.110.10", "192.168.100.161", "192.168.110.11"
 role :web, "192.168.100.164", "192.168.100.165", "192.168.100.166", "192.168.110.10", "192.168.100.161", "192.168.110.11"
 role :db,  "192.168.100.161", :primary => true
+role :resque_workers,  "192.168.100.164"
 
 before "deploy:symlink", "production_yaml_files"
+before "deploy:cleanup", "restart_resque_workers"
+
+task :restart_resque_workers, :roles => :resque_workers do
+  run "sudo /home/jwynne/scripts/stop_resque_workers"
+  run "sudo /home/jwynne/scripts/start_resque_workers"
+end
 
 task :production_yaml_files, :except => { :no_release => true } do
   run "cp #{shared_path}/system/database.yml #{release_path}/config/database.yml"
