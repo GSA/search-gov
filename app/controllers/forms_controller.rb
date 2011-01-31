ba3bf479fe21575d905a276b5cde0b4dca54ff57
Greg Gershman <greggersh@yahoo.com>
2011-01-31 15:26:20 -0500

    [Completes #9036431] Searchers can find Search.USA.gov Forms via search engines

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,7 +1,6 @@
 class FormsController < ApplicationController
 
   def index
-    @title = "Form Search Home - "
     @top_forms = []
     1.upto(3) do |index|
       @top_forms << TopForm.find_all_by_column_number(index, :order => 'sort_order ASC')
