ef23547502a5ddfbaf83ae669cd2e1f07056f08d
Irrational Design <services@irrationaldesign.com>
2011-02-17 11:42:05 -0800

    refactor screenshots spec
    - move browser spec up
    - move waiting and capturing down

--- screenshots/screenshots_spec.rb
+++ screenshots/screenshots_spec.rb
@@ -1,41 +1,33 @@
-require "base64"
-require "rubygems"
-require "spec"
 require File.dirname(__FILE__) + "/support/sauce.rb"
 
-describe "search.usa.gov", :type => :screenshot do
-  it "should have image searches" do
-    page.open "/"
-    page.is_text_present("Advanced Search").should be_true
-    capture_page(page, "home")
-
-    page.type "search_query", "snow"
-    page.click "search_button"
-    page.wait_for_page_to_load
-    capture_page(page, "search_snow")
-
-    page.click "link=Images"
-    page.wait_for_page_to_load
-    capture_page(page, "image_search_snow")
-
-    page.type "search_query", "fire"
-    page.click "search_button"
-    page.wait_for_page_to_load
-    capture_page(page, "image_search_fire")
+BROWSERS = [
+    Browser.new("Windows 2003", "iexploreproxy", "6."),
+    Browser.new("Windows 2003", "firefox", "3."),
+    Browser.new("Windows 2003", "safari", "4."),
+]
 
 
+BROWSERS.each do |browser|
+  describe browser.to_s, :type => :screenshot do
+    before :all do
+      @browser = browser
+    end
+
+    it "should visit the home page" do
+      page.open "/"
+      capture_page(page, "home")
+
+      page.type "search_query", "snow"
+      page.click "search_button"
+      capture_page(page, "search_snow")
+
+      page.click "link=Images"
+      capture_page(page, "image_search_snow")
+
+      page.type "search_query", "fire"
+      page.click "search_button"
+      capture_page(page, "image_search_fire")
+    end
   end
-end
 
-def capture_page(page, page_name)
-  @@steps ||= Hash.new {|h,k| h[k] = 0}
-  browser_hash = JSON.parse(page.browser_string)
-  browser_identifier = "#{browser_hash["os"]}-#{browser_hash["browser"]}-#{browser_hash["browser-version"]}"
-  FileUtils.mkdir_p(File.dirname(__FILE__) + "/report/" + browser_identifier)
-
-  png = page.capture_screenshot_to_string
-  File.open(File.dirname(__FILE__) + "/report/" + browser_identifier + "/%03i-%s-screenshot.png" % [@@steps[browser_identifier]+=1, page_name], 'wb') do |f|
-    f.write(Base64.decode64(png))
-    f.close
-  end
 end
--- screenshots/support/sauce.rb
+++ screenshots/support/sauce.rb
@@ -1,46 +1,56 @@
-INSTALL_SAUCE_MESSAGE = "To enable sauce integration, run 'gem install sauce -v 0.15.1 && sauce configure  usa_search 5060039c-fa2b-403c-9fef-617142894173'"
+require "base64"
+require "rubygems"
+require "spec"
 begin
   gem 'sauce', '0.15.1'
   require 'sauce'
-
-  Sauce.config do |conf|
-    conf.browser_url = "http://demo:***REMOVED***@searchdemo.usa.gov"
-    conf.browsers = [
-        ["Windows 2003", "iexploreproxy", "6."],
-        ["Windows 2003", "firefox", "3."],
-        ["Windows 2003", "safari", "4."],
-    ]
-  end
-
-  class ScreenshotExampleGroup < Spec::Example::ExampleGroup
-    attr_reader :selenium
-    alias_method :page, :selenium
-    alias_method :s, :selenium
-
-    def execute(*args)
-      config = Sauce::Config.new
-      description = [self.class.description, self.description].join(" ")
-      config.browsers.each do |os, browser, version|
-        @selenium = Sauce::Selenium.new({:os => os, :browser => browser, :browser_version => version, :job_name => "#{description}"})
-        @selenium.start
-        begin
-          super(*args)
-        ensure
-          @selenium.stop
-        end
-      end
-    end
-  end
-
 rescue LoadError => e
-  $stderr.puts INSTALL_SAUCE_MESSAGE
+  $stderr.puts "To enable sauce integration, run 'gem install sauce -v 0.15.1"
+  raise e
+end
+
+Sauce.config do |conf|
+  conf.browser_url = "http://demo:***REMOVED***@searchdemo.usa.gov"
+  conf.username = "usa_search"
+  conf.access_key = "5060039c-fa2b-403c-9fef-617142894173"
+end
+
+class Browser < Struct.new(:os, :browser, :browser_version)
+  def to_s
+    "#{browser} #{browser_version} (#{os})"
+  end
 end
 
 class ScreenshotExampleGroup < Spec::Example::ExampleGroup
-  before :all do
-    unless defined?(Sauce)
-      raise INSTALL_SAUCE_MESSAGE
+  attr_reader :selenium
+  alias_method :page, :selenium
+
+  before :each do
+    description = [self.class.description, self.description].join(" ")
+    @selenium = Sauce::Selenium.new({:os => @browser.os, :browser => @browser.browser, :browser_version => @browser.browser_version, :job_name => "#{description}"})
+    @selenium.start
+  end
+
+  after :each do
+    @selenium.stop
+  end
+
+  Spec::Example::ExampleGroupFactory.register(:screenshot, self)
+
+  def capture_page(page, page_name)
+    page.wait_for_page_to_load
+    @@steps ||= Hash.new {|h,k| h[k] = 0}
+    browser_hash = JSON.parse(page.browser_string)
+    browser_identifier = "#{browser_hash["browser"]}-#{browser_hash["browser-version"]}-#{browser_hash["os"]}"
+    report_path = File.dirname(__FILE__) + "/../report/" + browser_identifier
+
+    png = page.capture_screenshot_to_string
+    FileUtils.mkdir_p(report_path)
+    File.open(report_path + "/%03i-%s-screenshot.png" % [@@steps[browser_identifier]+=1, page_name], 'wb') do |f|
+      f.write(Base64.decode64(png))
+      f.close
     end
   end
-  Spec::Example::ExampleGroupFactory.register(:screenshot, self)
+
 end
+
