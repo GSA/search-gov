ad9afb8655ee1571da416ab4982db53cb72d3d0a
Greg Gershman <greggersh@yahoo.com>
2011-02-18 12:00:18 -0500

    [Completes #9042179] USAAdmins able to view and edit new users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,11 +1,13 @@
 class Admin::UsersController < Admin::AdminController
   active_scaffold :user do |config|
-    config.actions.exclude :create, :delete
-    config.columns = [:email, :contact_name, :affiliates, :last_login_at]
-    config.update.columns = [:affiliates, :email, :contact_name, :organization_name, :address, :address2, :phone, :city, :state, :zip]
+    config.actions.exclude :create
+    config.columns = [:email, :contact_name, :affiliates, :last_login_at, :last_request_at, :created_at]
+    config.update.columns = [:affiliates, :email, :contact_name, :organization_name, :address, :address2, :phone, :city, :state, :zip, :is_affiliate_admin, :is_analyst, :is_affiliate, :is_analyst_admin]
     config.list.sorting = { :email => :asc }
     config.list.per_page = 100
-    config.columns[:affiliates].form_ui= :select
-    config.columns[:state].form_ui= :usa_state
+    config.columns[:affiliates].form_ui = :select
+    config.columns[:affiliates].options = { :draggable_lists => true }
+    config.columns[:state].form_ui = :select
+    config.columns[:state].options = ActionView::Helpers::FormOptionsHelper::US_STATES
   end
 end
