32916e98267873c51694fa801cdf5c2f138218ef
Greg Gershman <greggersh@yahoo.com>
2011-03-07 10:18:27 -0500

    [Completes #10549419] Remove some fields from Agencies admin table view to make Action links accessible

--- app/controllers/admin/agencies_controller.rb
+++ app/controllers/admin/agencies_controller.rb
@@ -1,6 +1,6 @@
 class Admin::AgenciesController < Admin::AdminController
   active_scaffold :agencies do |config|
     config.columns = [:name, :abbreviation, :domain, :url, :es_url, :phone, :toll_free_phone, :tty_phone, :twitter_username, :facebook_username, :youtube_username, :flickr_username, :name_variants, :agency_queries]
-    config.list.columns.exclude [:agency_queries, :es_url, :created_at, :updated_at]
+    config.list.columns.exclude [:agency_queries, :es_url, :created_at, :updated_at, :toll_free_phone, :tty_phone, :flickr_username]
   end
 end
