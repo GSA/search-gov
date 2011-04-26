8c7a3b5b2328b612322a127232e82c2e1f953a9d
David Pranata <david@eigenpath.com>
2011-04-26 19:23:43 -0400

    [Fixes #12210277] Affiliate options clipped out in admin area (active scaffold)
    - Removed header and footer from the affiliate list
    - Added css to provides scrollbars when necessary in active scaffold pages

--- app/controllers/admin/affiliates_controller.rb
+++ app/controllers/admin/affiliates_controller.rb
@@ -1,7 +1,7 @@
 class Admin::AffiliatesController < Admin::AdminController
 
   active_scaffold :affiliate do |config|
-    config.columns = [:display_name, :name, :domains, :header, :footer, :affiliate_template, :boosted_contents, :is_sayt_enabled, :is_affiliate_suggestions_enabled, :created_at, :updated_at]
+    config.columns = [:display_name, :name, :domains, :affiliate_template, :boosted_contents, :is_sayt_enabled, :is_affiliate_suggestions_enabled, :created_at, :updated_at]
     config.list.sorting = { :display_name => :asc }
     config.update.columns = [:display_name, :domains, :staged_domains, :search_results_page_title, :staged_search_results_page_title, :header, :staged_header, :footer, :staged_footer, :affiliate_template, :staged_affiliate_template, :is_sayt_enabled, :is_affiliate_suggestions_enabled, :has_staged_content]
     config.create.columns = [:display_name, :name, :domains, :header, :footer, :affiliate_template]
