34c37f2db52a8e2dc8c7a22abc5a259aaae90997
Greg Gershman <greggersh@yahoo.com>
2011-05-26 10:49:12 -0400

    Deploy from rails3 branch for staging.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,4 +1,5 @@
 set :user,        "search"
-set :deploy_to,   "/home/jwynne/#{application}"
+set :deploy_to,   "/home/jwynne/#{application}-rails3"
 set :domain,      "192.168.100.160"
+set :branch,      "rails3"
 server domain, :app, :web, :db, :primary => true
