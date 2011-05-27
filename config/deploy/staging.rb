fe70e53adbe06b98ddf0e3dc82acc94ed6dd1c29
Greg Gershman <greggersh@yahoo.com>
2011-05-27 14:19:15 -0400

    Point staging deploy ~/usasearch so we don't have to update all the scripts.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,4 @@
 set :user,        "search"
 set :deploy_to,   "/home/jwynne/#{application}-rails3"
 set :domain,      "192.168.100.160"
-set :branch,      "rails3"
 server domain, :app, :web, :db, :primary => true
