361e21ce5051e7d72d25f9496a1a122e978554d7
Greg Gershman <greggersh@yahoo.com>
2011-05-29 12:57:27 -0400

    Deploy from master branch to staging.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,4 @@
 set :user,        "search"
 set :deploy_to,   "/home/jwynne/#{application}"
 set :domain,      "192.168.100.160"
-set :branch, "rails3"
 server domain, :app, :web, :db, :primary => true
