90f31f7715b3b0dab224a95ffa64c4239ea71571
Loren Siebert <loren@siebert.org>
2011-05-29 14:55:09 -0700

    [fixes #13156193] Automate delivery of English and Spanish FAQ feeds
    - Copy production version into the right spots.
    - Remove sudo call for Resque workers, as it can run as user search

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -10,12 +10,13 @@ before "deploy:symlink", "production_yaml_files"
 before "deploy:cleanup", "restart_resque_workers"
 
 task :restart_resque_workers, :roles => :resque_workers do
-  run "sudo /home/jwynne/scripts/stop_resque_workers"
-  run "sudo /home/jwynne/scripts/start_resque_workers"
+  run "/home/jwynne/scripts/stop_resque_workers"
+  run "/home/jwynne/scripts/start_resque_workers"
 end
 
 task :production_yaml_files, :except => { :no_release => true } do
   run "cp #{shared_path}/system/database.yml #{release_path}/config/database.yml"
   run "cp #{shared_path}/system/sunspot.yml #{release_path}/config/sunspot.yml"
   run "cp #{shared_path}/system/redis.yml #{release_path}/config/redis.yml"
+  run "cp #{shared_path}/system/faq.yml #{release_path}/config/faq.yml"
 end
