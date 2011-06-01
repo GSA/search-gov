3b169eab72d99959530c8669598b6a264800f951
Greg Gershman <greggersh@yahoo.com>
2011-06-01 09:29:58 -0400

    Update specs to cover boosted site admin controllers; update coverage.

---
+++ spec/controllers/admin/global_boosted_contents_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'spec/spec_helper'
+
+describe Admin::GlobalBoostedContentsController do
+  describe "#after_create_save" do
+    it "should tell Sunspot to reindex" do
+      bc = BoostedContent.new
+      Sunspot.should_receive(:index).with(bc).and_return true
+      controller.after_create_save(bc)
+    end
+  end
+  
+  describe "#after_update_save" do
+    it "should tell Sunspot to reindex" do
+      bc = BoostedContent.new
+      Sunspot.should_receive(:index).with(bc).and_return true
+      controller.after_update_save(bc)
+    end
+  end
+end
