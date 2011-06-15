4aa627af9fa501a36a5d51755b97d9e312eedcf0
David Pranata <david@eigenpath.com>
2011-06-15 08:55:29 -0400

    [Fixes #14532981] Make Agency URLs human readable in ActiveScaffold
    - Add an example for to_label

--- spec/models/agency_url_spec.rb
+++ spec/models/agency_url_spec.rb
@@ -5,4 +5,9 @@ describe AgencyUrl do
   it { should validate_presence_of :url }
   it { should validate_presence_of :locale }
 
+  describe "#to_label" do
+    it "returns url" do
+      AgencyUrl.new(:url => 'http://search.usa.gov').to_label.should == 'http://search.usa.gov'
+    end
+  end
 end
