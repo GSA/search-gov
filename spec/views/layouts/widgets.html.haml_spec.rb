8670904789fb7f3200aef15f7db3c2f9fbde24b3
David Pranata <david@eigenpath.com>
2011-08-16 11:02:20 -0400

    Fix warning in widgets.html.haml_spec.rb
    - add description to the code example because webrat have_selector
      matcher does have one

--- spec/views/layouts/widgets.html.haml_spec.rb
+++ spec/views/layouts/widgets.html.haml_spec.rb
@@ -2,9 +2,9 @@ require 'spec/spec_helper'
 
 describe "layouts/widgets.html.haml" do
   context "when page is displayed" do
-    before do
+    it "should have ROBOTS meta tag" do
       render
+      rendered.should have_selector("meta[name='ROBOTS'][content='NOINDEX, NOFOLLOW']")
     end
-    specify { rendered.should have_selector("meta[name='ROBOTS'][content='NOINDEX, NOFOLLOW']") }
   end
 end
