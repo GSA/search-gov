24a4781953ce732ccc6b3fc81b703bcc46ff8993
Greg Gershman <greggersh@yahoo.com>
2011-08-19 10:05:34 -0400

    [Fixes #17220555] Trying to add new agency in Admin center gives invalid URL error

--- spec/models/agency_url_spec.rb
+++ spec/models/agency_url_spec.rb
@@ -1,7 +1,6 @@
 require 'spec/spec_helper'
 
 describe AgencyUrl do
-  it { should validate_presence_of :agency_id }
   it { should validate_presence_of :url }
   it { should validate_presence_of :locale }
 
