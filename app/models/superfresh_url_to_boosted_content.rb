2c1ee79b4422722d3a0c1e394ee824f96969fd0f
Greg Gershman <greggersh@yahoo.com>
2011-08-31 10:59:23 -0400

    Improve error handling of SuperfreshUrlToBoostedContent.

--- app/models/superfresh_url_to_boosted_content.rb
+++ app/models/superfresh_url_to_boosted_content.rb
@@ -5,42 +5,46 @@ class SuperfreshUrlToBoostedContent
 
   def self.perform(url, affiliate_id)
     return unless SuperfreshUrl.find_by_url_and_affiliate_id(url, affiliate_id)
+    if is_pdf?(url)
+      boosted_content = crawl_pdf(url)
+    else
+      boosted_content = crawl_html(url)
+    end
+    if boosted_content
+      boosted_content.affiliate_id = affiliate_id
+      boosted_content.save!
+      Sunspot.index!(boosted_content)
+    end
+  end
+  
+  def self.crawl_html(url)
     begin
-      if is_pdf?(url)
-        boosted_content = crawl_pdf(url)
-      else
-        boosted_content = crawl_html(url)
-      end
-      if boosted_content
-        boosted_content.affiliate_id = affiliate_id
-        boosted_content.save!
-        Sunspot.index!(boosted_content)
-      end
+      doc = Nokogiri::HTML(open(url))
+      return unless (title = doc.xpath("//title").first.content.squish.truncate(TRUNCATED_TITLE_LENGTH,:separator=>" ") rescue nil)
+      description = doc.xpath("//meta[translate(@name, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz') = 'description' ] ").
+        first.attributes["content"].value.squish rescue nil
+        if description.nil?
+          doc.xpath('//script').each { |x| x.remove }
+          doc.xpath('//style').each { |x| x.remove }
+          description = doc.inner_text.strip.gsub(/[\t\n\r]/, ' ').gsub(/(\s)\1+/, '. ').truncate(TRUNCATED_DESC_LENGTH, :separator => ' ')
+        end
+        BoostedContent.new(:url => url, :title=> title, :description => description, :auto_generated => true)
     rescue Exception => e
       Rails.logger.error "Trouble fetching #{url} for boosted content creation: #{e}"
     end
   end
   
-  def self.crawl_html(url)
-    doc = Nokogiri::HTML(open(url))
-    return unless (title = doc.xpath("//title").first.content.squish.truncate(TRUNCATED_TITLE_LENGTH,:separator=>" ") rescue nil)
-    description = doc.xpath("//meta[translate(@name, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz') = 'description' ] ").
-      first.attributes["content"].value.squish rescue nil
-    if description.nil?
-      doc.xpath('//script').each { |x| x.remove }
-      doc.xpath('//style').each { |x| x.remove }
-      description = doc.inner_text.strip.gsub(/[\t\n\r]/, ' ').gsub(/(\s)\1+/, '. ').truncate(TRUNCATED_DESC_LENGTH, :separator => ' ')
-    end
-    BoostedContent.new(:url => url, :title=> title, :description => description, :auto_generated => true)    
-  end
-  
   def self.crawl_pdf(url)
-    pdf_io = open(url)
-    reader = PDF::Reader.new
-    pdf_string = StringIO.new
-    receiver = PDF::Reader::TextReceiver.new(pdf_string)
-    reader.parse(pdf_io, receiver)
-    BoostedContent.new(:url => url, :title => title_from_pdf(pdf_string.string), :description => pdf_string.string, :auto_generated => true)
+    begin
+      pdf_io = open(url)
+      reader = PDF::Reader.new
+      pdf_string = StringIO.new
+      receiver = PDF::Reader::TextReceiver.new(pdf_string)
+      reader.parse(pdf_io, receiver)
+      BoostedContent.new(:url => url, :title => title_from_pdf(pdf_string.string), :description => pdf_string.string, :auto_generated => true)
+    rescue Exception => e
+      Rails.logger.error "Trouble fetching #{url} for boosted content creation: #{e}"
+    end
   end
   
   def self.title_from_pdf(body)
