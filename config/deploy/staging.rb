e9883d2d421b8afd188dffe3a0ab78279004d5ef
Greg Gershman <greggersh@yahoo.com>
2011-09-02 09:44:34 -0400

    Add start/stop resque worker to staging deploy script

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,3 +2,10 @@ set :user,        "search"
 set :deploy_to,   "/home/jwynne/#{application}"
 set :domain,      "192.168.100.160"
 server domain, :app, :web, :db, :primary => true
+
+before "deploy:cleanup", "restart_resque_workers"
+
+task :restart_resque_workers, :roles => :resque_workers do
+  run "sudo /home/jwynne/scripts/stop_resque_workers"
+  run "sudo /home/jwynne/scripts/start_resque_workers"
+end
