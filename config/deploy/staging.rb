0dac55cf7554113253411e08a45d1141dd09631c
Greg Gershman <greggersh@yahoo.com>
2011-09-02 09:51:05 -0400

    Add app role to staging resque workers call.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,7 +5,7 @@ server domain, :app, :web, :db, :primary => true
 
 before "deploy:cleanup", "restart_resque_workers"
 
-task :restart_resque_workers, :roles => :resque_workers do
+task :restart_resque_workers, :roles => :web do
   run "sudo /home/jwynne/scripts/stop_resque_workers"
   run "sudo /home/jwynne/scripts/start_resque_workers"
 end
