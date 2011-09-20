46336d2a5a3678e350e13a4c5d323cabf70dc6db
Loren Siebert <loren@siebert.org>
2011-09-20 10:01:01 -0700

    [Fixes #18449939] Deployment to staging server now warms up Phusion Passenger and Rails app by hitting home page
    - Should get around issue where Passenger balks if HTTPS page is first page accessed.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,8 +4,13 @@ set :domain,      "192.168.100.160"
 server domain, :app, :web, :db, :primary => true
 
 before "deploy:cleanup", "restart_resque_workers"
+after :deploy, "warmup"
 
 task :restart_resque_workers, :roles => :web do
   run "sudo /home/jwynne/scripts/stop_resque_workers"
   run "sudo /home/jwynne/scripts/start_resque_workers"
+end
+
+task :warmup, :roles => :web do
+  run "wget --delete-after --user=demo --password=***REMOVED*** http://searchdemo.usa.gov"
 end
