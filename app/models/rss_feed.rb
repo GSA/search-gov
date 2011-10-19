7196c8a83577ae8570ce86551ed42a6cc4b027cb
Greg Gershman <greggersh@yahoo.com>
2011-10-18 23:11:23 -0400

    [Fixes #19767517] Affiliate reports being unable to able to upload an RSS feed

--- app/models/rss_feed.rb
+++ app/models/rss_feed.rb
@@ -3,20 +3,25 @@ class RssFeed < ActiveRecord::Base
   validate :is_valid_rss_feed?
   belongs_to :affiliate
   has_many :news_items, :dependent => :destroy, :order => "published_at DESC"
+  RSS_ELEMENTS = { "item" => "item", "pubDate" => "pubDate", "link" => "link", "title" => "title", "guid" => "guid", "description" => "description" }
+  ATOM_ELEMENTS = { "item" => "xmlns:entry", "pubDate" => "xmlns:published", "link" => "xmlns:link/@href", "title" => "xmlns:title", "guid" => "xmlns:id", "description" => "xmlns:content" }
+  FEED_ELEMENTS = { :rss => RSS_ELEMENTS, :atom => ATOM_ELEMENTS }
 
   def freshen(ignore_older_items = true)
     begin
       most_recently = news_items.present? ? news_items.first.published_at : nil
-      Nokogiri::XML(open(url)).xpath('//item').each do |item|
-        published_at = DateTime.parse(item.xpath('pubDate').inner_text)
+      rss_document = Nokogiri::XML(open(url))
+      feed_type = detect_feed_type(rss_document)
+      rss_document.xpath("//#{FEED_ELEMENTS[feed_type]["item"]}").each do |item|
+        published_at = DateTime.parse(item.xpath(FEED_ELEMENTS[feed_type]["pubDate"]).inner_text)
         break if most_recently and published_at < most_recently and ignore_older_items
-        link = item.xpath('link').inner_text
-        title = item.xpath('title').inner_text
-        guid = item.xpath('guid').inner_text
-        raw_description = item.xpath('description').inner_text
+        link = item.xpath(FEED_ELEMENTS[feed_type]["link"]).inner_text
+        title = item.xpath(FEED_ELEMENTS[feed_type]["title"]).inner_text
+        guid = item.xpath(FEED_ELEMENTS[feed_type]["guid"]).inner_text
+        raw_description = item.xpath(FEED_ELEMENTS[feed_type]["description"]).inner_text
         description = Nokogiri::HTML(raw_description).inner_text.gsub(/[\t\n\r]/, ' ').squish
         NewsItem.create!(:rss_feed => self, :link => link, :title => title, :description => description, :published_at => published_at, :guid => guid) unless news_items.exists?(:guid => guid)
-      end
+      end unless feed_type.nil?
       Sunspot.commit
     rescue Exception => e
       HoptoadNotifier.notify(e)
@@ -34,10 +39,21 @@ class RssFeed < ActiveRecord::Base
       errors.add(:url, "The URL entered is not a valid URL.")
     else
       begin
-        errors.add(:url, "The RSS feed URL specified does not appear to be a valid RSS feed.") if Nokogiri::XML(Kernel.open(url)).xpath('//channel').empty?
+        rss_doc = Nokogiri::XML(Kernel.open(url))
+        errors.add(:url, "The RSS feed URL specified does not appear to be a valid RSS feed.") unless %{rss feed}.include?(rss_doc.root.name)
       rescue Exception => e
         errors.add(:url, "The RSS feed URL specified does not appear to be a valid RSS feed.  Additional information: " + e.message)
       end
+    end
+  end
+  
+  def detect_feed_type(document)
+    if document.root.name == "rss"
+      :rss
+    elsif document.root.name == "feed"
+      :atom
+    else
+      nil
     end
   end
 end
--- spec/models/rss_feed_spec.rb
+++ spec/models/rss_feed_spec.rb
@@ -75,90 +75,117 @@ describe RssFeed do
   end
 
   describe "#freshen" do
-    let(:feed) { rss_feeds(:white_house_blog) }
-    before do
-      doc = Nokogiri::XML(open(Rails.root.to_s + '/spec/fixtures/rss/wh_blog.xml'))
-      Nokogiri::XML::Document.should_receive(:parse).and_return(doc)
-    end
-
-    context "when there are no news items associated with the source" do
+    context "when the feed is in the RSS 2.0 format" do
       before do
-        feed.news_items.delete_all
+        @feed = rss_feeds(:white_house_blog)
+        doc = Nokogiri::XML(open(Rails.root.to_s + '/spec/fixtures/rss/wh_blog.xml'))
+        Nokogiri::XML::Document.should_receive(:parse).and_return(doc)
       end
 
-      it "should populate news items from the RSS feed source with HTML stripped from the description" do
-        feed.freshen
-        feed.reload
-        feed.news_items.count.should == 3
-        newest = feed.news_items.first
-        newest.guid.should == "80731 at http://www.whitehouse.gov"
-        newest.link.should == "http://www.whitehouse.gov/blog/2011/09/26/famine-horn-africa-be-part-solution"
-        newest.published_at.should == DateTime.parse("26 Sep 2011 21:33:05 +0000")
-        newest.description[0, 40].should == "Dr. Biden and David Letterman refer to a"
-        newest.title.should == "Famine in the Horn of Africa: Be a Part of the Solution"
-      end
-    end
+      context "when there are no news items associated with the source" do
+        before do
+          @feed.news_items.delete_all
+        end
 
-    context "when some news items are newer and some are older than the most recent published_at time for the feed" do
-      before do
-        NewsItem.delete_all
-        NewsItem.create!(:link => 'http://www.whitehouse.gov/latest_story.html',
-                         :title => "Big story here",
-                         :description => "Corps volunteers have promoted blah blah blah.",
-                         :published_at => DateTime.parse("26 Sep 2011 18:31:23 +0000"),
-                         :guid => 'unique',
-                         :rss_feed_id => feed.id
-        )
-      end
-
-      context "when ignore_older_items set to true (default)" do
-        it "should populate news items with only the new ones from the RSS feed source based on the pubDate" do
-          feed.freshen
-          feed.news_items.count.should == 3
+        it "should populate news items from the RSS feed source with HTML stripped from the description" do
+          @feed.freshen
+          @feed.reload
+          @feed.news_items.count.should == 3
+          newest = @feed.news_items.first
+          newest.guid.should == "80731 at http://www.whitehouse.gov"
+          newest.link.should == "http://www.whitehouse.gov/blog/2011/09/26/famine-horn-africa-be-part-solution"
+          newest.published_at.should == DateTime.parse("26 Sep 2011 21:33:05 +0000")
+          newest.description[0, 40].should == "Dr. Biden and David Letterman refer to a"
+          newest.title.should == "Famine in the Horn of Africa: Be a Part of the Solution"
         end
       end
 
-      context "when ignore_older_items set to false" do
-        it "should populate news items with both the new and old ones from the RSS feed source based on the pubDate" do
-          feed.freshen(false)
-          feed.news_items.count.should == 4
+      context "when some news items are newer and some are older than the most recent published_at time for the feed" do
+        before do
+          NewsItem.delete_all
+          NewsItem.create!(:link => 'http://www.whitehouse.gov/latest_story.html',
+                           :title => "Big story here",
+                           :description => "Corps volunteers have promoted blah blah blah.",
+                           :published_at => DateTime.parse("26 Sep 2011 18:31:23 +0000"),
+                           :guid => 'unique',
+                           :rss_feed_id => @feed.id
+          )
+        end
+
+        context "when ignore_older_items set to true (default)" do
+          it "should populate news items with only the new ones from the RSS feed source based on the pubDate" do
+            @feed.freshen
+            @feed.news_items.count.should == 3
+          end
+        end
+
+        context "when ignore_older_items set to false" do
+          it "should populate news items with both the new and old ones from the RSS feed source based on the pubDate" do
+            @feed.freshen(false)
+            @feed.news_items.count.should == 4
+          end
         end
       end
-    end
 
-    context "when there are duplicate news items" do
+      context "when there are duplicate news items" do
+        before do
+          NewsItem.delete_all
+          NewsItem.create!(:link => 'http://www.whitehouse.gov/latest_story.html',
+                           :title => "Big story here",
+                           :description => "Corps volunteers have promoted blah blah blah.",
+                           :published_at => DateTime.parse("26 Sep 2011 18:31:21 +0000"),
+                           :guid => '80653 at http://www.whitehouse.gov',
+                           :rss_feed_id => @feed.id
+          )
+        end
+
+        it "should ignore them" do
+          NewsItem.should_receive(:create!).twice
+          @feed.freshen
+        end
+      end
+
+      context "when an exception is raised somewhere along the way" do
+        before do
+          Sunspot.stub!(:commit).and_raise Exception
+        end
+
+        it "should report it to Hoptoad/Airbrake" do
+          HoptoadNotifier.should_receive(:notify).with(an_instance_of(Exception))
+          @feed.freshen
+        end
+      end
+
+      it "should commit the news items to Solr" do
+        Sunspot.should_receive(:commit).once
+        @feed.freshen
+      end
+    end
+    
+    context "when the feed is in the Atom format" do
       before do
-        NewsItem.delete_all
-        NewsItem.create!(:link => 'http://www.whitehouse.gov/latest_story.html',
-                         :title => "Big story here",
-                         :description => "Corps volunteers have promoted blah blah blah.",
-                         :published_at => DateTime.parse("26 Sep 2011 18:31:21 +0000"),
-                         :guid => '80653 at http://www.whitehouse.gov',
-                         :rss_feed_id => feed.id
-        )
+        @feed = rss_feeds(:atom_feed)
+        doc = Nokogiri::XML(open(Rails.root.to_s + '/spec/fixtures/rss/atom_feed.xml'))
+        Nokogiri::XML::Document.should_receive(:parse).and_return(doc)
       end
 
-      it "should ignore them" do
-        NewsItem.should_receive(:create!).twice
-        feed.freshen
-      end
-    end
+      context "when there are no news items associated with the source" do
+        before do
+          @feed.news_items.delete_all
+        end
 
-    context "when an exception is raised somewhere along the way" do
-      before do
-        Sunspot.stub!(:commit).and_raise Exception
+        it "should populate news items from the RSS feed source with HTML stripped from the description" do
+          @feed.freshen
+          @feed.reload
+          @feed.news_items.count.should == 25
+          newest = @feed.news_items.first
+          newest.guid.should == "http://www.icpsr.umich.edu/icpsrweb/ICPSR/studies/22642"
+          newest.link.should == "http://www.icpsr.umich.edu/icpsrweb/ICPSR/studies/22642"
+          newest.published_at.should == DateTime.parse("2009-11-30T12:00:00-05:00")
+          newest.description[0, 40].should == "Assessing Consistency and Fairness in Se"
+          newest.title.should == "Assessing Consistency and Fairness in Sentencing in Michigan, Minnesota, and Virginia, 2001-2002, 2004"
+        end
       end
-
-      it "should report it to Hoptoad/Airbrake" do
-        HoptoadNotifier.should_receive(:notify).with(an_instance_of(Exception))
-        feed.freshen
-      end
-    end
-
-    it "should commit the news items to Solr" do
-      Sunspot.should_receive(:commit).once
-      feed.freshen
     end
   end
-
 end
