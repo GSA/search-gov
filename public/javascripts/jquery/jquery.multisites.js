65c166fdef8994701744b2883c582e93d3eeac56
David Pranata <david@eigenpath.com>
2011-11-14 12:40:44 -0500

    [Completes #20387893] Affiliates get friendly form code that allows them to send traffic to different affiliate IDs
    - assign default id from search form in 'Get code' page
    - update javascript to always set affiliate value

--- public/javascripts/jquery/jquery.multisites.js
+++ public/javascripts/jquery/jquery.multisites.js
@@ -1,9 +1,8 @@
 (function($) {
   $.fn.multiSites = function() {
-
     this.each(function() {
       var searchForm = this;
-      $(this).children('input[type="button"][affiliate]').click(function() {
+      $(this).children('input[affiliate]').click(function() {
         var affiliate = $(this).attr('affiliate');
         $(searchForm).children('input[name="affiliate"]').first().val(affiliate);
         $(searchForm).submit();
