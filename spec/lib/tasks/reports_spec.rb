7bdd4b80f3948fdc77e02656c663df3341ebed46
Loren Siebert <loren@siebert.org>
2012-04-16 13:50:30 -0700

    [Completes #27932697] Delete monthly_report (code + cron)

--- spec/lib/tasks/reports_spec.rb
+++ spec/lib/tasks/reports_spec.rb
@@ -119,59 +119,5 @@ describe "Report generation rake tasks" do
         end
       end
     end
-
-    describe "usasearch:reports:weekly_report" do
-      before do
-        @task_name = "usasearch:reports:weekly_report"
-        @start_date = Date.yesterday.beginning_of_week
-        @zip_filename = "/tmp/weekly_report_#{@start_date.strftime('%Y-%m-%d')}.zip"
-      end
-
-      it "should have 'environment' as a prereq" do
-        @rake[@task_name].prerequisites.should include("environment")
-      end
-
-      context "when running the task" do
-        it "should create a zip file in a temporary location, add a bunch of files to it, email it, and delete it after it's been sent" do
-          Emailer.should_receive(:monthly_report).with(@zip_filename).and_return @emailer
-          File.should_receive(:delete).with(@zip_filename).and_return true
-          @rake[@task_name].invoke
-          Zip::ZipFile.open(@zip_filename) do |zip_file|
-            zip_file.get_entry('affiliate_report.txt')
-          end
-        end
-      end
-    end
-
-    describe "usasearch:reports:monthly_report" do
-      before do
-        @task_name = "usasearch:reports:monthly_report"
-        @start_date = Date.current.beginning_of_month - 1.day
-        @zip_filename = "/tmp/monthly_report_#{@start_date.strftime('%Y-%m')}.zip"
-      end
-
-      it "should have 'environment' as a prereq" do
-        @rake[@task_name].prerequisites.should include("environment")
-      end
-
-      context "when running the task" do
-        it "should create a zip file in a temporary location, add a bunch of files to it, email it, and delete it after it's been sent" do
-          Emailer.should_receive(:monthly_report).with(@zip_filename).and_return @emailer
-          File.should_receive(:delete).with(@zip_filename).and_return true
-          @rake[@task_name].invoke
-          Zip::ZipFile.open(@zip_filename) do |zip_file|
-            zip_file.get_entry('top_monthly_queries_en.txt')
-            zip_file.get_entry('top_monthly_queries_es.txt')
-            zip_file.get_entry('top_monthly_queries_affiliates.txt')
-            zip_file.get_entry('top_affiliates.txt')
-            zip_file.get_entry('affiliate_queries_by_month.txt')
-            zip_file.get_entry('affiliate_click_totals.txt')
-            zip_file.get_entry('affiliate_report.txt')
-            zip_file.get_entry('total_queries_by_profile.txt')
-            zip_file.get_entry('total_queries_report.txt')
-          end
-        end
-      end
-    end
   end
 end
