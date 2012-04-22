a91014d0fc761cc7e1b7b9ae665ed8093028b28e
David Pranata <david@eigenpath.com>
2012-04-22 14:58:48 -0400

    [Completes #27005029] Customers may input multiple RSS feed URLs for each label/title
    - only migrate valid RSS feed URL

--- db/migrate/20120419033137_migrate_rss_feed_urls.rb
+++ db/migrate/20120419033137_migrate_rss_feed_urls.rb
@@ -3,8 +3,8 @@ class MigrateRssFeedUrls < ActiveRecord::Migration
     RssFeed.all.each do |rss_feed|
       next if rss_feed.url.blank? or rss_feed.rss_feed_urls.present?
       RssFeed.transaction do
-        rss_feed_url = rss_feed.rss_feed_urls.create!(:url => rss_feed.url)
-        rss_feed.news_items.update_all(:rss_feed_url_id => rss_feed_url.id)
+        rss_feed_url = rss_feed.rss_feed_urls.build(:url => rss_feed.url)
+        rss_feed.news_items.update_all(:rss_feed_url_id => rss_feed_url.id) if rss_feed_url.save
       end
     end unless Rails.env.test?
   end
