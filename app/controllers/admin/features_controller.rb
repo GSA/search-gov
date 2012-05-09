4b242d236d41dbd69340fd3fb7693c07189e709a
Loren Siebert <loren@siebert.org>
2012-05-09 07:50:48 -0700

    [Fixes #29324545] Changing a feature display name in admin center deletes all affiliate-feature relationships for that feature
    - This was the issue: https://github.com/activescaffold/active_scaffold/issues/115

--- app/controllers/admin/features_controller.rb
+++ app/controllers/admin/features_controller.rb
@@ -1,9 +1,9 @@
 class Admin::FeaturesController < Admin::AdminController
   active_scaffold :features do |config|
+    config.update.columns = [:display_name]
     config.actions.exclude :delete
     config.columns.exclude :affiliate_feature_addition
     config.list.columns.exclude :created_at, :updated_at
-    config.update.columns.exclude :internal_name
     config.create.columns.exclude :affiliates
     config.columns[:affiliates].form_ui = :select
     config.columns[:affiliates].associated_limit = 0
