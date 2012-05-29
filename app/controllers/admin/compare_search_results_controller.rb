b9e828a2fc3c9f3a61976d711419caa754024b25
Greg Gershman <greggersh@yahoo.com>
2012-05-29 09:20:18 -0400

    [Completes #29940397] SuperAdmin can see Bing vs Odie results side by side in single UI - modify UI to line up results side by side.

--- app/controllers/admin/compare_search_results_controller.rb
+++ app/controllers/admin/compare_search_results_controller.rb
@@ -3,6 +3,7 @@ class Admin::CompareSearchResultsController < Admin::AdminController
     @page_title = 'Compare Search Results'
     @affiliate_picklist = Affiliate.select(:name).order(:name).collect{|aff| [aff.name, aff.name]}
     if params[:affiliate_pick]
+      @affiliate_pick = params[:affiliate_pick]
       @query = params[:query]
       @affiliate = Affiliate.find_by_name(params[:affiliate_pick])
       @search_options = search_options_from_params(@affiliate, params)
