eaad4865e876abf8a832789f0f38d09ae9967347
David Pranata <david@eigenpath.com>
2012-06-22 17:01:28 -0400

    [Fixes #31661301] Searchers see multiple left nav items selected

--- app/helpers/navigations_helper.rb
+++ app/helpers/navigations_helper.rb
@@ -1,9 +1,12 @@
 module NavigationsHelper
   def link_to_navigable(navigable)
     case navigable.class.name
-    when 'DocumentCollection' then link_to('Collection', [navigable.affiliate, navigable])
-    when 'RssFeed' then link_to('RSS', [navigable.affiliate, navigable])
-    when 'ImageSearchLabel' then 'Bing'
+    when 'DocumentCollection'
+      link_to('Collection', [navigable.affiliate, navigable])
+    when 'RssFeed'
+      link_to('RSS', [navigable.affiliate, navigable])
+    when 'ImageSearchLabel'
+      'Bing'
     end
   end
 
@@ -11,44 +14,75 @@ module NavigationsHelper
     navigation.navigable.instance_of?(ImageSearchLabel) ? navigation.navigable_type.underscore : navigation.navigable_type.underscore.pluralize
   end
 
-  def render_navigations(navigations, search_params, left_nav_tag, is_image_search, dc_and_feed_hash)
-    content = []
-    navigations.each do |navigation|
+  def render_navigations(affiliate, search, search_params)
+    return if affiliate.navigations.active.blank?
+
+    dc = search.is_a?(OdieSearch) ? search.document_collection : nil
+    rss_feed = search.is_a?(NewsSearch) ? search.rss_feed : nil
+
+    nav_items = []
+    nav_items << render_navigation_for_search_everything(search,
+                                                         search_params,
+                                                         rss_feed,
+                                                         affiliate.default_search_label)
+
+    affiliate.navigations.active.each do |navigation|
       navigable = navigation.navigable
-      html = case navigation.navigable_type
-             when 'DocumentCollection'
-               render_document_collection_navigation(navigable, search_params, left_nav_tag, dc_and_feed_hash[:document_collection])
-             when 'ImageSearchLabel'
-               render_image_search_label_navigation(navigable, search_params, left_nav_tag, is_image_search)
-             when 'RssFeed'
-               render_rss_feed_navigation(navigable, search_params, left_nav_tag, dc_and_feed_hash[:rss_feed])
-             end
-      content << content_tag(:li, html.html_safe)
+      nav_items << case navigation.navigable_type
+                   when 'ImageSearchLabel'
+                     render_navigation_for_image_search(navigable,
+                                                        search_params,
+                                                        search.instance_of?(ImageSearch))
+                   when 'DocumentCollection'
+                     render_navigation_for_document_collection(navigable,
+                                                               search_params,
+                                                               dc)
+                   when 'RssFeed'
+                     render_navigation_for_rss_feed(navigable,
+                                                    search_params,
+                                                    rss_feed)
+                   end
     end
-    content.join("\n").html_safe
+    nav_html = nav_items.collect { |nav_item| content_tag(:li, nav_item.html_safe) }
+    content_tag(:ul, nav_html.join("\n").html_safe, :class => 'navigations')
   end
 
-  def render_document_collection_navigation(navigable, search_params, left_nav_tag, document_collection)
+  def render_navigation_for_search_everything(search, search_params, rss_feed, default_search_label)
+    search_everything = (search.instance_of?(WebSearch) or
+        (search.is_a?(NewsSearch) && rss_feed.nil?))
+    default_search_path = search_params[:tbs] ? news_search_path(search_params.remove(:channel, :dc)) : search_path(search_params.remove(:channel))
+    link_to_unless(search_everything,
+                   default_search_label,
+                   default_search_path,
+                   :class => 'updatable') do |everything|
+      content_tag(:div, everything)
+    end
+  end
+
+  def render_navigation_for_image_search(navigable, search_params, is_image_search)
+    link_to_unless(is_image_search,
+                   navigable.name,
+                   image_search_path(search_params.remove(:channel, :tbs)),
+                   :class => 'updatable') do |images|
+      content_tag(:div, images)
+    end
+  end
+
+  def render_navigation_for_document_collection(navigable, search_params, document_collection)
     link_to_unless((document_collection && document_collection.id == navigable.id),
                    navigable.name,
                    docs_search_path(search_params.merge(:dc => navigable.id).remove(:channel, :tbs)),
                    :class => 'updatable') do |collection_name|
-      content_tag left_nav_tag, collection_name
+      content_tag(:div, collection_name)
     end
   end
 
-  def render_image_search_label_navigation(navigable, search_params, left_nav_tag, is_image_search)
-    link_to_unless(is_image_search, navigable.name, image_search_path(search_params.remove(:channel, :tbs)), :class => 'updatable') do |images|
-      content_tag(left_nav_tag, images)
-    end
-  end
-
-  def render_rss_feed_navigation(navigable, search_params, left_nav_tag, rss_feed)
+  def render_navigation_for_rss_feed(navigable, search_params, rss_feed)
     link_to_unless((rss_feed && rss_feed.id == navigable.id),
                    navigable.name,
                    news_search_path(search_params.merge(:channel => navigable.id)),
                    :class => 'updatable') do |feed_name|
-      content_tag(left_nav_tag, feed_name)
+      content_tag(:div, feed_name)
     end
   end
 end
---
+++ spec/helpers/navigations_helper_spec.rb
@@ -0,0 +1,195 @@
+require 'spec/spec_helper'
+
+describe NavigationsHelper do
+  shared_examples_for 'doing search on everything' do
+    it 'should render default search label' do
+      html = helper.render_navigations(affiliate, search, search_params)
+      html.should have_selector('.navigations', :content => 'Everything')
+    end
+
+    it 'should not render a link to default search label' do
+      html = helper.render_navigations(affiliate, search, search_params)
+      html.should_not have_selector('.navigations a', :content => 'Everything')
+    end
+  end
+
+  shared_examples_for 'doing non web search' do
+    it 'should render a link to default search label' do
+      html = helper.render_navigations(affiliate, search, search_params)
+      html.should have_selector('.navigations a', :content => 'Everything')
+    end
+  end
+
+  shared_examples_for 'doing non image search' do
+    it 'should render a link to image search label' do
+      html = helper.render_navigations(affiliate, search, search_params)
+      html.should have_selector('.navigations a', :content => 'Images')
+    end
+  end
+
+  shared_examples_for 'doing non odie search' do
+    it 'should render a link to document collection' do
+      html = helper.render_navigations(affiliate, search, search_params)
+      html.should have_selector('.navigations a', :content => 'Blog')
+    end
+  end
+
+  shared_examples_for 'doing non news channel specific search' do
+    it 'should render a link to rss feed' do
+      html = helper.render_navigations(affiliate, search, search_params)
+      html.should have_selector('.navigations a', :content => 'News')
+    end
+  end
+
+  describe '#render_navigations' do
+    let(:affiliate) { mock_model(Affiliate, :name => 'myaff', :default_search_label => 'Everything') }
+
+    let(:search_params) { { :query => 'gov', :affiliate => 'myaff' } }
+
+    let(:image_search_label) { mock_model(ImageSearchLabel, :name => 'Images') }
+    let(:image_nav) do
+      mock_model(Navigation,
+                 :navigable => image_search_label,
+                 :navigable_type => image_search_label.class.name)
+    end
+
+    let(:rss_feed) { mock_model(RssFeed, :name => 'News') }
+    let(:rss_feed_nav) do
+      mock_model(Navigation,
+                 :navigable => rss_feed,
+                 :navigable_type => rss_feed.class.name)
+    end
+
+    let(:another_rss_feed) { mock_model(RssFeed, :name => 'Press Releases') }
+    let(:another_rss_feed_nav) do
+      mock_model(Navigation,
+                 :navigable => another_rss_feed,
+                 :navigable_type => another_rss_feed.class.name)
+    end
+
+    let(:document_collection) { mock_model(DocumentCollection, :name => 'Blog') }
+    let(:document_collection_nav) do
+      mock_model(Navigation,
+                 :navigable => document_collection,
+                 :navigable_type => document_collection.class.name)
+    end
+
+    let(:search_params) { { :query => 'gov', :affiliate => affiliate.name } }
+
+    context 'when there is no active navigation' do
+      before { affiliate.stub_chain(:navigations, :active).and_return([]) }
+
+      specify { helper.render_navigations(affiliate, mock(WebSearch), search_params).should be_blank }
+    end
+
+    context 'when there are active navigations' do
+      before do
+        affiliate.stub_chain(:navigations, :active).
+            and_return([image_nav, rss_feed_nav, document_collection_nav])
+      end
+
+      context 'when doing web search' do
+        let(:search) { mock(WebSearch) }
+
+        before do
+          search.should_receive(:instance_of?).at_least(:once) { |arg| arg == WebSearch }
+        end
+
+        it_behaves_like 'doing search on everything'
+        it_behaves_like 'doing non image search'
+        it_behaves_like 'doing non odie search'
+        it_behaves_like 'doing non news channel specific search'
+      end
+
+      context 'when doing image search' do
+        let(:search) { mock(ImageSearch) }
+
+        before do
+          search.should_receive(:instance_of?).at_least(:once) { |arg| arg == ImageSearch }
+        end
+
+        it 'should render image search label' do
+          html = helper.render_navigations(affiliate, search, search_params)
+          html.should have_selector('.navigations', :content => 'Images')
+        end
+
+        it 'should not render a link to image search label' do
+          html = helper.render_navigations(affiliate, search, search_params)
+          html.should_not have_selector('.navigations a', :content => 'Images')
+        end
+
+        it_behaves_like 'doing non web search'
+        it_behaves_like 'doing non odie search'
+        it_behaves_like 'doing non news channel specific search'
+      end
+
+      context 'when doing search on a specific document collection' do
+        let(:search) { mock(OdieSearch) }
+
+        before do
+          search.should_receive(:instance_of?).at_least(:once) { |arg| arg == OdieSearch }
+          search.should_receive(:is_a?).at_least(:once) { |arg| arg == OdieSearch }
+          search.should_receive(:document_collection).and_return(document_collection)
+        end
+
+        it 'should render document collection name' do
+          html = helper.render_navigations(affiliate, search, search_params)
+          html.should have_selector('.navigations', :content => 'Blog')
+        end
+
+        it 'should not render a link to document collection' do
+          html = helper.render_navigations(affiliate, search, search_params)
+          html.should_not have_selector('.navigations a', :content => 'Blog')
+        end
+
+        it_behaves_like 'doing non web search'
+        it_behaves_like 'doing non image search'
+        it_behaves_like 'doing non news channel specific search'
+      end
+
+      context 'when doing search on a specific news channel' do
+        let(:search) { mock(NewsSearch) }
+
+        before do
+          search.should_receive(:instance_of?).at_least(:once) { |arg| arg == NewsSearch }
+          search.should_receive(:is_a?).at_least(:once) { |arg| arg == NewsSearch }
+          search.should_receive(:rss_feed).and_return(rss_feed)
+        end
+
+        it 'should render rss feed name' do
+          html = helper.render_navigations(affiliate, search, search_params)
+          html.should have_selector('.navigations', :content => 'News')
+        end
+
+        it 'should not render a link to rss feed' do
+          html = helper.render_navigations(affiliate, search, search_params)
+          html.should_not have_selector('.navigations a', :content => 'News')
+        end
+
+        it_behaves_like 'doing non web search'
+        it_behaves_like 'doing non image search'
+        it_behaves_like 'doing non odie search'
+      end
+    end
+
+    context 'when there are more than 1 active rss feed navigations' do
+      let(:search) { mock(NewsSearch) }
+
+      before do
+        affiliate.stub_chain(:navigations, :active).and_return(
+            [image_nav, rss_feed_nav, document_collection_nav, another_rss_feed_nav])
+        search.should_receive(:instance_of?).at_least(:once) { |arg| arg == NewsSearch }
+        search.should_receive(:is_a?).at_least(:once) { |arg| arg == NewsSearch }
+      end
+
+      context 'when not doing search on a specific news channel' do
+        before { search.should_receive(:rss_feed).and_return(nil) }
+
+        it_behaves_like 'doing search on everything'
+        it_behaves_like 'doing non image search'
+        it_behaves_like 'doing non odie search'
+        it_behaves_like 'doing non news channel specific search'
+      end
+    end
+  end
+end
