cc3daf02fcc3d707f15aa2ee134b39045d25d54e
David Pranata <david@eigenpath.com>
2012-06-26 15:41:18 -0400

    [Completes #31820743] Customers may implement Type Ahead js w/o calling external CSS
    - set usasearch_host if it's not already define

--- public/javascripts/sayt/remote.js
+++ public/javascripts/sayt/remote.js
@@ -1,11 +1,15 @@
+if (usasearch_host === undefined) {
+  var usasearch_host = "http://search.usa.gov";
+}
+
 var link = document.createElement("link");
 link.type = "text/css";
-link.href = document.location.protocol + "//" + document.location.host + "/stylesheets/compiled/sayt.css";
+link.href = usasearch_host + "/stylesheets/compiled/sayt.css";
 link.rel = "stylesheet";
 link.media = "screen";
 document.getElementsByTagName("head")[0].appendChild(link);
 
 var script = document.createElement("script");
 script.type = "text/javascript";
-script.src = document.location.protocol + "//" + document.location.host + "/javascripts/sayt/all.js";
+script.src = usasearch_host + "/javascripts/sayt/all.js";
 document.getElementsByTagName("head")[0].appendChild(script);
