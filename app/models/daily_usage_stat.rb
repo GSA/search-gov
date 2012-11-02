07bb9db0688348cfcc885ad2794d4e9fcab46105
Loren Siebert <loren@siebert.org>
2012-11-01 22:32:03 -0700

    Small DRYing up of DailyUsageStat.monthly_totals

--- app/models/daily_usage_stat.rb
+++ app/models/daily_usage_stat.rb
@@ -5,7 +5,7 @@ class DailyUsageStat < ActiveRecord::Base
 
   def self.monthly_totals(year, month, affiliate_name = nil)
     report_date = Date.civil(year, month)
-    affiliate_clause = affiliate_name ? "affiliate = #{affiliate_name} AND" : ""
+    affiliate_clause = affiliate_name ? "affiliate = '#{affiliate_name}' AND" : ""
     DailyUsageStat.sum(:total_queries, :conditions => ["#{affiliate_clause} (day between ? and ?)",
                                                        report_date.beginning_of_month, report_date.end_of_month])
   end
