a3ab7188415f6592944d76a7d5f13ff0b6a74b7e
Loren Siebert <loren@siebert.org>
2012-11-13 17:40:08 -0800

    fixed coverage

--- spec/models/odie_image_search_spec.rb
+++ spec/models/odie_image_search_spec.rb
@@ -1,15 +1,31 @@
 require 'spec_helper'
 
-describe OdieSearch do
+describe OdieImageSearch do
   fixtures :affiliates
 
   before do
     @affiliate = affiliates(:basic_affiliate)
+    flickr_profile = FlickrProfile.create(:url => 'http://flickr.com/photos/USAgency', :affiliate => @affiliate, :profile_type => 'user', :profile_id => '12345')
+    FlickrPhoto.create!(:flickr_id => 5, :flickr_profile => flickr_profile, :title => 'President Obama walks his daughters to school', :description => '', :tags => 'barack obama,sasha,malia')
+    FlickrPhoto.create!(:flickr_id => 6, :flickr_profile => flickr_profile, :title => 'POTUS gets in car.', :description => 'Barack Obama gets into his super protected car.', :tags => "car,batman", :date_taken => Time.now - 14.days)
+    FlickrPhoto.create!(:flickr_id => 7, :flickr_profile => flickr_profile, :title => 'irrelevant photo', :description => 'irrelevant', :tags => "car,batman", :date_taken => Time.now - 14.days)
+    FlickrPhoto.reindex
   end
 
-  describe "#cache_key" do
+  describe ".search" do
+    it "should find relevant Flickr photos" do
+      image_search = OdieImageSearch.new(:query => 'obama', :affiliate => @affiliate)
+      image_search.run
+      image_search.total.should == 2
+      image_search.results.first["title"].should == 'President Obama walks his daughters to school'
+      image_search.results.last["title"].should == 'POTUS gets in car.'
+    end
+  end
+
+  describe ".cache_key" do
     it "should output a key based on the query, affiliate id, and page parameters" do
       OdieImageSearch.new(:query => 'element', :affiliate => @affiliate, :page => 4).cache_key.should == "odie_image:element:#{@affiliate.id}:4:10"
     end
   end
+
 end
