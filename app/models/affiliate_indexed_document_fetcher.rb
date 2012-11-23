fefff7fd6bdca348b69d90f9124a0dd5dad91301
Loren Siebert <loren@siebert.org>
2012-11-23 13:37:39 -0800

    [Completes #40055161] Try to process more of our 2M unfetched Odie docs
    - Uses env var if available

--- app/models/affiliate_indexed_document_fetcher.rb
+++ app/models/affiliate_indexed_document_fetcher.rb
@@ -4,10 +4,11 @@ class AffiliateIndexedDocumentFetcher
   MAX_RUNTIME_IN_MINUTES = 60
 
   def self.perform(affiliate_id, start_id, end_id, scope)
+    max_runtime_in_minutes = ENV['MAX_RUNTIME_IN_MINUTES'] || MAX_RUNTIME_IN_MINUTES
     start_time = Time.now
     conditions = ["id between ? and ?", start_id, end_id]
     Affiliate.find(affiliate_id).indexed_documents.select(:id).send(scope.to_sym).where(conditions).order(:last_crawled_at).each do |indexed_document|
-      return if ((Time.now-start_time)/60).to_i > MAX_RUNTIME_IN_MINUTES
+      return if ((Time.now-start_time)/60).to_i > max_runtime_in_minutes.to_i
       IndexedDocument.find(indexed_document.id).fetch
     end
   end
