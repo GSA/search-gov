23b19e6f66eaf939692b1ed883a613165c3eedc3
Loren Siebert <loren@siebert.org>
2013-02-14 18:06:45 -0800

    [Fixes #44477577] Applying SaytFilters takes waaaaaaay too long now
    - test coverage

--- spec/models/apply_sayt_filters_spec.rb
+++ spec/models/apply_sayt_filters_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe ApplySaytFilters, "#perform" do
+  fixtures :sayt_suggestions
+
   it 'should enqueue ApplyFiltersToSaytSuggestion for each SaytSuggestion' do
     SaytSuggestion.all.each { |ss| Resque.should_receive(:enqueue_with_priority).with(:high, ApplyFiltersToSaytSuggestion, ss.id) }
     ApplySaytFilters.perform
