13b339f0ab5620789473902bc4e16395d62bfd05
Loren Siebert <loren@siebert.org>
2013-03-06 19:42:48 -0800

    [Completes #45151851] Admins should not see agencies w/out USAJobs codes in SuperAdmin > Sites
    - Trying to solve the problem from the Agency direction, not the Affiliate direction, with field search and seeing the affiliates association.

--- app/controllers/admin/agencies_controller.rb
+++ app/controllers/admin/agencies_controller.rb
@@ -1,6 +1,11 @@
 class Admin::AgenciesController < Admin::AdminController
   active_scaffold :agency do |config|
-    config.columns = [:name, :abbreviation, :organization_code, :domain, :agency_urls, :phone, :toll_free_phone, :tty_phone, :twitter_username, :facebook_username, :youtube_username, :flickr_url, :name_variants, :agency_queries]
+    config.columns = [:name, :abbreviation, :organization_code, :domain, :affiliates, :agency_urls, :phone, :toll_free_phone, :tty_phone, :twitter_username, :facebook_username, :youtube_username, :flickr_url, :name_variants, :agency_queries]
+    config.columns[:affiliates].form_ui = :select
+    config.columns[:affiliates].options = {:draggable_lists => true}
+    config.actions.exclude :search
+    config.actions.add :field_search
+    config.field_search.columns = [:name, :abbreviation, :organization_code, :domain, :affiliates, :twitter_username, :facebook_username, :youtube_username]
     config.list.columns.exclude [:agency_queries, :created_at, :updated_at, :toll_free_phone, :tty_phone, :flickr_url]
   end
 end
