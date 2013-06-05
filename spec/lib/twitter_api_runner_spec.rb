676a69f072fb34f9b6995a21efba08f105773787
David Pranata <david@eigenpath.com>
2013-06-05 17:00:03 -0400

    Fix failing spec/lib/twitter_api_runner_spec.rb

--- spec/lib/twitter_api_runner_spec.rb
+++ spec/lib/twitter_api_runner_spec.rb
@@ -7,17 +7,17 @@ describe TwitterApiRunner do
       it 'should sleep and retry 3 times before raising the error' do
         Twitter.should_receive(:user_timeline).exactly(4).times.and_raise(Twitter::Error::ServiceUnavailable)
         TwitterApiRunner.should_receive(:sleep).exactly(3).times.with(15.minutes)
-        lambda { TwitterApiRunner.run { Twitter.user_timeline('usasearch') } }.should raise_error(Twitter::Error::ServiceUnavailable)
+        expect { TwitterApiRunner.run { Twitter.user_timeline('usasearch') } }.to raise_error(Twitter::Error::ServiceUnavailable)
       end
     end
 
     context 'the block raises Twitter::Error::TooManyRequests 3 times' do
-      xit 'should sleep and retry 3 times before raising the error' do
-        error = Twitter::Error::TooManyRequests.new
+      it 'should sleep and retry 3 times before raising the error' do
+        error = Twitter::Error::TooManyRequests.new# RuntimeError.new('too many request')
         error.stub_chain(:rate_limit, :reset_in).and_return(5.minutes)
         Twitter.should_receive(:user_timeline).exactly(4).times.and_raise(error)
         TwitterApiRunner.should_receive(:sleep).exactly(3).times.with(5.minutes + 60)
-        lambda { TwitterApiRunner.run { Twitter.user_timeline('usasearch') } }.should raise_error(error)
+        expect { TwitterApiRunner.run { Twitter.user_timeline('usasearch') } }.to raise_error(Twitter::Error::TooManyRequests)
       end
     end
   end
