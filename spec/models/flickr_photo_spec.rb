4ba3c8958d923cc594b240ebeca08d6629fa79f2
Loren Siebert <loren@siebert.org>
2013-09-24 10:32:38 -0700

    Try to see why FlickrPhoto spec is failing on staging (but not on dev)

--- spec/models/flickr_photo_spec.rb
+++ spec/models/flickr_photo_spec.rb
@@ -18,19 +18,19 @@ describe FlickrPhoto do
   it { should validate_presence_of :flickr_profile }
 
   it "should create a new instance given valid attributes" do
-    FlickrPhoto.create!(@valid_attributes.merge(:flickr_profile => FlickrProfile.create))
+    FlickrPhoto.create!(@valid_attributes.merge(:flickr_profile => @flickr_profile))
     should validate_uniqueness_of(:flickr_id).scoped_to(:flickr_profile_id)
   end
 
   describe "#search_for" do
     before do
       FlickrPhoto.destroy_all
-      FlickrPhoto.create(:flickr_id => 1, :flickr_profile => @flickr_profile, :title => 'A picture of Barack Obama', :description => 'Barack Obama playing with his dog at the White House.', :tags => 'barackobama,barack obama,dog,white house', :date_taken => Time.now - 3.days)
-      FlickrPhoto.create(:flickr_id => 2, :flickr_profile => @flickr_profile, :title => 'Barack Obama and Joe Biden in Air Force One', :description => 'President Barack Obama and Vice President Joe Biden boarding Air Force One for a quick trip somewhere.', :tags => "joe biden vice president barack obama", :date_taken => Time.now - 2.days)
-      FlickrPhoto.create(:flickr_id => 3, :flickr_profile => @flickr_profile, :title => 'Barack and Michelle Obama', :description => 'President Barack Obama and First Lady Michelle Obama attend a state dinner at the White House', :tags => "barack obama,michelle,whitehouse", :date_taken => Time.now - 4.days)
-      FlickrPhoto.create(:flickr_id => 4, :flickr_profile => @flickr_profile, :title => 'Barack Obama Throws First Pitch', :description => 'President Barack Obama throws out the first pitch at a Washington Nationals baseball game.', :date_taken => Time.now - 5.days)
-      FlickrPhoto.create(:flickr_id => 5, :flickr_profile => @flickr_profile, :title => "President Obama walks his daughters to school", :description => '', :tags => 'barack obama,sasha,malia')
-      FlickrPhoto.create(:flickr_id => 6, :flickr_profile => @flickr_profile, :title => 'POTUS gets in car.', :description => 'Barack Obama gets into his super protected car.', :tags => "car,batman", :date_taken => Time.now - 14.days)
+      FlickrPhoto.create!(:flickr_id => 1, :flickr_profile => @flickr_profile, :title => 'A picture of Barack Obama', :description => 'Barack Obama playing with his dog at the White House.', :tags => 'barackobama,barack obama,dog,white house', :date_taken => Time.now - 3.days)
+      FlickrPhoto.create!(:flickr_id => 2, :flickr_profile => @flickr_profile, :title => 'Barack Obama and Joe Biden in Air Force One', :description => 'President Barack Obama and Vice President Joe Biden boarding Air Force One for a quick trip somewhere.', :tags => "joe biden vice president barack obama", :date_taken => Time.now - 2.days)
+      FlickrPhoto.create!(:flickr_id => 3, :flickr_profile => @flickr_profile, :title => 'Barack and Michelle Obama', :description => 'President Barack Obama and First Lady Michelle Obama attend a state dinner at the White House', :tags => "barack obama,michelle,whitehouse", :date_taken => Time.now - 4.days)
+      FlickrPhoto.create!(:flickr_id => 4, :flickr_profile => @flickr_profile, :title => 'Barack Obama Throws First Pitch', :description => 'President Barack Obama throws out the first pitch at a Washington Nationals baseball game.', :date_taken => Time.now - 5.days)
+      FlickrPhoto.create!(:flickr_id => 5, :flickr_profile => @flickr_profile, :title => "President Obama walks his daughters to school", :description => '', :tags => 'barack obama,sasha,malia')
+      FlickrPhoto.create!(:flickr_id => 6, :flickr_profile => @flickr_profile, :title => 'POTUS gets in car.', :description => 'Barack Obama gets into his super protected car.', :tags => "car,batman", :date_taken => Time.now - 14.days)
       FlickrPhoto.reindex
     end
 
