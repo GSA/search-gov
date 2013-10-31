0eb3cbc92866ca9042d467e7b503f9b22098d000
Loren Siebert <loren@siebert.org>
2013-10-30 17:12:29 -0700

    [Completes #59840232] Add DAP tag to all SERPs
    - Fix broken specs
    - Modernized hash rockets

--- spec/views/layouts/searches.html.haml_spec.rb
+++ spec/views/layouts/searches.html.haml_spec.rb
@@ -3,27 +3,28 @@ require 'spec_helper'
 
 describe 'layouts/searches' do
   let(:affiliate) { mock_model(Affiliate,
-                               :is_sayt_enabled? => true,
-                               :nested_header_footer_css => nil,
-                               :header => 'header',
-                               :footer => 'footer',
-                               :favicon_url => 'http://cdn.agency.gov/favicon.ico',
-                               :external_css_url => 'http://cdn.agency.gov/custom.css',
-                               :css_property_hash => {},
-                               :page_background_image_file_name => nil,
-                               :uses_managed_header_footer? => false,
-                               :managed_header_css_properties => nil,
-                               :show_content_border? => true,
-                               :show_content_box_shadow? => true,
-                               :connections => [],
-                               :locale => 'en',
-                               :ga_web_property_id => nil,
-                               :external_tracking_code => 'TRACKING CODE',
-                               :look_and_feel_css => '#container{background-color:#abc}')
+                               is_sayt_enabled?: true,
+                               nested_header_footer_css: nil,
+                               header: 'header',
+                               footer: 'footer',
+                               favicon_url: 'http://cdn.agency.gov/favicon.ico',
+                               external_css_url: 'http://cdn.agency.gov/custom.css',
+                               css_property_hash: {},
+                               page_background_image_file_name: nil,
+                               uses_managed_header_footer?: false,
+                               managed_header_css_properties: nil,
+                               show_content_border?: true,
+                               show_content_box_shadow?: true,
+                               connections: [],
+                               locale: 'en',
+                               dap_enabled?: false,
+                               ga_web_property_id: nil,
+                               external_tracking_code: 'TRACKING CODE',
+                               look_and_feel_css: '#container{background-color:#abc}')
   }
   before do
     assign(:affiliate, affiliate)
-    search = mock(WebSearch, :query => 'america')
+    search = mock(WebSearch, query: 'america')
     assign(:search, search)
     view.stub(:external_tracking_code_disabled?).and_return(false)
   end
