223284fc0364cfa31c53229dd29ae0bdf90b2472
David Pranata <david@eigenpath.com>
2013-11-15 10:34:15 -0500

    [Completes #60589188] Customers see the queries that surface a best bet
    - upgrade uglifier and yui-compressor
    - fix failing assets:precompile by switching to uglifier JS compressor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ UsasearchRails3::Application.configure do
   # Compress JavaScripts and CSS
   config.assets.compress = true
   config.assets.css_compressor = :yui
-  config.assets.js_compressor = :yui
+  config.assets.js_compressor = :uglifier
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
