4a49aa010dcd742e236dd58c6f5b7faab209da39
Loren Siebert <loren@siebert.org>
2013-11-20 14:25:44 -0800

    [#60524196] Set up dev/testing credentials for Keen.io
    - Reverted prior change to force http vs https. Not seeing any performance gains with http, so might as well stick with https.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,7 +8,7 @@ role :resque_workers,  "192.168.100.170", "192.168.100.173"
 role :daemon, "192.168.100.170"
 role :solr, "192.168.100.174", "192.168.110.12"
 
-before 'deploy:assets:precompile', 'production_yaml_files'
+before 'deploy:assets:precompile', 'production_specific_files'
 before 'deploy:create_symlink', 'production_solrconfig'
 before "deploy:cleanup", "restart_resque_workers"
 
@@ -21,9 +21,10 @@ task :production_solrconfig, :roles => :solr, :except => { :no_release => true }
   run "cp #{shared_path}/system/solrconfig.xml #{release_path}/solr/conf/solrconfig.xml"
 end
 
-task :production_yaml_files, :except => { :no_release => true } do
+task :production_specific_files, :except => { :no_release => true } do
   run "cp #{shared_path}/system/database.yml #{release_path}/config/database.yml"
   run "cp #{shared_path}/system/sunspot.yml #{release_path}/config/sunspot.yml"
   run "cp #{shared_path}/system/redis.yml #{release_path}/config/redis.yml"
   run "cp #{shared_path}/system/geoip.dat #{release_path}/db/geoip/geoip.dat"
+  run "cp #{shared_path}/system/keen.rb #{release_path}/config/initializers/keen.rb"
 end
