39d772969128bf7b8219634b9538b339d4ccd437
Loren Siebert <loren@siebert.org>
2013-12-19 09:23:58 -0800

    [Completes #62506702] Customers should NOT be able to enter comma- or pipe-delimited keywords on best bets
    - Update error message

--- app/models/boosted_content_keyword.rb
+++ app/models/boosted_content_keyword.rb
@@ -1,6 +1,6 @@
 class BoostedContentKeyword < ActiveRecord::Base
   validates_presence_of :value
   validates_uniqueness_of :value, scope: :boosted_content_id, case_sensitive: false
-  validates_format_of :value, with: /\A[^,|]+\z/i, message: 'must be a single keyword per entry. Add separate keywords individually by clicking Add Another Keyword.'
+  validates_format_of :value, with: /\A[^,|]+\z/i, message: "can't contain commas or pipes. Add each keyword (word or phrase) individually by clicking Add Another Keyword."
   belongs_to :boosted_content
 end
--- app/models/featured_collection_keyword.rb
+++ app/models/featured_collection_keyword.rb
@@ -1,6 +1,6 @@
 class FeaturedCollectionKeyword < ActiveRecord::Base
   validates_presence_of :value
   validates_uniqueness_of :value, scope: :featured_collection_id, case_sensitive: false
-  validates_format_of :value, with: /\A[^,|]+\z/i, message: 'must be a single keyword per entry. Add separate keywords individually by clicking Add Another Keyword.'
+  validates_format_of :value, with: /\A[^,|]+\z/i, message: "can't contain commas or pipes. Add each keyword (word or phrase) individually by clicking Add Another Keyword."
   belongs_to :featured_collection
 end
