cb178fc03da55d6f7c596e82d4f4e14e915d9842
Loren Siebert <loren@siebert.org>
2014-01-08 07:21:47 -0800

    [#63395304] Migrate BB-Text (BoostedContents) to ElasticSearch
    - DRY'd up index mappings

--- app/models/elastic_boosted_content.rb
+++ app/models/elastic_boosted_content.rb
@@ -4,19 +4,8 @@ class ElasticBoostedContent
   self.settings = ElasticSettings::COMMON
 
   self.mappings = {
-      index_type => {
-          dynamic: false,
-          _analyzer: { path: "language" },
-          properties: {
-              language: { type: "string", index: :not_analyzed },
-              affiliate_id: { type: 'integer' },
-              status: { type: 'string', index: :not_analyzed },
-              publish_start_on: { type: 'date', format: 'YYYY-MM-dd' },
-              publish_end_on: { type: 'date', format: 'YYYY-MM-dd', null_value: '9999-12-31' },
-              title: { type: 'string', term_vector: 'with_positions_offsets' },
-              description: { type: 'string', term_vector: 'with_positions_offsets' },
-              keyword_values: { type: 'string', analyzer: 'case_insensitive_keyword_analyzer' },
-              id: { type: 'integer', index: :not_analyzed, include_in_all: false } } }
-  }.freeze
+      index_type => ElasticMappings::BEST_BET.deep_merge(
+          properties: { description: { type: 'string', term_vector: 'with_positions_offsets' }})
+  }
 
 end
