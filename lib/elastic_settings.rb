5fbfe351b734079ff23c6410cf8777a731407ec8
Loren Siebert <loren@siebert.org>
2014-01-27 08:18:30 -0800

    [#63608132] Searchers see matches for synonyms in our new ES indexes
    - Changed settings to apply synonym filter AFTER the stemmer. That way, we don't have to specify all the plurals, etc, in the synonym declaration. Needed to modify "visa, visas" to "visa, vis" because the Spanish stemmer stems "visas" to "vis".

--- lib/elastic_settings.rb
+++ lib/elastic_settings.rb
@@ -1,11 +1,16 @@
 module ElasticSettings
-  ES_SYNONYMS = ["visa, visas"]
-
+  ES_SYNONYMS = ["visa, vis"]
+  EN_SYNONYMS = [
+    'sequestered, sequestration => sequester',
+    'bomber, bombed, bombing => bomb',
+    'fracturing, fracking, hydrofracking, hydrofracturing'
+  ]
   COMMON = {
     index: {
       analysis: {
         filter: {
           es_synonym: { type: 'synonym', synonyms: ES_SYNONYMS },
+          en_synonym: { type: 'synonym', synonyms: EN_SYNONYMS },
           en_stop_filter: { type: "stop", stopwords: ["_english_"] },
           en_stem_filter: { type: "stemmer", name: "minimal_english" },
           es_stop_filter: { type: "stop", stopwords: ["_spanish_"] },
@@ -15,11 +20,11 @@ module ElasticSettings
           en_analyzer: {
             type: "custom",
             tokenizer: "standard",
-            filter: %w(standard asciifolding lowercase en_stop_filter en_stem_filter) },
+            filter: %w(standard asciifolding lowercase en_stop_filter en_stem_filter en_synonym) },
           es_analyzer: {
             type: "custom",
             tokenizer: "standard",
-            filter: %w(standard asciifolding lowercase es_synonym es_stop_filter es_stem_filter) },
+            filter: %w(standard asciifolding lowercase es_stop_filter es_stem_filter es_synonym) },
           case_insensitive_keyword_analyzer: {
             tokenizer: 'keyword',
             filter: %w(standard asciifolding lowercase) } } } }
