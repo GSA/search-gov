8b42c711b4b8debc81568f9d37d429dba6e7b40e
Loren Siebert <loren@siebert.org>
2014-03-25 16:30:41 -0700

    [Completes #68134146] Migrate flickr photos from Solr to ES
    - Tweaked test to break tie in score

--- spec/models/odie_image_search_spec.rb
+++ spec/models/odie_image_search_spec.rb
@@ -8,7 +8,7 @@ describe OdieImageSearch do
     flickr_profile = FlickrProfile.create(:url => 'http://flickr.com/photos/USAgency', :affiliate => @affiliate, :profile_type => 'user', :profile_id => '12345')
     FlickrPhoto.delete_all
     ElasticFlickrPhoto.recreate_index
-    FlickrPhoto.create!(:flickr_id => 5, :flickr_profile => flickr_profile, :title => 'President Obama walks his daughters to school', :description => '', :tags => 'barack obama,sasha,malia')
+    FlickrPhoto.create!(:flickr_id => 5, :flickr_profile => flickr_profile, :title => 'President Obama walks the Obama daughters to school', :description => '', :tags => 'barack obama,sasha,malia')
     FlickrPhoto.create!(:flickr_id => 6, :flickr_profile => flickr_profile, :title => 'POTUS gets in car.', :description => 'Barack Obama gets into his super protected car.', :tags => "car,batman", :date_taken => Time.now - 14.days)
     FlickrPhoto.create!(:flickr_id => 7, :flickr_profile => flickr_profile, :title => 'irrelevant photo', :description => 'irrelevant', :tags => "car,batman", :date_taken => Time.now - 14.days)
     ElasticFlickrPhoto.commit
@@ -18,7 +18,7 @@ describe OdieImageSearch do
     it "should find relevant Flickr photos" do
       image_search = OdieImageSearch.new(:query => 'obama', :affiliate => @affiliate)
       image_search.run
-      image_search.results.first["title"].should == 'President Obama walks his daughters to school'
+      image_search.results.first["title"].should == 'President Obama walks the Obama daughters to school'
       image_search.results.last["title"].should == 'POTUS gets in car.'
       image_search.total.should == 2
     end
