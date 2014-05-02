52784b1a983f19cc5d2ada09ddd4b60933ceed9e
Loren Siebert <loren@siebert.org>
2014-05-01 22:04:48 -0700

    [#69558074] WH searchers see "news first" search experience
    - use default ES client host instead of forcing use of production logstash index

--- app/models/link_popularity.rb
+++ app/models/link_popularity.rb
@@ -1,11 +1,10 @@
 require 'typhoeus/adapters/faraday'
 
 class LinkPopularity
-  @@client = Elasticsearch::Client.new(log: Rails.env == 'development', host: "192.168.100.171")
 
   def self.popularity_for(url, days_back)
     link_popularity_query = ElasticLinkPopularityQuery.new(url, days_back)
-    total = @@client.count(index: "logstash-*", type: 'click', body: link_popularity_query.body)["count"]
+    total = ES.client_reader.count(index: "logstash-*", type: 'click', body: link_popularity_query.body)["count"]
     [Math.log10(total), 1.0].max
   end
 
