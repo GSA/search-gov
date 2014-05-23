3f9116c2f6c75cc5dce41c8c3081d1ecfe1eb8a1
David Pranata <david@eigenpath.com>
2014-05-22 23:52:08 -0400

    [#71870460] Rescue and retry when Twitter API raises "Twitter::Error: execution expired"

--- lib/twitter_api_runner.rb
+++ lib/twitter_api_runner.rb
@@ -8,14 +8,29 @@ module TwitterApiRunner
     begin
       num_attempts += 1
       yield
-    rescue Twitter::Error::ServiceUnavailable, Twitter::Error::TooManyRequests => error
-      if num_attempts <= MAX_RETRY_ATTEMPTS
-        sleep_duration = error.is_a?(Twitter::Error::TooManyRequests) ? (error.rate_limit.reset_in + RATE_LIMIT_BUFFER) : SLEEP_INTERVAL
-        sleep(sleep_duration)
+    rescue Twitter::Error => error
+      sleep_duration = extract_sleep_duration error
+      if num_attempts <= MAX_RETRY_ATTEMPTS && sleep_duration > 0
+        sleep sleep_duration
         retry
       else
         raise error
       end
+    end
+  end
+
+  private
+
+  def self.extract_sleep_duration(error)
+    case
+      when Twitter::Error::TooManyRequests === error
+          error.rate_limit.reset_in + RATE_LIMIT_BUFFER
+      when Twitter::Error::ServiceUnavailable === error
+        SLEEP_INTERVAL
+      when error.cause =~ /execution expired/i
+        SLEEP_INTERVAL
+      else
+        0
     end
   end
 end
--- spec/lib/twitter_api_runner_spec.rb
+++ spec/lib/twitter_api_runner_spec.rb
@@ -13,11 +13,20 @@ describe TwitterApiRunner do
 
     context 'the block raises Twitter::Error::TooManyRequests 3 times' do
       it 'should sleep and retry 3 times before raising the error' do
-        error = Twitter::Error::TooManyRequests.new# RuntimeError.new('too many request')
+        error = Twitter::Error::TooManyRequests.new
         error.stub_chain(:rate_limit, :reset_in).and_return(5.minutes)
         Twitter.should_receive(:user_timeline).exactly(4).times.and_raise(error)
         TwitterApiRunner.should_receive(:sleep).exactly(3).times.with(5.minutes + 60)
         expect { TwitterApiRunner.run { Twitter.user_timeline('usasearch') } }.to raise_error(Twitter::Error::TooManyRequests)
+      end
+    end
+
+    context 'the block raises Twitter::Error execution expired 3 times' do
+      it 'should sleep and retry 3 times before raising the error' do
+        error = Twitter::Error.new 'execution expired'
+        Twitter.should_receive(:user_timeline).exactly(4).times.and_raise(error)
+        TwitterApiRunner.should_receive(:sleep).exactly(3).times.with(15.minutes)
+        expect { TwitterApiRunner.run { Twitter.user_timeline('usasearch') } }.to raise_error(Twitter::Error)
       end
     end
   end
