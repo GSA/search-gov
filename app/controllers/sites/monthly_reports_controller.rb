b14a61a86651b38febbed0932ead0841982d85ac
Loren Siebert <loren@siebert.org>
2014-06-02 11:08:32 -0700

    [Completes #72124814] Customers see RTU Impressions and Clicks by Module

--- app/controllers/sites/monthly_reports_controller.rb
+++ app/controllers/sites/monthly_reports_controller.rb
@@ -2,14 +2,20 @@ class Sites::MonthlyReportsController < Sites::SetupSiteController
   before_filter :establish_aws_connection
 
   def show
-    month, year = (params[:mmyyyy] || Date.yesterday.strftime('%m/%Y')).split('/')
+    month, year = (params[:mmyyyy] || default_mm_yyyy).split('/')
     @monthly_report = monthly_report(month, year)
   rescue ArgumentError => e
-    month, year = Date.yesterday.strftime('%m/%Y').split('/')
+    month, year = default_mm_yyyy.split('/')
     @monthly_report = monthly_report(month, year)
   end
 
   private
+
+  def default_mm_yyyy
+    date = params[:rtu].present? ? Date.current : Date.yesterday
+    date.strftime('%m/%Y')
+  end
+
   def monthly_report(month, year)
     params[:rtu].present? ? RtuMonthlyReport.new(@site, year, month) : MonthlyReport.new(@site, year, month)
   end
--- app/models/rtu_monthly_report.rb
+++ app/models/rtu_monthly_report.rb
@@ -8,6 +8,11 @@ class RtuMonthlyReport < MonthlyReport
     month_count('click')
   end
 
+  def search_module_stats
+    module_stats_analytics = RtuModuleStatsAnalytics.new(@month_range, @site.name)
+    module_stats_analytics.module_stats
+  end
+
   private
   def month_count(type)
     count_query = CountQuery.new(@site.name)
