d18422d84742adf7ad4861f7786862064d51b30d
Loren Siebert <loren@siebert.org>
2014-06-11 23:29:53 -0700

    [Completes #73070312] Log IPv6 addresses for search and clicks when available
    - Rails will use X-Forwarded-For for the remote_ip even though we've set REMOTE_ADDR with the True-Client-IP, so I delete it from the env hash so it doesn't happen.

--- lib/middlewares/adjust_client_ip.rb
+++ lib/middlewares/adjust_client_ip.rb
@@ -5,6 +5,7 @@ class AdjustClientIp
 
   def call(env)
     env['REMOTE_ADDR'] = env['HTTP_TRUE_CLIENT_IP'] if env['HTTP_TRUE_CLIENT_IP'].present?
+    env.delete('HTTP_X_FORWARDED_FOR')
     @app.call(env)
   end
 end
