3ae277d2653c99d8641a56d774d7ce6d75bee8e0
David Pranata <david@eigenpath.com>
2014-06-17 16:38:15 -0400

    [#73422274] Super admin can export agencies

--- app/controllers/admin/agencies_controller.rb
+++ app/controllers/admin/agencies_controller.rb
@@ -4,8 +4,9 @@ class Admin::AgenciesController < Admin::AdminController
     config.columns[:affiliates].form_ui = :select
     config.columns[:affiliates].options = {:draggable_lists => true}
     config.actions.exclude :search
-    config.actions.add :field_search
+    config.actions.add :field_search, :export
     config.field_search.columns = [:name, :abbreviation, :organization_code, :domain, :affiliates, :twitter_username, :facebook_username, :youtube_username]
     config.list.columns.exclude [:agency_queries, :created_at, :updated_at, :toll_free_phone, :tty_phone, :flickr_url]
+    config.export.columns.exclude :agency_queries
   end
 end
