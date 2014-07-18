bf8561f611135e0d7cb1d1c8db3d8193f04e5e3e
David Pranata <david@eigenpath.com>
2014-07-18 13:42:20 -0400

    [#75031772] Searchers see multiple agencies for documents posted by more than one agency
    - add rake task to import FR agencies

--- lib/tasks/federal_register.rake
+++ lib/tasks/federal_register.rake
@@ -1,7 +1,12 @@
 namespace :usasearch do
-  namespace :federal_register_documents do
+  namespace :federal_register do
+    desc 'Import federal register agencies'
+    task :import_agencies => :environment do
+      FederalRegisterAgencyData.import
+    end
+
     desc 'Import federal register documents'
-    task :import, [:load_all] => :environment do |_t, args|
+    task :import_documents, [:load_all] => :environment do |_t, args|
       args.with_defaults(load_all: false)
       load_all = args.load_all =~ /true/i ? true : false
       FederalRegisterDocumentData.import load_all: load_all
--- spec/lib/tasks/federal_register_spec.rb
+++ spec/lib/tasks/federal_register_spec.rb
@@ -1,15 +1,29 @@
 require 'spec_helper'
 
-describe 'Federal register documents rake tasks' do
+describe 'Federal register rake tasks' do
   before(:all) do
     @rake = Rake::Application.new
     Rake.application = @rake
-    Rake.application.rake_require('tasks/federal_register_documents')
+    Rake.application.rake_require('tasks/federal_register')
     Rake::Task.define_task(:environment)
   end
 
-  describe 'usasearch:federal_register_documents:import' do
-    let(:task_name) { 'usasearch:federal_register_documents:import' }
+  describe 'usasearch:federal_register:import_agencies' do
+    let(:task_name) { 'usasearch:federal_register:import_agencies' }
+    before { @rake[task_name].reenable }
+
+    it "should have 'environment' as a prereq" do
+      @rake[task_name].prerequisites.should include('environment')
+    end
+
+    it 'should import federal register agencies' do
+      FederalRegisterAgencyData.should_receive(:import)
+      @rake[task_name].invoke
+    end
+  end
+
+  describe 'usasearch:federal_register:import_documents' do
+    let(:task_name) { 'usasearch:federal_register:import_documents' }
     before { @rake[task_name].reenable }
 
     it "should have 'environment' as a prereq" do
