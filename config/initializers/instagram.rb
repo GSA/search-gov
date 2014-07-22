a7485e2c6313d2ef6d30c4149c48bf01c441d347
Loren Siebert <loren@siebert.org>
2014-07-22 13:47:24 -0700

    [#75465082] Add configurable Instagram oauth config

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -5,4 +5,4 @@ Instagram.configure do |config|
   config.client_secret = '***REMOVED***'
 end
 
-INSTAGRAM_ACCESS_TOKEN = '1317698369.e9abd93.b8d4032672004ef79a248b74bf56ce52'.freeze
+INSTAGRAM_ACCESS_TOKEN = '***REMOVED***'.freeze
