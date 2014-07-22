6e5dcbf57c7c435fc30964915278e4abd09eb8d0
David Pranata <david@eigenpath.com>
2014-07-22 14:52:06 -0400

    [#75375522] Admins can see various "is_enabled" flags as part of site export in admin center

--- app/controllers/admin/affiliates_controller.rb
+++ app/controllers/admin/affiliates_controller.rb
@@ -3,19 +3,51 @@ class Admin::AffiliatesController < Admin::AdminController
   active_scaffold :affiliate do |config|
     config.label = 'Sites'
     config.actions.exclude :delete
-    config.columns = [:id, :display_name, :name, :website, :user_emails, :status, :last_month_query_count, :tags, :site_domains, :affiliate_note,
-                      :force_mobile_format, :is_bing_image_search_enabled, :uses_managed_header_footer, :mobile_logo_url, :header_image_url,
-                      :created_at, :updated_at]
-    config.columns[:affiliate_note].label = 'Note'
-    config.columns[:website].label = 'Homepage URL'
-    config.columns[:mobile_logo_url].label = 'Mobile Logo URL'
-    config.columns[:header_image_url].label = 'Logo URL'
-    config.list.sorting = { :display_name => :asc }
 
-    virtual_columns = [:go_live_date,
+    all_columns = config.columns.reject do |column|
+      column.association or column.name =~ /(_created_at|_updated_at|agency_id|css_properties|content_type|file_name|_image|json|key|label|_logo|_mappings|scope_ids|size|status_id|uses_managed_header_footer)\z/
+    end.map(&:name)
+    all_columns << :agency
+    all_columns.sort!
+
+    all_columns |= %i(mobile_logo_url header_image_url uses_managed_header_footer staged_uses_managed_header_footer)
+
+    virtual_columns = [:dc_contributor, :dc_subject, :dc_publisher,
+                       :go_live_date, :last_month_query_count,
                        :header_footer_css, :staged_header_footer_css, :header, :staged_header, :footer, :staged_footer,
                        :features, :external_tracking_code, :submitted_external_tracking_code]
-    config.columns << virtual_columns
+    all_columns |= virtual_columns
+    config.columns = all_columns
+
+    list_columns = %i(id display_name name website status tags site_domains affiliate_note created_at updated_at)
+    config.list.columns = list_columns
+
+    export_columns = [list_columns, all_columns].flatten.uniq
+    actions.add :export
+    config.export.columns = export_columns
+    config.export.default_deselected_columns = [:dc_contributor,
+                                                :dc_subject,
+                                                :dc_publisher,
+                                                :external_tracking_code,
+                                                :fetch_concurrency,
+                                                :footer,
+                                                :ga_web_property_id,
+                                                :has_staged_content,
+                                                :header,
+                                                :header_footer_css,
+                                                :last_month_query_count,
+                                                :staged_footer,
+                                                :staged_header,
+                                                :staged_header_footer_css,
+                                                :submitted_external_tracking_code,
+                                                :staged_uses_managed_header_footer,
+                                                :uses_managed_header_footer]
+
+    config.columns[:affiliate_note].label = 'Note'
+    config.columns[:website].label = 'Homepage URL'
+    config.columns[:mobile_logo_url].label = 'Logo URL'
+    config.columns[:header_image_url].label = 'Legacy Logo URL'
+    config.list.sorting = { :display_name => :asc }
 
     [:header_footer_css, :staged_header_footer_css,
      :header, :staged_header, :footer, :staged_footer,
@@ -54,17 +86,7 @@ class Admin::AffiliatesController < Admin::AdminController
       name_group.collapsed = true
     end
 
-    config.list.columns.exclude virtual_columns
-    config.list.columns.exclude :force_mobile_format,
-                                :header_image_url,
-                                :is_bing_image_search_enabled,
-                                :is_federal_register_document_govbox_enabled,
-                                :last_month_query_count,
-                                :mobile_logo_url,
-                                :user_emails,
-                                :uses_managed_header_footer
-
-    config.create.columns = [:display_name, :name, :header_footer_css, :header, :footer, :locale]
+    config.create.columns = [:display_name, :name, :website, :locale]
     config.columns[:is_sayt_enabled].label = "Enable SAYT"
     config.columns[:theme].form_ui = :select
     config.columns[:features].associated_limit = nil
@@ -72,14 +94,7 @@ class Admin::AffiliatesController < Admin::AdminController
     theme_options = Affiliate::THEMES.keys.collect { |key| [Affiliate::THEMES[key][:display_name], key.to_s] }
     config.columns[:theme].options = { :include_blank => '', :options => theme_options }
     config.action_links.add "analytics", :label => "Analytics", :type => :member, :page => true
-    actions.add :export
-    config.export.default_deselected_columns = [:last_month_query_count,
-                                                :header_footer_css,
-                                                :staged_header_footer_css,
-                                                :header,
-                                                :staged_header,
-                                                :footer, :staged_footer,
-                                                :external_tracking_code, :submitted_external_tracking_code]
+
     config.columns[:search_engine].form_ui = :select
     config.columns[:search_engine].options = { :options => %w(Bing Google) }
 
--- app/helpers/admin/export_columns_helper.rb
+++ app/helpers/admin/export_columns_helper.rb
@@ -1,4 +1,8 @@
 module Admin::ExportColumnsHelper
+  def format_export_column_header_name(column)
+    column.label
+  end
+
   def user_emails_export_column(record)
     record.user_emails.html_safe if record.is_a? Affiliate
   end
