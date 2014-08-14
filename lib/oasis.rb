3775af17e522bfafe4ad077fff68cb8208f805a8
Loren Siebert <loren@siebert.org>
2014-08-14 10:56:03 -0700

    [Completes #75375716] integrate "subscription" API calls to DGSearch

--- lib/oasis.rb
+++ lib/oasis.rb
@@ -1,6 +1,6 @@
 module Oasis
-  INSTAGRAM_API_ENDPOINT = 'http://localhost:3000/api/v1/instagram_profiles.json'
-  FLICKR_API_ENDPOINT = 'http://localhost:3000/api/v1/flickr_profiles.json'
+  INSTAGRAM_API_ENDPOINT = 'http://localhost:8080/api/v1/instagram_profiles.json'
+  FLICKR_API_ENDPOINT = 'http://localhost:8080/api/v1/flickr_profiles.json'
 
   def self.subscribe_to_instagram(id, username)
     params = { id: id, username: username }
