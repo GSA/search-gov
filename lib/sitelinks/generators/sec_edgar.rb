d4d658c3fb84788219eb8973c792baf60e502b84
David Pranata <david@eigenpath.com>
2014-09-11 08:41:11 -0400

    [#74224302] Searchers see value-added links on select URLs
    - Show "Full Filing" link first

--- lib/sitelinks/generators/sec_edgar.rb
+++ lib/sitelinks/generators/sec_edgar.rb
@@ -1,3 +1,54 @@
+require 'uri/common'
+
+module Sitelinks
+  module Generators
+    class SecEdgar
+      include Sitelinks::Generators
+
+      self.url_prefix = 'www.sec.gov/Archives/edgar/data/'.freeze
+      MATCHING_URL_REGEX = %r[^(?!.*-index\.htm$)https?://(www\.)?sec\.gov/Archives/edgar/data/\d+/\d+\/.+$]i
+
+      DEFAULT_BROWSE_EDGAR_PARAMS = { Find: 'Search',
+                                      action: 'getcompany',
+                                      owner: 'exclude'
+      }.freeze
+
+      def self.generate(url)
+        return [] unless url =~ MATCHING_URL_REGEX
+
+        path_as_array = URI.parse(url).path.split('/')
+        return [] unless path_as_array.length == 7
+
+        [generate_full_filing_url(path_as_array),
+         generate_browse_edgar_url(path_as_array)]
+      end
+
+      def self.generate_browse_edgar_url(path_as_array)
+        cik = path_as_array[4]
+        browse_edgar_params = DEFAULT_BROWSE_EDGAR_PARAMS.merge(CIK: cik)
+
+        { title: 'Most Recent Filings for this Company',
+          url: "http://www.sec.gov/cgi-bin/browse-edgar?#{browse_edgar_params.to_param}" }
+      end
+
+      def self.generate_full_filing_url(path_as_array)
+        cik = path_as_array[4]
+        cik_part_1 = path_as_array[5].slice(0, 10)
+        cik_part_2 = path_as_array[5].slice(10, 2)
+        cik_part_3 = path_as_array[5].slice(12..-1)
+
+        # url = "http://www.sec.gov/Archives/edgar/data/#{cik}/#{cik_part_1}-#{cik_part_2}-#{cik_part_3}-index.htm"
+
+        full_filing_path = "#{cik}/#{cik_part_1}-#{cik_part_2}-#{cik_part_3}-index.htm"
+        url = "http://www.sec.gov/Archives/edgar/data/#{full_filing_path}"
+
+        { title: 'Full Filing', url: url }
+      end
+    end
+  end
+end
+
+# original script from SEC
 # <script type="text/javascript">
 # var numResults = document.getElementsByTagName("h4").length;
 # for (i=0; i < numResults; i++) {
@@ -26,53 +77,3 @@
 #     }
 # }
 # </script>
-
-require 'uri/common'
-
-module Sitelinks
-  module Generators
-    class SecEdgar
-      include Sitelinks::Generators
-
-      self.url_prefix = 'www.sec.gov/Archives/edgar/data/'.freeze
-      MATCHING_URL_REGEX = %r[^(?!.*-index\.htm$)https?://(www\.)?sec\.gov/Archives/edgar/data/\d+/\d+\/.+$]i
-
-      DEFAULT_BROWSE_EDGAR_PARAMS = { Find: 'Search',
-                                      action: 'getcompany',
-                                      owner: 'exclude'
-      }.freeze
-
-      def self.generate(url)
-        return [] unless url =~ MATCHING_URL_REGEX
-
-        path_as_array = URI.parse(url).path.split('/')
-        return [] unless path_as_array.length == 7
-
-        [generate_browse_edgar_url(path_as_array),
-         generate_full_filing_url(path_as_array)]
-      end
-
-      def self.generate_browse_edgar_url(path_as_array)
-        cik = path_as_array[4]
-        browse_edgar_params = DEFAULT_BROWSE_EDGAR_PARAMS.merge(CIK: cik)
-
-        { title: 'Most Recent Filings for this Company',
-          url: "http://www.sec.gov/cgi-bin/browse-edgar?#{browse_edgar_params.to_param}" }
-      end
-
-      def self.generate_full_filing_url(path_as_array)
-        cik = path_as_array[4]
-        cik_part_1 = path_as_array[5].slice(0, 10)
-        cik_part_2 = path_as_array[5].slice(10, 2)
-        cik_part_3 = path_as_array[5].slice(12..-1)
-
-        # url = "http://www.sec.gov/Archives/edgar/data/#{cik}/#{cik_part_1}-#{cik_part_2}-#{cik_part_3}-index.htm"
-
-        full_filing_path = "#{cik}/#{cik_part_1}-#{cik_part_2}-#{cik_part_3}-index.htm"
-        url = "http://www.sec.gov/Archives/edgar/data/#{full_filing_path}"
-
-        { title: 'Full Filing', url: url }
-      end
-    end
-  end
-end
