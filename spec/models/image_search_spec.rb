14a93ffcc61308248e8ce74fc5ce8e20b3541077
Loren Siebert <loren@siebert.org>
2014-09-23 11:45:35 -0700

    Fix code coverage

--- spec/models/image_search_spec.rb
+++ spec/models/image_search_spec.rb
@@ -2,11 +2,10 @@ require 'spec_helper'
 
 describe ImageSearch do
   fixtures :affiliates
+  let(:affiliate) { affiliates(:basic_affiliate) }
 
   describe ".new" do
     context 'when affiliate has no social media for images' do
-      let(:affiliate) { affiliates(:basic_affiliate) }
-
       before do
         affiliate.stub(:has_no_social_image_feeds?).and_return true
       end
@@ -16,5 +15,24 @@ describe ImageSearch do
         image_search.uses_cr.should be_true
       end
     end
+  end
+
+  describe "#run" do
+    context 'when Oasis results are blank AND we are on page 1 AND no commercial results override is set AND Bing image results are enabled' do
+      let(:image_search) { ImageSearch.new(affiliate: affiliate, query: "lsdkjflskjflskjdf") }
+
+      before do
+        affiliate.update_attribute(:is_bing_image_search_enabled, true)
+        affiliate.stub(:has_no_social_image_feeds?).and_return false
+      end
+
+      it 'should perform a Bing image search' do
+        search_engine_adapter = mock(SearchEngineAdapter, results: nil)
+        SearchEngineAdapter.should_receive(:new).and_return search_engine_adapter
+        search_engine_adapter.should_receive(:run)
+        image_search.run
+      end
+    end
+
   end
 end
