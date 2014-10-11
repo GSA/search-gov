fe052b8588ba42221eacbaf12d67881df92f6187
David Pranata <david@eigenpath.com>
2014-10-10 23:10:16 -0400

    [#80223720 ] Set a daily quota for Google searches
    - clarify limit

--- app/controllers/admin/outbound_rate_limits_controller.rb
+++ app/controllers/admin/outbound_rate_limits_controller.rb
@@ -2,5 +2,6 @@ class Admin::OutboundRateLimitsController < Admin::AdminController
   active_scaffold :outbound_rate_limit do |config|
     config.actions = [:list, :update]
     config.list.sorting = { name: :asc }
+    config.columns[:limit].label = 'Limit / data center'
   end
 end
