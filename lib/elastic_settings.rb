4846366de8c987d41c4a164537c4dcef1ee502ab
Loren Siebert <loren@siebert.org>
2014-11-21 16:05:23 -0800

    [#83222566] Add a folder in punchcard repo for the en/es stopword files

--- lib/elastic_settings.rb
+++ lib/elastic_settings.rb
@@ -1,7 +1,5 @@
 module ElasticSettings
   KEYWORD = { type: 'string', analyzer: 'case_insensitive_keyword_analyzer' }
-  SPANISH_STOPWORDS = %w(a al ante aquel aquello bajo cabe cada como con conmigo consigo contigo contra cual cuando de del desde despues donde durante e el en entonces entre es esta esto fin fue ha hacia has hasta la las le les los mas mediante menos mi ni o para pero por que quien salvo segun ser si sin so sobre solamente solo somos son soy su suya suyo suyos tal tambien tras u un una unas unos via y)
-  ENGLISH_STOPWORDS = %w(a an and are as at be but by for if in into is no not of on or s such t that the their then there these they this to was with)
 
   COMMON = {
     index: {
@@ -11,11 +9,11 @@ module ElasticSettings
         },
         filter: {
           bigram_filter: { type: 'shingle' },
-          en_stop_filter: { type: "stop", stopwords: ENGLISH_STOPWORDS },
+          en_stop_filter: { type: "stop", stopwords: File.readlines(Rails.root.join("config", "locales", "analysis", "en_stopwords.txt")) },
           en_synonym: { type: 'synonym', synonyms: File.readlines(Rails.root.join("config", "locales", "analysis", "en_synonyms.txt")).map(&:chomp) },
           en_protected_filter: { type: 'keyword_marker', keywords: File.readlines(Rails.root.join("config", "locales", "analysis", "en_protwords.txt")).map(&:chomp) },
           en_stem_filter: { type: "stemmer", name: "minimal_english" },
-          es_stop_filter: { type: "stop", stopwords: SPANISH_STOPWORDS },
+          es_stop_filter: { type: "stop", stopwords: File.readlines(Rails.root.join("config", "locales", "analysis", "es_stopwords.txt")).map(&:chomp) },
           es_protected_filter: { type: 'keyword_marker', keywords: File.readlines(Rails.root.join("config", "locales", "analysis", "es_protwords.txt")).map(&:chomp) },
           es_synonym: { type: 'synonym', synonyms: File.readlines(Rails.root.join("config", "locales", "analysis", "es_synonyms.txt")).map(&:chomp) },
           es_stem_filter: { type: "stemmer", name: "light_spanish" }
