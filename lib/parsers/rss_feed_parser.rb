bd7bacc73eea49a68a4c61d1d4afe86702590907
David Pranata <david@eigenpath.com>
2014-12-31 17:41:56 -0500

    [#84335988] Searchers should not see blank page on /search/news

--- lib/parsers/rss_feed_parser.rb
+++ lib/parsers/rss_feed_parser.rb
@@ -1,12 +1,19 @@
 module RssFeedParser
+  VALID_YEAR_RANGE = (1000..9999).freeze
+
   def extract_published_at(item, *pub_date_paths)
     published_at = nil
     pub_date_paths.each do |pub_date_path|
       published_at_str = item.xpath(pub_date_path).inner_text
       next if published_at_str.blank?
-      published_at = DateTime.parse published_at_str
+      published_at = parse_datetime published_at_str
       break if published_at.present?
     end
     published_at
+  end
+
+  def parse_datetime(datetime_str)
+    datetime = DateTime.parse datetime_str
+    datetime if datetime && VALID_YEAR_RANGE.include?(datetime.year)
   end
 end
