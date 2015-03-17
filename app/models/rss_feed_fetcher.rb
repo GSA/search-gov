c99219ba4f2ff3edee6f9170fd56fafe04134683
Loren Siebert <loren@siebert.org>
2015-03-17 10:36:09 -0700

    [Fixes #90513924] Investigate slow RSSFeedFetcher jobs

--- app/models/rss_feed_fetcher.rb
+++ app/models/rss_feed_fetcher.rb
@@ -1,6 +1,12 @@
+require 'resque-lock-timeout'
 class RssFeedFetcher
   extend Resque::Plugins::Priority
+  extend Resque::Plugins::LockTimeout
+
   @queue = :primary
+  @loner = true
+  @lock_timeout = 3600
+  Resque::Plugins::Timeout.timeout = 599
 
   def self.perform(rss_feed_url_id, ignore_older_items = true)
     rss_feed_url = RssFeedUrl.find_by_id rss_feed_url_id
--- spec/models/rss_feed_fetcher_spec.rb
+++ spec/models/rss_feed_fetcher_spec.rb
@@ -10,5 +10,16 @@ describe RssFeedFetcher do
       rss_feed_data.should_receive :import
       RssFeedFetcher.perform(100)
     end
+
+    it 'should timeout if it runs too long' do
+      Resque::Plugins::Timeout.timeout.should == 599
+    end
+  end
+
+  describe "enqueueing" do
+    it 'should not enqueue two RssFeedFetcher jobs with the same args' do
+      Resque.enqueue(RssFeedFetcher, 31415, false).should be_true
+      Resque.enqueue(RssFeedFetcher, 31415, false).should be_nil
+    end
   end
 end
