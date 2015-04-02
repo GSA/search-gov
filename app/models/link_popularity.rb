f18c8e8c13104b0c3e5c6b7ec46b94f0ca9a7198
Loren Siebert <loren@siebert.org>
2015-04-02 12:16:08 -0700

    [#91709966] Fix queue capacity issues on Elasticsearch
    - Only query the 7 logstash indexes that are relevant for link popularity, not all of them.

--- app/models/link_popularity.rb
+++ app/models/link_popularity.rb
@@ -1,8 +1,8 @@
 class LinkPopularity
-
+  extend LogstashPrefix
   def self.popularity_for(url, days_back)
     link_popularity_query = ElasticLinkPopularityQuery.new(url, days_back)
-    total = ES.client_reader.count(index: "logstash-*", type: 'click', body: link_popularity_query.body)["count"]
+    total = ES.client_reader.count(index: indexes_to_date(days_back, true), type: 'click', body: link_popularity_query.body)["count"]
     [Math.log10(total), 1.0].max
   end
 
--- spec/test_services.rb
+++ spec/test_services.rb
@@ -38,10 +38,23 @@ module TestServices
       klass = File.basename(filename, '.rb').camelize.constantize
       klass.recreate_index if klass.kind_of?(Indexable) and klass != ElasticBlended
     end
+    logstash_index_range.each do |date|
+      ES::client_reader.indices.create(index: "logstash-#{date.strftime("%Y.%m.%d")}")
+      ES::client_reader.indices.put_alias(index: "logstash-#{date.strftime("%Y.%m.%d")}", name: "human-logstash-#{date.strftime("%Y.%m.%d")}")
+    end
   end
 
   def delete_es_indexes
-    ES::client_reader.indices.delete(index: "test-usasearch-*") rescue nil
+    ES::client_reader.indices.delete(index: "test-usasearch-*")
+    logstash_index_range.each do |date|
+      ES::client_reader.indices.delete(index: "logstash-#{date.strftime("%Y.%m.%d")}")
+    end
+  rescue Exception => e
   end
 
+  def logstash_index_range
+    end_date = Date.current
+    start_date = end_date - 10.days
+    start_date..end_date
+  end
 end
