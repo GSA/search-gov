e5fad1579a95bfb358608fba10003b35cc283d6e
Loren Siebert <loren@siebert.org>
2015-05-23 16:07:31 -0700

    [#95260582] Searchers see localized "helper text" on all sites for which DOS has provided translations
    - Fixed active scaffold issue with affiliates

--- app/controllers/admin/languages_controller.rb
+++ app/controllers/admin/languages_controller.rb
@@ -1,6 +1,7 @@
 class Admin::LanguagesController < Admin::AdminController
   active_scaffold :language do |config|
     config.update.columns.exclude :affiliates
+    config.create.columns.exclude :affiliates
     config.list.sorting = { name: :asc }
   end
 end
