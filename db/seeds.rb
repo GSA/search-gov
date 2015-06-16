27b96ec64baede064d69e7dde1fbe9060abc5f42
DigitalGov Search - [DP] <search@support.digitalgov.gov>
2015-06-16 15:23:00 -0400

    [Fixes #97132722] Load seeds in development, but not test
    - create language first before affiliate
    - seed admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,9 @@
 if Rails.env.development?
   require_relative 'seeds/agency.rb'
-  require_relative 'seeds/affiliate.rb'
   require_relative 'seeds/language.rb'
+  require_relative 'seeds/affiliate.rb'
   require_relative 'seeds/email_template.rb'
+  require_relative 'seeds/user.rb'
 elsif Rails.env.test?
   puts 'Skipping seeds in test environment; specs should create their own data'
 else
---
+++ db/seeds/user.rb
@@ -0,0 +1,10 @@
+u = User.where(email: 'admin@email.gov').first_or_initialize
+u.assign_attributes contact_name: 'admin',
+                    default_affiliate: Affiliate.find_by_name('usagov'),
+                    is_affiliate: true,
+                    password: 'password'
+
+u.approval_status = 'approved'
+u.is_affiliate_admin = true
+u.save!
+u.affiliate_ids = Affiliate.pluck(:id)
