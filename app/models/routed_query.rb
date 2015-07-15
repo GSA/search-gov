41248701f15605943ce01434dc912eb703699587
Nick Marden <nick@marden.org>
2015-07-14 21:21:50 -0400

    [Fixes #99058260] Validate uniqueness of pre-processed keywords before saving routed queries

--- app/models/routed_query.rb
+++ app/models/routed_query.rb
@@ -14,6 +14,9 @@ class RoutedQuery < ActiveRecord::Base
 
   validate :keywords_cannot_be_blank
 
+  # https://github.com/rails/rails/issues/4568
+  validate :keywords_cannot_be_duplicated
+
   accepts_nested_attributes_for :routed_query_keywords, allow_destroy: true, reject_if: ->(k) { k['keyword'].blank? }
 
   def destroy_and_update_attributes(params)
@@ -23,6 +26,17 @@ class RoutedQuery < ActiveRecord::Base
 
   def keywords_cannot_be_blank
     errors.add(:base, 'Routed query must have 1 or more search terms') if routed_query_keywords.blank? || routed_query_keywords.all?(&:marked_for_destruction?)
+  end
+
+  def keywords_cannot_be_duplicated
+    return if (dk = duplicated_keywords).empty?
+    errors.add(:routed_query_keywords, "The following #{dk.count == 1 ? 'keyword has' : 'keywords have'} been duplicated: #{dk.map { |k| "'#{k}'" }.join(', ')}. Each keyword is case-insensitive and should be added only once.")
+  end
+
+  def duplicated_keywords
+    routed_query_keywords.each(&:valid?) # Normalize keywords via validation
+    kw = routed_query_keywords.map(&:keyword).compact.sort
+    kw.select { |k| kw.count(k) > 1 }.uniq
   end
 
   def label
