62f4ff73ea501f15e95a7586eb9195de55cef1c7
Loren Siebert <loren@siebert.org>
2015-08-06 13:15:31 -0700

    [#99774632] Customers see drill down data for routed queries
    - removed unused method

--- app/models/search_module_type.rb
+++ app/models/search_module_type.rb
@@ -4,9 +4,5 @@ class SearchModuleType
     def get_klass(module_tag)
       MAPPING[module_tag.downcase.to_sym]
     end
-
-    def module_tags
-      MAPPING.keys.map {|key| key.to_s.upcase}
-    end
   end
 end
