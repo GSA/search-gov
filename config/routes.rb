5bcec32a2eeafef7dadea8bf0622859854fed2a7
Loren Siebert <loren@siebert.org>
2015-09-21 10:48:09 -0700

    Revert "[#103024792] Customers can see top N referrers for a query and top N queries for a referrer"
    This reverts commit efa1b363ed5c0ec10f2baf971b03c33aae18369d.

--- config/routes.rb
+++ config/routes.rb
@@ -52,12 +52,10 @@ UsasearchRails3::Application.routes.draw do
       resource :type_ahead_api_instructions, only: [:show]
       resource :clicks, only: [:new, :create]
       resource :query_clicks, only: [:show]
-      resource :query_referrers, only: [:show]
       resource :query_downloads, only: [:show]
       resource :query_drilldowns, only: [:show]
       resource :click_drilldowns, only: [:show]
       resource :click_queries, only: [:show]
-      resource :referrer_queries, only: [:show]
       resource :queries, only: [:new, :create]
 
       resource :content, only: [:show]
