bc5f8d2cecf16248ed044d957a1334d834d3c472
Loren Siebert <loren@siebert.org>
2015-11-09 14:00:22 -0800

    [#107764824] Help links should allow numerics in path

--- app/models/help_link.rb
+++ app/models/help_link.rb
@@ -2,7 +2,7 @@ class HelpLink < ActiveRecord::Base
   before_validation :filter_request_path, if: :request_path?
   validates_presence_of :request_path, :help_page_url
   validates_uniqueness_of :request_path
-  validates :request_path, format: { with: /^(\/[a-z_]+)+$/ }
+  validates :request_path, format: { with: /^(\/[a-z0-9_]+)+$/ }
 
   def self.sanitize_request_path(request_path)
     URI.parse(request_path).path.gsub(/\/[0-9]+/, '').gsub(/\/$/, '')
--- spec/models/help_link_spec.rb
+++ spec/models/help_link_spec.rb
@@ -5,6 +5,7 @@ describe HelpLink do
   it { should validate_presence_of :request_path }
   it { should validate_presence_of :help_page_url }
   it { should validate_format_of(:request_path).with('/affiliates/rss_feeds/edit') }
+  it { should allow_value('/sites/i14y_drawers/new').for(:request_path) }
 
   describe '.lookup(request, action_name)' do
     before do
