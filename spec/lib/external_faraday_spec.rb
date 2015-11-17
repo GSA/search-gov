aed488f76fab3657ad615bccae262359bdb1322b
Nick Marden <nick@marden.org>
2015-11-17 15:24:24 -0500

    [#108379888] [#108380660] Correct Azure API namespacing + more reasonable timeouts for Azure

--- spec/lib/external_faraday_spec.rb
+++ spec/lib/external_faraday_spec.rb
@@ -4,15 +4,18 @@ describe ExternalFaraday do
   context '#get_config' do
     context 'when namespaced config is present' do
       it 'contains values for adapter and options' do
-        expect(described_class.get_config('azure_api')['adapter']).to be_present
-        expect(described_class.get_config('azure_api')['options']).to be_present
+        expect(described_class.get_config('azure_web_api')['adapter']).to eq(:typhoeus)
+        expect(described_class.get_config('azure_web_api')['options']).to be_present
       end
     end
 
     context 'when namespaced config is not present' do
       it 'contains values for adapter and options' do
+        nonexistent = described_class.get_config('nonexistent')
         expect(described_class.get_config('google_api')['adapter']).to be_present
+        expect(described_class.get_config('google_api')['adapter']).to eq(nonexistent['adapter'])
         expect(described_class.get_config('google_api')['options']).to be_present
+        expect(described_class.get_config('google_api')['options']).to eq(nonexistent['options'])
       end
     end
   end
