04eae671243494ecef08fcee81f4821c6fd5281e
Ari Perez <ariperez83@gmail.com>
2015-12-21 12:13:15 -0500

    [#108467518] Add SearchType to /config (web, blended, i14y); Refactor file names

--- app/api/search_consumer/api.rb
+++ app/api/search_consumer/api.rb
@@ -2,12 +2,12 @@ module SearchConsumer
   class API < Grape::API
     format :json
 
-    after_validation do
+    before_validation do
       error!('401 Unauthorized', 401) unless params[:sc_access_key] == SC_ACCESS_KEY
     end
 
     mount SearchConsumer::Resources::Affiliates
-    mount SearchConsumer::Resources::RssChannel
+    mount SearchConsumer::Resources::RssSearch
   end
 end
 
--- app/api/search_consumer/entities/affiliate_facets.rb
+++ app/api/search_consumer/entities/affiliate_facets.rb
@@ -1,16 +1,16 @@
 module SearchConsumer
   module Entities
-    class Facets < Grape::Entity
+    class AffiliateFacets < Grape::Entity
       expose :name, documentation: { type: 'string', desc: 'Name of the Facet.'} do |instance|
         instance.navigable.name if instance.navigable
       end
       expose :navigable_facet_type, as: :type, documentation: { type: 'string', desc: 'Facet Type'}
 
       expose :docs_id, if: lambda{|instance,options| instance.navigable_type == "DocumentCollection"}, documentation: { type: 'string', desc: 'DocumentCollection `id` if relevant.'} do |instance|
-        instance.navigable.id if instance.navigable
+        instance.navigable_facet_id
       end
       expose :channel_id, if: lambda{|instance,options| instance.navigable_type == "RssFeed"}, documentation: { type: 'string', desc: 'DocumentCollection `id` if relevant.'} do |instance|
-        instance.navigable.id if instance.navigable
+        instance.navigable_facet_id
       end
       expose :is_active, as: :active, documentation: { type: 'boolean', desc: 'TRUE if facet is active.'}
     end
