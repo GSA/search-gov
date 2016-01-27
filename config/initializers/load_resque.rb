570121665dfe8e3eb49a45d05b64d946ec6e98ce
Nick Marden <nick@marden.org>
2016-01-26 19:36:49 -0500

    [#112451025] Airbrake behaviors should be driven by configuration file

--- config/initializers/load_resque.rb
+++ config/initializers/load_resque.rb
@@ -1,10 +1,5 @@
 require 'resque/failure/airbrake'
 
-Airbrake.configure do |config|
-  config.api_key = '***REMOVED***'
-  config.secure = false
-end
-
 config = YAML::load(ERB.new(File.read("#{Rails.root}/config/redis.yml")).result)[Rails.env]
 Resque.redis = [config['host'], config['port']].join(':')
 REDIS_HOST = config['host']
