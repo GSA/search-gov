bd55e2ecf9ee74ff57626d24131aa56f910f2bd1
Joshua K. Farrar <joshua@sent1nel.me>
2016-02-05 11:18:25 -0500

    [#104194254] add affiliate's search_engine to endpoint for consumption

--- app/api/search_consumer/entities/affiliate.rb
+++ app/api/search_consumer/entities/affiliate.rb
@@ -8,6 +8,7 @@ module SearchConsumer
       expose :api_access_key, documentation: { type: 'string', desc: 'API access token for the affiliate'}
       expose :default_search_label, as: :page_one_label, documentation: { type: 'string', desc: 'Name of the Page One Facet.'}
       expose :locale, documentation: { type: 'string', desc: 'Default locale for the affiliate.'}
+      expose :search_engine, documentation: { type: 'string', desc: 'Search engine to be used [Bing, Google, Azure]'}
       expose :search_type, documentation: { type: 'string', desc: 'Default Search Type, [i14y, Blended, Web].'} do |instance|
         if instance.gets_i14y_results?
           'i14y'
