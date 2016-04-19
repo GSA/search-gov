72d75b76ac90b14e3d48a576cdeec8ce236c5e41
Martha <mothonmars@gmail.com>
2016-04-19 15:27:21 -0700

    [#116797267] expose footer_fragment, page_one_more_results_pointer

--- app/api/search_consumer/entities/footer.rb
+++ app/api/search_consumer/entities/footer.rb
@@ -5,6 +5,8 @@ module SearchConsumer
       expose :CSS, documentation: { type: 'hash', desc: 'Expose CSS values related to the Header'} do |affiliate|
         affiliate.load_template_schema.css.colors.footer.to_hash
       end
+      expose :page_one_more_results_pointer, documentation: { type: 'string', desc: 'Text directing the user to additional search results' }
+      expose :footer_fragment, documentation: { type: 'string', desc: 'Custom footer text (may contain HTML tags)' }
     end
   end
 end
