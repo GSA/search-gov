9927bc55999acc701d4730e660fe784fa82877fb
Joshua K. Farrar <joshua.farrar@gmail.com>
2016-09-13 13:29:59 -0400

    [#120381455] logs X-Original-Request header with SearchImpressions to be picked up by logstash (#224)

--- app/models/search_impression.rb
+++ app/models/search_impression.rb
@@ -2,14 +2,31 @@ class SearchImpression
   IRRELEVANT_KEYS = %w(access_key action api_key controller cx m utf8)
 
   def self.log(search, vertical, params, request)
-    request_pairs = { clientip: request.remote_ip, request: request.url, referrer: request.referer, user_agent: request.user_agent }
+    url = get_url_from_request(request)
+
+    request_pairs = {
+      clientip: request.remote_ip,
+      request: url,
+      referrer: request.referer,
+      user_agent: request.user_agent
+    }
+
     request_pairs[:diagnostics] = flatten_diagnostics_hash(search.diagnostics)
     relevant_params = params.reject { |k, v| IRRELEVANT_KEYS.include?(k.to_s) || k.to_s.include?('.') }
     hash = request_pairs.merge(time: Time.now.to_formatted_s(:db),
                                vertical: vertical,
                                modules: search.modules.join('|'),
                                params: relevant_params)
+
     Rails.logger.info("[Search Impression] #{hash.to_json}")
+  end
+
+  def self.get_url_from_request(request)
+    if ! request.headers['HTTP_X_ORIGINAL_REQUEST'].to_s.empty?
+      request.headers['HTTP_X_ORIGINAL_REQUEST']
+    else
+      request.url
+    end
   end
 
   def self.flatten_diagnostics_hash(diagnostics_hash)
