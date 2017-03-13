44566de3087ef97f628f766871ebbd3de0c64089
Martha Thompson <martha@rrsoft.co>
2017-03-13 08:56:23 -0700

    [#141490871] only send daily snapshot emails to approved users with active sites (#328)

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -4,7 +4,10 @@ class Membership < ActiveRecord::Base
   belongs_to :affiliate
   belongs_to :user
 
-  scope :daily_snapshot_receivers, where(gets_daily_snapshot_email: true)
+  scope :daily_snapshot_receivers,
+    where(gets_daily_snapshot_email: true).
+    joins(:user).where(users: { approval_status: 'approved' }).
+    joins(:affiliate).merge(Affiliate.active)
 
   def label
     "#{affiliate.name}:#{user.email}"
