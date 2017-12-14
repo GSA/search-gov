ca9b082fb9c5094dda0dc8a87900809ed67751b2
Martha Thompson <martha@rrsoft.co>
2017-12-14 12:40:43 -0500

    [#153599780] add error logging for site cloning (#395)

--- app/controllers/sites/clones_controller.rb
+++ app/controllers/sites/clones_controller.rb
@@ -9,6 +9,7 @@ class Sites::ClonesController < Sites::SetupSiteController
 
     redirect_to site_path(cloned_instance), flash: { success: "Site '#{@site.name}' has been cloned as '#{cloned_instance.name}'" }
   rescue StandardError => e
+    Rails.logger.error "Site cloning failed for affiliate #{@site.id}: #{e.message}\n#{e.backtrace.join("\n")}"
     flash.now[:error] = e.message
     render action: :new
   end
