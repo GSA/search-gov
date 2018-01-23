714d7e67914f193816e57393d5d53c6ef78d2c88
Nick Marden <nick@marden.org>
2018-01-23 17:23:31 -0500

    [#140414991] Get Keen data display working again (#418)

--- config/initializers/keen.rb
+++ config/initializers/keen.rb
@@ -1,4 +1,11 @@
-Keen.project_id = '***REMOVED***'
-Keen.master_key = '***REMOVED***'
-Keen.write_key = '***REMOVED***8bd37cebce5b7546cd9050da6c9361ff19b99eeea2ca6322995e59cfcb6c184a90c86a5423069a94'
-Keen.read_key = '***REMOVED***88263804f3cd11c61abfb4bdd6c464a7566ef44b1ff1a34b9b6632b1e63d2231081c9fa0b00d78e'
+require 'keen'
+
+config = (YAML.load_file("#{Rails.root}/config/keen.yml") || {})[Rails.env]
+
+if config.try(:keys).try(:any?)
+  %w(project_id master_key write_key read_key).each do |field|
+    Keen.send(:"#{field}=", config[field]) if config[field]
+  end
+else
+  STDERR.puts "No Keen configuration provided!"
+end
