1e430817cb1f882943d48ebb9bf8a2df5110d6b4
Martha Thompson <martha@rrsoft.co>
2018-02-21 11:15:06 -0800

    randomize spec order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -58,6 +58,18 @@ RSpec.configure do |config|
   # do NOT play nicely with our custom, string primary key 'code' in the language table
   config.global_fixtures = [:languages]
 
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = :random
+
+  # Seed global randomization in this process using the `--seed` CLI option.
+  # Setting this allows you to use `--seed` to deterministically reproduce
+  # test failures related to randomization by passing the same `--seed` value
+  # as the one that triggered the failure.
+  Kernel.srand config.seed
+
   config.before(:suite) do
     FileUtils.mkdir_p(File.join(Rails.root.to_s, 'tmp'))
 
