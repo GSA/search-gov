826ca62921584daa258452a2996c3c5c33d606d7
MothOnMars <mothonmars@gmail.com>
2018-03-14 14:24:57 -0700

    squashme: log lines

--- app/models/sitemap_indexer.rb
+++ app/models/sitemap_indexer.rb
@@ -8,6 +8,7 @@ class SitemapIndexer
 
   def index
     sitemap = Sitemaps.discover(domain)
+    Rails.logger.info "[Searchgov SitemapIndexer] #{log_info.merge(sitemap_entries_found: sitemap.entries.count).to_json}"
     sitemap.entries.each{ |entry| process_entry(entry) }
   end
 
@@ -17,11 +18,19 @@ class SitemapIndexer
     searchgov_url = SearchgovUrl.find_or_create_by(url: entry.loc.to_s)
     if !searchgov_url.fetched? || outdated?(entry.lastmod, searchgov_url.last_crawled_at)
       searchgov_url.fetch
+      Rails.logger.info "[Searchgov SitemapIndexer] #{log_info.merge(sitemap_entry_updated: searchgov_url.url).to_json}"
       sleep(delay)
     end
   end
 
   def outdated?(lastmod, last_crawled_at)
     lastmod && lastmod > last_crawled_at
+  end
+
+  def log_info
+    {
+      time: Time.now.utc.to_formatted_s(:db),
+      domain: domain
+    }
   end
 end
