a5e213f8acaab4924cd021f37d8ed21dcf9da5e6
Martha Thompson <martha@rrsoft.co>
2018-05-16 14:10:32 -0700

    [#157634748] reject sitemap URLs from another domain (#60)

--- app/models/sitemap_indexer.rb
+++ app/models/sitemap_indexer.rb
@@ -8,12 +8,18 @@ class SitemapIndexer
   end
 
   def index
-    sitemap = Sitemaps.discover(domain)
-    Rails.logger.info "[Searchgov SitemapIndexer] #{log_info.merge(sitemap_entries_found: sitemap.entries.count).to_json}"
-    sitemap.entries.each{ |entry| process_entry(entry) }
+    Rails.logger.info "[Searchgov SitemapIndexer] #{log_info.merge(sitemap_entries_found: sitemap_entries.count).to_json}"
+    sitemap_entries.each{ |entry| process_entry(entry) }
   end
 
   private
+
+  def sitemap_entries
+    # Eventually we might add an option to the Sitemaps gem to limit the URLS
+    # to those strictly adhering to the sitemap protocol, but this should suffice for now
+    # https://www.pivotaltracker.com/story/show/157485118
+    @sitemap_entries ||= Sitemaps.discover(domain) { |entry| entry.loc.host == domain }.entries
+  end
 
   def process_entry(entry)
     begin
