8edc049d80f7c70d1ea030ce26c4c09bd3431a1c
Martha Thompson <martha@rrsoft.co>
2018-05-22 08:51:39 -0700

    [#157752890] send Resque errors to Airbrake (#64)

--- config/initializers/load_resque.rb
+++ config/initializers/load_resque.rb
@@ -1,4 +1,9 @@
-require 'resque/failure/airbrake'
+require 'airbrake/resque'
+require 'resque/failure/multiple'
+require 'resque/failure/redis'
 
 config = Rails.application.secrets.system_redis
 Resque.redis = [config['host'], config['port']].join(':')
+
+Resque::Failure::Multiple.classes = [Resque::Failure::Redis, Resque::Failure::Airbrake]
+Resque::Failure.backend = Resque::Failure::Multiple
