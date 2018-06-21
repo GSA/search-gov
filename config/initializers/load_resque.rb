ee0da012e67b671493485c7f70fa78fc0c7780cb
MothOnMars <mothonmars@gmail.com>
2018-06-21 08:53:32 -0700

    [#158531268] raise default Resque timeout

--- config/initializers/load_resque.rb
+++ config/initializers/load_resque.rb
@@ -1,9 +1,12 @@
 require 'airbrake/resque'
 require 'resque/failure/multiple'
 require 'resque/failure/redis'
+require 'resque-timeout'
 
 config = Rails.application.secrets.system_redis
 Resque.redis = [config['host'], config['port']].join(':')
 
 Resque::Failure::Multiple.classes = [Resque::Failure::Redis, Resque::Failure::Airbrake]
 Resque::Failure.backend = Resque::Failure::Multiple
+
+Resque::Plugins::Timeout.timeout = 1.hour
