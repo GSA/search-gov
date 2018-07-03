99c161e947bc275261bcbc631c6613482af93190
Ethan Ewing <ethanewing52@gmail.com>
2018-07-03 16:43:54 -0400

    sgov_url_fetcher_job specs now passing

--- spec/jobs/searchgov_url_fetcher_job.rb
+++ spec/jobs/searchgov_url_fetcher_job.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe SearchgovUrlFetcherJob do
   subject(:perform) { SearchgovUrlFetcherJob.perform_now(args[:searchgov_url]) }
-  let!(:searchgov_url) { SearchgovUrl.create(url: 'https://agency.gov/') }
+  let!(:searchgov_domain) { SearchgovDomain.create(domain: 'agency.gov', status: '200') }
+  let(:searchgov_url) { SearchgovUrl.create(url: 'https://agency.gov/') }
   let(:args) do
     { searchgov_url: searchgov_url }
   end
@@ -16,15 +17,8 @@ describe SearchgovUrlFetcherJob do
     end
 
     it 'fetches a searchgov_url' do
-      # byebug
       perform
       expect(searchgov_url.last_crawl_status).to_not be(nil)
     end
   end
-
-  # it 'fetches a searchgov_url' do
-  #   perform
-  #   expect(searchgov_url.last_crawl_status).to_not be(nil)
-  # end
-
 end
