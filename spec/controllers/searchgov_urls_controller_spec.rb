3c80e46bc91da5b1b1a07dd20e3e57da00ac88c8
Ethan Ewing <ethanewing52@gmail.com>
2018-07-12 16:57:51 -0400

    all passing

--- spec/controllers/searchgov_urls_controller_spec.rb
+++ spec/controllers/searchgov_urls_controller_spec.rb
@@ -1,26 +1,27 @@
 require 'spec_helper'
 
 describe Admin::SearchgovUrlsController do
-  # let!(:searchgov_domain) { SearchgovDomain.create(domain: 'agency.gov')}
+  fixtures :users
+
+  let(:admin) { users(:affiliate_admin) }
   let(:searchgov_url) { SearchgovUrl.create(url: 'agency.gov/test') }
   let(:params) do
     { association: 'searchgov_urls', parent_scaffold: 'admin/searchgov_domains',
     searchgov_domain_id: searchgov_url.searchgov_domain.id, id: searchgov_url.id }
   end
 
-  describe '#fetch' do
-    # before do
-    #   # post :fetch, association: 'searchgov_urls', parent_scaffold: 'admin/searchgov_domains',
-    #   # searchgov_domain_id: searchgov_url.searchgov_domain.id, id: searchgov_url.id
-    #   # post "/admin/searchgov_domains/#{searchgov_url.searchgov_domain.id}/searchgov_urls/#{searchgov_url.id}/fetch?_method=post&association=searchgov_urls&parent_scaffold=admin%2Fsearchgov_domains&searchgov_domain_id=#{searchgov_url.searchgov_domain.id}"
-    #   post :fetch, params
-    #   p response.body
-    # end
+  context 'when logged in as an admin' do
+    describe '#fetch' do
+      before do
+        activate_authlogic
+        UserSession.create admin
+        allow(SearchgovDomainPreparerJob).to receive(:perform_later)
+      end
 
-    it 'enqueues a searchgov_url_fetcher_job to the searchgov queue' do
-      # ActiveJob::Base.queue_adapter = :test
-      expect{ post :fetch, params }.
-        to have_enqueued_job(SearchgovUrlFetcherJob).on_queue("searchgov")
+      it 'enqueues a searchgov_url_fetcher_job to the searchgov queue' do
+        expect{ post :fetch, params }.
+          to have_enqueued_job(SearchgovUrlFetcherJob).on_queue("searchgov")
+      end
     end
   end
 end
