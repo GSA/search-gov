481a18f57e8a4d116a439833bcb464c4829857be
Ethan Ewing <ethanewing52@gmail.com>
2018-07-13 13:46:31 -0400

    uses shared example; test arguments of serachgov_url_fetcher_job

--- spec/controllers/searchgov_urls_controller_spec.rb
+++ spec/controllers/searchgov_urls_controller_spec.rb
@@ -4,23 +4,19 @@ describe Admin::SearchgovUrlsController do
   fixtures :users
 
   let(:admin) { users(:affiliate_admin) }
-  let(:searchgov_url) { SearchgovUrl.create(url: 'agency.gov/test') }
+  let(:searchgov_url) { SearchgovUrl.create!(url: 'agency.gov/test') }
   let(:params) do
     { association: 'searchgov_urls', parent_scaffold: 'admin/searchgov_domains',
     searchgov_domain_id: searchgov_url.searchgov_domain.id, id: searchgov_url.id }
   end
 
-  context 'when logged in as an admin' do
+  include_context 'super admin logged in' do
     describe '#fetch' do
-      before do
-        activate_authlogic
-        UserSession.create admin
-        allow(SearchgovDomainPreparerJob).to receive(:perform_later)
-      end
 
       it 'enqueues a searchgov_url_fetcher_job to the searchgov queue' do
         expect{ post :fetch, params }.
-          to have_enqueued_job(SearchgovUrlFetcherJob).on_queue("searchgov")
+          to have_enqueued_job(SearchgovUrlFetcherJob).on_queue("searchgov").
+          with(searchgov_url: searchgov_url)
       end
     end
   end
