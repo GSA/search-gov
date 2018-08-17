d22cd446808b537a9ad5b1be2be692c18a4764d7
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2018-08-17 15:37:14 -0700

    [#158500373] pass conditions to SearchgovDomain#index_urls

--- spec/jobs/searchgov_domain_indexer_job_spec.rb
+++ spec/jobs/searchgov_domain_indexer_job_spec.rb
@@ -7,8 +7,10 @@ describe SearchgovDomainIndexerJob do
     SearchgovDomain.create(domain: 'agency.gov', status: '200', activity: 'indexing')
   end
   let(:args) do
-    { searchgov_domain: searchgov_domain, delay: 10 }
+    { searchgov_domain: searchgov_domain, delay: 10, start: start, conditions: fetch_required }
   end
+  let(:start) { 1.minute.ago.to_s }
+  let(:fetch_required) { 'last_crawled_at IS NULL OR lastmod > last_crawled_at' }
 
   it_behaves_like 'a searchgov job'
 
@@ -32,14 +34,21 @@ describe SearchgovDomainIndexerJob do
 
       it 'enqueues the next job after the specified delay' do
         expect{ perform }.to have_enqueued_job(SearchgovDomainIndexerJob).
-          with(searchgov_domain: searchgov_domain, delay: 10).at(10.seconds.from_now)
+          with(
+               searchgov_domain: searchgov_domain,
+               delay: 10,
+               conditions: fetch_required,
+               start: start
+              ).at(10.seconds.from_now)
       end
     end
   end
 
   context 'when a domain has outdated urls' do
     let!(:searchgov_url) do
-      SearchgovUrl.create!(url: 'https://agency.gov/', last_crawled_at: 1.week.ago, lastmod: 1.day.ago)
+      SearchgovUrl.create!(url: 'https://agency.gov/',
+                           last_crawled_at: 1.week.ago,
+                           lastmod: 1.day.ago)
     end
 
     it 'fetches the url' do
@@ -54,6 +63,61 @@ describe SearchgovDomainIndexerJob do
 
     it 'does not enqueue subsequent jobs' do
       expect{ perform }.not_to have_enqueued_job(SearchgovDomainIndexerJob)
+    end
+  end
+
+  context 'when indexing only URLs meeting specific conditions' do
+    let!(:ok_url) do
+      SearchgovUrl.create!(url: 'https://agency.gov/',
+                           last_crawled_at: 1.week.ago,
+                           last_crawl_status: 'OK')
+    end
+    let!(:failed_url) do
+      SearchgovUrl.create!(url: 'https://agency.gov/404',
+                           last_crawled_at: 1.week.ago,
+                           last_crawl_status: '404')
+    end
+    let(:args) do
+      {
+        searchgov_domain: searchgov_domain,
+        delay: 10,
+        conditions: { last_crawl_status: '404' },
+        start: start
+      }
+    end
+
+    it 'fetches the specified URLs' do
+      expect{ perform }.to change{ failed_url.reload.last_crawled_at }
+    end
+
+    it 'does not fetch the URLs that do not meet the conditions ' do
+      expect{ perform }.not_to change{ ok_url.reload.last_crawled_at }
+    end
+
+    context 'when multiple URLs need to be fetched' do
+      let!(:another_404) do
+        SearchgovUrl.create!(url: 'https://agency.gov/404_b',
+                             last_crawled_at: 1.week.ago,
+                             last_crawl_status: '404')
+      end
+
+      it 'enqueues another job' do
+        expect{ perform }.to have_enqueued_job(SearchgovDomainIndexerJob).
+          with(searchgov_domain: searchgov_domain,
+               delay: 10,
+               conditions: { last_crawl_status: '404' },
+               start: start)
+      end
+    end
+
+    context 'when re-fetching does not change the record conditions' do
+      before do
+        stub_request(:get, failed_url.url).to_return(status: 404)
+      end
+
+      it 'does not enqueue another job' do
+        expect{ perform }.not_to have_enqueued_job(SearchgovDomainIndexerJob)
+      end
     end
   end
 end
