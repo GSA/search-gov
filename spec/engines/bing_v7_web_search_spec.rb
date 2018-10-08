6f83e9590651718128faf6fd4950af9e3bb2e14c
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2018-10-08 13:48:27 -0700

    add spec for Bing locale bug

--- spec/engines/bing_v7_web_search_spec.rb
+++ spec/engines/bing_v7_web_search_spec.rb
@@ -3,4 +3,19 @@ describe BingV7WebSearch do
 
   it_behaves_like 'a Bing search'
   it_behaves_like 'a web search engine'
+
+  # This was an edge-case bug on Bing's side:
+  # https://www.pivotaltracker.com/story/show/160807845
+  # Move this spec to shared bing_search_behavior after Bing V5 and Azure classes are removed
+  context 'when a Spanish site runs a search that could return location results' do
+    let(:params) do
+      { query: 'hotel (site:search.gov)' }
+    end
+
+    before { allow(Language).to receive(:bing_market_for_code).and_return('es-US') }
+
+    it 'returns no results' do
+      expect(described_class.new(params).execute_query.total).to eq 0
+    end
+  end
 end
