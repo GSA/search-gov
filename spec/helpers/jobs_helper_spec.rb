446977027ec26c2097103cf57a9a60dac504ddee
peggles2 <peggles2@users.noreply.github.com>
2018-11-09 15:32:17 -0500

    SRCH-94 - fix more jobs search to not return 404 (#168)
    * SRCH-94 - fix more jobs search to not return 404
    
    * fix cucumber spec error
    
    * fix the link to remove p=1
    
    * pass &a= as the seperator for joined_organization_codes
    
    * add hp=public so only public search results

--- spec/helpers/jobs_helper_spec.rb
+++ spec/helpers/jobs_helper_spec.rb
@@ -44,7 +44,7 @@ describe JobsHelper do
         allow(search).to receive_message_chain(:affiliate, :agency).and_return(nil)
         expect(helper).to receive(:job_link_with_click_tracking).with(
             'More federal job openings on USAJobs.gov',
-            'https://www.usajobs.gov/JobSearch/Search/GetResults?PostingChannelID=USASearch',
+            'https://www.usajobs.gov/Search/Results?hp=public',
             search.affiliate, 'gov', -1, nil)
         helper.legacy_link_to_more_jobs(search)
       end
@@ -67,7 +67,7 @@ describe JobsHelper do
       it 'should render an organization specific link to usajobs.gov' do
         expect(helper).to receive(:job_link_with_click_tracking).with(
             'More GSA job openings on USAJobs.gov',
-            'https://www.usajobs.gov/JobSearch/Search/GetResults?organizationid=GS;HI&PostingChannelID=USASearch&ApplicantEligibility=all',
+            'https://www.usajobs.gov/Search/Results?a=GS&a=HI&hp=public',
             search.affiliate, 'gov', -1, nil)
         helper.legacy_link_to_more_jobs(search)
       end
@@ -79,7 +79,7 @@ describe JobsHelper do
         it 'should render an organization specific link to usajobs in Spanish' do
           expect(helper).to receive(:job_link_with_click_tracking).with(
               'Más trabajos en GSA en USAJobs.gov',
-              'https://www.usajobs.gov/JobSearch/Search/GetResults?organizationid=GS;HI&PostingChannelID=USASearch&ApplicantEligibility=all',
+              'https://www.usajobs.gov/Search/Results?a=GS&a=HI&hp=public',
               search.affiliate, 'gov', -1, nil)
           helper.legacy_link_to_more_jobs(search)
         end
