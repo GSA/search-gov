f5c3c7f44b7e2640f1a642baf1d5f0b5acbc4ca4
Parissa Eggleston <parissa@Parissas-MBP.home>
2018-11-12 16:31:09 -0500

    migration file for enqueued for reindex to searachgov_url

---
+++ db/migrate/20181109212904_add_enqueued_for_reindex_to_searchgov_urls.rb
@@ -0,0 +1,8 @@
+class AddEnqueuedForReindexToSearchgovUrls < ActiveRecord::Migration
+  def change
+    add_column :searchgov_urls,
+               :enqueued_for_reindex,
+               :boolean,
+                default: false
+  end
+end
