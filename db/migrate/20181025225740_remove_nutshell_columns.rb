633d7c092f4e4af4aadc0695df0557d754fb5050
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2018-11-29 13:00:16 -0800

    fix remove_index syntax

--- db/migrate/20181025225740_remove_nutshell_columns.rb
+++ db/migrate/20181025225740_remove_nutshell_columns.rb
@@ -1,6 +1,6 @@
 class RemoveNutshellColumns < ActiveRecord::Migration
   def change
-    remove_index :users, :nutshell_id
+    remove_index :users, column: :nutshell_id
     remove_column :users, :nutshell_id, :integer
     remove_column :affiliates, :nutshell_id, :integer
   end
