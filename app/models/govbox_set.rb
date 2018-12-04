4970a011b0663ce37dc8e3b10945fc76af330c66
peggles2 <peggles2@users.noreply.github.com>
2018-12-04 10:58:52 -0500

    SRCH-202 - scrub job keywords from query (#187)
    * SRCH-179 - Add a boolean column to the searchgov_urls
    
    * Feature/bool db column urls (#172)
    
    * update structure.sql
    
    * migration file for enqueued for reindex to searachgov_url
    
    * update migration, update test cases
    
    * migrated database
    
    * SRCH-202 - scrub job keywords from query
    
    * update yml to get key
    
    * update jobs.rb to have options in the jobs module
    
    * change job_scrub to scrub_keyword, squish instead of strip, delete instead of gsub
    
    * update jobs_spec
    
    * remove scrub_keyword
    
    * changes made to tighten up the regex
    
    * fix word boundaries, update tests

--- app/models/govbox_set.rb
+++ app/models/govbox_set.rb
@@ -104,20 +104,18 @@ class GovboxSet
 
   def init_jobs
     if @affiliate.jobs_enabled?
-      job_results = Jobs.search(build_jobs_search_options)&.search_result&.search_result_items
+      job_results = Jobs.search({
+        query: @query,
+        organization_codes: @affiliate.agency&.joined_organization_codes,
+        location_name: @geoip_info&.location_name,
+        results_per_page: 10
+      })&.search_result&.search_result_items
+
       if job_results.present?
         @jobs = JobResultsPostProcessor.new(results: job_results)&.post_processed_results
         @modules << 'JOBS'
       end
     end
-  end
-
-  def build_jobs_search_options
-    jobs_options = { Keyword: @query, ResultsPerPage: 10 }
-    org_hash = { Organization: @affiliate.agency&.joined_organization_codes }
-    jobs_options.merge!(org_hash)
-    jobs_options.merge!(LocationName: @geoip_info&.location_name)
-    jobs_options.compact
   end
 
   def init_federal_register_documents
