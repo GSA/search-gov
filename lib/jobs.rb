66ca81b248ce06116d2f19a8cfa6e7600b1facc5
Parissa Eggleston <parissa@Parissas-MBP.fios-router.home>
2018-12-27 16:27:09 -0500

    update

--- lib/jobs.rb
+++ lib/jobs.rb
@@ -1,18 +1,26 @@
 module Jobs
   SIMPLE_SEARCHES = '(job|employment|internship)s?'
-  JOB_RELATED_KEYWORDS = '((position|opening|posting|job|employment|intern(ship)?|seasonal|trabajo|puesto|empleo|vacante)s?|(opportunit|vacanc)(y|ies))|(posicion|ocupacion|oportunidad|federal)(es)?|gobierno'
-  SCRUB_KEYWORDS = JOB_RELATED_KEYWORDS.remove(/\|intern\(ship\)|\|seasonal|\|federal\||gobierno/)
-  SIMPLE_SINGULARS = %w{
-    statistic number level rate description trend growth projection survey forecast figure report verification record
-    authorization card classification form hazard poster fair board outlook grant funding factor other cut
+  JOB_RELATED_KEYWORDS = '((position|opening|posting|job|employment|' \
+                         'intern(ship)?|seasonal|trabajo|puesto|empleo|vacante+?)(s\b|\b)|' \
+                         '(opportunit|vacanc)(y|ies))|(posicion|ocupacion|' \
+                         'oportunidad|federal)(es)?|gobierno'.freeze
+  SCRUB_KEYWORDS = JOB_RELATED_KEYWORDS.
+    remove(/\|intern\(ship\)|\|seasonal|\|federal\||gobierno/).
+    freeze
+  SIMPLE_SINGULARS = %w[
+    statistic number level rate description trend growth
+    projection survey forecast figure report verification record
+    authorization card classification form hazard poster fair board
+    outlook grant funding factor other cut
     application
-  }
-  BLOCKED_PHRASES = '(job|employment) (contract|law|training|safety)s?'
-  BLOCKED_KEYWORDS = 'data|at will|equal|status|eligibility|analysis|300 log|delayed'+
-    '|(histor)(y|ies)'+
-    "|#{Date.current.year.to_s}"+
-    "|#{BLOCKED_PHRASES}"+
-    "|(#{SIMPLE_SINGULARS.join('|')})s?"
+  ].freeze
+  BLOCKED_PHRASES = '(job|employment) (contract|law|training|safety)s?'.freeze
+  BLOCKED_KEYWORDS =
+    'data|at will|equal|status|eligibility|analysis|300 log|delayed' \
+    '|(histor)(y|ies)' \
+    "|#{Date.current.year.to_s}" \
+    "|#{BLOCKED_PHRASES}" \
+    "|(#{SIMPLE_SINGULARS.join('|')})s?".freeze
   SEARCH_RADIUS = 75
 
   def self.establish_connection!
@@ -43,7 +51,9 @@ module Jobs
   end
 
   def self.query_eligible?(query)
-    query =~ /\b#{JOB_RELATED_KEYWORDS}\b/i && !(query =~ /\b#{BLOCKED_KEYWORDS}\b/i) && !(query =~ /["():]|^-| -\S+/)
+    ((query =~ /\b#{JOB_RELATED_KEYWORDS}\b/i) && \
+     (query !~ /\b#{BLOCKED_KEYWORDS}\b/i) && \
+     (query !~ /["():]|^-| -\S+/))
   end
 
   def self.params(options)
