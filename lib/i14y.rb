3068e6683d204ceb8eb5302d60c3f6d64a1dcfe0
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2019-01-11 09:58:20 -0800

    SRCH-294 Revert "Merge pull request #222 from MothOnMars/cache_i14y_294_hotfix"
    This reverts commit 9ddb84841b5ae83a4f440fe74926385c5302a7f7, reversing
    changes made to dc5b55a86340a30ed5f5b345632a0b73e742315c.

--- lib/i14y.rb
+++ lib/i14y.rb
@@ -1,14 +1,11 @@
-# frozen_string_literal: true
-
-# I14y connection configuration
 module I14y
   def self.establish_connection!(user: admin_user, password: admin_password)
-    Faraday.new(host) do |conn|
-      conn.request(:json)
-      conn.response(:mashify)
-      conn.response(:json)
-      conn.use(:instrumentation)
-      conn.adapter(:net_http_persistent)
+    Faraday.new host do |conn|
+      conn.request :json
+      conn.response :mashify
+      conn.response :json
+      conn.use :instrumentation
+      conn.adapter :net_http_persistent
       conn.basic_auth(user, password)
     end
   end
@@ -23,10 +20,5 @@ module I14y
 
   def self.admin_password
     Rails.application.secrets.i14y['admin_password']
-  end
-
-  def self.cached_connection
-    conn = CachedSearchApiConnection.new('i14y', host, I14Y_CACHE_DURATION)
-    conn.tap { |conn| conn.basic_auth(admin_user, admin_password) }
   end
 end
