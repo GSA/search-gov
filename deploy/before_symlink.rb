28f648a7764f511ce5d315ae98e89b109cd7a1b5
Parissa Eggleston <parissa@Parissas-MBP.localdomain>
2019-02-04 14:42:20 -0500

    add extra space after comment

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -17,12 +17,12 @@ TTHC = "?"*32
 # need to be available without digest fingerprints in their
 # filenames - assets that live "in the wild" and can't be
 # updated whenever our asset fingerprints change.
-#run <<COMPILE
-#  cd #{release_path} && \
-#  RAILS_ENV=#{rails_env} bundle exec rake assets:precompile && \
-#  cd #{release_path}/public/assets && \
-#  for js in sayt_loader_libs sayt_loader stats; do cp ${js}-#{TTHC}.js ${js}.js && cp ${js}-#{TTHC}.js.gz ${js}.js.gz; done && \
-#  for css in sayt; do cp ${css}-#{TTHC}.css ${css}.css && cp ${css}-#{TTHC}.css.gz ${css}.css.gz; done && \
-#  for png in bootstrap/glyphicons-halflings bootstrap/glyphicons-halflings-white; do cp ${png}-#{TTHC}.png ${png}.png; done && \
-#  find . -type f -perm 600 | xargs --no-run-if-empty chmod 644
-#COMPILE
+# run <<COMPILE
+#   cd #{release_path} && \
+#   RAILS_ENV=#{rails_env} bundle exec rake assets:precompile && \
+#   cd #{release_path}/public/assets && \
+#   for js in sayt_loader_libs sayt_loader stats; do cp ${js}-#{TTHC}.js ${js}.js && cp ${js}-#{TTHC}.js.gz ${js}.js.gz; done && \
+#   for css in sayt; do cp ${css}-#{TTHC}.css ${css}.css && cp ${css}-#{TTHC}.css.gz ${css}.css.gz; done && \
+#   for png in bootstrap/glyphicons-halflings bootstrap/glyphicons-halflings-white; do cp ${png}-#{TTHC}.png ${png}.png; done && \
+#   find . -type f -perm 600 | xargs --no-run-if-empty chmod 644
+# COMPILE
