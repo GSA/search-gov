03467dcfa0a1a3464e3733f5277113ce58ae8630
Parissa Eggleston <parissa@Parissas-MBP.localdomain>
2019-02-04 14:04:54 -0500

    add extra space

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -104,6 +104,7 @@ describe UsersController do
       include_context 'approved user logged in'
 
       it { is_expected.to permit(*permitted_params).for(:update, params: { user: update_params }) }
+
       context 'when changing the password' do
         let(:update_params) do
           { 'current_password': current_user.password,
