4b2db4edb7482427fe8782520e2ebf60af2aa476
Parissa Eggleston <parissa@Parissas-MacBook-Pro.local>
2019-02-27 11:25:13 -0500

    SRCH-418 - include_in_all is not allowed for indices and is deprecated

--- app/models/elastic_tweet.rb
+++ app/models/elastic_tweet.rb
@@ -6,13 +6,14 @@ class ElasticTweet
   self.mappings = {
     index_type => {
       dynamic: :strict,
-      _analyzer: { path: "language" },
+      _analyzer: { path: 'language' },
       properties: {
-        language: { type: "string", index: :not_analyzed },
+        language: { type: 'string', index: :not_analyzed },
         twitter_profile_id: { type: 'long' },
         tweet_text: { type: 'string', term_vector: 'with_positions_offsets' },
         published_at: { type: 'date' },
-        id: { type: 'long', index: :not_analyzed, include_in_all: false } }
+        id: { type: 'long', index: :not_analyzed }
+      }
     }
   }
 
