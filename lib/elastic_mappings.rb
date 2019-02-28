49237e2a7cd30e12a5b2fd30fe83442f6cd85287
Parissa Eggleston <parissa@Parissas-MBP.fios-router.home>
2019-02-28 17:33:05 -0500

    SRCH-416-update queries to use text/keywords instead of string

--- lib/elastic_mappings.rb
+++ lib/elastic_mappings.rb
@@ -14,7 +14,7 @@ module ElasticMappings
       publish_start_on: { type: 'date', format: 'YYYY-MM-dd' },
       publish_end_on: { type: 'date', format: 'YYYY-MM-dd', null_value: '9999-12-31' },
       title: { type: 'text', term_vector: 'with_positions_offsets' },
-      match_keyword_values_only: { type: 'boolean', index: :not_analyzed, include_in_all: false, null_value: 'false' },
+      match_keyword_values_only: { type: 'boolean', index: :not_analyzed, null_value: 'false' },
       keyword_values: ElasticSettings::KEYWORD }
   ).freeze
 
