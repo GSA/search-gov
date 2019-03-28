7d8ddbda137839e0846b4c49558e5ab4f1ce8842
Parissa Eggleston <parissa@Parissas-MBP.fios-router.home>
2019-03-28 19:20:56 -0400

    update keyword to bool

--- lib/elastic_mappings.rb
+++ lib/elastic_mappings.rb
@@ -13,7 +13,7 @@ module ElasticMappings
       publish_start_on: { type: 'date', format: 'YYYY-MM-dd' },
       publish_end_on: { type: 'date', format: 'YYYY-MM-dd', null_value: '9999-12-31' },
       title: { type: 'text', term_vector: 'with_positions_offsets' },
-      match_keyword_values_only: { type: 'keyword',
+      match_keyword_values_only: { type: 'boolean',
                                    null_value: 'false' },
       keyword_values: ElasticSettings::KEYWORD }
   ).freeze
