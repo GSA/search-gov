39a9a64ac24bbe2dca5dcd4d97870fd2f403d93f
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2019-06-07 09:25:01 -0700

    combine presence validations

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class Language < ActiveRecord::Base
-  validates :code, presence: true
-  validates :name, presence: true
+  validates :code, :name, presence: true
 
   validates_uniqueness_of :code, case_sensitive: false
 
