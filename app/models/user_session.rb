c961c7cbc8cc5e2293ebe65834b4e618bfeb59ee
Parissa <parissa.eggleston@gmail.com>
2019-06-21 13:01:41 -0400

    fix cucumber error

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -2,7 +2,7 @@ class UserSession < Authlogic::Session::Base
   allow_http_basic_auth false
   consecutive_failed_logins_limit 10
   failed_login_ban_for 30.minutes
-  generalize_credentials_error_messages ''
+  generalize_credentials_error_messages 'These credentials are not recognized as valid for accessing Search.gov. Please contact search@support.digitalgov.gov if you believe this is in error.'
   logout_on_timeout true
 
   def persisted?
