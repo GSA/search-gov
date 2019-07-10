cfb685603f02f4359d0e1e6c520478eda43b5306
Parissa <parissa.eggleston@gmail.com>
2019-07-10 12:21:42 -0400

    fix space

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -162,8 +162,8 @@ describe User do
     let(:non_affiliate) { users(:developer) }
     let(:not_active_user) { users(:not_active_user) }
     let(:active_user) { users(:active_user) }
-    let(:never_active_user) { users(:never_active_user)}
-    let(:new_non_active_user) { users(:new_non_active_user)}
+    let(:never_active_user) { users(:never_active_user) }
+    let(:new_non_active_user) { users(:new_non_active_user) }
 
     describe '.approved_affiliate' do
       subject(:approved_affiliate) { User.approved_affiliate }
