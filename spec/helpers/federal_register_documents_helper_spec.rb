732ea4c6c774e3316a04ec037163a3db9f452a49
Parissa <parissa.eggleston@gmail.com>
2019-07-31 15:02:51 -0400

    SRCH-750 - add test for federal register document info

--- spec/helpers/federal_register_documents_helper_spec.rb
+++ spec/helpers/federal_register_documents_helper_spec.rb
@@ -1,6 +1,34 @@
 require 'spec_helper'
 
 describe FederalRegisterDocumentsHelper do
+  describe '#federal_register_document_info' do
+    subject(:federal_register_document_info) do
+      helper.federal_register_document_info(document)
+    end
+
+    let(:document) do
+      mock_model(
+        FederalRegisterDocument,
+        document_type: 'Notice',
+        publication_date: 'Mon, 09 Jun 2014'.to_date,
+        contributing_agency_names: ['Internal Revenue Service',
+                                    'International Trade Administration',
+                                    'National Oceanic and Atmospheric Administration']
+      )
+    end
+    let(:result) do
+      <<~HTML
+        A <span>Notice</span> by the <span>Internal
+        Revenue Service</span>, the <span>International
+        Trade Administration</span> and the <span>National
+        Oceanic and Atmospheric Administration</span>
+        posted on <span>June 9, 2014</span>.
+      HTML
+    end
+
+    it { is_expected.to eq result }
+  end
+
   describe '#federal_register_document_comment_period' do
     let(:document) { mock_model(FederalRegisterDocument) }
 
