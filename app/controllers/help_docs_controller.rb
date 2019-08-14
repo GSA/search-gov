3ce22a5b5554032bb1295fa7960df0d2da8b82e4
Parissa <parissa.eggleston@gmail.com>
2019-08-14 13:41:56 -0400

    update validations to use \A instead of ^

--- app/controllers/help_docs_controller.rb
+++ app/controllers/help_docs_controller.rb
@@ -10,7 +10,7 @@ class HelpDocsController < ApplicationController
   private
 
   def require_usasearch_url_param
-    unless help_docs_params[:url] =~ %r[^https?://search\.gov/.+]i
+    unless help_docs_params[:url] =~ %r[\Ahttps?://search\.gov/.+]i
       redirect_to(Rails.application.secrets.organization['page_not_found_url'])
     end
   end
