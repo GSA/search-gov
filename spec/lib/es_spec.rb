324af1f7ab18763139baf57ea91c5b974bab9251
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2019-08-23 12:35:15 -0700

    clean up double quotes and "should"s

--- spec/lib/es_spec.rb
+++ spec/lib/es_spec.rb
@@ -1,16 +1,16 @@
 require 'spec_helper'
 
 describe ES do
-  context "when working in ES submodules" do
+  context 'when working in ES submodules' do
     let(:elk_objs) { Array.new(3, ES::ELK.client_reader) }
     let(:ci_objs) { Array.new(3, ES::ELK.client_reader) }
 
-    describe ".client_reader" do
-      it "should return a different object in different submodules" do
+    describe '.client_reader' do
+      it 'returns a different object in different submodules' do
         expect(ES::ELK.client_reader).to_not eq(ES::CustomIndices.client_reader)
       end
 
-      it "should return the same object given successive invocations" do
+      it 'returns the same object given successive invocations' do
         2.times do |i|
           expect(elk_objs[i]).to eq(elk_objs[i+1])
           expect(ci_objs[i]).to eq(ci_objs[i+1])
@@ -18,12 +18,12 @@ describe ES do
       end
     end
 
-    describe ".client_writers" do
-      it "should return a different object in different submodules" do
+    describe '.client_writers' do
+      it 'returns a different object in different submodules' do
         expect(ES::ELK.client_writers).to_not eq(ES::CustomIndices.client_writers)
       end
 
-      it "should return the same object given successive invocations" do
+      it 'returns the same object given successive invocations' do
         2.times do |i|
           expect(elk_objs[i]).to eq(elk_objs[i+1])
           expect(ci_objs[i]).to eq(ci_objs[i+1])
@@ -32,15 +32,15 @@ describe ES do
     end
   end
 
-  context "when working in ES::ELK submodule" do
-    describe ".client_reader" do
-      it 'should use the value from the secrets.yml analytics[elasticsearch][reader] entry' do
+  context 'when working in ES::ELK submodule' do
+    describe '.client_reader' do
+      it 'uses the value from the secrets.yml analytics[elasticsearch][reader] entry' do
         expect(ES::ELK.client_reader.transport.hosts.first[:host]).to eq(Rails.application.secrets.analytics['elasticsearch']['reader'])
       end
     end
 
-    describe ".client_writers" do
-      it 'should use the value(s) from the secrets.yml analytics[elasticsearch][writers] entry' do
+    describe '.client_writers' do
+      it 'uses the value(s) from the secrets.yml analytics[elasticsearch][writers] entry' do
         count = Rails.application.secrets.analytics['elasticsearch']['writers'].count
         expect(ES::ELK.client_writers.size).to eq(count)
         count.times do |i|
@@ -50,16 +50,16 @@ describe ES do
     end
   end
 
-  describe "when working in ES::CustomIndices submodule" do
-    describe ".client_reader" do
-      it 'should use the value from the secrets.yml custom_indices[elasticsearch][reader] entry' do
+  describe 'when working in ES::CustomIndices submodule' do
+    describe '.client_reader' do
+      it 'uses the value from the secrets.yml custom_indices[elasticsearch][reader] entry' do
         expect(ES::CustomIndices.client_reader.transport.hosts.first[:host]).
           to eq(URI(Rails.application.secrets.custom_indices['elasticsearch']['reader']).host)
       end
     end
 
-    describe ".client_writers" do
-      it 'should use the value(s) from the secrets.yml custom_indices[elasticsearch][writers] entry' do
+    describe '.client_writers' do
+      it 'uses the value(s) from the secrets.yml custom_indices[elasticsearch][writers] entry' do
         count = Rails.application.secrets.custom_indices['elasticsearch']['writers'].count
         expect(ES::CustomIndices.client_writers.size).to eq(count)
         count.times do |i|
