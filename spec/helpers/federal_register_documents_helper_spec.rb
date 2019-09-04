33e208551e7b4759c360d28b248f8c5cac2585ff
peggles2 <peggles2@users.noreply.github.com>
2019-09-04 14:25:25 -0400

    SRCH-830 - Rails 5 - spec cleanup (#363)
    * SRCH-830 - Rails 5 - spec cleanup
    
    * remove redundant let document

--- spec/helpers/federal_register_documents_helper_spec.rb
+++ spec/helpers/federal_register_documents_helper_spec.rb
@@ -61,18 +61,6 @@ describe FederalRegisterDocumentsHelper do
   end
 
   describe '#federal_register_document_comment_period' do
-    context 'when federal_register_document_info is called' do
-      let(:result) do
-        "A <span>Notice</span> by the <span>Internal " \
-        "Revenue Service</span>, the <span>International " \
-        "Trade Administration</span> and the <span>National " \
-        "Oceanic and Atmospheric Administration</span> " \
-        "posted on <span>June 9, 2014</span>."
-      end
-
-      specify { expect(helper.federal_register_document_info(document)).to eq result }
-    end
-
     context 'when the document comments_close_on is before today' do
       before { allow(document).to receive(:comments_close_on).and_return Date.current.prev_week }
 
