63d1581c9d08ca78cab3c082e083ea5e01043afa
Parissa <parissa.eggleston@gmail.com>
2019-09-24 14:56:20 -0400

    add name to omniauth.rb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :login_dot_gov, {
+    name: :login_dot_gov,
     client_id: 'urn:gov:gsa:openidconnect.profiles:sp:sso:gsa:search',
     idp_base_url: 'https://idp.int.identitysandbox.gov',
     ial: 1,
