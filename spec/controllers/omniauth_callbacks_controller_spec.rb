953e55704e4079335684d007e0f0152ed10fb288
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2019-10-03 14:15:29 -0700

    add spec for error

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe OmniauthCallbacksController do
-
   describe '#login_dot_gov' do
     let(:user) { users(:omniauth_user) }
     let(:auth) { mock_user_auth }
@@ -10,11 +9,6 @@ describe OmniauthCallbacksController do
       request.env['omniauth.auth'] = auth
       get :login_dot_gov
     end
-
-    #before do
-    #  request.env['omniauth.auth'] = auth
-    #  get :login_dot_gov
-    #end
 
     context 'when the login is successful' do
       before { get_login_dot_gov }
@@ -50,38 +44,14 @@ describe OmniauthCallbacksController do
       end
     end
 
+    context 'when the user record cannot be persisted to the database' do
+      before do
+        allow_any_instance_of(User).to receive(:persisted?).and_return(false)
+      end
 
-=begin
-
-
-    context 'when the existing user with no uid info is saved' do
-      let(:auth) { mock_user_auth('user_without_uid@fixtures.org', '22222') }
-      let(:user) { users(:user_without_uid) }
-
-      it 'saves the uid' do
-        expect(user.uid).to eq '22222'
+      it 'raises an error' do
+        expect { get_login_dot_gov }.to raise_error(ActionController::UnknownFormat)
       end
     end
-
-    context 'when the existing user with uid info is saved' do
-      let(:auth) { mock_user_auth('user_with_uid@fixtures.org', '11111') }
-      let(:user) { users(:user_with_uid) }
-
-      it 'saves the uid' do
-        expect(user.uid).to eq '11111'
-      end
-    end
-  end
-
-  context 'when the user record cannot be persisted to the database' do
-    before do
-      allow(user).to receive(:persisted?).and_return(:false)
-    end
-
-    let(:user) { users(:user_with_uid) }
-
-    it { is_expected.not_to redirect_to(admin_home_page_path) }
-  end
-=end
   end
 end
