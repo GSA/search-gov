3a73606b2cfc95ef5044a22bc08c455275356529
Parissa <parissa.eggleston@gmail.com>
2019-10-18 16:21:06 -0400

    remove request and move to request folder

--- spec/requests/omniauth-rails_csrf_protection_spec.rb
+++ spec/requests/omniauth-rails_csrf_protection_spec.rb
@@ -1,5 +1,5 @@
 # Make sure that https://nvd.nist.gov/vuln/detail/CVE-2015-9284 is mitigated
-RSpec.describe 'CVE-2015-9284', type: :request do
+RSpec.describe 'CVE-2015-9284' do
   describe 'POST /auth/:provider without CSRF token' do
     before do
       @allow_forgery_protection = ActionController::Base.allow_forgery_protection
