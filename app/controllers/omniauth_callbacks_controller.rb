a158d7b1b345271f5b17bac2123a3cf4821fa961
Parissa <parissa.eggleston@gmail.com>
2019-10-22 14:40:02 -0400

    SRCH-912 - reset session after login

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class OmniauthCallbacksController < ApplicationController
+  def login_dot_gov
+    auth = request.env['omniauth.auth']
+    @user = User.from_omniauth(auth)
+    return unless @user.persisted?
+
+    reset_session
+    @user_session = UserSession.create(@user)
+
+    redirect_to(admin_home_page_path)
+  end
+end
