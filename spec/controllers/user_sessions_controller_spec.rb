c7158558f2618185d980d55004545636f9aac967
Parissa <parissa.eggleston@gmail.com>
2019-10-23 08:50:15 -0400

    changes made to add test for user already logged in

--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -8,8 +8,20 @@ describe UserSessionsController do
   it { is_expected.to use_before_filter(:reset_session) }
 
   describe '#security_notification' do
-    before { get :security_notification }
+    context 'when a user is not logged in' do
+      before { get :security_notification }
 
-    it { is_expected.to render_template(:security_notification) }
+      it { is_expected.to render_template(:security_notification) }
+    end
+
+    context 'when a user is already logged in' do
+      before { activate_authlogic }
+
+      include_context 'approved user logged in'
+
+      before { get :security_notification }
+
+      it { is_expected.to redirect_to(account_path) }
+    end
   end
 end
