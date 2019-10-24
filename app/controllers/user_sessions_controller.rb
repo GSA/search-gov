c63c870475b4181a0688d9f9b63a338e1616a977
Parissa <parissa.eggleston@gmail.com>
2019-10-24 13:12:52 -0400

    remove extra space

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -2,7 +2,6 @@ class UserSessionsController < ApplicationController
   before_action :reset_session, only: [:destroy]
   before_action :require_user, only: :destroy
 
-
   def security_notification
     redirect_to(account_path) if current_user
   end
