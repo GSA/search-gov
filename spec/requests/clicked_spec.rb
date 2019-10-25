bb283efe342a4ac2f33bbebef0475cb0cf9beb12
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2019-10-25 11:38:23 -0700

    clean up clicked_spec

--- spec/requests/clicked_spec.rb
+++ spec/requests/clicked_spec.rb
@@ -1,22 +1,22 @@
 require 'spec_helper'
 
-describe "Clicked" do
+describe 'Clicked' do
   before do
-    @url = "http://localhost:3000/search?locale=en&m=false&query=electrocoagulation++%29++%28site%3Awww.uspto.gov+%7C+site%3Aeipweb.uspto.gov%29+"
+    @url = 'http://localhost:3000/search?locale=en&m=false&query=electrocoagulation++%29++%28site%3Awww.uspto.gov+%7C+site%3Aeipweb.uspto.gov%29+'
     @unescaped_url = CGI::unescape(@url).gsub(' ', '+')
-    @query = "chicken & beef recall"
-    @timestamp = "1271978905"
-    @affiliate_name = "some affiliate"
-    @position = "7"
-    @module = "RECALL"
-    @vertical = "web"
-    @locale = "en"
+    @query = 'chicken & beef recall'
+    @timestamp = '1271978905'
+    @affiliate_name = 'some affiliate'
+    @position = '7'
+    @module = 'RECALL'
+    @vertical = 'web'
+    @locale = 'en'
     @model_id = '1234'
   end
 
-  context "when correct information is passed in" do
+  context 'when correct information is passed in' do
 
-    it "should return success with a blank message body" do
+    it 'returns success with a blank message body' do
       get '/clicked', params: { u: @url,
                                 q: @query,
                                 t: @timestamp,
@@ -30,8 +30,18 @@ describe "Clicked" do
       expect(response.body).to eq('')
     end
 
-    it "should log the click" do
-      expect(Click).to receive(:log).with(@unescaped_url, @query, '2010-04-22 23:28:25', '127.0.0.1', @affiliate_name, @position, @module, @vertical, @locale, anything(), @model_id)
+    it 'logs the click' do
+      expect(Click).to receive(:log).with(@unescaped_url,
+                                          @query,
+                                          '2010-04-22 23:28:25',
+                                          '127.0.0.1',
+                                          @affiliate_name,
+                                          @position,
+                                          @module,
+                                          @vertical,
+                                          @locale,
+                                          anything(),
+                                          @model_id)
       get '/clicked', params: { u: @url,
                                 q: @query,
                                 t: @timestamp,
@@ -45,7 +55,7 @@ describe "Clicked" do
 
   end
 
-  context "when click url is missing" do
+  context 'when click url is missing' do
     before do
       get '/clicked', params: { q: @query,
                                 t: @timestamp,
@@ -57,12 +67,12 @@ describe "Clicked" do
                                 i: @model_id }
     end
 
-    it "should return success with a blank message body" do
+    it 'returns success with a blank message body' do
       expect(response.success?).to be(true)
       expect(response.body).to eq('')
     end
 
-    it "should not log the click information" do
+    it 'does not log the click information' do
       expect(Click).not_to receive(:log)
     end
   end
