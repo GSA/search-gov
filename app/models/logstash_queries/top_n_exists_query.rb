643189cf99b2d06c279f3c09b966fc793df69018
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2019-11-06 15:02:07 -0800

    SRCH-1027 update TopNExists Query for Elasticsearch 5.6

--- app/models/logstash_queries/top_n_exists_query.rb
+++ app/models/logstash_queries/top_n_exists_query.rb
@@ -1,11 +1,11 @@
-class TopNExistsQuery < TopNModulesQuery
+# frozen_string_literal: true
 
-  def modules_must(json)
-    json.child! { json.exists { json.field "modules" } }
+class TopNExistsQuery < TopNModulesQuery
+  def additional_musts(json)
+    json.child! { json.exists { json.field 'modules' } }
   end
 
   def additional_must_nots(json)
-    json.child! { json.term { json.modules "QRTD" } }
+    json.child! { json.term { json.modules 'QRTD' } }
   end
-
 end
