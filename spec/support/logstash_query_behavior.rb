cbb8e85d5b57ba7c7d58f2823c4e7d6477b32ead
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2019-11-06 14:39:40 -0800

    SRCH-1026 update TopNMissingQuery for Elasticsearch 5.6

--- spec/support/logstash_query_behavior.rb
+++ spec/support/logstash_query_behavior.rb
@@ -1,11 +1,11 @@
-shared_context 'querying logstash indexes' do
-  subject(:body) { query.body }
-end
-
 shared_examples_for 'a logstash query' do
-  it 'does not raise an error' do
-    ES::ELK.client_reader.search(index: 'logstash-*',  body: body)
-  end
+  describe '#body' do
+    subject(:body) { query.body }
 
-  it { is_expected.to eq(expected_body) }
+    it 'does not raise an error' do
+      ES::ELK.client_reader.search(index: 'logstash-*',  body: body)
+    end
+
+    it { is_expected.to eq(expected_body) }
+  end
 end
