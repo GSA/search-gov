7c0ce699f592a8a54b03fd6c4ad133c6c296d278
Pat Kelsh <pkelsh@gmail.com>
2019-11-07 10:24:26 -0500

    SRCH-976 cleaned up commented out code

--- lib/fast_delete_from_db_and_es.rb
+++ lib/fast_delete_from_db_and_es.rb
@@ -8,10 +8,6 @@ module FastDeleteFromDbAndEs
       return if ids.blank?
 
       "Elastic#{name}".constantize.delete(ids)
-      # puts self
-      # puts ids
-      # puts(self.find(ids))
-      # where(:id => ids).delete_all
       where(:id => ids).delete_all unless (where(:id => ids) == nil)
     end
   end
