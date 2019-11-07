5ec2fa39e5888f946bf13867415971fd60a074a8
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2019-11-07 15:05:08 -0800

    SRCH-1036 update DateRangeTopNQuery to run on Elasticsearch 5.6

--- spec/models/logstash_queries/date_range_top_n_query_spec.rb
+++ spec/models/logstash_queries/date_range_top_n_query_spec.rb
@@ -1,10 +1,48 @@
 require 'spec_helper'
 
-describe DateRangeTopNQuery, "#body" do
-  let(:query) { DateRangeTopNQuery.new('foo', Date.parse("2014-06-28"), Date.parse("2014-06-29"), {field: 'raw', size: 1000}) }
+describe DateRangeTopNQuery do
+  let(:query) do
+    DateRangeTopNQuery.new('affiliate_name',
+                           Date.parse('2019-11-01'),
+                           Date.parse('2019-11-07'),
+                           { field: 'params.query.raw', size: 1000 })
+  end
+  let(:expected_body) do
+    {
+      "query": {
+        "bool": {
+          "must": [
+            {
+              "term": {
+                "params.affiliate": "affiliate_name"
+              }
+            },
+            {
+              "range": {
+                "@timestamp": {
+                  "gte": "2019-11-01",
+                  "lte": "2019-11-07"
+                }
+              }
+            }
+          ],
+          "must_not": {
+            "term": {
+              "useragent.device": "Spider"
+            }
+          }
+        }
+      },
+      "aggs": {
+        "agg": {
+          "terms": {
+            "field": "params.query.raw",
+            "size": 1000
+          }
+        }
+      }
+    }.to_json
+  end
 
-  subject(:body) { query.body }
-
-  it { is_expected.to eq(%q({"query":{"filtered":{"filter":{"bool":{"must":[{"term":{"affiliate":"foo"}},{"range":{"@timestamp":{"gte":"2014-06-28","lte":"2014-06-29"}}}],"must_not":{"term":{"useragent.device":"Spider"}}}}}},"aggs":{"agg":{"terms":{"field":"raw","size":1000}}}}))}
-
+  it_behaves_like 'a logstash query'
 end
