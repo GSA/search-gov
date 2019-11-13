6801933761d239c890e3b9fed950437bffeb1a1d
peggles2 <peggles2@users.noreply.github.com>
2019-11-13 09:28:20 -0500

    Feature/config updates (#439)
    * SRCH-1055 Update omniauth.rb file to be more dynamic

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :login_dot_gov, {
     name: :logindotgov,
-    client_id: 'urn:gov:gsa:openidconnect.profiles:sp:sso:gsa:search',
-    idp_base_url: 'https://idp.int.identitysandbox.gov',
+    client_id: Rails.application.secrets.login_dot_gov['client_id'],
+    idp_base_url: Rails.application.secrets.login_dot_gov['idp_base_url'],
     ial: 1,
     private_key: OpenSSL::PKey::RSA.new(File.read('config/logindotgov.pem')),
-    redirect_uri: 'http://localhost:3000/auth/logindotgov/callback'
+    redirect_uri: "http://#{Rails.application.secrets.login_dot_gov['host']}/auth/logindotgov/callback"
   }
 end
