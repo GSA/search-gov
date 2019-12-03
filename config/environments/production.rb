35c1b0d218cc9a2f4214756413ae92f09ab71c11
Parissa <parissa.eggleston@gmail.com>
2019-12-03 16:17:23 -0500

    update production config file to set force ssl to false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,9 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  #  This is set to false because we redirect to https. Setting this to true causes 
+  #  issues with the healthcheck called from the loadbalancer
+  config.force_ssl = false 
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
