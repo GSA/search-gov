888f3952e17e329c856f33ccfb7bebd7e56d013c
Parissa <parissa.eggleston@gmail.com>
2020-01-15 12:05:48 -0500

    update migration

--- db/migrate/20200108160640_remove_user_unused_columns.rb
+++ db/migrate/20200108160640_remove_user_unused_columns.rb
@@ -1,10 +1,22 @@
 class RemoveUserUnusedColumns < ActiveRecord::Migration[5.2]
-  def change
+  def up
     remove_column :users, :password_updated_at, :datetime
     remove_column :users, :perishable_token, :string
     remove_column :users, :crypted_password, :string
     remove_column :users, :password_salt, :string
     remove_column :users, :failed_login_count, :integer, null: false, default: 0
     remove_column :users, :email_verification_token, :string
+  end
+
+  def down
+    add_column :users, :password_updated_at, :datetime
+    add_column :users, :perishable_token, :string
+    add_column :users, :crypted_password, :string
+    add_column :users, :password_salt, :string
+    add_column :users, :failed_login_count, :integer, null: false, default: 0
+    add_column :users, :email_verification_token, :string
+
+    add_index :users, :perishable_token
+    add_index :users, :email_verification_token, unique: true
   end
 end
