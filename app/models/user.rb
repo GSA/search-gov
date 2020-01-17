ab1945e8b79e8f3ac34130b388bad36a2339ed5e
Parissa <parissa.eggleston@gmail.com>
2020-01-17 12:56:32 -0500

    update query

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,13 +40,9 @@ class User < ApplicationRecord
         }
 
   scope :not_active_for,
-        lambda { |days|
-          where('current_login_at >= ? AND current_login_at <= ? OR
-                (current_login_at IS NULL AND created_at >= ? AND created_at <=? )',
-                days.beginning_of_day,
-                days.end_of_day,
-                days.beginning_of_day,
-                days.end_of_day)
+        lambda { |date|
+          where('DATE(current_login_at) = ? OR
+                (current_login_at IS NULL AND DATE(created_at) = ?)', date, date)
         }
 
   acts_as_authentic do |c|
