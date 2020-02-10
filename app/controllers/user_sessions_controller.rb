bfe2622e08cc87d5decdb300e02c46b67dd8708d
Parissa <parissa.eggleston@gmail.com>
2020-02-10 12:37:53 -0500

    fix some rspec tests

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -2,7 +2,9 @@ class UserSessionsController < ApplicationController
   before_action :reset_session, only: [:destroy]
   before_action :require_user, only: :destroy
 
-  def security_notification; end
+  def security_notification
+    redirect_to(account_path) if current_user
+  end
 
   def destroy
     current_user_session.destroy
