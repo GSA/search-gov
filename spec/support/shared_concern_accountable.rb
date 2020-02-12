0870c84e24a827f1c974bb894c72d40c6a33f613
Parissa <parissa.eggleston@gmail.com>
2020-02-12 17:14:35 -0500

    added more tests

--- spec/support/shared_concern_accountable.rb
+++ spec/support/shared_concern_accountable.rb
@@ -3,25 +3,35 @@ require 'spec_helper'
 shared_examples 'incomplete account' do
 
   context 'when user did not supply contact_name' do
-    fixtures :users
-    let(:current_user) { users(:affiliate_admin) }
+    let(:current_user) { users(:no_contact_name) }
 
     before do
-      activate_authlogic
       UserSession.create current_user
       expect(User).to receive(:find_by_id).and_return(current_user)
       get :index
     end
 
-    let(:current_user) { users(:no_contact_name) }
-
     it 'redirects to account edit page' do
       expect(response).to redirect_to('/account/edit')
     end
 
-    it 'returns error for user to supply contact name' do
-      expect(current_user.errors.messages[:contact_name].first).
-        to eq('You must supply a contact name')
+    context 'when the user has no contact_name' do
+      let(:current_user) { users(:no_contact_name) }
+
+      it 'returns error for user to supply contact name' do
+        expect(current_user.errors.messages[:contact_name].first).
+          to eq('You must supply a contact name')
+      end
     end
+
+    context 'when the user has no organization_name' do
+      let(:current_user) { users(:no_organization_name) }
+
+      it 'returns error for user to supply organization name' do
+        expect(current_user.errors.messages[:organization_name].first).
+          to eq('You must supply an organization name')
+      end
+    end
+
   end
 end
