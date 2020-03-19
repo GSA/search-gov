0e2cc2c42852a35f7869d9159d55e7797c1f74fe
Parissa <parissa.eggleston@gmail.com>
2020-03-19 13:08:54 -0400

    SRCH-1300 - Change 90 day warning text

--- app/mailers/emailer.rb
+++ app/mailers/emailer.rb
@@ -32,7 +32,7 @@ class Emailer < ApplicationMailer
 
   def account_deactivation_warning(user, date)
     @user = user
-    @remaining_days = (date - 90.days.ago.to_date).to_i
+    @deactivation_date = (date - 90.days.ago.to_date).to_i.days.from_now.strftime("%m/%d/%Y")
     @user_contact_name = get_contact_name(user)
     generic_user_html_email(user, __method__)
   end
