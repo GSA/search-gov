50a189b3e3e205cc4823cfb46a44117c5129670e
Andrew Hyder <andrew.hyder@gsa.gov>
2020-04-30 10:41:38 -0700

    Rubocop refactor of SearchImpression spec

--- spec/models/search_impression_spec.rb
+++ spec/models/search_impression_spec.rb
@@ -2,11 +2,22 @@
 
 require 'spec_helper'
 
-describe 'SearchImpression' do
+describe SearchImpression do
   describe '.log' do
-    let!(:request) { double('request', remote_ip: '1.2.3.4', url: 'http://www.gov.gov/', referer: 'http://www.gov.gov/ref', user_agent: 'whatevs', headers: {}) }
-    let!(:search) { double(Search, modules: ['BWEB'], diagnostics: { AWEB: { snap: 'judgement' } }) }
-    let!(:params) { { 'foo' => 'yep' } }
+    let(:request) do
+      double('request',
+             remote_ip: '1.2.3.4',
+             url: 'http://www.gov.gov/',
+             referer: 'http://www.gov.gov/ref',
+             user_agent: 'whatevs',
+             headers: {})
+    end
+    let(:search) do
+      double(Search,
+             modules: ['BWEB'],
+             diagnostics: { AWEB: { snap: 'judgement' } })
+    end
+    let(:params) { { 'foo' => 'yep' } }
     let(:time) { Time.now }
 
     before do
@@ -17,18 +28,28 @@ describe 'SearchImpression' do
     end
 
     context 'with regular params' do
-      it 'has the expected log line' do
-        expect(Rails.logger).to have_received(:info).with("[Search Impression] {\"clientip\":\"1.2.3.4\",\"request\":\"http://www.gov.gov/\",\"referrer\":\"http://www.gov.gov/ref\",\"user_agent\":\"whatevs\",\"diagnostics\":[{\"snap\":\"judgement\",\"module\":\"AWEB\"}],\"time\":\"#{time.to_formatted_s(:db)}\",\"vertical\":\"web\",\"modules\":\"BWEB\",\"params\":{\"foo\":\"yep\"}}")
+      it 'has the single expected log line' do
+        expect(Rails.logger).to have_received(:info).once
+        expect(Rails.logger).to have_received(:info).with(
+          '[Search Impression] {"clientip":"1.2.3.4",'\
+          '"request":"http://www.gov.gov/",'\
+          '"referrer":"http://www.gov.gov/ref",'\
+          '"user_agent":"whatevs","diagnostics":'\
+          '[{"snap":"judgement","module":"AWEB"}],'\
+          "\"time\":\"#{time.to_formatted_s(:db)}\","\
+          '"vertical":"web","modules":"BWEB",'\
+          '"params":{"foo":"yep"}}'
+        )
       end
     end
 
-    context "with routed query module and empty diagnostics" do
-      let!(:search) { double(Search, modules: ['QRTD'], diagnostics: {}) }
+    context 'with routed query module and empty diagnostics' do
+      let(:search) { double(Search, modules: ['QRTD'], diagnostics: {}) }
 
       it 'has the expected log line parts' do
-        expect(Rails.logger).to have_received(:info).with(include('[Search Impression]'))
-        expect(Rails.logger).to have_received(:info).with(include('"modules":"QRTD"'))
-        expect(Rails.logger).to have_received(:info).with(include('"diagnostics":[]'))
+        expect(Rails.logger).to have_received(:info).with(
+          include('"modules":"QRTD"', '"diagnostics":[]')
+        )
       end
     end
 
@@ -36,16 +57,30 @@ describe 'SearchImpression' do
       let(:params) { { 'foo' => 'yep', 'bar.blat' => 'nope' } }
 
       it 'omits that parameter' do
-        expect(Rails.logger).to have_received(:info).with("[Search Impression] {\"clientip\":\"1.2.3.4\",\"request\":\"http://www.gov.gov/\",\"referrer\":\"http://www.gov.gov/ref\",\"user_agent\":\"whatevs\",\"diagnostics\":[{\"snap\":\"judgement\",\"module\":\"AWEB\"}],\"time\":\"#{time.to_formatted_s(:db)}\",\"vertical\":\"web\",\"modules\":\"BWEB\",\"params\":{\"foo\":\"yep\"}}")
+        expect(Rails.logger).to have_received(:info).with(
+          include('"params":{"foo":"yep"}')
+        )
       end
     end
 
     context 'headers contains X-Original-Request header' do
-      let!(:request) { double('request', remote_ip: '1.2.3.4', url: 'http://www.gov.gov/', referer: 'http://www.gov.gov/ref', user_agent: 'whatevs', headers: { 'X-Original-Request' => 'http://test.gov' }) }
+      let(:request) do
+        double('request',
+               remote_ip: '1.2.3.4',
+               url: 'http://www.gov.gov/',
+               referer: 'http://www.gov.gov/ref',
+               user_agent: 'whatevs',
+               headers: { 'X-Original-Request' => 'http://test.gov' })
+      end
 
-      it 'should log a non-null value for original_request' do
-        expect(Rails.logger).to have_received(:info).with('[X-Original-Request] ("http://test.gov")')
-        expect(Rails.logger).to have_received(:info).with("[Search Impression] {\"clientip\":\"1.2.3.4\",\"request\":\"http://test.gov\",\"referrer\":\"http://www.gov.gov/ref\",\"user_agent\":\"whatevs\",\"diagnostics\":[{\"snap\":\"judgement\",\"module\":\"AWEB\"}],\"time\":\"#{time.to_formatted_s(:db)}\",\"vertical\":\"web\",\"modules\":\"BWEB\",\"params\":{\"foo\":\"yep\"}}")
+      it 'should log two lines, the original-request header and the search impression' do
+        expect(Rails.logger).to have_received(:info).twice
+        expect(Rails.logger).to have_received(:info).with(
+          '[X-Original-Request] ("http://test.gov")'
+        )
+        expect(Rails.logger).to have_received(:info).with(
+          include('[Search Impression]', '"request":"http://test.gov"')
+        )
       end
     end
   end
