6e50e9c3869585691d9920aca6a5d9fce83047ad
Andrew Hyder <andrew.hyder@gsa.gov>
2020-05-21 15:20:34 -0700

    Error messages and status codes for click tracking api

--- app/controllers/clicked_controller.rb
+++ app/controllers/clicked_controller.rb
@@ -1,16 +1,31 @@
-class ClickedController < ActionController::Metal
+# frozen_string_literal: true
+
+class ClickedController < ApplicationController
+  before_action :check_required_params
+
   def index
     url = CGI.unescape(params['u']).gsub(' ', '+') rescue nil
-    if url.present?
-      query = params['q']
-      position = params['p']
-      affiliate_name = params['a'].blank? ? nil : params['a']
-      results_source = params['s']
-      vertical = params['v']
-      click_ip = request.env['REMOTE_ADDR']
-      user_agent = request.env['HTTP_USER_AGENT']
-      Click.log(url, query, click_ip, affiliate_name, position, results_source, vertical, user_agent)
-    end
-    self.response_body = ""
+    query = params['q']
+    position = params['p']
+    affiliate_name = params['a'].presence
+    results_source = params['s']
+    vertical = params['v']
+    click_ip = request.env['REMOTE_ADDR']
+    user_agent = request.env['HTTP_USER_AGENT']
+    Click.log(url, query, click_ip, affiliate_name, position, results_source, vertical, user_agent)
+
+    head :ok
+  end
+
+  private
+
+  def check_required_params
+    # Error messages to match activemodels, used in the rest of the api
+    errors = []
+    errors << 'url must be present' if params[:u].blank?
+    errors << 'query must be present' if params[:q].blank?
+    errors << 'position must be present' if params[:p].blank?
+    errors << 'source must be present' if params[:s].blank?
+    render json: { errors: errors }.to_json, status: :bad_request if errors.present?
   end
 end
--- spec/requests/clicked_spec.rb
+++ spec/requests/clicked_spec.rb
@@ -37,24 +37,77 @@ describe 'Clicked' do
     end
   end
 
-  context 'when click url is missing' do
-    before { post '/clicked', params: params.without(:u) }
+  context 'when required params are missing' do
+    before { post '/clicked', params: params.without(missing_param) }
 
-    it 'returns success with a blank message body' do
-      expect(response.success?).to be(true)
-      expect(response.body).to eq('')
+    context 'missing url' do
+      let(:missing_param) { :u }
+
+      it 'returns a 400 and an error message' do
+        expect(response.status).to eq 400
+        expect(response.body).to eq('{"errors":["url must be present"]}')
+      end
+
+      it 'does not log the click information' do
+        expect(Click).not_to receive(:log)
+      end
     end
 
-    it 'does not log the click information' do
-      expect(Click).not_to receive(:log)
+    context 'missing query' do
+      let(:missing_param) { :q }
+
+      it 'returns a 400 and an error message' do
+        expect(response.status).to eq 400
+        expect(response.body).to eq('{"errors":["query must be present"]}')
+      end
+
+      it 'does not log the click information' do
+        expect(Click).not_to receive(:log)
+      end
+    end
+
+    context 'missing position' do
+      let(:missing_param) { :p }
+
+      it 'returns a 400 and an error message' do
+        expect(response.status).to eq 400
+        expect(response.body).to eq('{"errors":["position must be present"]}')
+      end
+
+      it 'does not log the click information' do
+        expect(Click).not_to receive(:log)
+      end
+    end
+
+    context 'missing source' do
+      let(:missing_param) { :s }
+
+      it 'returns a 400 and an error message' do
+        expect(response.status).to eq 400
+        expect(response.body).to eq('{"errors":["source must be present"]}')
+      end
+
+      it 'does not log the click information' do
+        expect(Click).not_to receive(:log)
+      end
+    end
+  end
+
+  context 'missing multiple params' do
+    it 'has the expected error message' do
+      post '/clicked', params: params.without(:u, :q)
+
+      expect(response.status).to eq 400
+      expected_long_error = '{"errors":["url must be present","query must be present"]}'
+      expect(response.body).to eq(expected_long_error)
     end
   end
 
   context 'a GET request' do
     it 'returns an error' do
-        get '/clicked', params: params
-        expect(response.success?).to be(false)
-        expect(response.status).to eq 302
+      get '/clicked', params: params
+      expect(response.success?).to be(false)
+      expect(response.status).to eq 302
     end
   end
 end
