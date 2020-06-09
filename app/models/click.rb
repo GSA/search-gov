2923c9d55ab6fd02a3f326946d621e7527199555
Andrew Hyder <andrew.hyder@gsa.gov>
2020-06-09 16:20:15 -0700

    Validate client ip in click tracking

--- app/models/click.rb
+++ app/models/click.rb
@@ -4,9 +4,10 @@ class Click
   attr_reader :url, :query, :position, :module_code, :client_ip, :user_agent
 
   validates :url, :query, :position, :module_code, :client_ip, :user_agent, presence: true
+  validate :client_ip_validation
 
   def initialize(params)
-    @url = cleaned_url(params[:url])
+    @url = unescape_url(params[:url])
     @query = params[:query]
     @client_ip = params[:client_ip]
     @affiliate = params[:affiliate]
@@ -22,7 +23,18 @@ class Click
 
   private
 
-  def cleaned_url(url)
+  def client_ip_validation
+    return if client_ip.blank?
+
+    errors.add(:client_ip, 'is invalid') unless valid_ip?
+  end
+
+  def valid_ip?
+    # https://stackoverflow.com/a/15157862/722860
+    !!(client_ip =~ Resolv::IPv4::Regex) || !!(client_ip =~ Resolv::IPv6::Regex)
+  end
+
+  def unescape_url(url)
     CGI.unescape(url).tr(' ', '+') rescue nil
   end
 end
--- spec/models/click_spec.rb
+++ spec/models/click_spec.rb
@@ -3,20 +3,23 @@
 require 'spec_helper'
 
 describe Click do
-  context 'with required params' do
-    let(:url) { 'http://www.fda.gov/foo.html' }
-    let(:click) do
-      described_class.new url: url,
-                    query: 'my query',
-                    client_ip: '12.34.56.789',
-                    affiliate: 'nps.gov',
-                    position: '7',
-                    module_code: 'RECALL',
-                    vertical: 'web',
-                    user_agent: 'mozilla'
-    end
-    before { allow(Rails.logger).to receive(:info) }
+  let(:url) { 'http://www.fda.gov/foo.html' }
+  let(:ip) { '0.0.0.0' }
+  let(:params) do
+    {
+      url: url,
+      query: 'my query',
+      client_ip: ip,
+      affiliate: 'nps.gov',
+      position: '7',
+      module_code: 'RECALL',
+      vertical: 'web',
+      user_agent: 'mozilla'
+    }
+  end
+  let(:click) { described_class.new params }
 
+  context 'with required params' do
     describe '#valid?' do
       it 'should be valid' do
         expect(click.valid?).to be_truthy
@@ -25,58 +28,39 @@ describe Click do
 
     describe '#log' do
       it 'should log almost-JSON info about the click' do
-        click.log
-
-        expect(Rails.logger).to have_received(:info) do |str|
+        expect(Rails.logger).to receive(:info) do |str|
           expect(str).to match(/^\[Click\] \{.*\}$/)
           expect(str).to include('"url":"http://www.fda.gov/foo.html"')
           expect(str).to include('"query":"my query"')
-          expect(str).to include('"client_ip":"12.34.56.789"')
+          expect(str).to include('"client_ip":"0.0.0.0"')
           expect(str).to include('"affiliate":"nps.gov"')
           expect(str).to include('"position":"7"')
           expect(str).to include('"module_code":"RECALL"')
           expect(str).to include('"vertical":"web"')
           expect(str).to include('"user_agent":"mozilla"')
         end
-      end
-    end
 
-    describe '#cleaned_url' do
-      let(:url) { 'https://search.gov/%28 %3A%7C%29' }
-
-      it 'should return an unescaped url' do
         click.log
-
-        expect(Rails.logger).to have_received(:info) do |str|
-          expect(str).to include('"url":"https://search.gov/(+:|)"')
-        end
-      end
-
-      context 'with invalid utf-8 in the url' do
-        # https://cm-jira.usa.gov/browse/SRCHAR-415
-        let(:url) { 'https://example.com/wymiana+teflon%F3w' }
-
-        it 'drops the unescapable url' do
-          expect(click.url).to be nil
-        end
       end
     end
   end
 
   context 'without required params' do
-    let(:click) do
-      described_class.new url: nil,
-                    query: nil,
-                    client_ip: nil,
-                    affiliate: nil,
-                    position: nil,
-                    module_code: nil,
-                    vertical: nil,
-                    user_agent: nil
+    let(:params) do
+      { 
+        url: nil,
+        query: nil,
+        client_ip: nil,
+        affiliate: nil,
+        position: nil,
+        module_code: nil,
+        vertical: nil,
+        user_agent: nil
+      }
     end
 
     describe '#valid?' do
-      it 'should not be valid' do
+      it 'is not valid' do
         expect(click.valid?).to be_falsey
       end
     end
@@ -93,6 +77,39 @@ describe Click do
                            "User agent can't be blank"]
         expect(click.errors.full_messages).to eq expected_errors
       end
+    end
+  end
+
+  describe '#unescape_url' do
+    let(:url) { 'https://search.gov/%28 %3A%7C%29' }
+
+    it 'returns an unescaped url' do
+      expect(Rails.logger).to receive(:info).with(include('"url":"https://search.gov/(+:|)"'))
+
+      click.log
+    end
+
+    context 'with invalid utf-8 in the url' do
+      # https://cm-jira.usa.gov/browse/SRCHAR-415
+      let(:url) { 'https://example.com/wymiana+teflon%F3w' }
+
+      it 'drops the unescapable url' do
+        expect(click.url).to be nil
+      end
+    end
+  end
+
+  context 'with invalid ip address' do
+    let(:ip) { 'bad_ip_address' }
+
+    it 'is not valid' do
+      expect(click.valid?).to be_falsey
+    end
+
+    it 'has expected errors' do
+      click.valid?
+
+      expect(click.errors.full_messages).to eq ["Client ip is invalid"]
     end
   end
 end
