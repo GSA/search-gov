67cfc967adf4a7b4d3a6a05d4803612759b5b41a
John Maxwell <jmaxwell@fearless.tech>
2020-07-08 13:28:45 -0400

    Fixed login.gov logout, and cleaned up OmniauthCallbacksController

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,22 +1,42 @@
 # frozen_string_literal: true
 
 class OmniauthCallbacksController < ApplicationController
-  def login_dot_gov
-    @user = User.from_omniauth(request.env['omniauth.auth'])
-
-    if @user.persisted? && @user.approval_status != 'not_approved'
-      reset_session
-      set_user_session
-      redirect_to(admin_home_page_path)
-    else
-      redirect_to('https://search.gov/access-denied')
-    end
+  class LoginError < StandardError
   end
 
-  private
+  def login_dot_gov
+    reset_session
+    set_id_token
+    set_user_session
+    redirect_to(admin_home_page_path)
+  rescue LoginError => e
+    flash[:error]= "login internal error: #{e.message}"
+    redirect_to('/login')
+  end
+
+  def user
+    @user ||= User.from_omniauth(omniauth_data)
+    raise LoginError.new("can't find user #{omniauth_data.info.email}") unless @user
+    raise LoginError.new("login not allowed for #{@user.email}") unless @user.login_allowed?
+    @user
+  end
+
+  def omniauth_data
+    raise LoginError.new('no omniauth data') unless request.env['omniauth.auth']
+    request.env['omniauth.auth']
+  end
+
+  def credentials
+    raise LoginError.new('no user credentials') unless omniauth_data['credentials']
+    omniauth_data['credentials']
+  end
+
+  def set_id_token
+    session[:id_token]= credentials['id_token']
+  end
 
   def set_user_session
-    user_session = UserSession.create(@user)
+    user_session = UserSession.create(user)
     user_session.secure = Rails.application.config.ssl_options[:secure_cookies]
   end
 end
--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,7 +7,20 @@ class UserSessionsController < ApplicationController
   end
 
   def destroy
+    id_token= session[:id_token]
+    login_dot_gov_host= 'https://idp.int.identitysandbox.gov'
+    login_dot_gov_logout_endpoint= '/openid_connect/logout'
+    login_dot_gov_logout_url= "#{login_dot_gov_host}#{login_dot_gov_logout_endpoint}"
+
+    redirect_uri= URI::HTTPS.build(host: 'idp.int.identitysandbox.gov',
+                                   path: '/openid_connect/logout',
+                                   query: {
+                                     id_token_hint: id_token,
+                                     post_logout_redirect_uri: 'http://localhost:3000/login',
+                                     state: '1234567890123456789012'
+                                   }.to_query).to_s
+    reset_session
     current_user_session.destroy
-    redirect_to(login_path)
+    redirect_to(redirect_uri)
   end
 end
