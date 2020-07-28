ce8ccf4d889d6468f4698cb7617191e71a1fa10c
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2020-07-28 10:31:06 -0700

    SRCH-184 add button to let super admins reindex a searchgov domain - add composite indices to searchgov_urls table

---
+++ spec/jobs/searchgov_domain_reindexer_job_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe SearchgovDomainReindexerJob do
+  subject(:perform) { SearchgovDomainReindexerJob.perform_now(args) }
+
+  let(:searchgov_domain) { searchgov_domains(:basic_domain) }
+  let(:args) do
+    { searchgov_domain: searchgov_domain }
+  end
+
+  it_behaves_like 'a searchgov job'
+
+  context 'when the domain has indexed URLs' do
+    let!(:indexed_url) do
+      searchgov_domain.searchgov_urls.create!(url: 'https://foo.gov/ok',
+                                              last_crawl_status: 'OK')
+    end
+    let!(:failed_url) do
+      searchgov_domain.searchgov_urls.create!(url: 'https://foo.gov/failed',
+                                              last_crawl_status: 'failed')
+    end
+
+    it 'sets the previously indexed URLs to be reindexed' do
+      expect { perform }.
+        to change { indexed_url.reload.enqueued_for_reindex }.from(false).to(true)
+    end
+
+    it 'does not set previously failed URLs to be reindexed' do
+      expect { perform }.not_to change { failed_url.reload.enqueued_for_reindex }
+    end
+  end
+
+  it 'triggers sitemap indexing' do
+    expect(searchgov_domain).to receive(:index_sitemaps)
+    perform
+  end
+end
