43f943fc71de9841b22cdbb8fa71eef7b17314da
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2020-09-02 14:41:58 -0400

    get page source

--- lib/js_fetcher.rb
+++ lib/js_fetcher.rb
@@ -2,6 +2,7 @@ module JsFetcher
   def self.fetch(url)
     options = Selenium::WebDriver::Chrome::Options.new(args: ['headless'])
     driver = Selenium::WebDriver.for(:chrome, options: options)
-    puts driver.get(url)
+    driver.get(url)
+    driver.page_source
   end
 end
