e08026e68e8f0340e0cac76c4c3d99949a2466b2
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2020-12-15 12:10:14 -0500

    SRCH-1795 fix CORS for api/v2/search/i14y endpoint

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     origins '*'
     resource '/assets/*', headers: :any, methods: %i[get head options]
     resource '/api/v2/click', headers: :any, methods: %i[post head options]
-    resource '/api/v2/search', headers: :any, methods: %i[get head options]
+    resource '/api/v2/search/*', headers: :any, methods: %i[get head options]
     resource '/sayt', headers: :any, methods: %i[get head options]
   end
 end
