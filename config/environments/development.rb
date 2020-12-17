3a1c9b25219fec14b637a55121f1a7649eeb1143
John Maxwell <jmaxwell@fearless.tech>
2020-12-17 15:15:01 -0500

    Turned on secure cookies.
    Disabled force_ssl for /healthcheck. Our ELB's use that path as a
    health check, and won't work right if we try to force SSL for it.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,6 +58,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.ssl_options[:secure_cookies] = false
 end
 
 ADDITIONAL_BING_PARAMS = { 'traffictype' => 'test' }
