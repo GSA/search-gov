bd780497bb4194a8172c3a4ae05000938094e418
jmax-fearless <66315527+jmax-fearless@users.noreply.github.com>
2021-02-19 10:15:02 -0500

    SRCH-1771 block html injection (#656)
    SRCH-1771 - block html injection

--- spec/models/routed_query_keyword_spec.rb
+++ spec/models/routed_query_keyword_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+# frozen_string_literal: true
 
 describe RoutedQueryKeyword do
   fixtures :affiliates, :routed_queries, :routed_query_keywords
@@ -10,38 +10,59 @@ describe RoutedQueryKeyword do
     it { is_expected.to validate_presence_of :keyword }
     it { is_expected.to validate_uniqueness_of(:keyword).scoped_to(:routed_query_id).case_insensitive }
 
-    it 'should create a new instance given valid attributes' do
+    it 'creates a new instance given valid attributes' do
       routed_query.routed_query_keywords.create!(keyword: 'route me')
     end
 
-    it 'should downcase, squish and strip whitespace from keyword before inserting in DB' do
+    it 'downcases, squish and strip whitespace from keyword before inserting in DB' do
       keyword = ' leading          and trailing whitespaces AND CAPITALS'
       rqk = routed_query.routed_query_keywords.create!(keyword: keyword)
       expect(rqk.keyword).to eq('leading and trailing whitespaces and capitals')
     end
 
-    it 'should not allow the same keyword to be reused within a single affiliate' do
+    it 'does not allow the same keyword to be reused within a single affiliate' do
       routed_queries(:unclaimed_money).routed_query_keywords.create!(keyword: 'route me')
-      rqk = routed_queries(:moar_unclaimed_money).routed_query_keywords.build(keyword: 'route me')
+      rqk = routed_queries(:moar_unclaimed_money).routed_query_keywords.build(
+        keyword: 'route me'
+      )
+      expected_error =
+        'The keyword \'route me\' is already in use for a different routed query'
+
       expect(rqk).not_to be_valid
-      expect(rqk.errors[:keyword]).to eq(["The keyword 'route me' is already in use for a different routed query"])
+      expect(rqk.errors[:keyword]).to eq([expected_error])
     end
 
     it 'creates a matching SaytSuggestion' do
       routed_query.routed_query_keywords.create!(keyword: 'route me')
-      sayt_suggestion = SaytSuggestion.find_by_affiliate_id_and_phrase_and_is_protected(routed_query.affiliate.id, 'route me', true)
+      sayt_suggestion = SaytSuggestion.find_by(affiliate_id: routed_query.affiliate.id,
+                                               phrase: 'route me',
+                                               is_protected: true)
       expect(sayt_suggestion).to be_present
+    end
+
+    context 'when a keyword contains html' do
+      let(:keyword) do
+        routed_query.routed_query_keywords.create!(
+          keyword: 'keyword <div>with html'
+        )
+      end
+
+      it 'strips the html from the keyword' do
+        expect(keyword.keyword).to eq('keyword with html')
+      end
     end
   end
 
   describe 'Updating a keyword' do
     before do
       keyword = routed_query.routed_query_keywords.create!(keyword: 'initial keyword')
-      keyword.update_attribute(:keyword, 'updated keyword')
+      keyword.update(keyword: 'updated keyword')
     end
 
     it 'updates the matching SaytSuggestion' do
-      sayt_suggestion = SaytSuggestion.find_by_affiliate_id_and_phrase_and_is_protected(routed_query.affiliate.id, 'updated keyword', true)
+      sayt_suggestion = SaytSuggestion.find_by(affiliate_id: routed_query.affiliate.id,
+                                               phrase: 'updated keyword',
+                                               is_protected: true)
       expect(sayt_suggestion).to be_present
     end
   end
@@ -49,13 +70,14 @@ describe RoutedQueryKeyword do
   describe 'deleting a keyword' do
     before do
       routed_query.routed_query_keywords.create!(keyword: 'some keyword')
-      sayt_suggestion = SaytSuggestion.find_by_affiliate_id_and_phrase_and_is_protected(routed_query.affiliate.id, 'some keyword', true)
-      expect(sayt_suggestion).to be_present
     end
 
     it 'deletes the matching SaytSuggestion' do
-      RoutedQueryKeyword.last.destroy
-      sayt_suggestion = SaytSuggestion.find_by_affiliate_id_and_phrase_and_is_protected(routed_query.affiliate.id, 'some keyword', true)
+      described_class.last.destroy
+      sayt_suggestion = SaytSuggestion.find_by(affiliate_id: routed_query.affiliate.id,
+                                               phrase: 'some keyword',
+                                               is_protected: true)
+
       expect(sayt_suggestion).to be_nil
     end
   end
@@ -68,6 +90,7 @@ describe RoutedQueryKeyword do
 
   describe '#dup' do
     subject(:original_instance) { routed_query_keywords(:one) }
-    include_examples 'dupable', %w(routed_query_id)
+
+    include_examples 'dupable', %w[routed_query_id]
   end
 end
