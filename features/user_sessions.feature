cb8a5ab8fb3952857853100fb849f4eefd5cb0e1
John Maxwell <jmaxwell@fearless.tech>
2021-03-02 12:14:28 -0500

    Logout tests.

--- features/user_sessions.feature
+++ features/user_sessions.feature
@@ -49,3 +49,21 @@ Feature: User sessions
     And the time becomes 2017-03-30 12:00
     And I follow "Add Site"
     Then I should be on the login page
+
+  @javascript
+  Scenario: Already logged-in user logs out
+    Given I am logged in with email "affiliate_manager@fixtures.org"
+    When I go to the usagov's Dashboard page
+    Then I should not see "Security Notification"
+    When I sign out
+    And I go to the usagov's Dashboard page
+    Then I should see "Security Notification"
+
+  @javascript
+  Scenario: Already logged-in super-user logs out
+    Given I am logged in with email "affiliate_admin@fixtures.org"
+    When I go to the usagov's Dashboard page
+    Then I should not see "Security Notification"
+    When I sign out
+    And I go to the usagov's Dashboard page
+    Then I should see "Security Notification"
--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -2,6 +2,10 @@
 
 require 'spec_helper'
 
+# We actually need to use multiple befores as part of our fake
+# login
+# rubocop:disable RSpec/ScatteredSetup
+
 describe UserSessionsController do
   fixtures :users
 
@@ -22,4 +26,38 @@ describe UserSessionsController do
       it { is_expected.to redirect_to(account_path) }
     end
   end
+
+  describe 'logging out' do
+    let(:login_uri) do
+      "#{request.protocol}#{request.host_with_port}/login"
+    end
+
+    let(:id_token) { 'fake_id_token' }
+
+    let(:expected_login_dot_gov_logout_uri) do
+      base_uri = URI(Rails.application.secrets.login_dot_gov[:idp_base_url])
+      URI::HTTPS.build(
+        host: base_uri.host,
+        path: '/openid_connect/logout',
+        query: {
+          id_token_hint: id_token,
+          post_logout_redirect_uri: login_uri,
+          state: '1234567890123456789012'
+        }.to_query
+      ).to_s
+    end
+
+    before do
+      activate_authlogic
+      session[:id_token] = id_token
+    end
+
+    include_context 'approved user logged in'
+
+    before { delete :destroy }
+
+    it { is_expected.to redirect_to(expected_login_dot_gov_logout_uri) }
+  end
 end
+
+# rubocop:enable RSpec/ScatteredSetup
