c6ca3d7c09bc785a67195c42f3db905612fc3c0a
John Maxwell <jmaxwell@fearless.tech>
2021-03-18 16:51:40 -0400

    Backed out changes that broke our code coverage tool.

--- spec/lib/tasks/youtube_spec.rb
+++ spec/lib/tasks/youtube_spec.rb
@@ -1,22 +1,31 @@
 # frozen_string_literal: true
 
 describe 'YouTube rake tasks' do
-  before(:all) { Rails.application.load_tasks }
+  let(:rake) { Rake::Application.new }
+
+  before do
+    Rake.application = rake
+    $LOADED_FEATURES.reject! { |f| f == 'tasks/youtube.rake' }
+    rake.rake_require('tasks/youtube')
+  end
 
   describe 'usasearch:youtube:refresh' do
     let(:task_name) { 'usasearch:youtube:refresh' }
 
-    before { allow(YoutubeData).to receive(:refresh) }
-
-    after { Rake::Task[task_name].reenable }
-
-    it "has 'environment' as a prereq" do
-      expect(Rake::Task[task_name].prerequisites).to include('environment')
+    it "has 'environment' as a prerequisite" do
+      expect(rake[task_name].prerequisites).to include('environment')
     end
 
-    it 'runs YoutubeData.refresh' do
-      Rake::Task[task_name].invoke
-      expect(YoutubeData).to have_received :refresh
+    describe 'when invoked' do
+      before do
+        allow(YoutubeData).to receive(:refresh)
+        Rake::Task.define_task(:environment)
+        rake[task_name].invoke
+      end
+
+      it 'runs YoutubeData.refresh' do
+        expect(YoutubeData).to have_received(:refresh)
+      end
     end
   end
 end
