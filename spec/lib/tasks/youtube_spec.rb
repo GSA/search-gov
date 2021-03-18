c4b1fc4b992f4f005eb7a36386bf1d462f871536
John Maxwell <jmaxwell@fearless.tech>
2021-03-18 15:59:21 -0400

    SRCH-1889 - More code review cleanup.

--- spec/lib/tasks/youtube_spec.rb
+++ spec/lib/tasks/youtube_spec.rb
@@ -1,31 +1,22 @@
 # frozen_string_literal: true
 
 describe 'YouTube rake tasks' do
-  let(:rake) { Rake::Application.new }
-
-  before do
-    Rake.application = rake
-    $LOADED_FEATURES.reject! { |f| f == 'tasks/youtube.rake' }
-    rake.rake_require('tasks/youtube')
-  end
+  before(:all) { Rails.application.load_tasks }
 
   describe 'usasearch:youtube:refresh' do
     let(:task_name) { 'usasearch:youtube:refresh' }
 
-    it "has 'environment' as a prerequisite" do
-      expect(rake[task_name].prerequisites).to include('environment')
+    before { allow(YoutubeData).to receive(:refresh) }
+
+    after { Rake::Task[task_name].reenable }
+
+    it "has 'environment' as a prereq" do
+      expect(Rake::Task[task_name].prerequisites).to include('environment')
     end
 
-    describe 'when invoked' do
-      before do
-        allow(YoutubeData).to receive(:refresh)
-        Rake::Task.define_task(:environment)
-        rake[task_name].invoke
-      end
-
-      it 'runs YoutubeData.refresh' do
-        expect(YoutubeData).to have_received(:refresh)
-      end
+    it 'runs YoutubeData.refresh' do
+      Rake::Task[task_name].invoke
+      expect(YoutubeData).to have_received :refresh
     end
   end
 end
