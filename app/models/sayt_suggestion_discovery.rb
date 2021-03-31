f999eace54044c66ba1aeb78ecb4ab4b95f285d8
Martha Thompson <437455+MothOnMars@users.noreply.github.com>
2021-03-31 13:21:35 -0400

    SRCH-1899 decrease suggestion discovery threshold (#677)
    - resolve some Rubocop offenses

--- app/models/sayt_suggestion_discovery.rb
+++ app/models/sayt_suggestion_discovery.rb
@@ -6,10 +6,10 @@ class SaytSuggestionDiscovery
   @queue = :primary
 
   class << self
-    MIN_DOC_COUNT = 100
+    MIN_DOC_COUNT = 30
 
     def perform(affiliate_name, affiliate_id, day_int, limit)
-      day = Date.strptime(day_int.to_s, "%Y%m%d")
+      day = Date.strptime(day_int.to_s, '%Y%m%d')
       run_rate_factor = Date.current == day ? compute_run_rate_factor : 1.0
       top_n_exists_query = TopNExistsQuery.new(affiliate_name,
                                                'search',
@@ -39,7 +39,7 @@ class SaytSuggestionDiscovery
     end
 
     def compute_run_rate_factor
-      1/ DateTime.current.day_fraction.to_f
+      1 / DateTime.current.day_fraction.to_f
     end
   end
 end
