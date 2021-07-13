8522c5911d8cccfa36bf709f907d9cb85af550c4
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2021-07-13 11:22:26 -0400

    SRCH-2381 add cucumber test for type-ahead suggestions

--- features/step_definitions/sayt_suggestions_steps.rb
+++ features/step_definitions/sayt_suggestions_steps.rb
@@ -9,10 +9,6 @@ Then /^I should see (\d+) related searches$/ do |count|
   page.should have_selector('#related-searches a', count: count)
 end
 
-Then /^I should see (\d+) type\-ahead search suggestions$/ do |count|
-  page.should have_selector("#sayt-suggestions .sayt_suggestion", :count => count)
-end
-
-Then /^I should not see any type\-ahead search suggestion$/ do
-  page.should_not have_selector("#sayt-suggestions .sayt_suggestion")
+Then /^I should see a suggestion to search for "(.*)"/ do |query|
+  expect(page.find('div.tt-suggestion', visible: :all).text(:all)).to eq query
 end
