4b9b7146982e757f806b28b212541c8a401807a6
Martha Thompson <437455+MothOnMars@users.noreply.github.com>
2021-07-16 14:06:04 -0400

    SRCH-2106 remove legacy assets (#750)
    - update asset-related gems

--- app/assets/javascripts/legacy/jquery.popup.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-    $.fn.simplePopup = function(options) {
-        this.click(function() {
-            window.open(this.href, '_blank', options);
-            return false;
-        });
-    }
-})(jQuery);
