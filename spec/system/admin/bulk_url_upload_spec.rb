7cf8172b5e99a17085074a041db7d50dcc06a147
John Maxwell <jmaxwell@fearless.tech>
2021-07-27 18:41:32 -0400

    Code review comments.

--- spec/system/admin/bulk_url_upload_spec.rb
+++ spec/system/admin/bulk_url_upload_spec.rb
@@ -1,35 +1,5 @@
 # frozen_string_literal: true
 
-shared_examples 'a successful bulk upload' do
-  it 'sends us back to the bulk upload page' do
-    do_bulk_upload
-    expect(page).to have_text('Bulk Search.gov URL Upload')
-  end
-
-  it 'shows a confirmation message' do
-    do_bulk_upload
-    expect(page).to have_text(
-      <<~CONFIRMATION_MESSAGE
-        Successfully uploaded #{upload_filename} for processing.
-        The results will be emailed to you.
-      CONFIRMATION_MESSAGE
-    )
-  end
-
-  it 'creates the URLs' do
-    do_bulk_upload
-
-    urls.each do |url|
-      expect(SearchgovUrl.find_by(url: url)).not_to be_blank
-    end
-  end
-
-  it 're-indexes the domains for the URLs' do
-    do_bulk_upload
-    expect(@reindexed_domains).to eq(searchgov_domains)
-  end
-end
-
 shared_examples 'a failed bulk upload with error' do |error_message|
   it 'sends us back to the bulk upload page' do
     do_bulk_upload
@@ -42,20 +12,20 @@ shared_examples 'a failed bulk upload with error' do |error_message|
   end
 end
 
-def do_bulk_upload
-  perform_enqueued_jobs do
-    visit url
-    attach_file('bulk_upload_urls', upload_file)
-    click_button('Upload')
-  end
-end
-
 describe 'Bulk URL upload' do
   include ActiveJob::TestHelper
 
+  subject(:do_bulk_upload) do
+    perform_enqueued_jobs do
+      visit url
+      attach_file('bulk_upload_urls', upload_file)
+      click_button('Upload')
+    end
+  end
+
   let(:url) { '/admin/bulk_url_upload' }
   let(:upload_file) { file_fixture("txt/#{upload_filename}") }
-  let(:urls) { File.open(upload_file, 'r:bom|utf-8').readlines.map(&:strip).map { |url| URI.escape(url) } }
+  let(:urls) { File.open(upload_file, 'r:bom|utf-8').readlines.map(&:strip) }
   let(:searchgov_domains) do
     urls.reduce(Set.new) do |searchgov_domains, raw_url|
       parsed_url = URI(raw_url)
@@ -75,45 +45,73 @@ describe 'Bulk URL upload' do
 
   it_behaves_like 'a page restricted to super admins'
 
-  describe 'bulk uploading a file of URLs' do
+  context 'when a super admin is logged in' do
     include_context 'log in super admin'
 
-    let(:upload_filename) { 'good_url_file.txt' }
+    describe 'bulk uploading a file of URLs' do
+      let(:upload_filename) { 'good_url_file.txt' }
 
-    it_behaves_like 'a successful bulk upload'
-  end
+      it 'sends us back to the bulk upload page' do
+        do_bulk_upload
+        expect(page).to have_text('Bulk Search.gov URL Upload')
+      end
 
-  describe 'bulk uploading a UTF-8 file of URLs' do
-    include_context 'log in super admin'
+      it 'shows a confirmation message' do
+        do_bulk_upload
+        expect(page).to have_text(
+          <<~CONFIRMATION_MESSAGE
+            Successfully uploaded #{upload_filename} for processing.
+            The results will be emailed to you.
+          CONFIRMATION_MESSAGE
+        )
+      end
 
-    let(:upload_filename) { 'utf8_urls.txt' }
+      it 'creates the URLs' do
+        do_bulk_upload
 
-    it_behaves_like 'a successful bulk upload'
-  end
+        urls.each do |url|
+          expect(SearchgovUrl.find_by(url: url)).not_to be_blank
+        end
+      end
 
-  describe 'trying to bulk upload a file of URLs when there is no file attached' do
-    include_context 'log in super admin'
+      it 're-indexes the domains for the URLs' do
+        do_bulk_upload
+        expect(@reindexed_domains).to eq(searchgov_domains)
+      end
+    end
 
-    let(:upload_file) { nil }
+    describe 'bulk uploading a UTF-8 file of URLs' do
+      let(:upload_filename) { 'utf8_urls.txt' }
 
-    it_behaves_like 'a failed bulk upload with error', 'Please choose a file to upload'
-  end
+      it 'loads the urls' do
+        do_bulk_upload
+        puts SearchgovUrl.pluck(:url)
 
-  describe 'trying to bulk upload a file of URLs that is not a text file' do
-    include_context 'log in super admin'
+        expect(SearchgovUrl.pluck(:url)).to include(
+          'https://www.agency.gov/foo%20%C2%A7%20208.pdf?open',
+          'https://www.agency.gov/Asesor%E2%88%9A%E2%89%A0a.pdf'
+        )
+      end
+    end
 
-    let(:upload_filename) { 'bogus_url_file.docx' }
-    let(:upload_file) { file_fixture('word/bogus_url_file.docx') }
+    describe 'trying to bulk upload a file of URLs when there is no file attached' do
+      let(:upload_file) { nil }
 
-    it_behaves_like 'a failed bulk upload with error',
-                    'Files of type application/vnd.openxmlformats-officedocument.wordprocessingml.document are not supported'
-  end
+      it_behaves_like 'a failed bulk upload with error', 'Please choose a file to upload'
+    end
 
-  describe 'trying to bulk upload a file of URLs that is too big' do
-    include_context 'log in super admin'
+    describe 'trying to bulk upload a file of URLs that is not a text file' do
+      let(:upload_filename) { 'bogus_url_file.docx' }
+      let(:upload_file) { file_fixture('word/bogus_url_file.docx') }
 
-    let(:upload_filename) { 'too_big_url_file.txt' }
+      it_behaves_like 'a failed bulk upload with error',
+                      'Files of type application/vnd.openxmlformats-officedocument.wordprocessingml.document are not supported'
+    end
 
-    it_behaves_like 'a failed bulk upload with error', 'too_big_url_file.txt is too big; please split it'
+    describe 'trying to bulk upload a file of URLs that is too big' do
+      let(:upload_filename) { 'too_big_url_file.txt' }
+
+      it_behaves_like 'a failed bulk upload with error', 'too_big_url_file.txt is too big; please split it'
+    end
   end
 end
