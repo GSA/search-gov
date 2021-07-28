182d6da29db681458f9fb5937bbc8f255d23e7a5
Martha Thompson <437455+MothOnMars@users.noreply.github.com>
2021-07-28 10:58:24 -0400

    SRCH-2334 remove WhitelistedV1ApiHandle class, table, & code (#757)

--- app/controllers/admin/whitelisted_v1_api_handles_controller.rb
+++
@@ -1,3 +0,0 @@
-class Admin::WhitelistedV1ApiHandlesController < Admin::AdminController
-  active_scaffold :whitelisted_v1_api_handle
-end
--- spec/models/whitelisted_v1_api_handle_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-describe WhitelistedV1ApiHandle do
-  it { is_expected.to validate_presence_of :handle }
-  it { is_expected.to validate_uniqueness_of(:handle).case_insensitive }
-end
