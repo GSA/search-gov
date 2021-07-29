de4a6df655e29ba87ba53a207598a692d7295898
John Maxwell <jmaxwell@fearless.tech>
2021-07-29 14:09:58 -0400

    CodeClimate complaint
    Moved the new spec for BulkUrlUploader::Results out to its own file,
    per CodeClimate's wishes.

--- spec/services/bulk_url_uploader_spec.rb
+++ spec/services/bulk_url_uploader_spec.rb
@@ -72,25 +72,3 @@ describe BulkUrlUploader do
     end
   end
 end
-
-describe BulkUrlUploader::Results do
-  subject(:results) { described_class.new('test bulk uploader') }
-
-  describe '#errors' do
-    context 'when there are "url not found" errors' do
-      before do
-        results.add_error('Validation failed: Url has already been taken', 'junk')
-        results.add_error('Validation failed: SearchgovDomain is not a valid SearchgovDomain', 'junk')
-      end
-
-      it 'puts them at the end of the list' do
-        expect(results.error_messages).to eq(
-          [
-            'Validation failed: SearchgovDomain is not a valid SearchgovDomain',
-            'Validation failed: Url has already been taken'
-          ]
-        )
-      end
-    end
-  end
-end
