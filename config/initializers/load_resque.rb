64ae817a482cd8c250c701337e497e1a4fe2a8b5
jmax-fearless <66315527+jmax-fearless@users.noreply.github.com>
2021-08-23 13:39:18 -0400

    SRCH-2249 Remove airbrake (#766)
    * SRCH-2249 Remove airbrake
    
    Ripped out all traces of airbrake and ran `bundle update`.

--- config/initializers/load_resque.rb
+++ config/initializers/load_resque.rb
@@ -1,4 +1,3 @@
-require 'airbrake/resque'
 require 'resque/failure/multiple'
 require 'resque/failure/redis'
 require 'resque-timeout'
@@ -6,7 +5,7 @@ require 'resque-timeout'
 config = Rails.application.secrets.system_redis
 Resque.redis = [config[:host], config[:port]].join(':')
 
-Resque::Failure::Multiple.classes = [Resque::Failure::Redis, Resque::Failure::Airbrake]
+Resque::Failure::Multiple.classes = [Resque::Failure::Redis]
 Resque::Failure.backend = Resque::Failure::Multiple
 
 Resque::Plugins::Timeout.timeout = 1.hour
