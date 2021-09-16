a0aee04d7e59760897a50c5637809c68e632e613
John Maxwell <jmaxwell@fearless.tech>
2021-09-16 15:16:19 -0400

    Fix broken Cucumber tests

--- features/step_definitions/superfresh_steps.rb
+++ features/step_definitions/superfresh_steps.rb
@@ -12,10 +12,10 @@ end
 Given /^the following IndexedDocuments exist:$/ do |table|
   table.hashes.each do |hash|
     attributes = hash.except('published_ago')
-    attributes[:affiliate] &&= Affiliate.find_by_name(attributes[:affiliate])
-    attributes[:doctype] ||= 'html'
-    if hash[:published_ago].present?
-      attributes[:published_at] = eval(hash[:published_ago].gsub(/ /, '.'))
+    attributes['affiliate'] &&= Affiliate.find_by_name(attributes['affiliate'])
+    attributes['doctype'] ||= 'html'
+    if hash['published_ago'].present?
+      attributes['published_at'] = eval(hash['published_ago'].gsub(/ /, '.'))
     end
     IndexedDocument.create! attributes
   end
