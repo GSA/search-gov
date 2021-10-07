bc1067e1c821889b3103bfb4deeab26b2b1b195a
jmax-fearless <66315527+jmax-fearless@users.noreply.github.com>
2021-10-07 13:06:52 -0400

    SRCH-2532 split bing requests (#802)
    * SRCH-2532 Split Requests to Bing Web and Image Queries
    
    Moved Bing endpoint and api key code down from the root BingSearch
    class into its children, as they no longer all have the same endpoint
    and api key.

--- app/engines/bing_v7_web_search.rb
+++ app/engines/bing_v7_web_search.rb
@@ -1,8 +1,19 @@
+# frozen_string_literal: true
+
 class BingV7WebSearch < BingSearch
-  API_ENDPOINT = '/bing/v7.0/search'.freeze
-  include BingV7HostedSubscriptionKey
+  API_ENDPOINT = '/v7.0/search'
+  API_HOST = 'https://api.bing.microsoft.com'
 
   self.api_endpoint = API_ENDPOINT
   self.api_cache_namespace = 'bing_v7'
   self.response_parser_class = BingWebResponseParser
+
+  def self.api_host
+    API_HOST
+  end
+
+  def hosted_subscription_key
+    @hosted_subscription_key ||=
+      Rails.application.secrets.bing_v7[:web_subscription_id].freeze
+  end
 end
