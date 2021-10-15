386c05be7bae8cdaad2d964e8e17f8fa8d6985a4
John Maxwell <jmaxwell@fearless.tech>
2021-10-15 13:40:13 -0400

    Updated cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,4 +1,10 @@
+# frozen_string_literal: true
 # Be sure to restart your server when you modify this file.
+
+# Avoid CORS issues when API is called from the frontend app.
+# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
+
+# Read more: https://github.com/cyu/rack-cors
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
@@ -9,18 +15,3 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource '/sayt', headers: :any, methods: %i[get head options]
   end
 end
-
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
