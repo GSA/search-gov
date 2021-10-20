c8f2f0a9c6efe7905f38a32b726f186b0da09925
jmax-fearless <66315527+jmax-fearless@users.noreply.github.com>
2021-10-20 15:09:01 -0400

    SRCH-2555 Remove old rails version settings from application.rb (#812)
    * SRCH-2555 Remove old rails version settings from application.rb
    
    * Enable per-form CSRF tokens.
    * Turn on origin-checking CSRF mitigation.
    * Turn on timezone preservation in to_time
    * Removed comment cruft
    * Move constants out of application.rb
    * Removed ancient copy-on-write settings
    * Moved NewRelic setup code out to its own initializer.
    * Re-arranged requires and removed the one for csv.

--- config/application.rb
+++ config/application.rb
@@ -5,10 +5,8 @@ require './lib/middlewares/reject_invalid_request_uri.rb'
 require './lib/middlewares/downcase_route.rb'
 require './lib/middlewares/adjust_client_ip.rb'
 require './lib/middlewares/filtered_jsonp.rb'
+require 'resque/plugins/priority'
 
-
-GC.copy_on_write_friendly = true if GC.respond_to?(:copy_on_write_friendly=)
-GC::Profiler.enable
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -56,10 +54,6 @@ module Usasearch
     config.middleware.use AdjustClientIp
     config.middleware.use FilteredJSONP
 
-    # Only load the plugins named here, in the order given (default is alphabetical).
-    # :all can be used as a placeholder for all plugins not explicitly named.
-    # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
     # Activate observers that should always be running, except during DB migrations.
     unless File.basename($0) == "rake" && ARGV.include?("db:migrate")
       config.active_record.observers = :sayt_filter_observer, :misspelling_observer, :indexed_document_observer,
@@ -67,32 +61,9 @@ module Usasearch
         :i14y_drawer_observer, :routed_query_keyword_observer, :watcher_observer
     end
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
     # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
     # the I18n.default_locale when a translation can not be found)
     config.i18n.fallbacks = true
-
-    # JavaScript files you want as :defaults (application.js is always included).
-    # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
-
-    # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
-
-    config.generators do |g|
-      g.test_framework :rspec
-    end
-
-    config.assets.enabled = true
-    config.assets.version = '1.0'
-
-    config.active_record.schema_format = :ruby
 
     config.i18n.enforce_available_locales = false
 
@@ -101,29 +72,8 @@ module Usasearch
 
     config.active_job.queue_adapter = :resque
 
-    ### Rails 5.0 config flags
-    ### SRCH-1058: The flags below should be flipped one by one to the new default.
-
-    # Enable per-form CSRF tokens. Versions before Rails 5.0 had false.
-    config.action_controller.per_form_csrf_tokens = false
-
-    # Enable origin-checking CSRF mitigation.  Versions before Rails 5.0 had false.
-    config.action_controller.forgery_protection_origin_check = false
-
-    # Require `belongs_to` associations by default. Versions before Rails 5.0 had false.
+    # Require `belongs_to` associations by default. Versions before
+    # Rails 5.0 had false.
     config.active_record.belongs_to_required_by_default = false
-
-    # Make Ruby 2.4+ preserve the timezone of the receiver when calling `to_time`.
-    # Versions before Rails 5.0 had false.
-    ActiveSupport.to_time_preserves_timezone = false
-
-    ### End Rails 5.0 config flags
   end
 end
-
-
-SEARCH_ENGINES = %w(BingV6 BingV7 Google SearchGov).freeze
-DEFAULT_USER_AGENT = Rails.application.secrets.organization[:default_user_agent].freeze
-
-require 'resque/plugins/priority'
-require 'csv'
---
+++ config/initializers/new_relic.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# For NewRelic heap monitoring. See
+# https://docs.newrelic.com/docs/agents/ruby-agent/features/ruby-vm-measurements/#minimum_requirements
+GC::Profiler.enable
+
