36f43d7b5f74db13308985531ff7ad88ade5bda0
John Maxwell <jmaxwell@fearless.tech>
2021-10-21 11:36:55 -0400

    Code review comments
    Re-arranged .rubocop.yml
    Cleaned up initializers constants.rb and new_relic.rb

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,3 +1,4 @@
-SEARCH_ENGINES = %w(BingV6 BingV7 Google SearchGov).freeze
-DEFAULT_USER_AGENT = Rails.application.secrets.organization[:default_user_agent].freeze
+# frozen_string_literal: true
 
+SEARCH_ENGINES = %w[BingV6 BingV7 Google SearchGov].freeze
+DEFAULT_USER_AGENT = Rails.application.secrets.organization[:default_user_agent].freeze
--- config/initializers/new_relic.rb
+++ config/initializers/new_relic.rb
@@ -3,4 +3,3 @@
 # For NewRelic heap monitoring. See
 # https://docs.newrelic.com/docs/agents/ruby-agent/features/ruby-vm-measurements/#minimum_requirements
 GC::Profiler.enable
-
