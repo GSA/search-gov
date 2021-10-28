22fcfc21f279d6cfdf608c2eff0600dcdd593e13
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2021-10-28 10:04:52 -0400

    mark remaining i18n specs as pending SRCH-2592

--- spec/i18n_spec.rb
+++ spec/i18n_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe I18n do
   let(:inconsistent_interpolations) { i18n.inconsistent_interpolations }
 
   it 'does not have missing keys' do
-    pending('to be resolved by SRCH-649')
+    pending('to be resolved by SRCH-2592')
     expect(missing_keys).to be_empty,
                             "Missing #{missing_keys.leaves.count} i18n keys, run `i18n-tasks missing' to show them"
   end
@@ -33,6 +33,7 @@ RSpec.describe I18n do
   end
 
   it 'does not have inconsistent interpolations' do
+    pending('to be resolved by SRCH-2592')
     error_message = "#{inconsistent_interpolations.leaves.count} i18n keys have inconsistent interpolations.\n" \
                     "Run `i18n-tasks check-consistent-interpolations' to show them"
     expect(inconsistent_interpolations).to be_empty, error_message
