9f96cf4188ac4af4ac1dda67f036b8259edc8331
Martha Thompson <437455+MothOnMars@users.noreply.github.com>
2021-12-01 11:53:54 -0500

    SRCH-2601 restrict resque-web access to super admins (#825)
    * SRCH-2601 restrict resque-web access to super admins
    - add unique index on users.persistence_token
    - clean up some Rubocop offenses in the User spec

--- config/routes.rb
+++ config/routes.rb
@@ -252,6 +252,9 @@ Rails.application.routes.draw do
         end
       end
     end
+
+    mount Resque::Server.new, at: '/resque', constraints: AffiliateAdminConstraint
+    get '/resque/(*all)', to: redirect(path: '/login')
   end
 
   match '/admin/affiliates/:id/analytics' => 'admin/affiliates#analytics', :as => :affiliate_analytics_redirect, via: :get
