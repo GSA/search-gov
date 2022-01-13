763ab29f93e5db935fadff9c58af6a6a4a376787
Lora Woodford <lora.woodford@gsa.gov>
2022-01-13 12:07:50 -0500

    SRCH-2726 update spelling suggestions helper tests

--- spec/helpers/spelling_suggestions_helper_spec.rb
+++ spec/helpers/spelling_suggestions_helper_spec.rb
@@ -11,9 +11,10 @@ describe SpellingSuggestionsHelper do
   end
 
   describe '#spelling_suggestion(search, search_options)' do
-    it 'should return HTML escaped output containing the initial query with site_limits and the suggestion' do
+    it 'returns HTML escaped output containing the initial query with site_limits and the suggestion' do
       html = helper.spelling_suggestion(search, site_limits: 'blogs.cdc.gov/niosh-science-blog www.cdc.gov/niosh')
-      expect(html).to have_link('<suggestion>', '/search?affiliate=usasearch&amp;query=%3Csuggestion%3E&amp;sitelimit=blogs.cdc.gov%2Fniosh-science-blog+www.cdc.gov%2Fniosh')
+      expect(html).to have_link('<suggestion>',
+                                href: '/search?affiliate=usasearch&query=%3Csuggestion%3E&sitelimit=blogs.cdc.gov%2Fniosh-science-blog+www.cdc.gov%2Fniosh')
       expect(html).to have_content('Showing results for <suggestion>')
       expect(html).to have_content('Search instead for <initialquery>')
     end
