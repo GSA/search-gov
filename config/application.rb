6d2ea8f78989a66513e4bbed655b8c9cc8de33fb
Lora Woodford <lora.woodford@gsa.gov>
2022-01-13 09:32:08 -0500

    SRCH-2697 Upgrade to Rails 6.1

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 require './lib/middlewares/reject_invalid_request_uri.rb'
 require './lib/middlewares/downcase_route.rb'
 require './lib/middlewares/adjust_client_ip.rb'
@@ -16,6 +16,14 @@ module Usasearch
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
