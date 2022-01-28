53ea2625186d77eb4a88dff1e98464e631379813
Lora Woodford <lora.woodford@gsa.gov>
2022-01-28 10:56:14 -0500

    SRCH-2764 Retire PhantomJS, adopt Selenium

--- features/step_definitions/switch_button_steps.rb
+++ features/step_definitions/switch_button_steps.rb
@@ -4,9 +4,11 @@ Then /^the "(.+)" should be switched (on|off)$/ do |label, status|
 end
 
 When /^I switch (on|off) "(.+)"$/ do |status, label|
+  # To switch on, you click the visible text "off", and vice versa.
+  opp_status = (status == 'on' ? 'OFF' : 'ON')
   cell_id = "##{label.downcase.gsub(' ', '_')}_switch"
   within cell_id do
-    find('span', text: status.upcase).trigger('click')
+    find('span', text: opp_status).click
   end
   selector = "#{cell_id} .switch-#{status}"
   find selector
