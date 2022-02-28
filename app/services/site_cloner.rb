c094fbf0f37c2b29282c3494724890695fef0e25
Lora Woodford <lora.woodford@gsa.gov>
2022-02-28 08:23:08 -0500

    rubocop fixes

--- app/services/site_cloner.rb
+++ app/services/site_cloner.rb
@@ -8,78 +8,78 @@ class SiteCloner
   end
 
   def clone
-    assign_unique_navigation_positions @origin_site
+    assign_unique_navigation_positions(@origin_site)
 
     cloned_site = create_site_shallow_copy
 
-    clone_simple_associations cloned_site
-    clone_boosted_contents cloned_site
-    clone_document_collections cloned_site
-    clone_featured_collections cloned_site
-    clone_flickr_profiles cloned_site
-    clone_image_search_label cloned_site
-    clone_routed_queries cloned_site
-    clone_rss_feeds cloned_site
-    clone_site_feed_url cloned_site
-    clone_images cloned_site
+    clone_simple_associations(cloned_site)
+    clone_boosted_contents(cloned_site)
+    clone_document_collections(cloned_site)
+    clone_featured_collections(cloned_site)
+    clone_flickr_profiles(cloned_site)
+    clone_image_search_label(cloned_site)
+    clone_routed_queries(cloned_site)
+    clone_rss_feeds(cloned_site)
+    clone_site_feed_url(cloned_site)
+    clone_images(cloned_site)
 
     cloned_site.instagram_profile_ids = @origin_site.instagram_profile_ids
     cloned_site.youtube_profile_ids = @origin_site.youtube_profile_ids
 
-    clone_memberships cloned_site
+    clone_memberships(cloned_site)
 
     cloned_site
   end
 
   def create_site_shallow_copy
     dup = @origin_site.dup
-    dup.assign_attributes display_name: @target_display_name,
-                          name: @target_handle
+    dup.assign_attributes(display_name: @target_display_name,
+                          name: @target_handle)
     dup.save!
     dup
   end
 
   def clone_simple_associations(cloned_site)
-    clone_associations @origin_site,
+    clone_associations(@origin_site,
                        cloned_site,
                        :affiliate_twitter_settings,
                        :connections,
                        :excluded_urls,
                        :i14y_memberships,
                        :indexed_documents,
-                       :site_domains
+                       :site_domains)
   end
 
   def clone_image_search_label(cloned_site)
     origin_label = @origin_site.image_search_label
     cloned_label = cloned_site.image_search_label
     cloned_label.update!(name: origin_label.name)
-    copy_navigation_attributes origin_label.navigation, cloned_label.navigation
+    copy_navigation_attributes(origin_label.navigation, cloned_label.navigation)
   end
 
   def clone_boosted_contents(cloned_site)
-    clone_association_with_children @origin_site,
+    clone_association_with_children(@origin_site,
                                     cloned_site,
                                     :boosted_contents,
-                                    :boosted_content_keywords
+                                    :boosted_content_keywords)
   end
 
   def clone_document_collections(cloned_site)
     @origin_site.document_collections.each do |dc|
       cloned_dc = dc.dup
-      clone_associations dc, cloned_dc, :url_prefixes
+      clone_associations(dc, cloned_dc, :url_prefixes)
       cloned_site.document_collections << cloned_dc
 
-      copy_navigation_attributes dc.navigation, cloned_dc.navigation
+      copy_navigation_attributes(dc.navigation, cloned_dc.navigation)
     end
   end
 
   def clone_featured_collections(cloned_site)
-    clone_association_with_children @origin_site,
+    clone_association_with_children(@origin_site,
                                     cloned_site,
                                     :featured_collections,
                                     :featured_collection_keywords,
-                                    :featured_collection_links
+                                    :featured_collection_links)
   end
 
   def clone_flickr_profiles(cloned_site)
@@ -90,16 +90,16 @@ class SiteCloner
   end
 
   def clone_memberships(cloned_site)
-    clone_associations @origin_site, cloned_site, :memberships
+    clone_associations(@origin_site, cloned_site, :memberships)
   end
 
   def clone_routed_queries(cloned_site)
     ActiveRecord::Base.observers.disable(:routed_query_keyword_observer)
 
-    clone_association_with_children @origin_site,
+    clone_association_with_children(@origin_site,
                                     cloned_site,
                                     :routed_queries,
-                                    :routed_query_keywords
+                                    :routed_query_keywords)
   ensure
     ActiveRecord::Base.observers.enable(:routed_query_keyword_observer)
   end
@@ -110,7 +110,7 @@ class SiteCloner
       cloned_feed.rss_feed_urls = rss_feed.rss_feed_urls
       cloned_site.rss_feeds << cloned_feed
 
-      copy_navigation_attributes rss_feed.navigation, cloned_feed.navigation
+      copy_navigation_attributes(rss_feed.navigation, cloned_feed.navigation)
     end
   end
 
@@ -139,9 +139,7 @@ class SiteCloner
   def available_handle_from(existing_handle)
     possibly_shortened_handle = existing_handle.first(Affiliate::MAX_NAME_LENGTH - 1)
     candidate = "#{possibly_shortened_handle}1"
-    while Affiliate.exists?(name: candidate)
-      candidate.succ!
-    end
+    candidate.succ! while Affiliate.exists?(name: candidate)
     candidate
   end
 
@@ -149,16 +147,16 @@ class SiteCloner
     origin_model.send(association).each do |parent_model|
       cloned_parent_model = parent_model.dup
       child_associations.each do |child_association|
-        clone_associations parent_model, cloned_parent_model, child_association
+        clone_associations(parent_model, cloned_parent_model, child_association)
       end
-      cloned_model.send(association).send(:'<<', cloned_parent_model)
+      cloned_model.send(association).send(:<<, cloned_parent_model)
     end
   end
 
   def clone_associations(origin_model, cloned_model, *associations)
     associations.each do |association|
       dups = origin_model.send(association).map(&:dup)
-      cloned_model.send :"#{association}=", dups
+      cloned_model.send(:"#{association}=", dups)
     end
   end
 
