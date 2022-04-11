685276dcab91a9eed610f2bba704be18ddf458c8
Lora Woodford <lora.woodford@gsa.gov>
2022-04-11 12:13:47 -0400

    Rubocop to remove freezes, add newline

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -2,7 +2,7 @@
 
 ADMIN_EMAIL_ADDRESS = Rails.application.secrets.organization[:admin_email_address].freeze
 DEFAULT_USER_AGENT = Rails.application.secrets.organization[:default_user_agent].freeze
-DELIVER_FROM_EMAIL_ADDRESS = 'no-reply@support.digitalgov.gov'.freeze
-NOTIFICATION_SENDER_EMAIL_ADDRESS = 'notification@support.digitalgov.gov'.freeze
+DELIVER_FROM_EMAIL_ADDRESS = 'no-reply@support.digitalgov.gov'
+NOTIFICATION_SENDER_EMAIL_ADDRESS = 'notification@support.digitalgov.gov'
 SEARCH_ENGINES = %w[BingV6 BingV7 Google SearchGov].freeze
 SUPPORT_EMAIL_ADDRESS = Rails.application.secrets.organization[:support_email_address].freeze
