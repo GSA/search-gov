fc4c5cfcf362d972799cccb5a7217ef85e953b00
Lora Woodford <lora.woodford@gsa.gov>
2022-04-15 11:16:43 -0400

    Remove freezes from secrets

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-ADMIN_EMAIL_ADDRESS = Rails.application.secrets.organization[:admin_email_address].freeze
-DEFAULT_USER_AGENT = Rails.application.secrets.organization[:default_user_agent].freeze
+ADMIN_EMAIL_ADDRESS = Rails.application.secrets.organization[:admin_email_address]
+DEFAULT_USER_AGENT = Rails.application.secrets.organization[:default_user_agent]
 DELIVER_FROM_EMAIL_ADDRESS = 'no-reply@support.digitalgov.gov'
 NOTIFICATION_SENDER_EMAIL_ADDRESS = 'notification@support.digitalgov.gov'
 SEARCH_ENGINES = %w[BingV6 BingV7 Google SearchGov].freeze
-SUPPORT_EMAIL_ADDRESS = Rails.application.secrets.organization[:support_email_address].freeze
+SUPPORT_EMAIL_ADDRESS = Rails.application.secrets.organization[:support_email_address]
