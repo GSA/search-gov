d865dd949601f2c9aa1925b0aa45ff3274a12688
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2022-06-17 16:29:49 -0400

    SRCH-3101 specify minimum_should_match in ElasticBlendedQuery

--- spec/support/elasticsearch_query_behavior.rb
+++ spec/support/elasticsearch_query_behavior.rb
@@ -1,15 +1,20 @@
-shared_examples_for 'a logstash query' do
+shared_examples_for 'an Elasticsearch query' do |index|
   describe '#body' do
     subject(:body) { query.body }
 
+    # Sanity-check to confirm that the query structure is valid
     it 'does not raise an error' do
       expect {
-        Es::ELK.client_reader.search(index: 'logstash-*',  body: body)
+        Es::ELK.client_reader.search(index: index,  body: body)
       }.not_to raise_error
     end
 
     it { is_expected.to eq(expected_body) }
   end
+end
+
+shared_examples_for 'a logstash query' do
+  it_behaves_like 'an Elasticsearch query', 'logstash-*'
 end
 
 shared_examples_for 'a watcher query' do
