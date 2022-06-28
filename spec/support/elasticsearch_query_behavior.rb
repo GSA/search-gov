fb1f4facb4abe0fd1015b7c502fc321d4bd6c68f
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2022-06-28 13:34:31 -0400

    rubocop cleanup

--- spec/support/elasticsearch_query_behavior.rb
+++ spec/support/elasticsearch_query_behavior.rb
@@ -1,12 +1,14 @@
+# frozen_string_literal: true
+
 shared_examples_for 'an Elasticsearch query' do |index|
   describe '#body' do
     subject(:body) { query.body }
 
     # Sanity-check to confirm that the query structure is valid
     it 'does not raise an error' do
-      expect {
-        Es::ELK.client_reader.search(index: index,  body: body)
-      }.not_to raise_error
+      expect do
+        Es::ELK.client_reader.search(index: index, body: body)
+      end.not_to raise_error
     end
 
     it { is_expected.to eq(expected_body) }
