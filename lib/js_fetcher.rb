b1d78e2f8f9faed8b69bc451e7cecf01a9681772
krbhavith <krbhavith@gmail.com>
2022-08-12 16:28:43 -0400

    execute html script from JS based page source

--- lib/js_fetcher.rb
+++ lib/js_fetcher.rb
@@ -4,7 +4,17 @@ module JsFetcher
   def self.fetch(url)
     options = Selenium::WebDriver::Chrome::Options.new(args: ['headless'])
     driver = Selenium::WebDriver.for(:chrome, options: options)
-    driver.get(url)
-    driver.page_source
+    wait = Selenium::WebDriver::Wait.new(:timeout => 5)
+
+    begin
+      driver.get(url)
+      wait.until{document_initialised driver}
+    ensure
+      driver.quit
+    end
+  end
+
+  def self.document_initialised(driver)
+    driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
   end
 end
