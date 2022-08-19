7c9eef59aeee512684cfc03c670f29ca8447325c
danswick <daniel.swick@gsa.gov>
2022-08-18 21:04:26 -0700

    add comment about user-agent for test purposes

--- lib/js_fetcher.rb
+++ lib/js_fetcher.rb
@@ -3,6 +3,8 @@ require 'selenium-webdriver'
 module JsFetcher
   def self.fetch(url)
     options = Selenium::WebDriver::Chrome::Options.new(args: ['headless'])
+    # Some test sites return a 403 when access with selenium webdriver.
+    # The following user-agent override should be removed when testing is complete.
     options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36")
     driver = Selenium::WebDriver.for(:chrome, options: options)
     wait = Selenium::WebDriver::Wait.new(:timeout => 5)
