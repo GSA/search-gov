dfa58407641605eb60da638b0ae26574cfbb23ec
Yogesh Asthana <yogesh.asthana@inadev.com>
2022-08-24 11:16:59 -0400

    SRCH-3276: did code refractor at uswds banner js

--- app/assets/javascripts/uswds.js
+++ app/assets/javascripts/uswds.js
@@ -2,13 +2,8 @@ var button = document.getElementsByClassName("usa-banner__button")[0];
 var banner_content = document.getElementsByClassName("usa-banner__content")[0];
 
 button.addEventListener("click", function(){
-  var is_hidden = banner_content.getAttribute("hidden");
-  if(is_hidden){
-    banner_content.removeAttribute("hidden");
-    this.setAttribute("aria-expanded", "true");
-  }
-  else{
-    banner_content.setAttribute("hidden", "true");
-    this.setAttribute("aria-expanded", "false");
-  }
+  banner_content.toggleAttribute("hidden");
+  this.setAttribute("aria-expanded", 
+    this.getAttribute("aria-expanded") === "true" 
+      ? "false" : "true");
 });
