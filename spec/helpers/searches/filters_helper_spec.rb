c3c1925224a2108fac12454168180169178a67b4
Yogesh Asthana <yogesh.asthana@inadev.com>
2022-08-25 12:52:01 -0400

    SRCH-2449: replaced {count} with {formatted_count} for results_count at locales, and added test to validate singular result

--- spec/helpers/searches/filters_helper_spec.rb
+++ spec/helpers/searches/filters_helper_spec.rb
@@ -7,5 +7,13 @@ describe Searches::FiltersHelper do
       allow(search).to receive_message_chain(:results, :present?) { true }
       expect(helper.results_count_html(search)).to have_content('1,555,888 results')
     end
+
+    context 'when there is only one result' do
+      let(:search) { double(Search, total: 1) }
+      it 'does not pluralize the result' do
+        allow(search).to receive_message_chain(:results, :present?) { true }
+        expect(helper.results_count_html(search)).to have_content('1 result')
+      end
+    end
   end
 end
