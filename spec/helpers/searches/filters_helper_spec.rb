89a28ee8dff5f340a5ae3fd43dec581b195d17d2
Yogesh Asthana <yogesh.asthana@inadev.com>
2022-08-25 13:35:00 -0400

    SRCH-2449: fixed rubocop issues

--- spec/helpers/searches/filters_helper_spec.rb
+++ spec/helpers/searches/filters_helper_spec.rb
@@ -3,13 +3,14 @@ require 'spec_helper'
 describe Searches::FiltersHelper do
   describe '#results_count_html' do
     it 'returns formatted results count' do
-      search = double(NewsSearch, total: 1_555_888)
+      search = instance_double(NewsSearch, total: 1_555_888)
       allow(search).to receive_message_chain(:results, :present?) { true }
       expect(helper.results_count_html(search)).to have_content('1,555,888 results')
     end
 
     context 'when there is only one result' do
-      let(:search) { double(Search, total: 1) }
+      let(:search) { instance_double(Search, total: 1) }
+
       it 'does not pluralize the result' do
         allow(search).to receive_message_chain(:results, :present?) { true }
         expect(helper.results_count_html(search)).to have_content('1 result')
