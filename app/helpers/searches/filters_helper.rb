c23bbff95985539d4b79b13a1988eef8e3de97f9
Yogesh Asthana <yogesh.asthana@inadev.com>
2022-08-25 15:01:41 -0400

    SRCH-2449: fixed rubocop issue

--- app/helpers/searches/filters_helper.rb
+++ app/helpers/searches/filters_helper.rb
@@ -125,13 +125,15 @@ module Searches::FiltersHelper
   end
 
   def results_count_html(search)
+    (return unless search.results.present)
+
     content_tag :li, id: 'results-count' do
       count = search.total
       result_count_str = I18n.t(:'searches.results_count',
                                 count: count,
                                 formatted_count: number_with_delimiter(count))
       content_tag :span, result_count_str
-    end if search.results.present?
+    end
   end
 
   def refine_search_html
--- spec/helpers/searches/filters_helper_spec.rb
+++ spec/helpers/searches/filters_helper_spec.rb
@@ -4,7 +4,7 @@ describe Searches::FiltersHelper do
   describe '#results_count_html' do
     it 'returns formatted results count' do
       search = instance_double(NewsSearch, total: 1_555_888)
-      allow(search).to receive_message_chain(:results, :present?) { true }
+      allow(search).to receive_message_chain(:results, :present) { true }
       expect(helper.results_count_html(search)).to have_content('1,555,888 results')
     end
 
@@ -12,7 +12,7 @@ describe Searches::FiltersHelper do
       let(:search) { instance_double(Search, total: 1) }
 
       it 'does not pluralize the result' do
-        allow(search).to receive_message_chain(:results, :present?) { true }
+        allow(search).to receive_message_chain(:results, :present) { true }
         expect(helper.results_count_html(search)).to have_content('1 result')
       end
     end
