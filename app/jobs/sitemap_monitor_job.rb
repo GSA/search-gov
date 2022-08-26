88aa9e327ef38cd59eb43ae39307164bdc185a5c
Bhavith Katpally <krbhavith@gmail.com>
2022-08-26 11:55:13 -0400

    SRCH-901 automatically re-check status of searchgov domain (#980)

--- app/jobs/sitemap_monitor_job.rb
+++ app/jobs/sitemap_monitor_job.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 class SitemapMonitorJob < ApplicationJob
   queue_as :sitemap
 
   def perform
-    SearchgovDomain.ok.each do |searchgov_domain|
-      searchgov_domain.index_sitemaps
-    end
+    SearchgovDomain.not_ok.find_each(&:check_status)
+    SearchgovDomain.ok.find_each(&:index_sitemaps)
   end
 end
