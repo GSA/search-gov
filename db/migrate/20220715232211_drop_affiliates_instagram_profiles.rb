2a6ad3e10d9dd575c8d92909385cc94686569901
Dan Swick <dan.swick@gmail.com>
2022-09-02 09:39:28 -0700

    Apply suggestions from code review
    Co-authored-by: Lora Woodford <lorawoodford@gmail.com>

--- db/migrate/20220715232211_drop_affiliates_instagram_profiles.rb
+++ db/migrate/20220715232211_drop_affiliates_instagram_profiles.rb
@@ -1,6 +1,6 @@
 class DropAffiliatesInstagramProfiles < ActiveRecord::Migration[6.1]
   def change
-    drop_table :affiliates_instagram_profiles do |t|
+    drop_table :affiliates_instagram_profiles, id: false do |t|
       t.integer "affiliate_id", null: false
       t.bigint "instagram_profile_id", null: false
       t.index ["affiliate_id", "instagram_profile_id"], name: "index_affiliates_instagram_profiles", unique: true
--- db/migrate/20220715232531_drop_instagram_profiles.rb
+++ db/migrate/20220715232531_drop_instagram_profiles.rb
@@ -1,6 +1,6 @@
 class DropInstagramProfiles < ActiveRecord::Migration[6.1]
   def change
-    drop_table :instagram_profiles do |t|
+    drop_table :instagram_profiles, id: :bigint, default: nil do |t|
       t.string "username", null: false
       t.datetime "created_at", null: false
       t.datetime "updated_at", null: false
