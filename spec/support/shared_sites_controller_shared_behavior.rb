43bd98bf3fe187fa88ab115025d71e56f4b75902
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2022-09-30 10:13:55 -0400

    rubocop fixes

--- spec/support/shared_sites_controller_shared_behavior.rb
+++ spec/support/shared_sites_controller_shared_behavior.rb
@@ -2,7 +2,7 @@
 
 shared_examples 'restricted to approved user' do |request_method, action, parameters = nil|
   context 'when user is not logged in' do
-    it 'should redirect to login page' do
+    it 'redirects to login page' do
       send request_method, action, params: parameters
       expect(response).to redirect_to(login_path)
     end
@@ -11,7 +11,7 @@ shared_examples 'restricted to approved user' do |request_method, action, parame
   context 'when user is pending approval' do
     before { UserSession.create(users(:affiliate_manager_with_pending_approval_status)) }
 
-    it 'should redirect to affiliates page' do
+    it 'redirects to affiliates page' do
       send request_method, action, params: parameters
       expect(response).to redirect_to(account_path)
     end
@@ -20,34 +20,34 @@ shared_examples 'restricted to approved user' do |request_method, action, parame
   describe 'when user is pending contact information status' do
     before { UserSession.create(users(:affiliate_manager_with_pending_contact_information_status)) }
 
-    it 'should redirect to affiliates page' do
+    it 'redirects to affiliates page' do
       send request_method, action, params: parameters
       expect(response).to redirect_to(account_path)
     end
   end
 end
 
-shared_context 'approved user logged in' do
+shared_context 'when an approved user is logged in' do
   let(:current_user) { users(:affiliate_manager) }
 
   before do
     UserSession.create current_user
-    expect(User).to receive(:find_by_id).and_return(current_user)
+    allow(User).to receive(:find_by_id).and_return(current_user)
   end
 end
 
-shared_context 'super admin logged in' do
+shared_context 'when a super admin is logged in' do
   fixtures :users
   let(:current_user) { users(:affiliate_admin) }
 
   before do
     activate_authlogic
     UserSession.create current_user
-    expect(User).to receive(:find_by_id).and_return(current_user)
+    allow(User).to receive(:find_by_id).and_return(current_user)
   end
 end
 
-shared_context 'approved user logged in to a site' do
+shared_context 'when an approved user is logged into a site' do
   let(:current_user) { users(:affiliate_manager) }
   let(:site) { affiliates(:basic_affiliate) }
 
@@ -58,13 +58,13 @@ shared_context 'approved user logged in to a site' do
   end
 end
 
-shared_context 'super admin logged in to a site' do
+shared_context 'when a super admin is logged into a site' do
   let(:current_user) { users(:affiliate_admin) }
   let!(:site) { affiliates(:basic_affiliate) }
 
   before do
     UserSession.create current_user
-    expect(User).to receive(:find_by_id).and_return(current_user)
-    expect(Affiliate).to receive(:find).with(site.id.to_s).and_return site
+    allow(User).to receive(:find_by_id).and_return(current_user)
+    allow(Affiliate).to receive(:find).with(site.id.to_s).and_return site
   end
 end
