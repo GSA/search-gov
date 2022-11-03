f22bde78124a5c3274e2ab55f24eeb7fcc56cf15
Darius Dzien <ddzien2@gmail.com>
2022-11-03 17:07:00 -0500

    Try adding Node to PATH in before_symlink.rb

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -12,6 +12,7 @@ end
 # filenames - assets that live "in the wild" and can't be
 # updated whenever our asset fingerprints change.
 run <<COMPILE
+  export PATH="/usr/local/nodejs-binary-8.12.0/bin:$PATH"
   cd #{release_path} && \
   RAILS_ENV=#{rails_env} bundle exec rake assets:precompile && \
   cd #{release_path}/public/assets && \
