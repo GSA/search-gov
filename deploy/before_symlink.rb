39ce26e0010bad3562851d4116a34044e5b3d210
Darius Dzien <ddzien2@gmail.com>
2022-11-04 16:24:24 -0500

    Move assets:precompile to execute block

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -10,7 +10,7 @@ end
 execute 'yarrnnn & webpack' do
   cwd release_path
   environment 'NODE_ENV': 'production', 'YARN_CACHE_FOLDER': 'tmp/cache/'
-  command "sudo su search -c 'yarn && ./bin/webpacker'"
+  command "sudo su search -c 'yarn && ./bin/webpacker && RAILS_ENV=production bundle exec rake assets:precompile'"
 end
 
 # Pre-compile assets. Also, a very small subset of the assets
@@ -19,7 +19,6 @@ end
 # updated whenever our asset fingerprints change.
 run <<COMPILE
   cd #{release_path} && \
-  RAILS_ENV=#{rails_env} bundle exec rake assets:precompile && \
   cd #{release_path}/public/assets && \
   for js in sayt_loader_libs sayt_loader stats; do cp ${js}-*.js ${js}.js && cp ${js}-*.js.gz ${js}.js.gz; done && \
   for css in sayt; do cp ${css}-*.css ${css}.css && cp ${css}-*.css.gz ${css}.css.gz; done && \
