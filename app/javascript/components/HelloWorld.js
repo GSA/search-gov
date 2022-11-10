1516cbc652ca64ec17963dc269b5cbe9883232ad
Darius Dzien <ddzien2@gmail.com>
2022-11-10 16:49:28 -0600

    Add more USWDS components

--- app/javascript/components/HelloWorld.js
+++ app/javascript/components/HelloWorld.js
@@ -94,7 +94,65 @@ class HelloWorld extends React.Component {
         </ExtendedNav></Grid>
     </Grid>
     </GridContainer>
-        Greeting: {this.props.greeting}
+    <GridContainer>
+    <Grid row>
+      <Grid tablet={{ col: true }}><a href= "#"><h4>{this.props.greeting[0]['title']}</h4></a></Grid>
+    </Grid>
+    <Grid row>
+      <Grid tablet={{ col: true }}><a href= "#">{this.props.greeting[0]['unescapedUrl']}</a></Grid>
+    </Grid>
+    <Grid row>
+      <Grid tablet={{ col: true }}><p>{this.props.greeting[0]['content']}</p></Grid>
+    </Grid>
+  </GridContainer>
+  <br></br>
+  <GridContainer>
+  <Grid row>
+      <Grid tablet={{ col: true }}><a href= "#"><h4>{this.props.greeting[1]['title']}</h4></a></Grid>
+    </Grid>
+    <Grid row>
+      <Grid tablet={{ col: true }}><a href= "#">{this.props.greeting[1]['unescapedUrl']}</a></Grid>
+    </Grid>
+    <Grid row>
+      <Grid tablet={{ col: true }}><p>{this.props.greeting[1]['content']}</p></Grid>
+    </Grid>
+  </GridContainer>
+  <br></br>
+  <GridContainer>
+  <Grid row>
+      <Grid tablet={{ col: true }}><a href= "#"><h4>{this.props.greeting[2]['title']}</h4></a></Grid>
+    </Grid>
+    <Grid row>
+      <Grid tablet={{ col: true }}><a href= "#">{this.props.greeting[2]['unescapedUrl']}</a></Grid>
+    </Grid>
+    <Grid row>
+      <Grid tablet={{ col: true }}><p>{this.props.greeting[2]['content']}</p></Grid>
+    </Grid>
+  </GridContainer>
+  <br></br>
+  <GridContainer>
+  <Grid row>
+      <Grid tablet={{ col: true }}><a href= "#"><h4>{this.props.greeting[3]['title']}</h4></a></Grid>
+    </Grid>
+    <Grid row>
+      <Grid tablet={{ col: true }}><a href= "#">{this.props.greeting[3]['unescapedUrl']}</a></Grid>
+    </Grid>
+    <Grid row>
+      <Grid tablet={{ col: true }}><p>{this.props.greeting[3]['content']}</p></Grid>
+    </Grid>
+  </GridContainer>
+  <br></br>
+  <GridContainer>
+  <Grid row>
+      <Grid tablet={{ col: true }}><a href= "#"><h4>{this.props.greeting[4]['title']}</h4></a></Grid>
+    </Grid>
+    <Grid row>
+      <Grid tablet={{ col: true }}><a href= "#">{this.props.greeting[4]['unescapedUrl']}</a></Grid>
+    </Grid>
+    <Grid row>
+      <Grid tablet={{ col: true }}><p>{this.props.greeting[4]['content']}</p></Grid>
+    </Grid>
+  </GridContainer>
       </React.Fragment>
     );
   }
