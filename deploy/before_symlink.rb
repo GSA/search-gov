3ace80a27a7d40886edb60921f439409720a7912
Darius Dzien <ddzien2@gmail.com>
2022-11-14 17:02:16 -0600

    Try using unmodified before_symlink.rb

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -7,18 +7,13 @@ dgsearch_rails_database :usasearch do
   group 'www-data'
 end
 
-# Install JavaScript dependencies and pre-compile assets.
-execute 'Install JavaScript dependencies and pre-compile assets' do
-  cwd release_path
-  environment NODE_ENV: 'production'
-  command "sudo su search -c 'yarn install && RAILS_ENV=#{rails_env} bundle exec rake assets:precompile'"
-end
-
-# A very small subset of the assets need to be available
-# without digest fingerprints in their filenames - assets
-# that live "in the wild" and can't be updated whenever
-# our asset fingerprints change.
+# Pre-compile assets. Also, a very small subset of the assets
+# need to be available without digest fingerprints in their
+# filenames - assets that live "in the wild" and can't be
+# updated whenever our asset fingerprints change.
 run <<COMPILE
+  cd #{release_path} && \
+  RAILS_ENV=#{rails_env} bundle exec rake assets:precompile && \
   cd #{release_path}/public/assets && \
   for js in sayt_loader_libs sayt_loader stats; do cp ${js}-*.js ${js}.js && cp ${js}-*.js.gz ${js}.js.gz; done && \
   for css in sayt; do cp ${css}-*.css ${css}.css && cp ${css}-*.css.gz ${css}.css.gz; done && \
