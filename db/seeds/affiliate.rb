819b2eced77d127b4b6e8cd2b8bcc42e75c1244d
MothOnMars <437455+MothOnMars@users.noreply.github.com>
2023-01-26 14:00:56 -0500

    SRCH-3757 add news seed data

--- db/seeds/affiliate.rb
+++ db/seeds/affiliate.rb
@@ -1,3 +1,5 @@
-Affiliate.create(name: 'usagov', display_name: 'USA.gov')
+# frozen_string_literal: true
+
 Affiliate.create(name: 'gobiernousa', display_name: 'GobiernoUSA.gov', locale: 'es')
-Affiliate.create!(name: 'i14y_enabled', display_name: 'I14y Affiliate', gets_i14y_results: true)
+Affiliate.create(name: 'i14y_enabled', display_name: 'I14y Affiliate', gets_i14y_results: true)
+Affiliate.create(name: 'test_affiliate', display_name: 'Test Affiliate')
