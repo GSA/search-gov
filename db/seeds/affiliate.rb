76aac57eb1d845fed87357b119fe0bd024428894
Martha Thompson <437455+MothOnMars@users.noreply.github.com>
2023-01-31 09:25:26 -0500

    SRCH-3757 add news seed data (#1110)

--- db/seeds/affiliate.rb
+++ db/seeds/affiliate.rb
@@ -1,3 +1,4 @@
-Affiliate.create(name: 'usagov', display_name: 'USA.gov')
-Affiliate.create(name: 'gobiernousa', display_name: 'GobiernoUSA.gov', locale: 'es')
-Affiliate.create!(name: 'i14y_enabled', display_name: 'I14y Affiliate', gets_i14y_results: true)
+# frozen_string_literal: true
+
+Affiliate.create(name: 'test_affiliate', display_name: 'Test Affiliate')
+Affiliate.create(name: 'spanish_affiliate', display_name: 'Test Spanish Affiliate', locale: 'es')
