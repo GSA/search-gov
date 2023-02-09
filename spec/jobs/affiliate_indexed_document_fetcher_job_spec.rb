52a11de16e6a64203c834694bf2195e626ff014e
Lora Woodford <lora.woodford@gsa.gov>
2023-02-09 10:19:15 -0500

    SRCH-3789 AffiliateIndexedDocumentFetcherJob on searchgov queue

--- spec/jobs/affiliate_indexed_document_fetcher_job_spec.rb
+++ spec/jobs/affiliate_indexed_document_fetcher_job_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
-describe AffiliateIndexedDocumentFetcher, '#perform(affiliate_id, start_id, end_id, scope)' do
-  fixtures :affiliates, :features, :site_domains
+describe AffiliateIndexedDocumentFetcherJob do
   before do
     IndexedDocument.destroy_all
     @affiliate = affiliates(:basic_affiliate)
@@ -22,45 +21,23 @@ describe AffiliateIndexedDocumentFetcher, '#perform(affiliate_id, start_id, end_
     @affiliate.save!
   end
 
-  it_behaves_like 'a ResqueJobStats job'
-
-  it "should handle scope 'ok'" do
+  it "handles scope 'ok'" do
     expect(IndexedDocument).to receive(:find).once.with(@ok.id).and_return @ok
     expect(@ok).to receive(:fetch)
-    described_class.perform(@affiliate.id, 1, 2**30, 'ok')
+    described_class.perform_now(@affiliate.id, 1, 2**30, 'ok')
   end
 
-  it "should handle scope 'not_ok'" do
+  it "handles scope 'not_ok'" do
     expect(IndexedDocument).to receive(:find).with(@not_ok.id).and_return @not_ok
     expect(IndexedDocument).to receive(:find).with(@unfetched.id).and_return @unfetched
     expect(@unfetched).to receive(:fetch)
     expect(@not_ok).to receive(:fetch)
-    described_class.perform(@affiliate.id, 1, 2**30, 'not_ok')
+    described_class.perform_now(@affiliate.id, 1, 2**30, 'not_ok')
   end
 
-  it "should handle scope 'unfetched'" do
+  it "handles scope 'unfetched'" do
     expect(IndexedDocument).to receive(:find).once.with(@unfetched.id).and_return @unfetched
     expect(@unfetched).to receive(:fetch)
-    described_class.perform(@affiliate.id, 1, 2**30, 'unfetched')
-  end
-
-  context 'when affiliate or indexed document have disappeared before job runs' do
-    before do
-      allow(IndexedDocument).to receive(:find).and_raise ActiveRecord::RecordNotFound
-    end
-
-    it 'should ignore the problem and move on' do
-      described_class.perform(@affiliate.id, 1, 2**30, 'unfetched')
-    end
-  end
-
-  describe '.before_perform_with_timeout' do
-    before { @original_timeout = Resque::Plugins::Timeout.timeout }
-    after { Resque::Plugins::Timeout.timeout = @original_timeout }
-
-    it 'sets Resque::Plugins::Timeout.timeout to 1 hour' do
-      described_class.before_perform_with_timeout
-      expect(Resque::Plugins::Timeout.timeout).to eq(1.hour)
-    end
+    described_class.perform_now(@affiliate.id, 1, 2**30, 'unfetched')
   end
 end
