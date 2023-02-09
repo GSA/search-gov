335279560b32544b83946e0073e8de01121a94fc
Lora Woodford <lora.woodford@gsa.gov>
2023-02-09 15:39:45 -0500

    rubocop

--- spec/jobs/affiliate_indexed_document_fetcher_job_spec.rb
+++ spec/jobs/affiliate_indexed_document_fetcher_job_spec.rb
@@ -1,44 +1,66 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 
 describe AffiliateIndexedDocumentFetcherJob do
+  let(:affiliate) { affiliates(:basic_affiliate) }
+  let(:unfetched) { IndexedDocument.find_by(unfetched_atts) }
+  let(:ok) { IndexedDocument.find_by(ok_atts) }
+  let(:not_ok) { IndexedDocument.find_by(not_ok_atts) }
+  let(:unfetched_atts) do
+    { url: 'http://nps.gov/foo.html',
+      title: 'Doc Title',
+      description: 'This is a document.' }
+  end
+  let(:ok_atts) do
+    { title: 'PDF Title',
+      description: 'This is a PDF document.',
+      url: 'http://nps.gov/pdf.pdf',
+      last_crawl_status: IndexedDocument::OK_STATUS,
+      last_crawled_at: Time.zone.now,
+      body: 'this is the doc body' }
+  end
+  let(:not_ok_atts) do
+    { title: 'Dupe PDF Title',
+      description: 'Dupe This is a PDF document.',
+      url: 'http://nps.gov/dupe_pdf.pdf',
+      last_crawl_status: 'duplicate',
+      last_crawled_at: Time.zone.now,
+      body: 'this is the doc body' }
+  end
+
   before do
     IndexedDocument.destroy_all
-    @affiliate = affiliates(:basic_affiliate)
-    @unfetched = @affiliate.indexed_documents.build(url: 'http://nps.gov/foo.html', title: 'Doc Title',
-                                                    description: 'This is a document.')
-    @ok = @affiliate.indexed_documents.build(title: 'PDF Title',
-                                             description: 'This is a PDF document.',
-                                             url: 'http://nps.gov/pdf.pdf',
-                                             last_crawl_status: IndexedDocument::OK_STATUS,
-                                             last_crawled_at: Time.now,
-                                             body: 'this is the doc body')
-    @not_ok = @affiliate.indexed_documents.build(title: 'Dupe PDF Title',
-                                                 description: 'Dupe This is a PDF document.',
-                                                 url: 'http://nps.gov/dupe_pdf.pdf',
-                                                 last_crawl_status: 'duplicate',
-                                                 last_crawled_at: Time.now,
-                                                 body: 'this is the doc body')
-    @affiliate.save!
+    affiliate.indexed_documents.build([unfetched_atts, ok_atts, not_ok_atts])
+    affiliate.save!
+    allow(unfetched).to receive(:fetch)
   end
 
   it "handles scope 'ok'" do
-    expect(IndexedDocument).to receive(:find).once.with(@ok.id).and_return @ok
-    expect(@ok).to receive(:fetch)
-    described_class.perform_now(@affiliate.id, 1, 2**30, 'ok')
+    allow(IndexedDocument).to receive(:find).and_return(ok)
+    allow(ok).to receive(:fetch)
+    described_class.perform_now(affiliate.id, 1, 2**30, 'ok')
+    expect(IndexedDocument).to have_received(:find).once.with(ok.id)
+    expect(ok).to have_received(:fetch)
   end
 
   it "handles scope 'not_ok'" do
-    expect(IndexedDocument).to receive(:find).with(@not_ok.id).and_return @not_ok
-    expect(IndexedDocument).to receive(:find).with(@unfetched.id).and_return @unfetched
-    expect(@unfetched).to receive(:fetch)
-    expect(@not_ok).to receive(:fetch)
-    described_class.perform_now(@affiliate.id, 1, 2**30, 'not_ok')
+    allow(IndexedDocument).to receive(:find).and_return(not_ok, unfetched)
+    allow(not_ok).to receive(:fetch)
+    described_class.perform_now(affiliate.id, 1, 2**30, 'not_ok')
+    expect(IndexedDocument).to have_received(:find).with(not_ok.id)
+    expect(IndexedDocument).to have_received(:find).with(unfetched.id)
+    expect(not_ok).to have_received(:fetch)
+    expect(unfetched).to have_received(:fetch)
   end
 
-  it "handles scope 'unfetched'" do
-    expect(IndexedDocument).to receive(:find).once.with(@unfetched.id).and_return @unfetched
-    expect(@unfetched).to receive(:fetch)
-    described_class.perform_now(@affiliate.id, 1, 2**30, 'unfetched')
+  context 'when the scope is unfetched' do
+    it "handles scope 'unfetched'" do
+      allow(IndexedDocument).to receive(:find).and_return(unfetched)
+      described_class.perform_now(affiliate.id, 1, 2**30, 'unfetched')
+      expect(IndexedDocument).to have_received(:find).once.with(unfetched.id)
+      expect(unfetched).to have_received(:fetch)
+    end
   end
 
   context 'when affiliate or indexed document have disappeared before job runs' do
@@ -48,7 +70,7 @@ describe AffiliateIndexedDocumentFetcherJob do
     end
 
     it 'logs the problem and moves on' do
-      described_class.perform_now(@affiliate.id, 1, 2**30, 'unfetched')
+      described_class.perform_now(affiliate.id, 1, 2**30, 'unfetched')
       expect(Rails.logger).to have_received(:warn).with(/Ignoring race condition in AffiliateIndexedDocumentFetcherJob/)
     end
   end
