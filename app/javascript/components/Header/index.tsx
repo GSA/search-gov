876ece9b338feac3174ea1fc0702f659315f6f07
yogesh27 <yogesh.asthana27@gmail.com>
2023-02-14 16:45:47 -0500

    removed useState ununsed import from header/index.js

--- app/javascript/components/Header/index.tsx
+++ app/javascript/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { GovBanner } from '@trussworks/react-uswds';
 
 import '@trussworks/react-uswds/lib/uswds.css';
