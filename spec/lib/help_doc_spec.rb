6b59a120104f23c7f44c240cbced726f1aaf1c66
Lora Woodford <lora.woodford@gsa.gov>
2023-02-22 14:36:09 -0500

    SRCH-3877 Resolve help_doc deprecation

--- spec/lib/help_doc_spec.rb
+++ spec/lib/help_doc_spec.rb
@@ -1,12 +1,19 @@
-require 'spec_helper'
+# frozen_string_literal: true
 
 describe HelpDoc do
   describe '.extract_article' do
     context 'when there is an error in retrieving the help doc' do
-      it 'should respond with alert' do
+      it 'responds with an alert' do
         url = 'https://search.gov/manual/site-information.html'
-        expect(described_class).to receive(:open).with(url).and_raise
+        allow(described_class).to receive(:open).with(url).and_raise
         expect(described_class.extract_article(url)).to include('Unable to retrieve')
+      end
+    end
+
+    context 'when the help doc exists' do
+      it 'returns that doc' do
+        url = 'https://search.gov/admin-center/dashboard/settings.html'
+        expect(described_class.extract_article(url)).to include('Editing Your Site Settings')
       end
     end
   end
