443abe0bc5373480a70daf8d7104c658606f3d4e
Yogesh Asthana <yogesh.asthana@gsa.gov>
2023-03-20 14:05:23 -0400

    SRCH-3633: fixed searchbar width and responsive issue for mobile view (#1141)
    * SRCH-3633: fixed searchbar width, responsive issue for mobile view and updating query params when user submits search query

--- app/javascript/test/SearchResultsLayout.test.tsx
+++ app/javascript/test/SearchResultsLayout.test.tsx
@@ -5,13 +5,13 @@ import SearchResultsLayout from '../components/SearchResultsLayout';
 
 describe('SearchResultsLayout', () => {
   it('shows a message when there are no results', () => {
-    render(<SearchResultsLayout params='foo' results={[]} vertical='web' />);
+    render(<SearchResultsLayout params={{ query: 'foo' }} results={[]} vertical='web' />);
     const message = screen.getByText(/Please enter a search term in the box above./i);
     expect(message).toBeInTheDocument();
   });
 
   it('renders all relevant links', () => {
-    render(<SearchResultsLayout params='foo' results={[]} vertical='web' />);
+    render(<SearchResultsLayout params={{ query: 'foo' }} results={[]} vertical='web' />);
     const everything = screen.getByText(/Everything/i);
     const news = screen.getByText(/News/i);
     const images = screen.getByText(/Images/i);
@@ -24,7 +24,7 @@ describe('SearchResultsLayout', () => {
 
   it('renders search results', () => {
     const results = [{ title: 'test result 1', unescapedUrl: 'https://www.search.gov', thumbnail: { url: 'https://www.search.gov/test_image.png' }, content: 'result body' }];
-    render(<SearchResultsLayout params='foo' results={results} vertical='web' />);
+    render(<SearchResultsLayout params={{ query: 'foo' }} results={results} vertical='web' />);
     const resultTitle = screen.getByText(/test result 1/i);
     const resultUrl = screen.getByText(/https:\/\/www.search.gov/i);
     const resultBody = screen.getByText(/result body/i);
@@ -35,7 +35,7 @@ describe('SearchResultsLayout', () => {
 
   it('renders image search results', () => {
     const results = [{ title: 'test result 1', unescapedUrl: 'https://www.search.gov', thumbnail: { url: 'https://www.search.gov/test_image.png' }, content: 'result body' }];
-    render(<SearchResultsLayout params='foo' results={results} vertical='image' />);
+    render(<SearchResultsLayout params={{ query: 'foo' }} results={results} vertical='image' />);
     const resultTitle = screen.getByText(/test result 1/i);
     const img = [...document.getElementsByClassName('result-image')].pop() as HTMLImageElement;
     expect(resultTitle).toBeInTheDocument();
