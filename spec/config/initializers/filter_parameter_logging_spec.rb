d06092fe4131ad33e8dd7051f6cacf95b94f9884
Lora Woodford <lora.woodford@gsa.gov>
2023-03-21 12:42:28 -0400

    linebreaks

--- spec/config/initializers/filter_parameter_logging_spec.rb
+++ spec/config/initializers/filter_parameter_logging_spec.rb
@@ -9,18 +9,22 @@ describe 'ActiveSupport::ParameterFilter' do
   end
 
   it 'filters sayt q parameter' do
-    expect(parameter_filter.filter({ 'q' => 'bar' })).to eq({ 'q' => '[FILTERED]' })
+    expect(parameter_filter.filter({ 'q' => 'bar' })).
+      to eq({ 'q' => '[FILTERED]' })
   end
 
   it 'redacts queries that may contain social security numbers' do
-    expect(parameter_filter.filter({ 'query' => '111-11-1111 tax return' })).to eq({ 'query' => 'REDACTED_SSN tax return' })
+    expect(parameter_filter.filter({ 'query' => '111-11-1111 tax return' })).
+      to eq({ 'query' => 'REDACTED_SSN tax return' })
   end
 
   it 'redacts queries that may contain email addresses' do
-    expect(parameter_filter.filter({ 'query' => 'contact someone@gsa.gov' })).to eq({ 'query' => 'contact REDACTED_EMAIL' })
+    expect(parameter_filter.filter({ 'query' => 'contact someone@gsa.gov' })).
+      to eq({ 'query' => 'contact REDACTED_EMAIL' })
   end
 
   it 'does not redact non-sensitive queries' do
-    expect(parameter_filter.filter({ 'query' => 'safe query' })).to eq({ 'query' => 'safe query' })
+    expect(parameter_filter.filter({ 'query' => 'safe query' })).
+      to eq({ 'query' => 'safe query' })
   end
 end
