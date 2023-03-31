366c3f9a4d17980e0c8b77cee36f160c0d48a610
Darius Dzien <ddzien2@gmail.com>
2023-03-31 16:53:29 -0500

    Update comment to reference ticket

--- app/models/news_search.rb
+++ app/models/news_search.rb
@@ -61,7 +61,7 @@ class NewsSearch < FilterableSearch
     @results.map { |r| r.serializable_hash }
   end
 
-  # TO-DO: Normalize news results
+  # TO-DO: Normalize news results in https://cm-jira.usa.gov/browse/SRCH-4071
   def normalized_results
     @results
   end
