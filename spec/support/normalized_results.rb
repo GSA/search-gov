a054ce1b0c6e913696d5f4662ccebc2a165935b0
Darius Dzien <ddzien2@gmail.com>
2023-04-06 08:44:51 -0500

    Rubocop fixes

--- spec/support/normalized_results.rb
+++ spec/support/normalized_results.rb
@@ -1,14 +1,16 @@
+# frozen_string_literal: true
+
 shared_examples 'a search with normalized results' do
   let(:normalized_result_keys) { [:description, :url, :title] }
 
   it 'returns normalized results' do
-      expect(normalized_results.length).to eq(5)
+    expect(normalized_results.length).to eq(5)
 
-      normalized_results.each_with_index do |result, index|
-        expect(result.keys).to contain_exactly(*normalized_result_keys)
-        expect(result[:title]).to eq('title')
-        expect(result[:description]).to eq('content')
-        expect(result[:url]).to eq("http://foo.gov/#{index}")
-      end
+    normalized_results.each_with_index do |result, index|
+      expect(result.keys).to contain_exactly(*normalized_result_keys)
+      expect(result[:title]).to eq('title')
+      expect(result[:description]).to eq('content')
+      expect(result[:url]).to eq("http://foo.gov/#{index}")
     end
   end
+end
