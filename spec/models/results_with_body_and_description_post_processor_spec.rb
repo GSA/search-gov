608f28ffd5dfdc05a7dd5f28b39f1f258f70caff
Darius Dzien <ddzien2@gmail.com>
2023-04-06 14:07:05 -0500

    Rubocop cleanup

--- spec/models/results_with_body_and_description_post_processor_spec.rb
+++ spec/models/results_with_body_and_description_post_processor_spec.rb
@@ -9,7 +9,6 @@ describe ResultsWithBodyAndDescriptionPostProcessor do
       5.times { |index| results << Hashie::Mash::Rash.new(title: "title #{index}", description: "content #{index}", url: "http://foo.gov/#{index}") }
       results
     end
-    let(:excluded_urls) { [] }
 
     it_behaves_like 'a search with normalized results' do
       let(:normalized_results) { described_class.new(results).normalized_results }
