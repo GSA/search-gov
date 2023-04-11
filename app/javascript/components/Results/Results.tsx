80fc4dcc249dcc96a1c67a245dc16fc605e4b145
yogesh27 <=>
2023-04-11 16:36:18 -0400

    SRCH-3639: updated result component

--- app/javascript/components/Results/Results.tsx
+++ app/javascript/components/Results/Results.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { GridContainer, Grid } from '@trussworks/react-uswds';
 
 import { Pagination } from './../Pagination/Pagination';
+
+import './Results.css';
 interface ResultsProps {
   results: {
     title: string,
@@ -19,29 +21,34 @@ export const Results = (props: ResultsProps) => {
   
   return (
     <>
-      <div id="serp-results-wrapper">
-        <div id="results">
+      <div className='search-result-wrapper'>
           {props.results.map((result, index) => {
             return (
-              <div className='result' key={index}>
-                <GridContainer>
-                  <Grid row>
-                    <Grid tablet={{ col: true }}><a href= "#" ><h4>{result.title}</h4></a></Grid>
+              <GridContainer key={index} className='search-result-item'>
+                <Grid row gap="md">
+                  { props.vertical === 'image' &&
+                  <Grid mobileLg={{ col: 4 }} className='result-thumbnail'>
+                    <img src={result.thumbnail.url} className="result-image"/>
                   </Grid>
-                  <Grid row>
-                    <Grid tablet={{ col: true }}><a href= "#">{result.url}</a></Grid>
+                  }
+                  <Grid col={true} className='result-meta-data'>
+                    <div className='published-date'>
+                      July 4th, 2022
+                    </div>
+                    <div className='result-title'>
+                      <a href={result.url} >
+                        <h2>{result.title}</h2>
+                      </a>
+                    </div>
+                    <div className='result-desc'>
+                      <p>{result.description}</p>
+                      <div className='result-url-text'>{result.url}</div>
+                    </div>
                   </Grid>
-                  {props.vertical === 'image' && <Grid row>
-                    <Grid tablet={{ col: true }}><img src={result.thumbnail.url} className="result-image"/></Grid>
-                  </Grid>}
-                  <Grid row>
-                    <Grid tablet={{ col: true }}><p>{result.description}</p></Grid>
-                  </Grid>
-                </GridContainer>
-              </div>
+                </Grid>
+              </GridContainer>
             );
           })}
-        </div>
       </div>
       {totalPages > 0 && 
         <Pagination 
