7abd8cb426f450b4f51bf24650e8cb85f8782f0a
Darius Dzien <dariusz.dzien@gsa.gov>
2023-04-26 09:55:13 -0500

    Add more tests

--- spec/models/post_processor_spec.rb
+++ spec/models/post_processor_spec.rb
@@ -7,5 +7,12 @@ describe PostProcessor do
     it 'returns zero when there are no results' do
       expect(post_processor.total_pages(0)).to eq(0)
     end
+
+    it 'returns one when there are 20 or fewer results' do
+      expect(post_processor.total_pages(1)).to eq(1)
+      expect(post_processor.total_pages(10)).to eq(1)
+      expect(post_processor.total_pages(20)).to eq(1)
+      expect(post_processor.total_pages(21)).not_to eq(1)
+    end
   end
 end
