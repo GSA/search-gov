2399069b318aa1d8403526c3cce17b82ef8688da
Darius Dzien <dariusz.dzien@gsa.gov>
2023-04-28 14:58:14 -0500

    WIP Fix new search query

--- app/javascript/utils/index.tsx
+++ app/javascript/utils/index.tsx
@@ -1,6 +1,7 @@
 export const getUriWithParam = (baseUrl: string, urlParam: string, urlParamQuery: string): string => {
   const Url = new URL(baseUrl);
   const urlParams: URLSearchParams = new URLSearchParams(Url.search);
+  urlParams.set('page', '1');
   urlParams.set(urlParam, urlParamQuery);
   Url.search = urlParams.toString();
   return Url.toString();
