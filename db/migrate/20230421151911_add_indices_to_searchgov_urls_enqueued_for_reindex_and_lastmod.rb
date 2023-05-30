1172c4223ce22c55916f3305a25dc0c211310427
krbhavith <krbhavith@gmail.com>
2023-05-30 10:52:32 -0400

    test multicolumn indexing

--- db/migrate/20230421151911_add_indices_to_searchgov_urls_enqueued_for_reindex_and_lastmod.rb
+++
@@ -1,6 +0,0 @@
-class AddIndicesToSearchgovUrlsEnqueuedForReindexAndLastmod < ActiveRecord::Migration[7.0]
-  def change
-    add_index :searchgov_urls, :enqueued_for_reindex
-    add_index :searchgov_urls, :lastmod
-  end
-end
