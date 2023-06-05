1d6bc4b6992ed9e3a4112f44c50889b384a2b679
Lora Woodford <lora.woodford@gsa.gov>
2023-06-05 12:59:09 -0400

    remove gss route

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,6 @@ Rails.application.routes.draw do
       get '/search/bing' => 'searches#bing'
       get '/search/azure/web' => 'searches#azure_web'
       get '/search/azure/image' => 'searches#azure_image'
-      get '/search/gss' => 'searches#gss'
       get '/search/i14y' => 'searches#i14y'
       get '/search/video' => 'searches#video'
       get '/search/docs' => 'searches#docs'
