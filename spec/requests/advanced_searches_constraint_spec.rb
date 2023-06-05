21df2bd821f09b3e6b9c689f405fe2678791e44c
Lora Woodford <lora.woodford@gsa.gov>
2023-06-05 10:41:45 -0400

    codeclimate

--- spec/requests/advanced_searches_constraint_spec.rb
+++ spec/requests/advanced_searches_constraint_spec.rb
@@ -7,19 +7,17 @@ describe AdvancedSearchesConstraint do
     let(:affiliate) { affiliates(:searchgov_affiliate).name }
 
     it 'issues a redirect' do
-      expect(response.status).to eq(301)
+      expect(response).to have_http_status(:moved_permanently)
     end
 
-    it 'redirects to /search' do
-      expect(response).to redirect_to("/search?affiliate=#{affiliate}")
-    end
+    it { is_expected.to redirect_to("/search?affiliate=#{affiliate}") }
   end
 
   context 'when a BingV7 site' do
     let(:affiliate) { affiliates(:bing_v7_affiliate).name }
 
     it 'does not issue a redirect' do
-      expect(response.status).to eq(200)
+      expect(response).to have_http_status(:ok)
     end
   end
 end
