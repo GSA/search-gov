f98eae2862a3d38917e731f665168f24155ad52d
Darius Dzien <dariusz.dzien@gsa.gov>
2023-08-22 09:25:09 -0500

    SRCH-3995, SRCH-3967 Prepare backend data for video results (#1274)

--- app/models/results_with_body_and_description_post_processor.rb
+++ app/models/results_with_body_and_description_post_processor.rb
@@ -1,9 +1,12 @@
+# frozen_string_literal: true
+
 class ResultsWithBodyAndDescriptionPostProcessor < ResultsPostProcessor
   attr_accessor :results
 
-  def initialize(results)
+  def initialize(results, _val: nil, youtube: false)
     super
     @results = results
+    @youtube = youtube
   end
 
   def post_process_results
@@ -40,9 +43,17 @@ class ResultsWithBodyAndDescriptionPostProcessor < ResultsPostProcessor
     @results.map do |result|
       {
         title: translate_highlights(result['title']),
-        url: result['url'],
-        description: truncate_description(translate_highlights(result['description'] || result['body']))
-      }
+        url: result['url'] || result['link'],
+        description: format_description(result),
+        youtube: @youtube,
+        youtubePublishedAt: (result&.published_at if @youtube),
+        youtubeThumbnailUrl: (result&.youtube_thumbnail_url if @youtube),
+        youtubeDuration: (result&.duration if @youtube)
+      }.compact_blank
     end
+  end
+
+  def format_description(result)
+    truncate_description(translate_highlights(result['description'] || result['body']))
   end
 end
