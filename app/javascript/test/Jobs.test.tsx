72a4a7b96d7317092581d2b9777b8c9726b6733f
yogesh27 <yogesh.asthana27@gmail.com>
2023-09-21 17:20:57 -0400

    SRCH-3645: updated imports for parse

--- app/javascript/test/Jobs.test.tsx
+++ app/javascript/test/Jobs.test.tsx
@@ -1,7 +1,6 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import parse from 'html-react-parser';
 
 import { Jobs } from '../components/Results/Jobs/Jobs';
 
@@ -73,8 +72,7 @@ describe('Jobs component', () => {
         applicationCloseDate: 'September 24, 2023'
       }
     ],
-    recommendedBy: 'gsa',
-    parse
+    recommendedBy: 'gsa'
   };
 
   it('renders Jobs component', () => {
