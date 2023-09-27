0d2ab81e78cd3ea887e078ef61112dfe02a5c615
Lora Woodford <lora.woodford@gsa.gov>
2023-09-27 17:27:18 -0400

    SRCH-4140 Add results format to admin center

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_20_210741) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_27_213029) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -100,6 +100,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_210741) do
     t.string "identifier_domain_name"
     t.string "parent_agency_name"
     t.string "parent_agency_link"
+    t.boolean "display_image_on_search_results", default: false, null: false
+    t.boolean "display_filetype_on_search_results", default: false, null: false
+    t.boolean "display_created_date_on_search_results", default: false, null: false
+    t.boolean "display_updated_date_on_search_results", default: false, null: false
     t.index ["name"], name: "index_affiliates_on_name", unique: true
   end
 
