3c7320a8d47e71f71623218a30edb659347bbe49
Steven Barragan Naranjo <me@steven.mx>
2023-10-05 09:27:14 -0700

    refactor: put unfetched urls tests into the same context

--- spec/jobs/searchgov_domain_indexer_job_spec.rb
+++ spec/jobs/searchgov_domain_indexer_job_spec.rb
@@ -33,7 +33,28 @@ describe SearchgovDomainIndexerJob do
     end
   end
 
-  context 'when a domain do not have unfetched urls' do
+  context 'when a domain has outdated urls' do
+    let!(:searchgov_url) do
+      SearchgovUrl.create!(url: 'https://agency.gov/',
+                           last_crawled_at: 1.week.ago,
+                           lastmod: 1.day.ago)
+    end
+
+    it 'fetches the url' do
+      expect { perform }.to(change{ searchgov_url.reload.last_crawled_at })
+    end
+  end
+
+  context 'when a domain has no unfetched urls' do
+    it 'does not raise an error' do
+      expect { perform }.not_to raise_error
+    end
+
+    it 'does not enqueue subsequent jobs' do
+      expect { perform }.
+        not_to have_enqueued_job(described_class)
+    end
+
     it 'transitions the domain activity back to "idle"' do
       expect { perform }.to change { searchgov_domain.activity }.
         from('indexing').to('idle')
@@ -56,29 +77,6 @@ describe SearchgovDomainIndexerJob do
         expect{ perform }.to have_enqueued_job(described_class).
           with(searchgov_domain: searchgov_domain, delay: 10).at(10.seconds.from_now)
       end
-    end
-  end
-
-  context 'when a domain has outdated urls' do
-    let!(:searchgov_url) do
-      SearchgovUrl.create!(url: 'https://agency.gov/',
-                           last_crawled_at: 1.week.ago,
-                           lastmod: 1.day.ago)
-    end
-
-    it 'fetches the url' do
-      expect { perform }.to(change{ searchgov_url.reload.last_crawled_at })
-    end
-  end
-
-  context 'when a domain has no unfetched urls' do
-    it 'does not raise an error' do
-      expect { perform }.not_to raise_error
-    end
-
-    it 'does not enqueue subsequent jobs' do
-      expect { perform }.
-        not_to have_enqueued_job(described_class)
     end
   end
 end
