11a4d1fd0ba7be2c457652cd845cdeebc92b5330
yogesh27 <yogesh.asthana27@gmail.com>
2023-10-23 13:36:06 -0400

    SRCH-3648: codeclimate fixes

--- app/javascript/components/Results/FedRegister/FedRegister.tsx
+++ app/javascript/components/Results/FedRegister/FedRegister.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { GridContainer, Grid } from '@trussworks/react-uswds';
 import parse from 'html-react-parser';
 
-type FedRegisterDocs = {
+type FedRegisterDoc = {
   commentsCloseOn: string;
   contributingAgencyNames: [string];
   documentNumber: string;
@@ -13,68 +13,64 @@ type FedRegisterDocs = {
   publicationDate: string;
   startPage: number;
   title: string;
-}[];
+};
+
 interface FedRegisterDocsProps {
-  fedRegisterDocs?: FedRegisterDocs;
+  fedRegisterDocs?: FedRegisterDoc[];
 }
 
-function federal_register_document_info(document) {
-  var document_type_span = document.documentType;
-  var fr_agency_names = document.contributingAgencyNames;
-  var fr_agencies_html = federal_register_agencies_html(fr_agency_names);
-  var publication_date_span = document.publicationDate
-  return "A " + document_type_span + " " + fr_agencies_html + " posted on " + publication_date_span + ".";
-}
+const getFedRegDocInfo = (document: FedRegisterDoc) => {
+  const docType = document.documentType;
+  const agencyNames = document.contributingAgencyNames;
+  const agenciesHtml = getFedRegisterAgenciesText(agencyNames);
+  const pubDate = document.publicationDate;
+  return `A ${docType} ${agenciesHtml} posted on ${pubDate}.`;
+};
 
-function federal_register_agencies_html(fr_agency_names) {
-  var agency_spans = fr_agency_names.sort().map(function(name) {
-      return name;
-  });
-  var last_agency_span = agency_spans.pop();
-  var agencies_html = 'by the ' + agency_spans.join(', the ');
-  if (agency_spans.length > 0) {
-      agencies_html += ' and the ';
+const getFedRegisterAgenciesText = (agencyNames: [string]) => {
+  const agencyText = agencyNames.sort().map((name) => name);
+  const lastAgencyText = agencyText.pop();
+  let agencies = `by the ${agencyText.join(', the ')}`;
+  if (agencyText.length > 0) {
+    agencies += ' and the ';
   }
-  agencies_html += last_agency_span;
-  return agencies_html;
-}
+  agencies += lastAgencyText;
+  return agencies;
+};
 
-function federal_register_document_page_info(document) {
-    var content = 'Pages ' + document.startPage + ' - ' + document.endPage + ' ';
-    content += '(' + document.pageLength + ' page) ';
-    content += '[FR DOC #: ' + document.documentNumber + ']';
-    return content;
-}
+const getFedRegDocPageInfo = (document: FedRegisterDoc) => {
+  let content = `Pages ${document.startPage} - ${document.endPage} `;
+  content += `(${document.pageLength} page) `;
+  content += `[FR DOC #: ${document.documentNumber}]`;
+  return content;
+};
 
-const isToday = (someDate) => {
-  const today = new Date()
-  someDate = new Date(someDate)
-  return someDate.getDate() == today.getDate() &&
-    someDate.getMonth() == today.getMonth() &&
-    someDate.getFullYear() == today.getFullYear()
-}
+const isToday = (dateStr: string) => {
+  const today = new Date();
+  const someDate = new Date(dateStr);
+  return someDate.getDate() === today.getDate() &&
+    someDate.getMonth() === today.getMonth() &&
+    someDate.getFullYear() === today.getFullYear();
+};
 
-function federal_register_document_comment_period(document) {
+const getFedRegDocCommentPeriod = (document: FedRegisterDoc) => {
   if (!document.commentsCloseOn) {
     return;
   }
+  const today = new Date().getTime();
+  const commentsCloseOn = new Date(document.commentsCloseOn).getTime();
 
-  var today = new Date().getTime();
-  var commentsCloseOn = new Date(document.commentsCloseOn).getTime();
-  
-  if(isToday(document.commentsCloseOn)){
+  if (isToday(document.commentsCloseOn))
     return (<div className='comment-period'>Comment period ends today</div>);
-  }else{
-    if (commentsCloseOn < today)
-      return (<div className='comment-period-ended'>Comment Period Closed</div>);
-    else{
-      var dateDelta = Math.floor((commentsCloseOn - today) / (1000 * 60 * 60 * 24));
-      var dateDeltaSpan = dateDelta === 1 ? '1 day' : dateDelta + ' days';
-      var commentsCloseOnSpan = document.commentsCloseOn ;
-      return (<div className='comment-period'>{'Comment period ends in ' + dateDeltaSpan + ' (' + commentsCloseOnSpan + ')'}</div>);
-    }
-  }
-}
+
+  if (commentsCloseOn < today)
+    return (<div className='comment-period-ended'>Comment Period Closed</div>);
+  
+  const dateDelta = Math.floor((commentsCloseOn - today) / (1000 * 60 * 60 * 24));
+  const dateDeltaSpan = dateDelta === 1 ? '1 day' : `${dateDelta} days`;
+  const commentsCloseOnSpan = document.commentsCloseOn ;
+  return (<div className='comment-period'>{`Comment period ends in ${dateDeltaSpan} (${commentsCloseOnSpan})`}</div>);
+};
 
 export const FedRegister = ({ fedRegisterDocs=[] }: FedRegisterDocsProps) => {
   return (
@@ -104,28 +100,15 @@ export const FedRegister = ({ fedRegisterDocs=[] }: FedRegisterDocsProps) => {
                       </a>
                     </div>
                     <div className='result-desc'>
-                      <p>
-                        {federal_register_document_info(fedRegisterDoc)} 
-                        {/* A {parse(fedRegisterDoc.documentType)} by {fedRegisterDoc.contributingAgencyNames} */}
-                      </p>
-                      <div className='pages-count'>
-                        {federal_register_document_page_info(fedRegisterDoc)}
-                        {/* Pages 29212 - 29215 (4 pages) [FR DOC #: 2016-10932] */}
-                      </div>
-
-                      {federal_register_document_comment_period(fedRegisterDoc)}
-
-                      {/* {federal_register_document_comment_period(fedRegisterDoc) && (
-                        <div className='comment-period'>
-                          {federal_register_document_comment_period(fedRegisterDoc)}
-                        </div>
-                      )} */}
+                      <p>{getFedRegDocInfo(fedRegisterDoc)}</p>
+                      <div className='pages-count'>{getFedRegDocPageInfo(fedRegisterDoc)}</div>
+                      {getFedRegDocCommentPeriod(fedRegisterDoc)}
                     </div>
                   </Grid>
                 </Grid>
                 <Grid row className="row-mobile-divider"></Grid>
               </GridContainer>
-              );
+            );
           })}
 
           <GridContainer className='result-divider'>
