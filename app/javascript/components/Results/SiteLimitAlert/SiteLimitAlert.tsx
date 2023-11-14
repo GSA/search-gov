ddfa0046a32290256309805f1fdc26000e6753e1
yogesh27 <yogesh.asthana27@gmail.com>
2023-11-14 11:34:35 -0500

    codeclimate fix

--- app/javascript/components/Results/SiteLimitAlert/SiteLimitAlert.tsx
+++ app/javascript/components/Results/SiteLimitAlert/SiteLimitAlert.tsx
@@ -16,8 +16,8 @@ export const SiteLimitAlert = ({ sitelimit, url, query }: SiteLimitAlertProps) =
   const i18n = useContext(LanguageContext);
 
   const query_from_all_sites = () => {
-    return `<a class='usa-link' href=${url}>${i18n.t('searches.siteLimits.queryFromAllSites', {query: query})}</a>`
-  }
+    return `<a class='usa-link' href=${url}>${i18n.t('searches.siteLimits.queryFromAllSites', { query })}</a>`;
+  };
 
   return (
     <div className='search-result-item-wrapper'>
@@ -26,16 +26,11 @@ export const SiteLimitAlert = ({ sitelimit, url, query }: SiteLimitAlertProps) =
           <Grid tablet={{ col: true }}>
             <div className='spelling-suggestion-wrapper'>
               <div>
-                {i18n.t('searches.siteLimits.includingResultsForQueryFromMatchingSites', {query: query, matching_sites: sitelimit})}
+                {i18n.t('searches.siteLimits.includingResultsForQueryFromMatchingSites', { query, matching_sites: sitelimit })}
               </div>
               <div className='spelling-search-instead-for'>
                 {
-                  parse(
-                    i18n.t('searches.siteLimits.doYouWantToSeeResultsFor', {
-                        query_from_all_sites: query_from_all_sites()
-                      }
-                    )
-                  )
+                  parse(i18n.t('searches.siteLimits.doYouWantToSeeResultsFor', { query_from_all_sites: query_from_all_sites() }))
                 }
               </div>
             </div>
