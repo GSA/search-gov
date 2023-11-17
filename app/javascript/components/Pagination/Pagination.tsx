aead2f2b480dbe62ef7554e60a5eb0647eda7f34
Yogesh Asthana <yogesh.asthana27@gmail.com>
2023-11-17 13:06:37 -0500

    SRCH-4102, SRCH-4103 fixing 508/accessibility issues (#1363)
    * Added the aria-label to fix the accessibility, removed all the @a11y_wip tags, updated cucumber test to use use_redesigned_results_page for the missing scenarios

--- app/javascript/components/Pagination/Pagination.tsx
+++ app/javascript/components/Pagination/Pagination.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { GridContainer, Grid } from '@trussworks/react-uswds';
 import { UswdsPagination } from '../UswdsOverrides/UswdsPagination';
 
@@ -12,6 +12,13 @@ interface PaginationProps {
 }
 
 export const Pagination = ({ pathname, totalPages = null, unboundedResults }: PaginationProps) => {
+  useEffect(() => {
+    // SRCH-4102: added the  aria label to fix the accessibility violation
+    Array.from(document.getElementsByClassName('usa-icon')).forEach((el) => { 
+      el.setAttribute('aria-label', 'previous next icon');
+    });
+  });
+  
   if ((!totalPages || totalPages < 2 || getCurrentPage() > totalPages)) {
     return (<></>);
   }
