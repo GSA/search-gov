1c74df1818267490f6e3b2787ee96bc67e8e3ccc
Darius Dzien <dariusz.dzien@gsa.gov>
2023-12-04 13:49:40 -0600

    Remove dummy data

--- app/javascript/components/Identifier/IdentifierLogoWrapper.tsx
+++ app/javascript/components/Identifier/IdentifierLogoWrapper.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 import { Identifier as IdentifierLogos, IdentifierLogo } from '@trussworks/react-uswds';
 
 interface IdentifierLogoWrapperProps {
-  logoUrl: string | null;
+  logoUrl: string;
   logoAltText?: string | null;
 }
-
-const logoImg = 'https://search.gov/assets/gsa-logo-893b811a49f74b06b2bddbd1cde232d2922349c8c8c6aad1d88594f3e8fe42bd097e980c57c5e28eff4d3a9256adb4fcd88bf73a5112833b2efe2e56791aad9d.svg';
 
 export const IdentifierLogoWrapper = ({ logoUrl, logoAltText }: IdentifierLogoWrapperProps) => {
   return (
@@ -15,7 +13,7 @@ export const IdentifierLogoWrapper = ({ logoUrl, logoAltText }: IdentifierLogoWr
         <IdentifierLogo href="">
           <img
             className="usa-identifier__logo-img"
-            src={logoUrl || logoImg}
+            src={logoUrl}
             alt={logoAltText || ''}
           />
         </IdentifierLogo>
