dfe3500c0c194dd6732cb96eabc78c7931a87c6d
Lora Woodford <lora.woodford@gsa.gov>
2024-01-08 13:52:25 -0500

    SRCH-4772 Remove Azure

--- app/engines/azure_composite_parameters.rb
+++
@@ -1,7 +0,0 @@
-class AzureCompositeParameters < AzureParameters
-  def initialize(options)
-    super
-    @params[:Sources] = wrap_param_in_quotes options[:sources]
-    @params[:ImageFilters] = wrap_param_in_quotes options[:image_filters]
-  end
-end
