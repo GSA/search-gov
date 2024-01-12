05b8ca6e1c5947d606aeb7704fd8239e7488be55
krbhavith <krbhavith@gmail.com>
2024-01-12 12:34:31 -0500

    corrected typo

--- spec/models/image_results_post_processor_spec.rb
+++ spec/models/image_results_post_processor_spec.rb
@@ -15,7 +15,7 @@ describe ImageResultsPostProcessor do
         results
       end
 
-      it 'has a alt text, link URL, thumbnaul URL, and image' do
+      it 'has a alt text, link URL, thumbnail URL, and image' do
         normalized_results[:results].each_with_index do |result, index|
           expect(result[:altText]).to eq("title #{index}")
           expect(result[:url]).to eq("http://foo.gov/#{index}")
@@ -37,7 +37,7 @@ describe ImageResultsPostProcessor do
       let(:total_results) { nil }
       let(:results) { nil }
 
-      it 'has a alt text, link URL, and thumbnail URL' do
+      it 'does not have alt text, link URL, and thumbnail URL' do
         expect(normalized_results[:results]).to be_nil
       end
 
