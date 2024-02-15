d743e1fa7bc21077cc36c20c399ae26d706a4169
Steven Barragan Naranjo <me@steven.mx>
2024-02-15 10:37:51 -0800

    ui: use headerTextColor in logo

--- app/javascript/components/Header/ExtendedHeader.tsx
+++ app/javascript/components/Header/ExtendedHeader.tsx
@@ -2,11 +2,11 @@ import React, { useContext } from 'react';
 import styled from 'styled-components';
 import { darken } from 'polished';
 import { Header as UswdsHeader, NavMenuButton, ExtendedNav } from '@trussworks/react-uswds';
-import { StyleContext } from '../../contexts/StyleContext';
 
 import { FontsAndColors } from '../SearchResultsLayout';
 import { HeaderProps } from './../props';
 import { Logo } from './Logo';
+import { StyleContext } from '../../contexts/StyleContext';
 
 import './ExtendedHeader.css';
 
--- app/javascript/components/Header/Logo.tsx
+++ app/javascript/components/Header/Logo.tsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import styled from 'styled-components';
 import { Logo as UswdsLogo, Title } from '@trussworks/react-uswds';
 
 import { PageData } from '../SearchResultsLayout';
+import { FontsAndColors } from '../SearchResultsLayout';
+import { StyleContext } from '../../contexts/StyleContext';
 
 interface LogoProps {
   page: PageData;
 }
 
-export const Logo = ({ page }: LogoProps) => (
-  <UswdsLogo
+const StyledLogo = styled(UswdsLogo).attrs<{ styles: FontsAndColors; }>((props) => ({ styles: props.styles }))`
+  color: ${(props) => props.styles.headerTextColor} !important;
+`;
+
+export const Logo = ({ page }: LogoProps) => {
+  const styles = useContext(StyleContext);
+
+  return <StyledLogo
     className="width-full"
-    size="slim"
-    image={page.logo?.url ? <img className="usa-identifier__logo" src={page.logo.url} alt={page.logo.text || page.title} /> : null}
     heading={page.displayLogoOnly ? <></> : <Title>{page.title}</Title>}
+    image={page.logo?.url ? <img className="usa-identifier__logo" src={page.logo.url} alt={page.logo.text || page.title} /> : null}
+    size="slim"
+    styles={styles}
   />
-);
+};
