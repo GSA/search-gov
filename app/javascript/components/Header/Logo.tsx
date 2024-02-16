ade5ffe7fab7737e18ff4be9b1091c085ee9c1bd
Lora Woodford <lora.woodford@gsa.gov>
2024-02-16 15:02:05 -0500

    SRCH-4839 Add link to header logo and title

--- app/javascript/components/Header/Logo.tsx
+++ app/javascript/components/Header/Logo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Logo as UswdsLogo, Title } from '@trussworks/react-uswds';
+import { Logo as UswdsLogo, Link, Title } from '@trussworks/react-uswds';
 
 import { PageData } from '../SearchResultsLayout';
 
@@ -7,11 +7,25 @@ interface LogoProps {
   page: PageData;
 }
 
-export const Logo = ({ page }: LogoProps) => (
-  <UswdsLogo
-    className="width-full"
-    size="slim"
-    image={page.logo?.url ? <img className="usa-identifier__logo" src={page.logo.url} alt={page.logo.text || page.title} /> : null}
-    heading={page.displayLogoOnly ? <></> : <Title>{page.title}</Title>}
-  />
-);
+export const Logo = ({ page }: LogoProps) => {
+  const imageContent = page.logo?.url ? 
+    <Link className='logo-link' href={page.homepageUrl}>
+      <img className="usa-identifier__logo" src={page.logo.url} alt={page.logo.text || page.title}/>
+    </Link> : 
+    null;
+  const titleContent = page.displayLogoOnly ? 
+    <></> : 
+    <Title>
+      <Link className='logo-link' href={page.homepageUrl}>{page.title}</Link>
+    </Title>
+    ;
+
+  return (
+    <UswdsLogo
+      className="width-full"
+      size="slim"
+      image={imageContent}
+      heading={titleContent}
+    />
+  );
+};
