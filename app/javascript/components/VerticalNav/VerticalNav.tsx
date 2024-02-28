1dcd09f9f539ed4b63a8ddaca90182cb5252b6c3
yogesh27 <yogesh.asthana27@gmail.com>
2024-02-27 22:52:49 -0500

    fix for identifier link:removed hover, vertical nav dropdown button to have same color underline

--- app/javascript/components/VerticalNav/VerticalNav.tsx
+++ app/javascript/components/VerticalNav/VerticalNav.tsx
@@ -16,6 +16,10 @@ const StyledPrimaryNav = styled(PrimaryNav).attrs<{ styles: FontsAndColors }>((p
     color: ${(props) => props.styles.searchTabNavigationLinkColor} !important;
   }
 
+  .usa-nav__primary > .usa-nav__primary-item .usa-nav__link:hover::after{
+    background-color: ${(props) => props.styles.searchTabNavigationLinkColor} !important;
+  }
+
   .usa-current::after,
   .usa-nav__primary > .usa-nav__primary-item button[aria-expanded=true] {
     background-color: ${(props) => props.styles.searchTabNavigationLinkColor} !important;
