f3b4609b952e97812df71e630adfddf8c8b6adb7
Luis Metzger <luismetzger@me.com>
2024-02-29 09:00:08 -0700

    SRCH-103: Refacto job file and spec.

--- app/jobs/searchgov_domain_destroyer_job.rb
+++ app/jobs/searchgov_domain_destroyer_job.rb
@@ -3,27 +3,10 @@ class SearchgovDomainDestroyerJob < ApplicationJob
 
   def perform(searchgov_domain)
     ActiveRecord::Base.transaction do
-      if destroy_associated_records(searchgov_domain.searchgov_urls) && destroy_record(searchgov_domain)
-        Rails.logger.info("Successfully destroyed SearchgovDomain #{searchgov_domain.id} and its URLs.")
-      else
-        Rails.logger.error("Failed to completely destroy SearchgovDomain #{searchgov_domain.id} and its URLs.")
+      searchgov_domain.searchgov_urls.find_each do |url|
+        url.destroy!
       end
+      searchgov_domain.destroy!
     end
-  end
-
-  private
-
-  def destroy_associated_records(records)
-    records.find_each(batch_size: 100) do |record|
-      return false unless destroy_record(record)
-    end
-
-    true
-  end
-
-  def destroy_record(record)
-    return false unless record.destroy
-
-    true
   end
 end
