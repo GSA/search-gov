3f28348ed843aae9297b642a5db773895bcfc01a
yogesh27 <yogesh.asthana27@gmail.com>
2024-03-06 16:11:35 -0500

    updated breakpoint to target mobile devices

--- app/javascript/components/Results/ResultGrid/ResultGridWrapper.tsx
+++ app/javascript/components/Results/ResultGrid/ResultGridWrapper.tsx
@@ -11,13 +11,17 @@ const ResultGridWrapper = ({ url, clickTracking, children }: ResultGridWrapperPr
   const [mobileResultDivStyle, setMobileResultDivStyle] = useState('');
   
   const isMobile = () => {
-    return window.innerWidth <= 480;
+    return window.innerWidth <= 767;
   };
   
   const handleResultDivClick = (url: string) => {
     if (isResultDivClickable) {
       if (clickTracking)  
         clickTracking();
+
+      
+      //[...document.getElementsByClassName('mobile-outline')].forEach((c)=>c.)
+      //[...document.getElementsByClassName('mobile-outline')][0].classList.remove('mobile-outline')
       setMobileResultDivStyle('mobile-outline');
       window.location.href = url;
     }
