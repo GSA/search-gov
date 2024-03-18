9641ab6da48f1c56db125449355841b8a10f4bc9
krbhavith <krbhavith@gmail.com>
2024-03-18 10:45:12 -0400

    code refactor for header and footer links and remove image content

--- app/models/header_and_footer_links_updater.rb
+++ app/models/header_and_footer_links_updater.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
-class ContentSettingsUpdater
+class HeaderAndFooterLinksUpdater
   def update(args)
     ids = args == 'all' ? Affiliate.all.ids : args.split
-    update_content_setings(ids)
+    update_header_and_footer_links(ids)
   end
 
   private
 
-  def update_content_setings(ids)
+  def update_header_and_footer_links(ids)
     updated_list = []
     failed_list = []
 
     Affiliate.where(id: ids).find_each do |affiliate|
-      update_affiliate_content_setings(affiliate)
+      update_affiliate_header_and_footer_links(affiliate)
       updated_list << affiliate.id
     rescue StandardError => e
       failed_list << { affiliate_id: affiliate.id, reason: e.inspect }
@@ -23,25 +23,10 @@ class ContentSettingsUpdater
     [updated_list, failed_list]
   end
 
-  def update_affiliate_content_setings(affiliate)
-    migrate_paperclip_image_to_active_storage(affiliate)
-    update_alt_text(affiliate)
+  def update_affiliate_header_and_footer_links(affiliate)
     update_header_links(affiliate)
     update_footer_links(affiliate)
     affiliate.save
-  end
-
-  def migrate_paperclip_image_to_active_storage(affiliate)
-    return if affiliate.mobile_logo_file_name.blank?
-
-    logo_url = affiliate.send(:mobile_logo).url.sub(Rails.env, 'production').gsub(' ', '%20')
-    affiliate.header_logo.attach(io: Tempfile.open(logo_url),
-                                 filename: affiliate.mobile_logo_file_name,
-                                 content_type: affiliate.mobile_logo_content_type)
-  end
-
-  def update_alt_text(affiliate)
-    affiliate.header_logo_blob.metadata.merge!({ 'custom' => { 'alt_text' => affiliate.logo_alt_text.to_s } }) if affiliate.logo_alt_text.present?
   end
 
   def update_header_links(affiliate)
@@ -67,7 +52,7 @@ class ContentSettingsUpdater
   end
 
   def log_completion(updated_list, failed_list)
-    Rails.logger.info("[content_settings_updater_task] The following affiliates were updated successfully: #{updated_list}.") if updated_list.present?
-    Rails.logger.error("[content_settings_updater_task] The following affiliates failed to update: #{failed_list}.") if failed_list.present?
+    Rails.logger.info("[header_and_footer_links_updater_task] The following affiliates were updated successfully: #{updated_list}.") if updated_list.present?
+    Rails.logger.error("[header_and_footer_links_updater_task] The following affiliates failed to update: #{failed_list}.") if failed_list.present?
   end
 end
--- lib/tasks/header_and_footer_links_updater.rake
+++ lib/tasks/header_and_footer_links_updater.rake
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
 
 namespace :searchgov do
-  # To update ALL affiliates content settings columns
-  # rake "searchgov:content_settings_updater[all]"
+  # To update ALL affiliates headers and footer columns
+  # rake "searchgov:header_and_footer_links_updater[all]"
 
   # To update select ids, add a space separated list of ids
-  # rake "searchgov:content_settings_updater[1 2 300]"
-  desc 'Update content settings for all/select affiliates'
-  task :content_settings_updater, [:ids] => :environment do |_t, args|
+  # rake "searchgov:header_and_footer_links_updater[1 2 300]"
+  desc 'Update header and footer links for all/select affiliates'
+  task :header_and_footer_links_updater, [:ids] => :environment do |_t, args|
     if args[:ids].nil?
       puts "Please provide a space-separated list of affiliate ids or 'all' as a task argument. No affiliates updated."
       next
     end
 
-    content_settings_updater = ContentSettingsUpdater.new
-    updated, failed = content_settings_updater.update(args[:ids])
+    header_and_footer_links_updater = HeaderAndFooterLinksUpdater.new
+    updated, failed = header_and_footer_links_updater.update(args[:ids])
 
     puts "#{updated.count} affiliate(s) updated successfully." if updated.present?
     puts "#{failed.count} affiliate(s) failed to update.  Consult logs for more info." if failed.present?
