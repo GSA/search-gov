9fb61aff86218b52383798dcefac86f7e4c4136d
krbhavith <krbhavith@gmail.com>
2024-03-18 10:45:12 -0400

    code refactor for header and footer links and remove image content

--- spec/lib/tasks/header_and_footer_links_updater_spec.rb
+++ spec/lib/tasks/header_and_footer_links_updater_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
-describe ContentSettingsUpdater do
-  let(:content_settings_updater) { instance_double(described_class) }
+describe HeaderAndFooterLinksUpdater do
+  let(:header_and_footer_links_updater) { instance_double(described_class) }
   let(:run_task) do
-    Rake.application.invoke_task "searchgov:content_settings_updater[#{ids}]"
+    Rake.application.invoke_task "searchgov:header_and_footer_links_updater[#{ids}]"
   end
   let(:first_affiliate_id) { affiliates(:basic_affiliate).id }
   let(:second_affiliate_id) { affiliates(:usagov_affiliate).id }
@@ -11,15 +11,15 @@ describe ContentSettingsUpdater do
   let(:ids) { "#{first_affiliate_id} #{second_affiliate_id} #{third_affiliate_id}" }
 
   before do
-    Rake.application.rake_require('tasks/content_settings_updater')
+    Rake.application.rake_require('tasks/header_and_footer_links_updater')
     Rake::Task.define_task(:environment)
-    allow(described_class).to receive(:new).and_return(content_settings_updater)
-    allow(content_settings_updater).to receive(:update)
+    allow(described_class).to receive(:new).and_return(header_and_footer_links_updater)
+    allow(header_and_footer_links_updater).to receive(:update)
   end
 
   context 'when all ids are passed as an argument' do
     before do
-      Rake::Task['searchgov:content_settings_updater'].reenable
+      Rake::Task['searchgov:header_and_footer_links_updater'].reenable
     end
 
     let(:ids) { 'all' }
@@ -28,13 +28,13 @@ describe ContentSettingsUpdater do
       run_task
 
       expect(described_class).to have_received(:new)
-      expect(content_settings_updater).to have_received(:update).with('all')
+      expect(header_and_footer_links_updater).to have_received(:update).with('all')
     end
   end
 
   context 'when select ids are passed as an argument' do
     before do
-      Rake::Task['searchgov:content_settings_updater'].reenable
+      Rake::Task['searchgov:header_and_footer_links_updater'].reenable
     end
 
     let(:ids) { "#{first_affiliate_id} #{second_affiliate_id}" }
@@ -43,16 +43,16 @@ describe ContentSettingsUpdater do
       run_task
 
       expect(described_class).to have_received(:new)
-      expect(content_settings_updater).to have_received(:update).with(ids)
+      expect(header_and_footer_links_updater).to have_received(:update).with(ids)
     end
   end
 
   context 'when id argument is absent' do
     before do
-      Rake::Task['searchgov:content_settings_updater'].reenable
+      Rake::Task['searchgov:header_and_footer_links_updater'].reenable
     end
 
-    let(:run_task) { Rake.application.invoke_task 'searchgov:content_settings_updater' }
+    let(:run_task) { Rake.application.invoke_task 'searchgov:header_and_footer_links_updater' }
     let(:warn_text) { "Please provide a space-separated list of affiliate ids or 'all' as a task argument. No affiliates updated.\n" }
 
     it 'raises a warning and exits' do
