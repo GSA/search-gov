2b49e138b283451803e84e23911db3dc9a51f215
Luis Metzger <luismetzger@me.com>
2024-03-27 15:26:22 -0600

    SRCH-103: Change order of operations for SearchgovDomainDestroyerJob.

--- app/jobs/searchgov_domain_destroyer_job.rb
+++ app/jobs/searchgov_domain_destroyer_job.rb
@@ -2,11 +2,11 @@ class SearchgovDomainDestroyerJob < ApplicationJob
   queue_as :searchgov
 
   def perform(searchgov_domain)
-    searchgov_domain.searchgov_urls.find_each(&:destroy!)
-    searchgov_domain.destroy!
     Resque::Job.destroy('searchgov',
                         'SearchgovDomainIndexerJob',
                         searchgov_domain_id: searchgov_domain.id,
                         delay: searchgov_domain.delay)
+    searchgov_domain.searchgov_urls.find_each(&:destroy!)
+    searchgov_domain.destroy!
   end
 end
