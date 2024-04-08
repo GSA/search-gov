649bc0da051fb9f8329b26b5bbbb489778f30d2a
Darius Dzien <dariusz.dzien@gsa.gov>
2024-04-08 13:44:09 -0500

    RSpec fixes

--- spec/controllers/admin/affiliates_controller_spec.rb
+++ spec/controllers/admin/affiliates_controller_spec.rb
@@ -100,7 +100,8 @@ describe Admin::AffiliatesController do
         let(:display_columns) do
           %i[ use_redesigned_results_page footer_fragment header_tagline_font_family
               header_tagline_font_size header_tagline_font_style no_results_pointer
-              page_one_more_results_pointer navigation_dropdown_label related_sites_dropdown_label ]
+              page_one_more_results_pointer navigation_dropdown_label related_sites_dropdown_label
+              looking_for_government_services ]
         end
 
         it 'contains the specified columns' do
@@ -163,6 +164,7 @@ describe Admin::AffiliatesController do
             jobs_enabled
             last_month_query_count
             locale
+            looking_for_government_services
             mobile_logo_url
             name
             raw_log_access_enabled
