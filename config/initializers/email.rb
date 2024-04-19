22556e35761f5f55b3a05185cf074c2732a08aa4
Steven Barragan Naranjo <me@steven.mx>
2024-04-19 13:04:37 -0700

    No need for action_mailer secrets
    * developer.rb alrady has the config that's on secrets.yml.dev
    * production doesn't have any action_mailer config on secrets.yml

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,15 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.config.to_prepare do
-  email_config = Rails.application.secrets.email || { }
+  interceptor = SearchGovInterceptor.new(ENV['MAIL_FORCE_TO'] || Rails.application.secrets.email)
 
-  if action_mailer_config = email_config[:action_mailer]
-    action_mailer_config.each do |name, value|
-      value.symbolize_keys! if value.instance_of?(Hash)
-      ActionMailer::Base.send("#{name}=", value)
-    end
-  end
-
-  force_to = email_config[:force_to]
-  Emailer.register_interceptor(SearchGovInterceptor.new(force_to))
+  Emailer.register_interceptor(interceptor)
 end
