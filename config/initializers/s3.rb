0e7089af3d43f74c1625bc8051b5cc88bd8d48b1
Steven Barragan Naranjo <me@steven.mx>
2024-04-19 12:51:06 -0700

    Use environment variables instead of secrets

---
+++ config/initializers/s3.rb
@@ -0,0 +1,7 @@
+S3_CREDENTIALS = {
+  access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+  bucket:            ENV['AWS_BUCKET'],
+  s3_host_alias:     ENV['AWS_S3_HOST_ALIAS'],
+  s3_region:         ENV['AWS_S3_REGION'],
+  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+}.freeze
