732b9b6f747b2c673f5d109cca769685aa07aaff
Steven Barragan Naranjo <me@steven.mx>
2024-04-25 14:39:04 -0700

    refactor initialize method.

--- app/engines/cached_search_api_connection.rb
+++ app/engines/cached_search_api_connection.rb
@@ -6,27 +6,31 @@ class CachedSearchApiConnection
   attr_reader :connection
 
   def initialize(namespace, host, cache_duration = DEFAULT_CACHE_DURATION)
-    @cache_duration = cache_duration
+    @namespace      = namespace
     @host           = host
-    @connection     = Faraday.new(host) do |conn|
+    @cache_duration = cache_duration
+  end
+
+  def get(api_endpoint, param_hash)
+    Rails.cache.fetch(cache_key(api_endpoint, param_hash), expires_in: @cache_duration) do
+      connection.get(api_endpoint, param_hash)
+    end
+  end
+
+  protected
+
+  def connection
+    @connection ||= Faraday.new(@host) do |conn|
       conn.request(:json)
-      conn.use(FaradayMiddleware::ExceptionNotifier, [namespace])
+      conn.use(FaradayMiddleware::ExceptionNotifier, [@namespace])
       conn.response(:raise_error)
       conn.response(:rashify)
       conn.response(:json)
       conn.headers[:user_agent] = 'USASearch'
 
-      ExternalFaraday.configure_connection(namespace, conn)
+      ExternalFaraday.configure_connection(@namespace, conn)
     end
   end
-
-  def get(api_endpoint, param_hash)
-    Rails.cache.fetch(cache_key(api_endpoint, param_hash), expires_in: @cache_duration) do
-      @connection.get(api_endpoint, param_hash)
-    end
-  end
-
-  protected
 
   def cache_key(api_endpoint, http_params)
     uri_args = { path: api_endpoint, host: @host }
