bf713a6497c2badc90625c21482c20e66b17aef9
Steven Barragan Naranjo <me@steven.mx>
2024-05-07 09:57:08 -0700

    Configure Login cert location from environment

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,7 +6,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     client_id:    ENV['LOGIN_CLIENT_ID'] || Rails.application.secrets.dig(:login_dot_gov, :client_id),
     idp_base_url: ENV['LOGIN_IDP_BASE_URL'] || Rails.application.secrets.dig(:login_dot_gov, :idp_base_url),
     ial:          1,
-    private_key:  OpenSSL::PKey::RSA.new(ENV['LOGIN_CERT'] || File.read('config/logindotgov.pem')),
+    private_key:  OpenSSL::PKey::RSA.new(File.read(ENV['LOGIN_CERT_LOCATION'] || 'config/logindotgov.pem')),
     redirect_uri: "#{protocol}#{ENV['LOGIN_HOST'] || Rails.application.secrets.dig(:login_dot_gov, :host)}/auth/logindotgov/callback"
   }
 end
