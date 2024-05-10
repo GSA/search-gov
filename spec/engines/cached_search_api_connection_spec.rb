e87730f832d94030a3a1d70869d1566a8900890c
Steven Barragan Naranjo <me@steven.mx>
2024-05-10 15:39:43 -0700

    Back to expect to receive

--- spec/engines/cached_search_api_connection_spec.rb
+++ spec/engines/cached_search_api_connection_spec.rb
@@ -34,7 +34,7 @@ describe CachedSearchApiConnection do
   describe '#get' do
     context 'when response has not been cached' do
       it 'calls the api' do
-        expect(cached_connection.connection).to have_received(:get).with(endpoint, params)
+        expect(cached_connection.connection).to receive(:get).with(endpoint, params)
 
         cached_connection.get(endpoint, params)
       end
