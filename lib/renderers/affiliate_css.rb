b6769a04aa2e23d44417e3cb850e5321a2cac913
Lora Woodford <lora.woodford@gsa.gov>
2024-05-14 13:16:16 -0400

    SRCH-5169 Handle missing image results in new serp

--- lib/renderers/affiliate_css.rb
+++ lib/renderers/affiliate_css.rb
@@ -3,20 +3,20 @@
 class AffiliateCss
   include AffiliateCssHelper
 
-  MOBILE_LOOK_AND_FEEL_TEMPLATE = File.read("#{Rails.root}/lib/renderers/templates/_mobile_look_and_feel.css.sass.erb").freeze
+  MOBILE_LOOK_AND_FEEL_TEMPLATE = Rails.root.join('lib/renderers/templates/_mobile_look_and_feel.css.sass.erb').read.freeze
 
   def initialize(css_hash)
     @css_hash = css_hash.freeze
   end
 
   def render_mobile_css
-    render_css render_sass_template(ERB.new MOBILE_LOOK_AND_FEEL_TEMPLATE, nil, '<>')
+    render_css(render_sass_template(ERB.new(MOBILE_LOOK_AND_FEEL_TEMPLATE, trim_mode: '<>')))
   end
 
   private
 
   def render_sass_template(template)
-    template.result binding
+    template.result(binding)
   end
 
   def render_css(sass_template)
