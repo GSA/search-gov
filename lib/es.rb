91aedaf523c813da98a201cb5372f6452edb9933
Steven Barragan Naranjo <me@steven.mx>
2024-05-23 16:10:46 -0700

    Do not validate logger level. Keep using elastic formatter

--- lib/es.rb
+++ lib/es.rb
@@ -29,7 +29,10 @@ module Es
 
   def initialize_client(config)
     Elasticsearch::Client.new(config.merge(CLIENT_CONFIG)).tap do |client|
-      client.transport.logger = Rails.logger
+      client.transport.logger = Rails.logger.clone
+      client.transport.logger.formatter = proc do |severity, time, _progname, msg|
+        "\e[2m[ES][#{time.utc.iso8601(4)}][#{severity}] #{msg}\n\e[0m"
+      end
     end
   end
 
