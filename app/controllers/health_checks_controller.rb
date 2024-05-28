d0f9138cc1a52f1b2191190d79da60adcca752d1
Steven Barragán Naranjo <850686+stevenbarragan@users.noreply.github.com>
2024-05-28 10:19:54 -0700

    SRCH-5217 SRCH-5230 SRCH-5231: Set production log level from environment. Validate Elasticsearch connection on health endpoint.  Fix Elasticsearch logs. (#1582)

--- app/controllers/health_checks_controller.rb
+++ app/controllers/health_checks_controller.rb
@@ -1,7 +1,13 @@
 class HealthChecksController < ApplicationController
   def new
     check_database
+    check_elasticsearch
+
     render(plain: 'OK')
+  end
+
+  def check_elasticsearch
+    Es::ELK.client_reader.cluster.health
   end
 
   def check_database
