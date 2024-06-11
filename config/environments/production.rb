7d794e0b3e0df7f3e915f24c06fc23d4a274aa5e
Steven Barragan Naranjo <me@steven.mx>
2024-06-11 11:18:43 -0700

    Add logger to i14y connections

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     $stdout.sync = true
     config.rails_semantic_logger.add_file_appender = false
-    config.semantic_logger.add_appender(io: $stdout, formatter: :json)
+    config.semantic_logger.add_appender(io: $stdout, formatter: config.rails_semantic_logger.format)
   end
 
   # Do not dump schema after migrations.
