7adee834263bca7304e5c8fd5b4e9ba0509c3c1e
Steven Barragan Naranjo <me@steven.mx>
2024-06-12 11:18:49 -0700

    Remove default log formatter

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,9 +80,6 @@ Rails.application.configure do
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
 
-  # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
-
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
