a1ef5027bc6c6f5554c914352d975ae83e0b13c1
krbhavith <krbhavith@gmail.com>
2024-07-17 14:42:09 -0400

    fix rspec after private method

--- spec/jobs/bulk_affiliate_styles_uploader_job_spec.rb
+++ spec/jobs/bulk_affiliate_styles_uploader_job_spec.rb
@@ -18,7 +18,6 @@ RSpec.describe BulkAffiliateStylesUploaderJob do
     allow(BulkAffiliateStylesUploadResultsMailer).to receive(:results_email).and_return(mailer_instance)
     allow(mailer_instance).to receive(:deliver_now!)
     allow(Rails.logger).to receive(:info)
-    # allow(job_instance).to receive(:report_results)
   end
 
   describe '#perform' do
@@ -36,36 +35,11 @@ RSpec.describe BulkAffiliateStylesUploaderJob do
       job_instance.instance_variable_set(:@filename, filename)
     end
 
-    it 'calls log_results and send_results_email' do
-      allow(job_instance).to receive(:log_results)
-      allow(job_instance).to receive(:send_results_email)
-      job_instance.report_results
-      expect(job_instance).to have_received(:log_results)
-      expect(job_instance).to have_received(:send_results_email)
-    end
-  end
-
-  describe '#log_results' do
-    before do
-      job_instance.instance_variable_set(:@uploader, uploader)
-    end
-
-    it 'logs the results' do
-      job_instance.log_results
+    it 'logs results and sends results email' do
+      job_instance.perform(user, filename, filepath)
       expect(Rails.logger).to have_received(:info).with("BulkAffiliateStylesUploaderJob: #{filename}")
       expect(Rails.logger).to have_received(:info).with('    100 affiliates')
       expect(Rails.logger).to have_received(:info).with('    2 errors')
-    end
-  end
-
-  describe '#send_results_email' do
-    before do
-      job_instance.instance_variable_set(:@uploader, uploader)
-      job_instance.instance_variable_set(:@user, user)
-    end
-
-    it 'sends the results email' do
-      job_instance.send_results_email
       expect(mailer_instance).to have_received(:deliver_now!)
     end
   end
