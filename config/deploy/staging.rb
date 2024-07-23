276e621939eed8d6282b077e0adc6163ca43dbdb
Luis Metzger <luismetzger@me.com>
2024-07-22 22:06:38 -0600

    SRCH-5358: Update configs.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -39,33 +39,12 @@ set :ssh_options, {
   auth_methods: %w(publickey)
 }
 
-# The server-based syntax can be used to override options:
-# ------------------------------------
-# server "staging.server.com",
-#   user: "deploy",
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: "deploy", # overrides user setting above
-#     keys: [fetch(:ssh_key_path, '/default/path/to/staging_ec2_keypair.pem')],
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: "please use keys"
-#   }
-
-
 # Keep only the last 5 releases to save disk space
 set :keep_releases, 5
 
-# Configurations for Capistrano and Puma
-set :puma_threads, [4, 16]
-set :puma_workers, 0
-
-set :puma_bind, "tcp://0.0.0.0:3000" # or "unix://#{shared_path}/tmp/sockets/puma.sock"
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log, "#{release_path}/log/puma.access.log"
 set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 set :puma_enable_socket_service, true
