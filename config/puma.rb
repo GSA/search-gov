c3660b3d4227cf5294d8615ca6f6543ce01a779a
Luis Metzger <luismetzger@me.com>
2024-07-24 22:01:35 -0600

    Update puma config file.

--- config/puma.rb
+++ config/puma.rb
@@ -12,8 +12,9 @@ threads min_threads_count, max_threads_count
 # terminating a worker in development environments.
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
+# Comment out the port setting if you are using bind
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
+# port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 environment ENV.fetch("RAILS_ENV") { "development" }
@@ -21,8 +22,8 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
+# Use the bind directive to specify the address and port to listen on
 bind "tcp://0.0.0.0:3000"
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
-
