2ae79c610b24734a2f4de69f7357d11215d9d8c6
Steven Barragan Naranjo <me@steven.mx>
2024-08-08 13:24:43 -0700

    Setup workers for all environments

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,3 +19,5 @@ set :rails_env,   'production'
 set :default_env, { SECRET_KEY_BASE: '1' }
 
 set :resque_environment_task, true
+
+role :resque_worker, JSON.parse(ENV.fetch('RESQUE_SERVER_ADDRESSES', '[]')), user: ENV['SERVER_DEPLOYMENT_USER']
