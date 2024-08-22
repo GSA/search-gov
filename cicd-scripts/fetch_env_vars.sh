7062055070d032e6de395672a278f412c78bbe86
sb-ebukaanene <98557321+sb-ebukaanene@users.noreply.github.com>
2024-08-22 12:16:49 -0700

    Update fetch_env_vars.sh

--- cicd-scripts/fetch_env_vars.sh
+++ cicd-scripts/fetch_env_vars.sh
@@ -41,4 +41,5 @@ cat .env
 cp /home/search/cicd_temp/.env /home/search/searchgov/shared
 
 
-aws secretsmanager get-secret-value --secret-id LOGIN_DOT_GOV_PEM --region us-east-2 --query 'SecretString' --output text > /home/search/searchgov/logindotgov.pem 
+aws ssm get-parameter --name "LOGIN_DOT_GOV_PEM" --region us-east-2 --with-decryption --query "Parameter.Value" --output text > /home/search/searchgov/logindotgov.pem
+
