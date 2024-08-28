5889c7e000cb0da28d86fb1f732ba783c6250cdd
Igor Zaytsev <igor.zaytsev.dev@gmail.com>
2024-08-28 19:09:00 -0400

    Added 100% testing coverage

--- spec/controllers/urls_controller_spec.rb
+++ spec/controllers/urls_controller_spec.rb
@@ -24,5 +24,22 @@ describe UrlsController do
       expect(response).to have_http_status('422')
       expect(response.body).to match(/The 'urls' parameter is required./)
     end
+
+    it 'permits the :urls parameter' do
+      controller.params = ActionController::Parameters.new(urls: urls_example)
+      permitted_params = controller.instance_variable_get(:@_params).instance_variable_get(:@parameters)
+      expect(permitted_params).to eq({ 'urls' => ['http://example.com'] })
+    end
+
+    it 'does not permit other parameters' do
+      controller.params = ActionController::Parameters.new(urls: ['http://example.com'], foo: 'bar')
+      expect(controller.instance_variable_get(:@permitted_params)).to be_nil
+    end
+
+    it 'returns an empty hash when :urls is not present' do
+      controller.params = ActionController::Parameters.new
+      permitted_params = controller.instance_variable_get(:@_params).instance_variable_get(:@parameters)
+      expect(permitted_params).to eq({})
+    end
   end
 end
