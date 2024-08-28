1779d32614f93f444472da46054b54350b1f942d
Igor Zaytsev <igor.zaytsev.dev@gmail.com>
2024-08-28 16:36:38 -0400

    Fixed unit tests and codeclimate

--- spec/controllers/urls_controller_spec.rb
+++ spec/controllers/urls_controller_spec.rb
@@ -2,30 +2,24 @@ require 'spec_helper'
 
 describe UrlsController do
   urls_example = ['http://example.com']
-  describe '#create' do
-    it 'returns a JSON response with a job ID' do
+  describe 'test controller #create' do
+    it 'returns a JSON response with a job_id' do
       post :create, params: { urls: urls_example }
-      expect(response.status).to eq(200)
+      expect(response).to have_http_status(:ok)
       expect(response.content_type).to eq('application/json; charset=utf-8')
-      expect(JSON.parse(response.body)).to have_key('job_id')
+      expect(response.parsed_body).to have_key('job_id')
     end
 
     it 'enqueues a SearchgovUrlsJob with the provided URLs' do
-      expect(SearchgovUrlsJob).to receive(:perform_later).with(anything, urls_example)
+      allow(SearchgovUrlsJob).to receive(:perform_later)
       post :create, params: { urls: urls_example }
+      expect(SearchgovUrlsJob).to have_received(:perform_later).with(anything, urls_example)
     end
 
     it 'returns a 422 error if no URLs are provided' do
       post :create
-      expect(response.status).to eq(422)
+      expect(response).to have_http_status('422')
       expect(response.body).to match(/The 'urls' parameter is required./)
-    end
-  end
-
-  describe '#create' do
-    it 'enqueues a SearchgovUrlsJob and calls perform_later() method' do
-      expect(SearchgovUrlsJob).to receive(:perform_later)
-      post :create, params: { urls: urls_example }
     end
   end
 end
--- spec/jobs/searchgov_urls_job_spec.rb
+++ spec/jobs/searchgov_urls_job_spec.rb
@@ -1,49 +1,35 @@
-# require 'rails_helper'
 require 'spec_helper'
 
-RSpec.describe SearchgovUrlsJob, type: :job do
+describe SearchgovUrlsJob do
   let(:job_id) { '12345' }
-  let(:urls) { ['http://example.com', 'http://example.org'] }
-  let(:time_started) { Time.zone.now }
+  let(:urls) do
+    [
+      'https://agency.gov/one-url',
+      'https://agency.gov/another-url'
+    ]
+  end
 
   describe '#perform' do
-    it 'sets the time started' do
-      expect(Time).to receive(:zone).and_return(double(now: time_started))
+    it 'urls are received by SearchgovUrl' do
       described_class.perform_now(job_id, urls)
+      expect(SearchgovUrl.find_by(url: urls[0])).not_to be_nil
+      expect(SearchgovUrl.find_by(url: urls[1])).not_to be_nil
     end
 
-    it 'sets the total count' do
-      expect { described_class.perform_now(job_id, urls) }.to change { described_class.new.total_count }.from(nil).to(2)
-    end
-
-    it 'creates a new BulkUrlUploader' do
-      expect(BulkUrlUploader).to receive(:new).with(job_id, urls)
-      described_class.perform_now(job_id, urls)
-    end
-
-    it 'calls upload_and_index on the BulkUrlUploader' do
-      uploader = double(upload_and_index: true)
+    it 'upload_and_index method is called' do
+      uploader = instance_spy(
+        BulkUrlUploader,
+        upload_and_index: nil,
+        results: instance_double(
+          BulkUrlUploader::Results,
+          name: 'Test Results',
+          total_count: 10,
+          error_count: 2
+        )
+      )
       allow(BulkUrlUploader).to receive(:new).and_return(uploader)
-      expect(uploader).to receive(:upload_and_index)
       described_class.perform_now(job_id, urls)
-    end
-
-    it 'logs the results' do
-      expect(Rails.logger).to receive(:info).with(/SearchgovUrlsJob/)
-      described_class.perform_now(job_id, urls)
-    end
-
-    it 'logs the correct information' do
-      uploader = instance_double(BulkUrlUploader, upload_and_index: true, results: double(name: 'Test Results', total_count: 10, error_count: 2, start_time: time_started, end_time: Time.zone.now))
-      allow(BulkUrlUploader).to receive(:new).and_return(uploader)
-      expect(Rails.logger).to receive(:info).with('SearchgovUrlsJob: Test Results', total_count: 10, errors_count: 2, start_time: time_started, end_time: Time.zone.now)
-      described_class.perform_now(job_id, urls)
-    end
-
-    it 'handles an exception' do
-      allow(BulkUrlUploader).to receive(:new).and_raise(StandardError, 'Test Error')
-      expect(Rails.logger).to receive(:error).with('SearchgovUrlsJob: StandardError - Test Error')
-      described_class.perform_now(job_id, urls)
+      expect(uploader).to have_received(:upload_and_index)
     end
   end
 end
