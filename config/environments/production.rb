1a821da9b09c5a364cccc828297b35115eef13fa
krbhavith <krbhavith@gmail.com>
2024-09-09 14:32:43 -0400

    update css compressor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,10 +23,9 @@ Rails.application.configure do
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
+  # Compress CSS using a preprocessor.
   # config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :yui
-
-  # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
   # Do not fall back to assets pipeline if a precompiled asset is missed.
