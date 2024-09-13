94692329025894850b9796d646c332b55b8592e8
Steven Barragan Naranjo <me@steven.mx>
2024-09-13 15:50:55 -0700

    Setup host and port for sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,9 +3,9 @@
 sidekiq = Rails.application.config_for(:sidekiq)
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: sidekiq['url'] }
+  config.redis = { host: sidekiq['host'], port: sidekiq['port'] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: sidekiq['url'] }
+  config.redis = { host: sidekiq['host'], port: sidekiq['port'] }
 end
