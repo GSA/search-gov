b647d0fda10958773c4979c265e087f20f1efba6
Luis Metzger <luismetzger@me.com>
2024-09-19 13:19:14 -0600

    Disable linter for this line.

--- lib/i18n_jsx_scanners/i18n_jsx_scanner.rb
+++ lib/i18n_jsx_scanners/i18n_jsx_scanner.rb
@@ -11,7 +11,9 @@ if Rails.env.development? || Rails.env.test?
       text = read_file(path)
       text.scan(/i18n\.t\(["']([\w.]+)/).map do |match|
         raw_key = match.first.underscore
+        # rubocop:disable Style/HashSyntax
         occurrence = occurrence_from_position(path, text, Regexp.last_match.offset(0).first, raw_key: raw_key)
+        # rubocop:enable Style/HashSyntax
 
         [raw_key, occurrence]
       end
