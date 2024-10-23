50f83714bef4a6ff9be54e0724fc871493f3e0bd
Steven Barragan Naranjo <me@steven.mx>
2024-10-22 18:07:35 -0700

    Migrate daily, weekly, and monthtly cron jobs.

--- config/schedule.rb
+++ config/schedule.rb
@@ -13,3 +13,60 @@ end
 every '18 9 * * 1-5', roles: [:cron]  do
   rake 'search:federal_register:import_documents'
 end
+
+every '0 2-20 * * * ', roles: [:cron] do
+  rake "usasearch:sayt_suggestions:compute[#{Time.now.strftime('%Y%m%d')},1000]"
+end
+
+every '0 2-20 * * * ', roles: [:cron] do
+  runner 'SearchgovUrl.counter_culture_fix_counts'
+end
+
+every '0 2-20 * * * ', roles: [:cron] do
+  runner 'usasearch:rss_feed:refresh_affiliate_feeds'
+end
+
+every '5 0 * * *', roles: [:cron] do
+  rake 'usasearch:sayt_suggestions:compute'
+  rake 'usasearch:sayt_suggestions:expire[21]'
+end
+
+every '5 0 * * *', roles: [:cron] do
+  rake 'usasearch:site_feed_url:refresh_all'
+end
+
+every '5 0 * * *', roles: [:cron] do
+  rake 'usasearch:reports:daily_snapshot'
+end
+
+every '5 0 * * *', roles: [:cron] do
+ rake 'usasearch:user:update_not_active_approval_status'
+end
+
+every '5 0 * * *', roles: [:cron] do
+  rake 'usasearch:user:warn_set_to_not_approved[76]'
+end
+
+every '5 0 * * *', roles: [:cron] do
+ rake 'usasearch:user:warn_set_to_not_approved[86]'
+end
+
+every '5 0 * * *', roles: [:cron] do
+ command 'bin/detect_future_usage'
+end
+
+every '25 2 * * 0', roles: [:cron] do
+ rake 'usasearch:sayt_filters:filtered_popular_terms'
+end
+
+every '25 2 * * 0', roles: [:cron] do
+ rake 'usasearch:medline:load'
+end
+
+every '25 2 * * 0', roles: [:cron] do
+ rake 'usasearch:rss_feed_urls:enqueue_destroy_all_inactive'
+end
+
+every '25 2 * * 0', roles: [:cron] do
+ rake 'usasearch:user:update_approval_status'
+end
