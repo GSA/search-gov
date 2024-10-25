62e20ab7e315ba687f0c9def642436ea899d1f73
luisgmetzger <148913056+luisgmetzger@users.noreply.github.com>
2024-10-25 14:47:06 -0600

    Add copy script to asset directory in public directory. (#1727) (#1728)
    Co-authored-by: Luis Metzger <luismetzger@me.com>

--- cicd-scripts/copy_non_fingerprinted_assets.sh
+++ cicd-scripts/copy_non_fingerprinted_assets.sh
@@ -2,9 +2,14 @@
 
 cd /home/search/searchgov/current
 
-# Copy non-fingerprinted JS and CSS assets
+# Copy non-fingerprinted JS and CSS assets in public/packs
 for file in $(find public/packs -name "*-*.js" -or -name "*-*.css"); do
   cp "$file" "${file%%-*}.${file##*.}"
 done
 
-echo "Non-fingerprinted assets copied successfully."
+# Copy non-fingerprinted assets in public/assets directory
+for file in $(find public/assets -name "*-*.js" -or -name "*-*.css" -or -name "*-*.js.gz" -or -name "*-*.css.gz"); do
+  cp "$file" "${file%%-*}.${file##*.}"
+done
+
+echo "Non-fingerprinted assets copied successfully in both packs and assets directories."
