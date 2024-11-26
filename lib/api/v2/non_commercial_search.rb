6431f65a122c8b53bb685515906b7ba0b1d81356
Steven Barragan Naranjo <me@steven.mx>
2024-11-26 10:48:32 -0800

    Setup highlight options for api correclty

--- lib/api/v2/non_commercial_search.rb
+++ lib/api/v2/non_commercial_search.rb
@@ -3,16 +3,4 @@
 module Api::V2::NonCommercialSearch
   include Api::V2::SearchAsJson
   include Api::V2::AsJsonAppendWebSpellingCorrection
-
-  # attr_reader :next_offset
-
-  # def initialize(options = {})
-  #   super
-  # end
-
-  protected
-
-  def result_url(result)
-    result.url
-  end
 end
