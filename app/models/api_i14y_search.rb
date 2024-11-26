4d2593cad8dc2d015b5a00560d8b61506ad50662
Steven Barragan Naranjo <me@steven.mx>
2024-11-26 10:48:32 -0800

    Setup highlight options for api correclty

--- app/models/api_i14y_search.rb
+++ app/models/api_i14y_search.rb
@@ -4,6 +4,7 @@ class ApiI14ySearch < I14ySearch
   def initialize(options = {})
     super
 
-    @highlight_options = options.slice(:pre_tags, :post_tags) || Api::V2::HighlightOptions::DEFAULT
+    @highlight_options[:pre_tags] ||= Api::V2::HighlightOptions::DEFAULT[:pre_tags]
+    @highlight_options[:post_tags] ||= Api::V2::HighlightOptions::DEFAULT[:post_tags]
   end
 end
--- app/models/search_initializer.rb
+++ app/models/search_initializer.rb
@@ -10,6 +10,7 @@ module SearchInitializer
     @total = 0
     @limit = options[:limit]
     @offset = options[:offset]
+    @next_offset_within_limit = options[:next_offset_within_limit]
     @highlight_options = options.slice(:pre_tags, :post_tags)
     @highlight_options[:highlighting] = options[:enable_highlighting]
   end
@@ -51,7 +52,7 @@ module SearchInitializer
   end
 
   def add_facets_to_results(result)
-    I14ySearch::FACET_FIELDS.reject{ |f| f == 'created' || result[f].nil? }.each_with_object({}) do |field, fields|
+    I14ySearch::FACET_FIELDS.reject { |f| f == 'created' || result[f].nil? }.each_with_object({}) do |field, fields|
       field_key = field.to_sym == :changed ? :updated_date : field.to_sym
       field_value = field.to_sym == :changed ? result['changed'].to_date : result[field]
       fields[field_key] = field_value
