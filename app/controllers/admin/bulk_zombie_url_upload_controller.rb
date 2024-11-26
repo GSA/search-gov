4bdd2661da3d54d0c74546899df561bed509e17e
krbhavith <krbhavith@gmail.com>
2024-11-26 11:20:26 -0600

    SRCH-5154 Bulk delete zombie records from Elastic Search

---
+++ app/controllers/admin/bulk_zombie_url_upload_controller.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+module Admin
+  class BulkZombieUrlUploadController < AdminController
+    def index
+      @page_title = 'Bulk Zombie URL Upload'
+    end
+
+    def upload
+      begin
+        @file = params[:bulk_upload_urls]
+        BulkZombieUrlUploader::UrlFileValidator.new(@file).validate!
+        enqueue_job
+        flash[:success] = success_message(@file.original_filename)
+      rescue BulkZombieUrlUploader::Error => e
+        Rails.logger.error 'Url upload failed', e
+        flash[:error] = e.message
+      end
+
+      redirect_to admin_bulk_zombie_url_upload_index_path
+    end
+
+    private
+
+    def success_message(filename)
+      <<~SUCCESS_MESSAGE
+        Successfully uploaded #{filename} for processing.
+        The results will be emailed to you.
+      SUCCESS_MESSAGE
+    end
+
+    def enqueue_job
+      BulkZombieUrlUploaderJob.perform_later(
+        current_user,
+        @file.original_filename,
+        @file.tempfile.set_encoding('UTF-8').readlines,
+        reindex: ActiveModel::Type::Boolean.new.cast(params[:reindex])
+      )
+    end
+  end
+end
