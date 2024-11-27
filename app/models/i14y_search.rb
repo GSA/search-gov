ac9170f8e43fdc7d0d0d9adb8b6d80649d893671
Steven Barragan Naranjo <me@steven.mx>
2024-11-27 15:55:11 -0800

    Make ApiBlendedSearch to inherit from BlendedSearch

--- app/models/i14y_search.rb
+++ app/models/i14y_search.rb
@@ -2,6 +2,8 @@ class I14ySearch < FilterableSearch
   include SearchInitializer
   include Govboxable
 
+  I14Y_SUCCESS = 200
+
   FACET_FIELDS = %w[audience
                     changed
                     content_type
@@ -96,6 +98,49 @@ class I14ySearch < FilterableSearch
     handles << 'searchgov' if affiliate.search_engine == 'SearchGov' || !@affiliate.gets_i14y_results
     handles.join(',')
   end
+
+  def handle_response(response)
+     return unless response && response.status == I14Y_SUCCESS
+
+     process_valid_response(response)
+   end
+
+   def process_valid_response(response)
+     @total = response.metadata.total
+     @next_offset = @offset + @limit if @next_offset_within_limit && @total > (@offset + @limit)
+     post_processor = I14yPostProcessor.new(@enable_highlighting, response.results, @affiliate.excluded_urls_set)
+     post_processor.post_process_results
+     process_pagination_values(post_processor, response)
+     process_metadata_values(response)
+   end
+
+   def process_pagination_values(post_processor, response)
+     @results = paginate(response.results)
+     @normalized_results = process_data_for_redesign(post_processor)
+     @startrecord = ((@page - 1) * @per_page) + 1
+     @endrecord = @startrecord + @results.size - 1
+   end
+
+   def process_metadata_values(response)
+     @spelling_suggestion = response.metadata.suggestion.text if response.metadata.suggestion.present?
+     @aggregations = response.metadata.aggregations if response.metadata.aggregations.present?
+   end
+
+   def result_url(result)
+     result.link
+   end
+
+   def as_json_result_hash(result)
+     @include_facets ? super.merge(add_facets_to_results(result)) : super
+   end
+
+   def add_facets_to_results(result)
+     I14ySearch::FACET_FIELDS.reject { |f| f == 'created' || result[f].nil? }.each_with_object({}) do |field, fields|
+       field_key = field.to_sym == :changed ? :updated_date : field.to_sym
+       field_value = field.to_sym == :changed ? result['changed'].to_date : result[field]
+       fields[field_key] = field_value
+     end
+   end
 
   def process_data_for_redesign(post_processor)
     post_processor.normalized_results(@total)
--- app/models/search_initializer.rb
+++ app/models/search_initializer.rb
@@ -1,7 +1,6 @@
 module SearchInitializer
   include Api::V2::NonCommercialSearch
 
-  I14Y_SUCCESS = 200
 
   def initialize(options)
     super
@@ -13,33 +12,6 @@ module SearchInitializer
     @next_offset_within_limit = options[:next_offset_within_limit]
     @highlight_options = options.slice(:pre_tags, :post_tags)
     @highlight_options[:highlighting] = options[:enable_highlighting]
-  end
-
-  def handle_response(response)
-    return unless response && response.status == I14Y_SUCCESS
-
-    process_valid_response(response)
-  end
-
-  def process_valid_response(response)
-    @total = response.metadata.total
-    @next_offset = @offset + @limit if @next_offset_within_limit && @total > (@offset + @limit)
-    post_processor = I14yPostProcessor.new(@enable_highlighting, response.results, @affiliate.excluded_urls_set)
-    post_processor.post_process_results
-    process_pagination_values(post_processor, response)
-    process_metadata_values(response)
-  end
-
-  def process_pagination_values(post_processor, response)
-    @results = paginate(response.results)
-    @normalized_results = process_data_for_redesign(post_processor)
-    @startrecord = ((@page - 1) * @per_page) + 1
-    @endrecord = @startrecord + @results.size - 1
-  end
-
-  def process_metadata_values(response)
-    @spelling_suggestion = response.metadata.suggestion.text if response.metadata.suggestion.present?
-    @aggregations = response.metadata.aggregations if response.metadata.aggregations.present?
   end
 
   def result_url(result)
