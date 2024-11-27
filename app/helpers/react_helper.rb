b1e4c74be33a2191d9a5733087b7d0fc806335c1
Luis Metzger <luismetzger@me.com>
2024-11-27 14:04:43 -0700

    Update implementation to use environment variable.

--- app/helpers/react_helper.rb
+++ app/helpers/react_helper.rb
@@ -27,7 +27,8 @@ module ReactHelper
       sitelimit: sitelimit_alert(search, params),
       spellingSuggestion: spelling_text(search, search_options),
       translations: translations(affiliate.locale),
-      vertical:
+      vertical: vertical,
+      facetsEnabled: ENV.fetch('FACETED_SEARCH_ENABLED', 'false')
     }
 
     react_component('SearchResultsLayout', data.compact_blank)
