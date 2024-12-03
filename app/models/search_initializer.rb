508031a7cb590f7988d081187bff5d72e926986b
Steven Barragan Naranjo <me@steven.mx>
2024-12-03 11:17:31 -0800

    Move i14y_search#initialize to search_initializer

--- app/models/search_initializer.rb
+++ app/models/search_initializer.rb
@@ -1,6 +1,8 @@
 module SearchInitializer
   include Api::V2::NonCommercialSearch
 
+  attr_reader :aggregations, :collection, :matching_site_limits
+
   def initialize(options)
     super
     @options = options
@@ -11,6 +13,19 @@ module SearchInitializer
     @next_offset_within_limit = options[:next_offset_within_limit]
     @highlight_options = options.slice(:pre_tags, :post_tags)
     @highlight_options[:highlighting] = options[:enable_highlighting]
+    @enable_highlighting = options[:enable_highlighting] != false
+    @include_facets = options[:include_facets] == 'true'
+    @collection = options[:document_collection]
+    @site_limits = options[:site_limits]
+    @matching_site_limits = formatted_query_instance.matching_site_limits
+  end
+
+  def formatted_query
+    formatted_query_instance.query
+  end
+
+  def formatted_query_instance
+    @formatted_query_instance ||= I14yFormattedQuery.new(@query, domains_scope_options)
   end
 
   def as_json_result_hash(result)
