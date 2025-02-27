099369db780a4222ccba7cf43132d62e69ed0970
Steven Barragan Naranjo <me@steven.mx>
2025-02-27 11:29:44 -0800

    Setup SearchElastic third search engine option

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -72,8 +72,10 @@ class SearchesController < ApplicationController
   def pick_klass_vertical_template
     if get_commercial_results?
       [WebSearch, :web, :index]
-    elsif gets_i14y_results?
+    elsif @affiliate.search_engine == 'SearchElastic'
       [SearchElasticEngine, :i14y, :i14y]
+    elsif gets_i14y_results?
+      [I14ySearch, :i14y, :i14y]
     elsif @affiliate.gets_blended_results
       [BlendedSearch, :blended, :blended]
     else
