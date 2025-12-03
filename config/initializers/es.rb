c55344fc9f0fad81e105d87627eb9050ce2898b4
igoristic <igor.zaytsev.dev@gmail.com>
2025-12-03 15:09:55 -0500

    Revert "SRCH-5952: Added multi-field keyword option (#1875)"
    This reverts commit f95fc213405de452a42b7635f420248da437ebce.

--- config/initializers/es.rb
+++ config/initializers/es.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
-require 'search_elastic/index_create'
 
 module ES
   ES_CONFIG = Rails.application.config_for(:elasticsearch_client).freeze
 
   def self.client
-    @client ||= Elasticsearch::Client.new(
+    Elasticsearch::Client.new(
       ES_CONFIG.merge(
         randomize_hosts: true,
         retry_on_failure: true,
@@ -21,15 +20,20 @@ module ES
     )
   end
 
-  @index_creator = SearchElasticIndexCreator.new(
-    service_name: 'ELASTICSEARCH',
-    index_name: ENV.fetch('SEARCHELASTIC_INDEX'),
-    shards: ENV.fetch('SEARCHELASTIC_INDEX_SHARDS', 1),
-    replicas: ENV.fetch('SEARCHELASTIC_INDEX_REPLICAS', 1)
-  )
-
   def self.create_index
-    @index_creator.create_or_update_index(self.client)
-  end
+    index_name = ENV.fetch('SEARCHELASTIC_INDEX')
 
+    template_generator = SearchElastic::Template.new("*#{index_name}*")
+
+    self.client.indices.put_template(
+      body: template_generator.body,
+      create: true,
+      include_type_name: false,
+      name: index_name,
+      order: 0
+    )
+
+    repo = SearchElastic::DocumentRepository.new
+    repo.create_index!(index: index_name, include_type_name: true)
+  end
 end
