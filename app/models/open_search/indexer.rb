5a2b079c6d07c4b5c65df300da59d1079c47f381
igoristic <igor.zaytsev.dev@gmail.com>
2025-12-03 15:42:09 -0500

    Revert "SRCH-5952: Added multi-field keyword option (#1875)" (#1918)
    This reverts commit f95fc213405de452a42b7635f420248da437ebce.

--- app/models/open_search/indexer.rb
+++ app/models/open_search/indexer.rb
@@ -1,16 +1,19 @@
 # frozen_string_literal: true
-require 'search_elastic/index_create'
 
 class OpenSearch::Indexer
-
-  @index_creator = SearchElasticIndexCreator.new(
-    service_name: 'OPENSEARCH',
-    index_name: ENV.fetch('OPENSEARCH_SEARCH_INDEX'),
-    shards: ENV.fetch('OPENSEARCH_INDEX_SHARDS', 1),
-    replicas: ENV.fetch('OPENSEARCH_INDEX_REPLICAS', 1)
-  )
-
   def self.create_index
-    @index_creator.create_or_update_index(OPENSEARCH_CLIENT)
+    index_name = ENV.fetch('OPENSEARCH_SEARCH_INDEX')
+
+    template_generator = OpenSearch::Template.new("*#{index_name}*")
+
+    OPENSEARCH_CLIENT.indices.put_template(
+      body: template_generator.body,
+      create: true,
+      name: index_name,
+      order: 0
+    )
+
+    repo = OpenSearch::DocumentRepository.new
+    repo.create_index!(index: index_name)
   end
 end
