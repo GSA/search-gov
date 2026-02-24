caaf6d2662f0f2ef52a9e0457b6ca0f6ca8e2973
amian-fearless <amian@fearless.com>
2026-02-24 16:05:45 -0500

    SRCH-6338 Added whoami echo.

--- cicd-scripts/fetch_env_vars.sh
+++ cicd-scripts/fetch_env_vars.sh
@@ -8,6 +8,7 @@ PARAM_PATH=""
 > .env
 
 echo "Starting the script"
+echo "whoami: " $(whoami)
 # Fetch all parameter names in the region using IMDSv2 method which new method
 TOKEN=$(curl -sS --fail --max-time 2 -X PUT "http://169.254.169.254/latest/api/token" \
   -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
