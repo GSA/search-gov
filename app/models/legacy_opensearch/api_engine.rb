b0ea4db091df0ae61ccf3048074a4b20cc152d70
Yaritza M. Garcia Chaparro <yaritza.garcia@gsa.gov>
2026-03-10 16:21:38 -0400

    SRCH-6424 Create the LegacyOpensearch search engine (#1983)
    * - Update SEARCH_ENGINES and VALID_SEARCH_ENGINES
    - Add LEGACY_OPENSEARCH_INDEX
    - Add engine
    
    * Add LegacyOpenSearch support in SearchesController, implement ApiEngine and tests.
    
    * Add spec for searches_controller
    
    * Fix case in LegacyOpenSearch constant
    
    * - Update environment variables
    - Fix constant naming for LegacyOpenSearch
    - Fix duplication in engine_spec.rb
    - Added mossing spaces and missing pragma.

---
+++ app/models/legacy_opensearch/api_engine.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class LegacyOpenSearch::ApiEngine < LegacyOpenSearch::Engine
+  include ApiSearch
+  def as_json_result_hash(result)
+    super.merge(thumbnail_url: result.thumbnail_url)
+  end
+end
