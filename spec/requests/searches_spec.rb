d1e9a281ca18192a77bc85170dffcd2e0b3f9239
luisgmetzger <luis.metzger@gsa.gov>
2026-04-27 10:06:21 -0600

    Remove orphaned news redirect specs (routes handled by PR #2013)
    The /search/news and /search/news/videos redirect routes were removed
    from routes.rb but these two test cases were left behind, causing them
    to hit the catch-all route and fail.

--- spec/requests/searches_spec.rb
+++ spec/requests/searches_spec.rb
@@ -3,16 +3,6 @@
 describe SearchesController do
   let(:affiliate) { affiliates(:basic_affiliate) }
 
-  context '#news' do
-    before do
-      get '/search/news', params: { query: 'element', affiliate: affiliate.name }
-    end
-
-    it 'redirects to /search' do
-      expect(response).to redirect_to('/search')
-    end
-  end
-
   context '#docs' do
     before do
       get '/search/docs', params: { query: 'pdf',
@@ -25,16 +15,6 @@ describe SearchesController do
 
     it 'responds with success' do
       expect(response).to have_http_status(:ok)
-    end
-  end
-
-  context 'when searching via the legacy video news path' do
-    before do
-      get '/search/news/videos', params: { query: 'element', affiliate: affiliate.name }
-    end
-
-    it 'redirects to /search' do
-      expect(response).to redirect_to('/search')
     end
   end
 end
