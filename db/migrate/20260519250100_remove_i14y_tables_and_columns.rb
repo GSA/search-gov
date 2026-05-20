19aae34977cb95fd10202f10625388d48ea79b20
luisgmetzger <luis.metzger@gsa.gov>
2026-05-19 19:15:30 -0600

    Add migration to drop i14y tables and columns
    Drop i14y_memberships and i14y_drawers tables, remove
    gets_i14y_results and i14y_date_stamp_enabled columns
    from affiliates (SRCH-6455).

---
+++ db/migrate/20260519250100_remove_i14y_tables_and_columns.rb
@@ -0,0 +1,35 @@
+class RemoveI14yTablesAndColumns < ActiveRecord::Migration[7.0]
+  def up
+    drop_table :i14y_memberships if table_exists?(:i14y_memberships)
+    drop_table :i14y_drawers if table_exists?(:i14y_drawers)
+
+    if column_exists?(:affiliates, :gets_i14y_results)
+      remove_column :affiliates, :gets_i14y_results
+    end
+
+    if column_exists?(:affiliates, :i14y_date_stamp_enabled)
+      remove_column :affiliates, :i14y_date_stamp_enabled
+    end
+  end
+
+  def down
+    create_table :i14y_drawers, id: :integer do |t|
+      t.string :handle, null: false
+      t.string :token, null: false
+      t.string :description
+      t.timestamps null: false
+    end
+
+    create_table :i14y_memberships, id: :integer do |t|
+      t.integer :affiliate_id, null: false
+      t.integer :i14y_drawer_id, null: false
+      t.timestamps null: false
+    end
+
+    add_index :i14y_memberships, [:affiliate_id, :i14y_drawer_id], unique: true
+    add_index :i14y_memberships, :i14y_drawer_id
+
+    add_column :affiliates, :gets_i14y_results, :boolean, default: false, null: false
+    add_column :affiliates, :i14y_date_stamp_enabled, :boolean, default: false, null: false
+  end
+end
