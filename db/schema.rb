7f079ca89fcd619495a0c2fff129a36991b7d30c
YaritzaGarcia <yaritza.garcia@gsa.gov>
2026-05-27 11:37:43 -0400

    Update schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2026_05_20_010100) do
+ActiveRecord::Schema[7.1].define(version: 2026_05_22_173818) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
