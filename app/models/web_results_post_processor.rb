90b24bf30aac984cc122a44d9d63da14485b3e19
YaritzaGarcia <yaritza.garcia@gsa.gov>
2026-05-28 15:42:01 -0400

    Add missing code.

--- app/models/web_results_post_processor.rb
+++ app/models/web_results_post_processor.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class WebResultsPostProcessor < ResultsPostProcessor
+  include ResultsRejector
   SPECIAL_URL_PATH_EXT_NAMES = %w[doc pdf ppt ps rtf swf txt xls docx pptx xlsx].freeze
 
   attr_reader :results
