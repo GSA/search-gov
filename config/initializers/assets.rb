3306ead1aeeb368f25e4a29037b10253136f051d
jayandra <jayandra.pokharel@gsa.gov>
2026-07-27 13:28:10 -0500

    SRCH-6596: Added the images from active_scaffold gem to precompile list.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -18,3 +18,12 @@ Rails.application.config.assets.precompile += %w(*.png *.gif)
 Rails.application.config.assets.precompile += %w(application.css searches.css sites.css)
 Rails.application.config.assets.precompile += Dir.entries("#{Rails.root}/app/assets/javascripts/").select { |e| e =~ /^(?!application\.js).+\.js$/ }
 Rails.application.config.assets.precompile += Dir.entries("#{Rails.root}/app/assets/stylesheets/").select { |e| e =~ /^(?!application\.css).+\.css$/ }
+
+# Precompile ActiveScaffold assets. It drops all images in `https://github.com/activescaffold/active_scaffold/tree/master/app/assets/images/active_scaffold` into public/assets/active_scaffold. 
+# This is necessary because ActiveScaffold uses `image_path` to reference images, which will not work if the images are not in the public/assets directory.
+active_scaffold_images_path = ActiveScaffold::Engine.root.join("app/assets/images")
+Rails.application.config.assets.precompile += Dir.glob(active_scaffold_images_path.join("active_scaffold/**/*")).select do |path|
+  File.file?(path)
+end.map do |path|
+  Pathname.new(path).relative_path_from(active_scaffold_images_path).to_s
+end
