4ce35d0d74bc67ba294ddf0b96f67296217f8c5d
jayandra <jayandra.pokharel@gsa.gov>
2026-08-19 10:19:25 -0500

    Added migration to drop sees_filtered_totals and reload the changed email templates

---
+++ db/migrate/20260817220000_remove_bot_traffic_analytics_toggle.rb
@@ -0,0 +1,11 @@
+class RemoveBotTrafficAnalyticsToggle < ActiveRecord::Migration[7.1]
+  def up
+    remove_column :users, :sees_filtered_totals
+    EmailTemplate.load_default_templates %w[affiliate_monthly_report affiliate_yearly_report]
+  end
+
+  def down
+    add_column :users, :sees_filtered_totals, :boolean, null: false, default: true
+    EmailTemplate.load_default_templates %w[affiliate_monthly_report affiliate_yearly_report]
+  end
+end
