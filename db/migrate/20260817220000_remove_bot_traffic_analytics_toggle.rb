1381372b56b75078ff21821ea162032c87b7a8e7
jayandra <jayandra.pokharel@gsa.gov>
2026-08-20 15:47:14 -0500

    blank commit to trigger a build

--- db/migrate/20260817220000_remove_bot_traffic_analytics_toggle.rb
+++ db/migrate/20260817220000_remove_bot_traffic_analytics_toggle.rb
@@ -1,6 +1,6 @@
 class RemoveBotTrafficAnalyticsToggle < ActiveRecord::Migration[7.1]
   def up
-    remove_column :users, :sees_filtered_totals
+    remove_column :users, :sees_filtered_totals 
     EmailTemplate.load_default_templates %w[affiliate_monthly_report affiliate_yearly_report]
   end
 
