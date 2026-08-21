c7e57acb45b8a9de9a0362a6a511a9be9b93f21e
jayandra <jayandra.pokharel@gsa.gov>
2026-08-21 12:56:53 -0500

    moved template reloading before remove_column such that failed template reload doesn't modify our database.

--- db/migrate/20260817220000_remove_bot_traffic_analytics_toggle.rb
+++ db/migrate/20260817220000_remove_bot_traffic_analytics_toggle.rb
@@ -1,11 +1,10 @@
 class RemoveBotTrafficAnalyticsToggle < ActiveRecord::Migration[7.1]
   def up
-    remove_column :users, :sees_filtered_totals
     EmailTemplate.load_default_templates %w[affiliate_monthly_report affiliate_yearly_report]
+    remove_column :users, :sees_filtered_totals
   end
 
   def down
     add_column :users, :sees_filtered_totals, :boolean, null: false, default: true
-    EmailTemplate.load_default_templates %w[affiliate_monthly_report affiliate_yearly_report]
   end
 end
