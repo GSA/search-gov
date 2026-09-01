e35c65f13a93ce44d6227e2c6f22126aaed0a1c1
luisgmetzger <luis.metzger@gsa.gov>
2026-09-01 13:19:02 -0600

    SRCH-6739 Recreate IndexedDocument index in ElasticMigration spec
    CI failed with a missing reader alias because this spec assumed
    suite-level ES indexes survived other examples. Recreate first,
    same as the other custom-index specs.

--- spec/lib/elastic_migration_spec.rb
+++ spec/lib/elastic_migration_spec.rb
@@ -2,12 +2,18 @@ require 'spec_helper'
 
 describe ElasticMigration do
   describe '.migrate' do
+    before do
+      skip 'custom Elasticsearch indices are disabled' unless Es.custom_indices_enabled?
+      ElasticIndexedDocument.recreate_index
+    end
+
     it 'should move reader and writer alias to a new timestamped index' do
-      old_index = Es::CustomIndices.client_reader.indices.get_alias(name: ElasticIndexedDocument.reader_alias).keys.first
+      client = ElasticIndexedDocument.client_reader
+      old_index = client.indices.get_alias(name: ElasticIndexedDocument.reader_alias).keys.first
       described_class.migrate('IndexedDocument')
-      new_index = Es::CustomIndices.client_writers.first.indices.get_alias(name: ElasticIndexedDocument.reader_alias).keys.first
+      new_index = client.indices.get_alias(name: ElasticIndexedDocument.reader_alias).keys.first
       expect(new_index).to be > old_index
-      new_index = Es::CustomIndices.client_writers.first.indices.get_alias(name: ElasticIndexedDocument.writer_alias).keys.first
+      new_index = client.indices.get_alias(name: ElasticIndexedDocument.writer_alias).keys.first
       expect(new_index).to be > old_index
     end
   end
