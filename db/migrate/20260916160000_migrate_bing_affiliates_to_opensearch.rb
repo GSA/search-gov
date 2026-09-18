245905e60e209cdcee715b3876acc1dc15157602
Luis Metzger <luis.metzger@gsa.gov>
2026-09-18 13:19:03 -0600

    SRCH-6771 Fix Bing affiliate migration constant for OpenSearch acronym
    Rails camelizes the filename with inflect.acronym OpenSearch, so the
    class must be MigrateBingAffiliatesToOpenSearch.

--- db/migrate/20260916160000_migrate_bing_affiliates_to_opensearch.rb
+++ db/migrate/20260916160000_migrate_bing_affiliates_to_opensearch.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class MigrateBingAffiliatesToOpensearch < ActiveRecord::Migration[7.1]
+class MigrateBingAffiliatesToOpenSearch < ActiveRecord::Migration[7.1]
   def up
     execute(<<~SQL.squish)
       UPDATE affiliates SET search_engine = 'OpenSearch'
