f65698bb407df4a45bfaf803410c5464d1c115e7
Jayandra Pokharel <jayandra.pokharel@gsa.gov>
2026-09-25 14:17:50 +0000

    SRCH-6754: SRCH-6782: Removed primary and secondary footer section when no Footer Link has been added (#2178)
    * SRCH-6782: Emptied primary and secondary footer section when no Footer Link has been added in Client view > Manage Display > Visual Design
    
    * Got rid of primary and secondary sections when no link is present
    
    * undoing earlier deletion

--- app/javascript/test/Footer.test.tsx
+++ app/javascript/test/Footer.test.tsx
@@ -24,4 +24,23 @@ describe('Footer', () => {
     expect(secondLink).toHaveAttribute('href', 'https://second.gov');
     expect(secondLink).toHaveTextContent('second footer link');
   });
+
+  it('shows Return to top and footer links when footer links are present', () => {
+    render(<Footer footerLinks={footerLinks} />);
+
+    expect(document.querySelector('.usa-footer__return-to-top')).toBeInTheDocument();
+    expect(document.querySelector('.usa-footer__primary-section')).toBeInTheDocument();
+    expect(document.querySelector('.usa-footer__primary-link')).toBeInTheDocument();
+  });
+
+  it('shows only Return to top when footer links are blank', () => {
+    render(<Footer footerLinks={[]} />);
+
+    expect(document.querySelector('.usa-footer__return-to-top')).toBeInTheDocument();
+    expect(document.querySelector('footer.usa-footer.usa-footer--slim')).toBeInTheDocument();
+    expect(document.getElementsByClassName('usa-footer__primary-link')).toHaveLength(0);
+    expect(document.querySelector('.usa-footer__nav')).not.toBeInTheDocument();
+    expect(document.querySelector('.usa-footer__primary-section')).not.toBeInTheDocument();
+    expect(document.querySelector('.usa-footer__secondary-section')).not.toBeInTheDocument();
+  });
 });
